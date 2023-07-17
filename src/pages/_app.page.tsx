9be079a4259cb19777980312cea7efdb4f3e94d2
canisminor1990 <i@canisminor.cc>
2023-07-17 19:47:07 +0800

    🔧 chore: Update

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,19 +2,13 @@ import { Analytics } from '@vercel/analytics/react';
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 
-import i18nConfig from '@/../next-i18next.config';
 import Layout from '@/layout';
 
-const App = appWithTranslation(
-  ({ Component, pageProps }: AppProps) => <Component {...pageProps} />,
-  i18nConfig,
-);
-
-export default (props: AppProps) => {
+export default appWithTranslation(({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
-      <App {...props} />
+      <Component {...pageProps} />
       <Analytics />
     </Layout>
   );
-};
+});
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,4 +1,1 @@
-import { makeI18nProps } from '@/utils/makeI18nProps';
-
-export { default } from './chat/index.page';
-export const getStaticProps = makeI18nProps(['common']);
+export { default, getStaticProps } from './chat/index.page';
