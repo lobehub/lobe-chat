1c047d637ab6c514c5710d2cad63f589abf3233d
canisminor1990 <i@canisminor.cc>
2023-07-17 22:16:59 +0800

    🔧 chore: Update

--- src/utils/makeI18nProps.ts
+++ src/utils/makeI18nProps.ts
@@ -4,20 +4,23 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import i18nextConfig from '@/../next-i18next.config';
 import { NS } from '@/types/resources';
 
+const isServerReq = (req: any) => !req?.url?.startsWith('/_next');
+
 export const getI18nProps = async (ctx: any, ns: NS[] = ['common']) => {
   const locale = ctx?.params?.locale || ctx?.locale || i18nextConfig.i18n.defaultLocale;
-  let props = {
-    ...(await serverSideTranslations(locale, ns)),
-  };
-  return props;
+  const req = ctx?.params?.req || ctx?.req;
+  return isServerReq(req) ? await serverSideTranslations(locale, ns) : {};
 };
 
 export const makeI18nProps =
   (ns: NS[] = []): GetStaticProps | GetServerSideProps =>
-  async (ctx: any) => ({
-    props: await getI18nProps(ctx, ns),
-    revalidate: false,
-  });
+  async (ctx: any) => {
+    const req = ctx?.params?.req || ctx?.req;
+    return {
+      props: isServerReq(req) ? await getI18nProps(ctx, ns) : {},
+      revalidate: false,
+    };
+  };
 
 export const getStaticPaths: GetStaticPaths = async () => {
   // We don't want to specify all possible countries as we get those from the headers
