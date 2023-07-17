4da282996680eed2f338ee54b4df532cd15aa2db
canisminor1990 <i@canisminor.cc>
2023-07-17 10:38:14 +0800

    🐛 fix: Fix ssr

--- src/pages/chat/index.page.tsx
+++ src/pages/chat/index.page.tsx
@@ -1,7 +1,1 @@
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-
 export { default } from './[id]/index.page';
-
-export const getStaticPaths = async (context: any) => ({
-  props: await serverSideTranslations(context.locale),
-});
