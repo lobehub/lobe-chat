dcb7c0715a984419fc4a0e3ed9f68da5d4da5b5b
arvinxx <arvinx@foxmail.com>
2023-08-10 20:49:55 +0800

    💄 style: 增加未初始化的 loading 态

--- src/pages/chat/SessionList/List/index.tsx
+++ src/pages/chat/SessionList/List/index.tsx
@@ -1,20 +1,32 @@
 import isEqual from 'fast-deep-equal';
 import Link from 'next/link';
-import { memo } from 'react';
+import { memo, useEffect, useState } from 'react';
 import { shallow } from 'zustand/shallow';
 
 import { sessionSelectors, useSessionStore } from '@/store/session';
 
 import SessionItem from './SessionItem';
+import SkeletonItem from './SkeletonItem';
 
 const SessionList = memo(() => {
+  const [isInit, setInit] = useState(false);
   const list = useSessionStore((s) => sessionSelectors.sessionList(s), isEqual);
   const [activeId, loading] = useSessionStore(
     (s) => [s.activeId, s.autocompleteLoading.title],
     shallow,
   );
 
-  return (
+  useEffect(() => {
+    const hasRehydrated = useSessionStore.persist.hasHydrated();
+
+    if (hasRehydrated) {
+      setInit(true);
+    }
+  }, []);
+
+  return !isInit ? (
+    <SkeletonItem />
+  ) : (
     <>
       {list.map(({ id }) => (
         <Link href={`/chat/${id}`} key={id}>
