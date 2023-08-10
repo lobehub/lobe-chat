fefae61b552491192dd43515d98025edd3968af0
arvinxx <arvinx@foxmail.com>
2023-08-10 23:18:49 +0800

    💄 style: 优化第一次水合逻辑

--- src/store/session/hooks/useSessionHydrated.ts
+++ src/store/session/hooks/useSessionHydrated.ts
@@ -3,12 +3,15 @@ import { useEffect, useState } from 'react';
 import { useSessionStore } from '@/store/session';
 
 export const useSessionHydrated = () => {
-  const [isInit, setInit] = useState(false);
+  // 根据 sessions 是否有值来判断是否已经初始化
+  const hasInited = !!Object.values(useSessionStore.getState().sessions).length;
+
+  const [isInit, setInit] = useState(hasInited);
 
   useEffect(() => {
     const hasRehydrated = useSessionStore.persist.hasHydrated();
 
-    if (hasRehydrated) {
+    if (hasRehydrated && !isInit) {
       setInit(true);
     }
   }, []);
