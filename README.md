2875400062f4b898a60d5b265caedac77a389123
canisminor1990 <i@canisminor.cc>
2023-08-15 18:03:36 +0800

    ♻️ refactor: Replace cdn

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 <div align="center">
 
-<img height="120" src="https://npm.elemecdn.com/@lobehub/assets-logo@1.0.0/assets/logo-3d.webp">
+<img height="120" src="https://registry.npmmirror.com/@lobehub/assets-logo/1.0.0/files/assets/logo-3d.webp">
 <img height="120" src="https://gw.alipayobjects.com/zos/kitchen/qJ3l3EPsdW/split.svg">
-<img height="120" src="https://npm.elemecdn.com/fluentui-emoji/icons/modern/robot.svg">
+<img height="120" src="https://registry.npmmirror.com/@lobehub/assets-emoji/1.3.0/files/assets/robot.webp">
 
 <h1>Lobe Chat</h1>
 
--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -1,3 +1,4 @@
+import { Meta } from '@lobehub/ui';
 import { StyleProvider, extractStaticStyle } from 'antd-style';
 import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
 
@@ -31,32 +32,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link
-            href="https://npm.elemecdn.com/@lobehub/assets-favicons/assets/apple-touch-icon.png"
-            rel="apple-touch-icon"
-            sizes="180x180"
-          />
-          <link
-            href="https://npm.elemecdn.com/@lobehub/assets-favicons/assets/favicon-32x32.png"
-            rel="icon"
-            sizes="32x32"
-            type="image/png"
-          />
-          <link
-            href="https://npm.elemecdn.com/@lobehub/assets-favicons/assets/favicon-16x16.png"
-            rel="icon"
-            sizes="16x16"
-            type="image/png"
-          />
-          <link
-            color="#000000"
-            href="https://npm.elemecdn.com/@lobehub/assets-favicons/assets/safari-pinned-tab.svg"
-            rel="mask-icon"
-          />
-          <meta content="LobeHub" name="apple-mobile-web-app-title" />
-          <meta content="LobeHub" name="application-name" />
-          <meta content="#000000" name="msapplication-TileColor" />
-          <meta content="#000000" name="theme-color" />
+          <Meta title={'LobeChat'} />
           <link href="/manifest.json" rel="manifest" />
         </Head>
         <body>
