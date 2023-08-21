17e8161bf8419ea24e638ac5171d74808c3a142c
arvinxx <arvinx@foxmail.com>
2023-08-22 00:25:15 +0800

    ♻️ refactor: 将网关实现代码集成进 Chat 本体
    TODO：未来更理想的是 Gateway 方法抽包，在 plugin 侧引入

--- src/const/url.ts
+++ src/const/url.ts
@@ -5,3 +5,7 @@ export const CHANGELOG = `${pkg.homepage}/blob/master/CHANGELOG.md`;
 export const ABOUT = pkg.homepage;
 export const FEEDBACK = pkg.bugs.url;
 export const DISCORD = 'https://discord.gg/AYFPHvv2jT';
+
+export const PLUGINS_INDEX_URL =
+  process.env.PLUGINS_INDEX_URL ??
+  'https://registry.npmmirror.com/@lobehub/lobe-chat-plugins/latest/files';
