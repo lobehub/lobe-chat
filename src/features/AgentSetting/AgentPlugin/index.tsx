8009691ce05dbd633ddfebe7883cbf2199886f0c
arvinxx <arvinx@foxmail.com>
2023-08-22 20:03:39 +0800

    💄 style: 完成插件市场 loading 态样式

--- src/features/AgentSetting/AgentPlugin/index.tsx
+++ src/features/AgentSetting/AgentPlugin/index.tsx
@@ -1,13 +1,31 @@
 import { Avatar, Form, ItemGroup } from '@lobehub/ui';
-import { Switch } from 'antd';
+import { useWhyDidYouUpdate } from 'ahooks';
+import { Skeleton, Switch } from 'antd';
+import { createStyles } from 'antd-style';
 import { ToyBrick } from 'lucide-react';
 import { memo, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { FORM_STYLE } from '@/const/layoutTokens';
-import { PluginsMap } from '@/plugins';
+import { useSessionStore } from '@/store/session';
 import { AgentAction } from '@/store/session/slices/agentConfig';
 import { LobeAgentConfig } from '@/types/session';
+
+const useStyles = createStyles(({ css }) => ({
+  avatar: css`
+    .ant-skeleton-header {
+      padding-right: 0;
+    }
+  `,
+  label: css`
+    li {
+      height: 14px !important;
+    }
+    li + li {
+      margin-block-start: 12px !important;
+    }
+  `,
+}));
 
 export interface AgentPluginProps {
   config: LobeAgentConfig;
@@ -16,27 +34,58 @@ export interface AgentPluginProps {
 
 const AgentPlugin = memo<AgentPluginProps>(({ config, updateConfig }) => {
   const { t } = useTranslation('setting');
+  const { styles } = useStyles();
+  const useFetchPluginList = useSessionStore((s) => s.useFetchPluginList);
+  const { data } = useFetchPluginList();
 
-  const plugin: ItemGroup = useMemo(
-    () => ({
-      children: Object.values(PluginsMap).map((item) => ({
-        avatar: <Avatar avatar={item.avatar} />,
-        children: (
-          <Switch
-            checked={!config.plugins ? false : config.plugins.includes(item.name)}
-            onChange={() => updateConfig(item.name)}
-          />
-        ),
-        desc: item.schema.description,
-        label: t(`plugins.${item.name}` as any, { ns: 'plugin' }),
-        minWidth: undefined,
-        tag: item.name,
-      })),
+  useWhyDidYouUpdate('AgentPlugin', { config, updateConfig });
+
+  const plugin: ItemGroup = useMemo(() => {
+    let children;
+
+    const loadingItem = {
+      avatar: (
+        <Skeleton
+          active
+          avatar={{ size: 40 }}
+          className={styles.avatar}
+          paragraph={false}
+          title={false}
+        />
+      ),
+      label: (
+        <Skeleton
+          active
+          avatar={false}
+          paragraph={{ className: styles.label, style: { marginBottom: 0 } }}
+          style={{ width: 300 }}
+          title={false}
+        />
+      ),
+    };
+
+    children = !data
+      ? [loadingItem, loadingItem, loadingItem]
+      : data.plugins.map((item) => ({
+          avatar: <Avatar avatar={item.meta.avatar} />,
+          children: (
+            <Switch
+              checked={!config.plugins ? false : config.plugins.includes(item.name)}
+              onChange={() => updateConfig(item.name)}
+            />
+          ),
+          desc: item.meta?.description,
+          label: t(`plugins.${item.name}` as any, { ns: 'plugin' }),
+          minWidth: undefined,
+          tag: item.name,
+        }));
+
+    return {
+      children,
       icon: ToyBrick,
       title: t('settingPlugin.title'),
-    }),
-    [config],
-  );
+    };
+  }, [config, data?.plugins]);
 
   return <Form items={[plugin]} {...FORM_STYLE} />;
 });
