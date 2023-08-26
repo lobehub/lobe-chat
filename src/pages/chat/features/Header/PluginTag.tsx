7e916ac0fd46c5140080d7df2e27ae741bf2a438
arvinxx <arvinx@foxmail.com>
2023-08-26 14:12:23 +0800

    ✨ feat: 支持展示插件插件状态

--- src/pages/chat/features/Header/PluginTag.tsx
+++ src/pages/chat/features/Header/PluginTag.tsx
@@ -1,33 +1,59 @@
-import { Avatar, Icon, Tag } from '@lobehub/ui';
+import { ActionIcon, Avatar, Icon, Tag } from '@lobehub/ui';
 import type { MenuProps } from 'antd';
-import { Dropdown } from 'antd';
-import { LucideToyBrick } from 'lucide-react';
-import { memo } from 'react';
+import { Badge, Dropdown } from 'antd';
+import isEqual from 'fast-deep-equal';
+import { LucideRotateCw, LucideToyBrick } from 'lucide-react';
+import { memo, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Flexbox } from 'react-layout-kit';
 
-import { usePluginStore } from '@/store/plugin';
+import { pluginHelpers, pluginSelectors, usePluginStore } from '@/store/plugin';
+
+const PluginStatus = memo<{ id: string; title?: string }>(({ title, id }) => {
+  const { t } = useTranslation('common');
+  const status = usePluginStore(pluginSelectors.getPluginManifestLoadingStatus(id));
+
+  const renderStatus = useMemo(() => {
+    switch (status) {
+      case 'loading': {
+        return <Badge color={'blue'} status={'processing'} />;
+      }
+      case 'error': {
+        return <ActionIcon icon={LucideRotateCw} size={'small'} title={t('retry')} />;
+      }
+      case 'success': {
+        return <Badge status={status} />;
+      }
+    }
+  }, [status]);
+
+  return (
+    <Flexbox gap={12} horizontal justify={'space-between'}>
+      {title} {renderStatus}
+    </Flexbox>
+  );
+});
 
 export interface PluginTagProps {
   plugins: string[];
 }
 
 const PluginTag = memo<PluginTagProps>(({ plugins }) => {
-  const { t } = useTranslation('plugin');
-
-  const list = usePluginStore((s) => s.pluginList);
+  const list = usePluginStore(pluginSelectors.displayPluginList);
+  const displayPlugin = usePluginStore(pluginSelectors.getPluginMetaById(plugins[0]), isEqual);
 
   if (plugins.length === 0) return null;
 
   const items: MenuProps['items'] = plugins.map((id) => {
-    const item = list?.find((p) => p.identifier === id);
+    const item = list.find((i) => i.identifier === id);
+
     return {
-      icon: (
-        <Avatar avatar={item?.meta.avatar} size={24} style={{ marginLeft: -6, marginRight: 2 }} />
-      ),
+      icon: <Avatar avatar={item?.avatar} size={24} style={{ marginLeft: -6, marginRight: 2 }} />,
       key: id,
-      label: t(`plugins.${id}` as any),
+      label: <PluginStatus id={id} title={item?.title} />,
     };
   });
+
   const count = plugins.length;
 
   return (
@@ -35,7 +61,7 @@ const PluginTag = memo<PluginTagProps>(({ plugins }) => {
       <div>
         <Tag>
           {<Icon icon={LucideToyBrick} />}
-          {t(`plugins.${plugins[0]}` as any)}
+          {pluginHelpers.getPluginTitle(displayPlugin?.meta)}
           {count > 1 && <div>({plugins.length - 1}+)</div>}
         </Tag>
       </div>
