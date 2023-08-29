3330829b947fec2838fbf9b1a65a8aa6750f04a3
arvinxx <arvinx@foxmail.com>
2023-08-29 21:20:21 +0800

    🌐 style: update i18n

--- src/locales/resources/en_US.ts
+++ src/locales/resources/en_US.ts
@@ -3,6 +3,7 @@ import empty from '../../../locales/en_US/empty.json';
 import error from '../../../locales/en_US/error.json';
 import plugin from '../../../locales/en_US/plugin.json';
 import setting from '../../../locales/en_US/setting.json';
+import welcome from '../../../locales/en_US/welcome.json';
 
 const resources = {
   common,
@@ -10,6 +11,7 @@ const resources = {
   error,
   plugin,
   setting,
+  welcome,
 } as const;
 
 export default resources;
