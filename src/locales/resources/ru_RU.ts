785d50fe8ada20324c0b877648b357f045a80df2
Sergey Pinus <57861007+bropines@users.noreply.github.com>
2023-09-02 11:03:08 +0500

    🌐 feat: Add russian localy (#137)

---
+++ src/locales/resources/ru_RU.ts
@@ -0,0 +1,17 @@
+import common from '../../../locales/ru_RU/common.json';
+import empty from '../../../locales/ru_RU/empty.json';
+import error from '../../../locales/ru_RU/error.json';
+import plugin from '../../../locales/ru_RU/plugin.json';
+import setting from '../../../locales/ru_RU/setting.json';
+import welcome from '../../../locales/ru_RU/welcome.json';
+
+const resources = {
+  common,
+  empty,
+  error,
+  plugin,
+  setting,
+  welcome,
+} as const;
+
+export default resources;
