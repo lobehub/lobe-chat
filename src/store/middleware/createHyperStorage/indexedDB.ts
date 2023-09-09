08ffc7560c70be9ccbfb1111b2007be999272144
arvinxx <arvinx@foxmail.com>
2023-09-09 16:05:00 +0800

    🐛 fix: 修正 indexedDB 为空时的判断逻辑

--- src/store/middleware/createHyperStorage/indexedDB.ts
+++ src/store/middleware/createHyperStorage/indexedDB.ts
@@ -4,6 +4,9 @@ import { StorageValue } from 'zustand/middleware/persist';
 export const createIndexedDB = <State extends any>(dbName: string = 'indexedDB') => ({
   getItem: async <T extends State>(name: string): Promise<StorageValue<T> | undefined> => {
     const [version, state] = await getMany(['version', 'state'], createStore(dbName, name));
+
+    if (!state) return undefined;
+
     return { state, version };
   },
   removeItem: async (name: string) => {
