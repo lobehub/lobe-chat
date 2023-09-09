7ae96e7f165a1115e2d44ef9250b3e601e8b31f7
arvinxx <arvinx@foxmail.com>
2023-09-09 20:24:13 +0800

    🐛 fix: fix mobile route

--- src/store/session/hooks/useEffectAfterHydrated.ts
+++ src/store/session/hooks/useEffectAfterHydrated.ts
@@ -10,7 +10,13 @@ export const useEffectAfterSessionHydrated = (
     const hasRehydrated = useSessionStore.persist.hasHydrated();
 
     if (hasRehydrated) {
+      // 等价 useEffect 多次触发
       fn(useSessionStore);
+    } else {
+      // 等价于 useEffect 第一次触发
+      useSessionStore.persist.onFinishHydration(() => {
+        fn(useSessionStore);
+      });
     }
   }, deps);
 };
