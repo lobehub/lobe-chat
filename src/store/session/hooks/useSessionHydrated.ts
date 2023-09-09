736f11e0e31ca984d3150a6eea8d77ec53b347dd
arvinxx <arvinx@foxmail.com>
2023-09-09 22:39:08 +0800

    🚨 ci: fix lint

--- src/store/session/hooks/useSessionHydrated.ts
+++ src/store/session/hooks/useSessionHydrated.ts
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 
-import { useEffectAfterSessionHydrated } from '@/store/session';
-
 import { useSessionStore } from '../store';
+import { useEffectAfterSessionHydrated } from './useEffectAfterHydrated';
 
 export const useSessionHydrated = () => {
   // 根据 sessions 是否有值来判断是否已经初始化
