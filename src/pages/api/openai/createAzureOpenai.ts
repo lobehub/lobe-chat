aba6011040ecaa23c641d1110f8b985213d965e4
arvinxx <arvinx@foxmail.com>
2023-09-12 01:52:20 +0800

    🐛 fix: fix error

--- src/pages/api/openai/createAzureOpenai.ts
+++ src/pages/api/openai/createAzureOpenai.ts
@@ -15,7 +15,8 @@ export const createAzureOpenai = (params: {
   const endpoint = !params.endpoint ? OPENAI_PROXY_URL : params.endpoint;
   const baseURL = urlJoin(endpoint, `/openai/deployments/${params.model.replace('.', '')}`); // refs: https://test-001.openai.azure.com/openai/deployments/gpt-35-turbo
 
-  const apiVersion = params.apiVersion ?? AZURE_API_VERSION ?? '2023-08-01-preview';
+  const defaultApiVersion = AZURE_API_VERSION || '2023-08-01-preview';
+  const apiVersion = !params.apiVersion ? defaultApiVersion : params.apiVersion;
   const apiKey = !params.userApiKey ? AZURE_API_KEY ?? '' : params.userApiKey;
 
   const config: ClientOptions = {
