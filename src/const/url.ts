e9e25b54b4b968cb69ea4b8c3eea14ac144a08b1
canisminor1990 <i@canisminor.cc>
2023-09-11 19:21:10 +0800

    ✨ feat: Add new settings for Azure OpenAI and OpenAI in locales files
    The commit introduces new settings for Azure OpenAI and OpenAI in JSON files. It also includes translations for these settings in English and Russian. Additionally, URLs for accessing plugin and agent indexes in different languages are added, with English set as the default language.
    
    Changes made:
    - Added new settings for Azure OpenAI and OpenAI in JSON files
    - Included translations for the new settings in English and Russian
    - Added URLs for accessing plugin and agent indexes in different languages
    - Set English as the default language for the URLs

--- src/const/url.ts
+++ src/const/url.ts
@@ -15,29 +15,31 @@ export const DISCORD = 'https://discord.gg/AYFPHvv2jT';
 export const PLUGINS_INDEX_URL =
   process.env.PLUGINS_INDEX_URL ?? 'https://chat-plugins.lobehub.com';
 
-const checkLang = (lang: Locales) => {
-  return !localeOptions.map((o) => o.value).includes(lang);
+export const DEFAULT_LANG = 'en-US';
+
+export const checkLang = (lang: Locales) => {
+  return lang === DEFAULT_LANG || !localeOptions.map((o) => o.value).includes(lang);
 };
-export const getPluginIndexJSON = (lang: Locales = 'en-US', baseUrl = PLUGINS_INDEX_URL) => {
-  if (lang === 'en-US' || checkLang(lang)) return baseUrl;
+export const getPluginIndexJSON = (lang: Locales = DEFAULT_LANG, baseUrl = PLUGINS_INDEX_URL) => {
+  if (checkLang(lang)) return baseUrl;
 
   return urlJoin(baseUrl, `index.${lang}.json`);
 };
 
 export const AGENTS_INDEX_URL = process.env.AGENTS_INDEX_URL ?? 'https://chat-agents.lobehub.com';
 
-export const getAgentIndexJSON = (lang: Locales = 'en-US', baseUrl = AGENTS_INDEX_URL) => {
-  if (lang === 'en-US' || checkLang(lang)) return baseUrl;
+export const getAgentIndexJSON = (lang: Locales = DEFAULT_LANG, baseUrl = AGENTS_INDEX_URL) => {
+  if (checkLang(lang)) return baseUrl;
 
   return urlJoin(baseUrl, `index.${lang}.json`);
 };
 
 export const getAgentJSON = (
   identifier: string,
-  lang: Locales = 'en-US',
+  lang: Locales = DEFAULT_LANG,
   baseUrl = AGENTS_INDEX_URL,
 ) => {
-  if (lang === 'en-US' || checkLang(lang)) return urlJoin(baseUrl, `${identifier}.json`);
+  if (checkLang(lang)) return urlJoin(baseUrl, `${identifier}.json`);
 
   return urlJoin(baseUrl, `${identifier}.${lang}.json`);
 };
