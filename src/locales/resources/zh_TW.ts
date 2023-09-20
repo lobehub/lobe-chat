eed70072dc0ceefd94653b8c75357c487901278b
Peter Dave Hello <hsu@peterdavehello.org>
2023-09-21 00:40:56 +0800

    Add basic zh_TW locale

---
+++ src/locales/resources/zh_TW.ts
@@ -0,0 +1,19 @@
+import common from '../../../locales/zh_TW/common.json';
+import empty from '../../../locales/zh_TW/empty.json';
+import error from '../../../locales/zh_TW/error.json';
+import market from '../../../locales/zh_TW/market.json';
+import plugin from '../../../locales/zh_TW/plugin.json';
+import setting from '../../../locales/zh_TW/setting.json';
+import welcome from '../../../locales/zh_TW/welcome.json';
+
+const resources = {
+  common,
+  empty,
+  error,
+  market,
+  plugin,
+  setting,
+  welcome,
+} as const;
+
+export default resources;
