8debeedd546562751b1fb30be26ced519f28c68d
arvinxx <arvinx@foxmail.com>
2023-09-30 13:06:42 +0800

    :green_heart: refactor: refactor the runtime config

--- src/app/api/plugins/route.ts
+++ src/app/api/plugins/route.ts
@@ -2,8 +2,6 @@ import { createLobeChatPluginGateway } from '@lobehub/chat-plugins-gateway';
 
 import { PLUGINS_INDEX_URL } from '@/const/url';
 
-export const config = {
-  runtime: 'edge',
-};
+export const runtime = 'edge';
 
 export const POST = createLobeChatPluginGateway({ pluginsIndexUrl: PLUGINS_INDEX_URL });
