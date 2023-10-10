236956b2f9899fbd2cbee51235e5acaccbd7d6cf
arvinxx <arvinx@foxmail.com>
2023-10-11 03:20:04 +0800

    :art: chore: clean code

--- .releaserc.js
+++ .releaserc.js
@@ -1,14 +1,1 @@
-const release = require('@lobehub/lint').semanticRelease;
-
-module.exports = {
-  ...release,
-  plugins: [
-    ...release.plugins,
-    // [
-    //   '@codedependant/semantic-release-docker',
-    //   {
-    //     dockerImage: 'lobe-chat',
-    //   },
-    // ],
-  ],
-};
+module.exports = require('@lobehub/lint').semanticRelease;
