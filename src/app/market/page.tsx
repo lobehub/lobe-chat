30fefce6e98d27ca22b503e28c6b1e655b5847e8
Arvin Xu <arvinx@foxmail.com>
2023-10-11 09:29:40 +0800

    更新 page.tsx

--- src/app/market/page.tsx
+++ src/app/market/page.tsx
@@ -1,32 +1,13 @@
-import { cookies } from 'next/headers';
-
-import { LOBE_LOCALE_COOKIE } from '@/const/locale';
-import { getAgentIndexJSON } from '@/const/url';
-import { Locales } from '@/locales/resources';
-import { LobeChatAgentsMarketIndex } from '@/types/market';
 import { isMobileDevice } from '@/utils/responsive';
 
 import DesktopPage from './(desktop)';
 import MobilePage from './(mobile)';
 
-const getAgentList = async (): Promise<LobeChatAgentsMarketIndex> => {
-  const cookieStore = cookies();
-  const lang = cookieStore.get(LOBE_LOCALE_COOKIE);
 
-  try {
-    const res = await fetch(getAgentIndexJSON(lang?.value as Locales));
-
-    return res.json();
-  } catch {
-    return { agents: [], schemaVersion: 1 };
-  }
-};
-
-export default async () => {
-  const data = await getAgentList();
+export default () => {
   const mobile = isMobileDevice();
 
   const Page = mobile ? MobilePage : DesktopPage;
 
-  return <Page defaultAgents={data.agents} />;
+  return <Page />;
 };
