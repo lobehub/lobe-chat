98efe02a2a4a41227dac4a2dda40c6258bcb6c3c
CanisMinor <i@canisminor.cc>
2023-10-17 16:33:07 +0800

    💄 style: Fix lazyload height

--- src/app/chat/features/SessionListContent/List/index.tsx
+++ src/app/chat/features/SessionListContent/List/index.tsx
@@ -1,4 +1,4 @@
-import { useResponsive } from 'antd-style';
+import { createStyles, useResponsive } from 'antd-style';
 import isEqual from 'fast-deep-equal';
 import Link from 'next/link';
 import { memo } from 'react';
@@ -12,10 +12,16 @@ import AddButton from './AddButton';
 import SessionItem from './Item';
 import SkeletonList from './SkeletonList';
 
+const useStyles = createStyles(
+  ({ css }) => css`
+    min-height: 70px;
+  `,
+);
+
 const SessionList = memo(() => {
   const list = useSessionStore(sessionSelectors.sessionList, isEqual);
   const [activeSession, switchSession] = useSessionStore((s) => [s.activeSession, s.switchSession]);
-
+  const { styles } = useStyles();
   const isInit = useSessionHydrated();
 
   const { mobile } = useResponsive();
@@ -24,7 +30,7 @@ const SessionList = memo(() => {
     <SkeletonList />
   ) : list.length > 0 ? (
     list.map(({ id }) => (
-      <LazyLoad height={70} key={id}>
+      <LazyLoad className={styles} key={id}>
         <Link
           aria-label={id}
           href={SESSION_CHAT_URL(id, mobile)}
