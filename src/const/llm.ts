9ea4d1c71d1090ce8c4e4e31e85be6eb06261dcd
arvinxx <arvinx@foxmail.com>
2023-11-07 10:28:01 +0800

    :sparkles: feat: support latest openai model

--- src/const/llm.ts
+++ src/const/llm.ts
@@ -1,3 +1,5 @@
+import { LanguageModel } from '@/types/llm';
+
 /**
  * A white list of language models that are allowed to display and be used in the app.
  */
@@ -9,9 +11,4 @@ export const LanguageModelWhiteList = [
   'gpt-4-32k',
 ];
 
-export const DEFAULT_OPENAI_MODEL_LIST = [
-  'gpt-3.5-turbo',
-  'gpt-3.5-turbo-16k',
-  'gpt-4',
-  'gpt-4-32k',
-];
+export const DEFAULT_OPENAI_MODEL_LIST: string[] = Object.values(LanguageModel);
