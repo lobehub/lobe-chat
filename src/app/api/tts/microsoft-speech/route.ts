0b11058b569eaf10c98fe8668aaf41986ed5155b
canisminor1990 <i@canisminor.cc>
2023-11-12 17:51:48 +0800

    🐛 fix: Fix api

--- src/app/api/tts/microsoft-speech/route.ts
+++ src/app/api/tts/microsoft-speech/route.ts
@@ -3,8 +3,7 @@ import { handleMicrosoftSpeechRequest } from '@lobehub/tts/es/server';
 export const runtime = 'edge';
 
 export const POST = async (req: Request) => {
-  // @ts-ignore
-  const res = await handleMicrosoftSpeechRequest(new Request(req.url, { ...req, duplex: 'half' }));
+  const res = await handleMicrosoftSpeechRequest(req, { duplex: 'half' });
 
   return new Response(res.body, res);
 };
