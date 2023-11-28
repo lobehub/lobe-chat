fdaf48f6c7aed3560fb59b741767d5c3f4a3c012
bujueone22 <143797363+bujueone22@users.noreply.github.com>
2023-11-28 12:56:02 +0800

    Update llm.ts

--- src/types/llm.ts
+++ src/types/llm.ts
@@ -14,7 +14,7 @@ export enum LanguageModel {
   GPT4 = 'gpt-4',
   GPT4_32K = 'gpt-4-32k',
   GPT4_PREVIEW = 'gpt-4-1106-preview',
-  GPT4_VISION_PREVIEW = 'gpt-4-vision-preview',
+  GPT4_VISION_PREVIEW = 'gpt-4-all',
 }
 
 // 语言模型的设置参数
