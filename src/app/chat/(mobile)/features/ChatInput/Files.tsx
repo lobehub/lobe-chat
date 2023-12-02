bbe14c467b0c3088166c8dd8979f9326075d0d2a
canisminor1990 <i@canisminor.cc>
2023-12-02 13:31:47 +0800

    💄 style: Fix empty files style

--- src/app/chat/(mobile)/features/ChatInput/Files.tsx
+++ src/app/chat/(mobile)/features/ChatInput/Files.tsx
@@ -6,6 +6,8 @@ import { useFileStore } from '@/store/file';
 const Files = memo(() => {
   const inputFilesList = useFileStore((s) => s.inputFilesList);
 
+  if (!inputFilesList || inputFilesList?.length === 0) return null;
+
   return (
     <div style={{ position: 'relative', width: '100vw' }}>
       <EditableFileList alwaysShowClose items={inputFilesList} />
