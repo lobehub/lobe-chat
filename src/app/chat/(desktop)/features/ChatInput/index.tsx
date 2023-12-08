8f0aaa02c2669dec8b5c0a5a3b51c9fa6ed075ca
canisminor1990 <i@canisminor.cc>
2023-12-08 23:44:51 +0800

    :zap: perf: Fix ChatIntpuArea rerender

--- src/app/chat/(desktop)/features/ChatInput/index.tsx
+++ src/app/chat/(desktop)/features/ChatInput/index.tsx
@@ -1,6 +1,6 @@
 import { ActionIcon, ChatInputArea, ChatSendButton } from '@lobehub/ui';
 import { Maximize2, Minimize2 } from 'lucide-react';
-import { memo } from 'react';
+import { memo, useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import ActionBar from '@/app/chat/features/ChatInput/ActionBar';
@@ -27,50 +27,64 @@ const ChatInputDesktopLayout = memo(() => {
     canUpload,
   } = useChatInput();
 
+  const handleSizeChange = useCallback(
+    (_: any, size: any) => {
+      if (!size) return;
+      updatePreference({
+        inputHeight: typeof size.height === 'string' ? Number.parseInt(size.height) : size.height,
+      });
+    },
+    [updatePreference],
+  );
+
+  const buttonAddons = useMemo(
+    () => (
+      <ChatSendButton
+        leftAddons={canUpload && <LocalFiles />}
+        loading={loading}
+        onSend={onSend}
+        onStop={onStop}
+        rightAddons={<SaveTopic />}
+        texts={{
+          send: t('send'),
+          stop: t('stop'),
+          warp: t('warp'),
+        }}
+      />
+    ),
+    [canUpload, loading, onSend, onStop, t],
+  );
+
+  const topAddons = useMemo(
+    () => (
+      <ActionBar
+        rightAreaEndRender={
+          <ActionIcon
+            icon={expand ? Minimize2 : Maximize2}
+            onClick={() => {
+              setExpand(!expand);
+            }}
+          />
+        }
+      />
+    ),
+    [expand],
+  );
+
   return (
     <>
       {canUpload && <DragUpload />}
       <ChatInputArea
-        bottomAddons={
-          <ChatSendButton
-            leftAddons={canUpload && <LocalFiles />}
-            loading={loading}
-            onSend={onSend}
-            onStop={onStop}
-            rightAddons={<SaveTopic />}
-            texts={{
-              send: t('send'),
-              stop: t('stop'),
-              warp: t('warp'),
-            }}
-          />
-        }
+        bottomAddons={buttonAddons}
         expand={expand}
         heights={{ headerHeight: HEADER_HEIGHT, inputHeight, minHeight: CHAT_TEXTAREA_HEIGHT }}
         loading={loading}
         onInput={onInput}
         onSend={onSend}
-        onSizeChange={(_, size) => {
-          if (!size) return;
-          updatePreference({
-            inputHeight:
-              typeof size.height === 'string' ? Number.parseInt(size.height) : size.height,
-          });
-        }}
+        onSizeChange={handleSizeChange}
         placeholder={t('sendPlaceholder')}
         ref={ref}
-        topAddons={
-          <ActionBar
-            rightAreaEndRender={
-              <ActionIcon
-                icon={expand ? Minimize2 : Maximize2}
-                onClick={() => {
-                  setExpand(!expand);
-                }}
-              />
-            }
-          />
-        }
+        topAddons={topAddons}
         value={value}
       />
     </>
