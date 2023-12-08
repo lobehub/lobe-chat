f45852e66d1765de27922401f694f692a2671160
CanisMinor <i@canisminor.cc>
2023-12-08 18:38:13 +0800

    💄 style: Optimize the operation of lobechat on mobile devices (fix #448, fix #579)

--- src/app/chat/(mobile)/features/ChatHeader.tsx
+++ src/app/chat/(mobile)/features/ChatHeader.tsx
@@ -1,21 +1,25 @@
-import { ActionIcon, Icon, MobileNavBar, MobileNavBarTitle } from '@lobehub/ui';
-import { Dropdown, MenuProps } from 'antd';
-import { Clock3, MoreHorizontal, Settings, Share2 } from 'lucide-react';
+import { ActionIcon, MobileNavBar, MobileNavBarTitle } from '@lobehub/ui';
+import { useTheme } from 'antd-style';
+import { ChevronDown } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { memo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Flexbox } from 'react-layout-kit';
 
-import { MOBILE_HEADER_ICON_SIZE } from '@/const/layoutTokens';
+import { useChatStore } from '@/store/chat';
+import { topicSelectors } from '@/store/chat/selectors';
 import { useGlobalStore } from '@/store/global';
 import { useSessionStore } from '@/store/session';
 import { agentSelectors, sessionSelectors } from '@/store/session/selectors';
-import { pathString } from '@/utils/url';
 
+import SettingButton from '../../features/ChatHeader/SettingButton';
 import ShareButton from '../../features/ChatHeader/ShareButton';
 
 const MobileHeader = memo(() => {
   const { t } = useTranslation('chat');
+  const topicLength = useChatStore(topicSelectors.currentTopicLength);
   const router = useRouter();
+  const theme = useTheme();
   const [open, setOpen] = useState(false);
 
   const [isInbox, title] = useSessionStore((s) => [
@@ -23,41 +27,59 @@ const MobileHeader = memo(() => {
     agentSelectors.currentAgentTitle(s),
   ]);
 
-  const [toggleConfig] = useGlobalStore((s) => [s.toggleMobileTopic]);
+  const toggleConfig = useGlobalStore((s) => s.toggleMobileTopic);
 
   const displayTitle = isInbox ? t('inbox.title') : title;
 
-  const items: MenuProps['items'] = [
-    {
-      icon: <Icon icon={Share2} />,
-      key: 'share',
-      label: t('share', { ns: 'common' }),
-      onClick: () => setOpen(true),
-    },
-    !isInbox && {
-      icon: <Icon icon={Settings} />,
-      key: 'settings',
-      label: t('header.session', { ns: 'setting' }),
-      onClick: () => router.push(pathString('/chat/settings', { hash: location.hash })),
-    },
-  ].filter(Boolean) as MenuProps['items'];
+  // const items: MenuProps['items'] = [
+  //   {
+  //     icon: <Icon icon={Share2} />,
+  //     key: 'share',
+  //     label: t('share', { ns: 'common' }),
+  //     onClick: () => setOpen(true),
+  //   },
+  //   !isInbox && {
+  //     icon: <Icon icon={Settings} />,
+  //     key: 'settings',
+  //     label: t('header.session', { ns: 'setting' }),
+  //     onClick: () => router.push(pathString('/chat/settings', { hash: location.hash })),
+  //   },
+  // ].filter(Boolean) as MenuProps['items'];
 
   return (
     <MobileNavBar
-      center={<MobileNavBarTitle title={displayTitle} />}
+      center={
+        <MobileNavBarTitle
+          desc={
+            <Flexbox align={'center'} gap={4} horizontal onClick={() => toggleConfig()}>
+              <ActionIcon
+                active
+                icon={ChevronDown}
+                size={{ blockSize: 14, borderRadius: '50%', fontSize: 12 }}
+                style={{
+                  background: theme.colorFillSecondary,
+                  color: theme.colorTextDescription,
+                }}
+              />
+              <span>{`${t('topic.title')} ${topicLength > 1 ? topicLength + 1 : ''}`}</span>
+            </Flexbox>
+          }
+          title={<div onClick={() => toggleConfig()}>{displayTitle}</div>}
+        />
+      }
       onBackClick={() => router.push('/chat')}
       right={
         <>
-          <ActionIcon icon={Clock3} onClick={() => toggleConfig()} size={MOBILE_HEADER_ICON_SIZE} />
           <ShareButton mobile open={open} setOpen={setOpen} />
-          <Dropdown
-            menu={{
-              items,
-            }}
-            trigger={['click']}
-          >
-            <ActionIcon icon={MoreHorizontal} />
-          </Dropdown>
+          <SettingButton mobile />
+          {/*<Dropdown*/}
+          {/*  menu={{*/}
+          {/*    items,*/}
+          {/*  }}*/}
+          {/*  trigger={['click']}*/}
+          {/*>*/}
+          {/*  <ActionIcon icon={MoreHorizontal} />*/}
+          {/*</Dropdown>*/}
         </>
       }
       showBackButton
--- src/app/chat/features/ChatInput/ActionBar/index.tsx
+++ src/app/chat/features/ChatInput/ActionBar/index.tsx
@@ -1,5 +1,5 @@
+import { ChatInputActionBar } from '@lobehub/ui';
 import { ReactNode, memo, useMemo } from 'react';
-import { Flexbox } from 'react-layout-kit';
 
 import { ActionKeys, actionMap, getLeftActionList, getRightActionList } from './config';
 
@@ -34,24 +34,23 @@ const ActionBar = memo<ActionBarProps>(
     const rightActionList = useMemo(() => getRightActionList(mobile), [mobile]);
 
     return (
-      <Flexbox
-        align={'center'}
-        flex={'none'}
-        horizontal
-        justify={'space-between'}
+      <ChatInputActionBar
+        leftAddons={
+          <>
+            {leftAreaStartRender}
+            <RenderActionList dataSource={leftActionList} />
+            {leftAreaEndRender}
+          </>
+        }
         padding={padding}
-      >
-        <Flexbox align={'center'} flex={1} gap={4} horizontal>
-          {leftAreaStartRender}
-          <RenderActionList dataSource={leftActionList} />
-          {leftAreaEndRender}
-        </Flexbox>
-        <Flexbox align={'center'} flex={0} gap={4} horizontal justify={'flex-end'}>
-          {rightAreaStartRender}
-          <RenderActionList dataSource={rightActionList} />
-          {rightAreaEndRender}
-        </Flexbox>
-      </Flexbox>
+        rightAddons={
+          <>
+            {rightAreaStartRender}
+            <RenderActionList dataSource={rightActionList} />
+            {rightAreaEndRender}
+          </>
+        }
+      />
     );
   },
 );
