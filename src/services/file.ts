d038d244a08d67c91cb4cc3c377f99c141501dc1
mushan0x0 <mushan0x0@gmail.com>
2023-12-18 20:51:28 +0800

    ⚡️ perf: Optimize the image upload size for `gpt-4-vision` (#669)
    * ♻️ refactor: `FileItem` changed to `ImageFileItem`
    
    * ⚡️ perf: Optimize the image upload size for `gpt-4-vision`
    
    * 🔧 test: add Test Configuration
    
    * :test_tube: test: add case
    
    * :pencil2: chore: typos
    
    ---------
    
    Co-authored-by: wuxh <wxh1220@gmail.com>

--- src/services/file.ts
+++ src/services/file.ts
@@ -1,11 +1,44 @@
 import { FileModel } from '@/database/models/file';
 import { DB_File } from '@/database/schemas/files';
 import { FilePreview } from '@/types/files';
+import compressImage from '@/utils/compressImage';
 
 class FileService {
+  private isImage(fileType: string) {
+    const imageRegex = /^image\//;
+    return imageRegex.test(fileType);
+  }
   async uploadFile(file: DB_File) {
+    // 跳过图片上传测试
+    const isTestData = file.size === 1;
+    if (this.isImage(file.fileType) && !isTestData) {
+      return this.uploadImageFile(file);
+    }
     // save to local storage
     // we may want to save to a remote server later
+    return FileModel.create(file);
+  }
+
+  async uploadImageFile(file: DB_File) {
+    // 加载图片
+    const url = file.url || URL.createObjectURL(new Blob([file.data]));
+    const img = new Image();
+    img.src = url;
+    await (() =>
+      new Promise((resolve) => {
+        img.addEventListener('load', resolve);
+      }))();
+
+    // 压缩图片
+    const fileType = 'image/webp';
+    const base64String = compressImage({
+      img,
+      type: fileType,
+    });
+    const binaryString = atob(base64String.split('base64,')[1]);
+    const uint8Array = Uint8Array.from(binaryString, (char) => char.charCodeAt(0));
+    file.data = uint8Array.buffer;
+
     return FileModel.create(file);
   }
 
