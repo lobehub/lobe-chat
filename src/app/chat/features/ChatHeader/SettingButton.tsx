447c006e01f8c1b96b79f92e1514c5b44ebb00af
canisminor1990 <i@canisminor.cc>
2023-12-25 21:32:33 +0800

    💄 style: Fix some style problem

--- src/app/chat/features/ChatHeader/SettingButton.tsx
+++ src/app/chat/features/ChatHeader/SettingButton.tsx
@@ -5,6 +5,8 @@ import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { DESKTOP_HEADER_ICON_SIZE, MOBILE_HEADER_ICON_SIZE } from '@/const/layoutTokens';
+import { useGlobalStore } from '@/store/global';
+import { SettingsTabs, SidebarTabKey } from '@/store/global/slices/common/initialState';
 import { useSessionStore } from '@/store/session';
 import { sessionSelectors } from '@/store/session/slices/session/selectors';
 import { pathString } from '@/utils/url';
@@ -18,9 +20,15 @@ const SettingButton = memo<{ mobile?: boolean }>(({ mobile }) => {
     <ActionIcon
       icon={AlignJustify}
       onClick={() => {
-        router.push(
-          pathString(isInbox ? '/settings/agent' : '/chat/settings', { hash: location.hash }),
-        );
+        if (isInbox) {
+          useGlobalStore.setState({
+            settingsTab: SettingsTabs.Common,
+            sidebarKey: SidebarTabKey.Setting,
+          });
+          router.push('/settings/agent');
+        } else {
+          router.push(pathString('/chat/settings', { hash: location.hash }));
+        }
       }}
       size={mobile ? MOBILE_HEADER_ICON_SIZE : DESKTOP_HEADER_ICON_SIZE}
       title={t('header.session', { ns: 'setting' })}
