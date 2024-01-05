0bfcb2f84d1ab42fd36f42070bd589cd6ff1d30d
arvinxx <arvinx@foxmail.com>
2024-01-05 22:41:45 +0800

    ⚡️ perf: refactor the topic to virtualized

--- src/app/chat/features/TopicListContent/Topic/index.tsx
+++ src/app/chat/features/TopicListContent/Topic/index.tsx
@@ -1,34 +1,59 @@
 import { EmptyCard } from '@lobehub/ui';
 import { useThemeMode } from 'antd-style';
 import isEqual from 'fast-deep-equal';
+import React, { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Flexbox } from 'react-layout-kit';
+import { Virtuoso } from 'react-virtuoso';
 
 import { useChatStore } from '@/store/chat';
 import { topicSelectors } from '@/store/chat/selectors';
 import { useGlobalStore } from '@/store/global';
+import { ChatTopic } from '@/types/topic';
 
 import SkeletonList from './SkeletonList';
 import TopicItem from './TopicItem';
 
-export const Topic = () => {
-  const { isDarkMode } = useThemeMode();
-
-  const topics = useChatStore(topicSelectors.displayTopics, isEqual);
-
-  const [topicsInit, activeTopicId] = useChatStore((s) => [s.topicsInit, s.activeTopicId]);
-
+export const Topic = memo(() => {
   const { t } = useTranslation('empty');
+  const { isDarkMode } = useThemeMode();
+  const [topicsInit, activeTopicId, topicLength] = useChatStore((s) => [
+    s.topicsInit,
+    s.activeTopicId,
+    topicSelectors.currentTopicLength(s),
+  ]);
   const [visible, updateGuideState] = useGlobalStore((s) => [
     s.preference.guide?.topic,
     s.updateGuideState,
   ]);
 
+  const topics = useChatStore(
+    (s) => [
+      {
+        favorite: false,
+        id: 'default',
+        title: t('topic.defaultTitle', { ns: 'chat' }),
+      } as ChatTopic,
+      ...topicSelectors.displayTopics(s),
+    ],
+    isEqual,
+  );
+
+  const itemContent = useCallback(
+    (index: number, { id, favorite, title }: ChatTopic) =>
+      index === 0 ? (
+        <TopicItem active={!activeTopicId} fav={favorite} title={title} />
+      ) : (
+        <TopicItem active={activeTopicId === id} fav={favorite} id={id} key={id} title={title} />
+      ),
+    [activeTopicId],
+  );
+
   return !topicsInit ? (
     <SkeletonList />
   ) : (
-    <Flexbox gap={2} style={{ marginBottom: 12 }}>
-      {topics?.length === 0 && (
+    <Flexbox gap={2} height={'100%'} style={{ marginBottom: 12 }}>
+      {topicLength === 0 && (
         <EmptyCard
           alt={t('topic.desc')}
           cover={`/images/empty_topic_${isDarkMode ? 'dark' : 'light'}.webp`}
@@ -43,16 +68,7 @@ export const Topic = () => {
           width={200}
         />
       )}
-
-      <TopicItem
-        active={!activeTopicId}
-        fav={false}
-        title={t('topic.defaultTitle', { ns: 'chat' })}
-      />
-
-      {topics.map(({ id, favorite, title }) => (
-        <TopicItem active={activeTopicId === id} fav={favorite} id={id} key={id} title={title} />
-      ))}
+      <Virtuoso data={topics} itemContent={itemContent} overscan={12} />
     </Flexbox>
   );
-};
+});
