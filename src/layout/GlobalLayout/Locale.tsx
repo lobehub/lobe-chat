ea241778bc379a287a6a8ec4ec0ac02a9a0e5fbf
wuxh <wxh1220@gmail.com>
2024-01-18 17:23:09 +0800

    :beers: chore: update

--- src/layout/GlobalLayout/Locale.tsx
+++ src/layout/GlobalLayout/Locale.tsx
@@ -1,5 +1,5 @@
 import { ConfigProvider } from 'antd';
-import { PropsWithChildren, memo, useState } from 'react';
+import { PropsWithChildren, memo, useEffect, useRef, useState } from 'react';
 import useSWR from 'swr';
 
 import { DEFAULT_LANG } from '@/const/locale';
@@ -12,15 +12,18 @@ interface LocaleLayoutProps extends PropsWithChildren {
   defaultLang?: string;
 }
 
+const FETCH_ANTD_LOCALE = 'fetch-antd-locale';
+
 const Locale = memo<LocaleLayoutProps>(({ children, defaultLang }) => {
   const [lang, setLang] = useState(defaultLang ?? DEFAULT_LANG);
   const [i18n] = useState(createI18nNext(defaultLang));
 
   const { data: locale } = useSWR(
-    lang,
-    async () => {
-      const localeName = lang?.includes('-') ? lang?.replace('-', '_') : 'en_US';
+    [FETCH_ANTD_LOCALE, lang],
+    async ([, l]) => {
+      const localeName = l?.includes('-') ? l?.replace('-', '_') : 'en_US';
       const antdLocale = await import(`antd/locale/${localeName}.js`);
+
       return antdLocale.default ?? antdLocale;
     },
     { revalidateOnFocus: false },
@@ -31,23 +34,30 @@ const Locale = memo<LocaleLayoutProps>(({ children, defaultLang }) => {
     i18n.init();
   } else {
     // if on browser side, init i18n instance only once
-    if (!i18n.instance.isInitialized)
-      // console.debug('locale', lang);
-      i18n.init().then(() => {
-        // console.debug('inited.');
-      });
+    if (!i18n.instance.isInitialized) {
+      i18n.init();
+    }
   }
 
-  useOnFinishHydrationGlobal(
-    (s) => {
-      if (s.settings.language !== defaultLang) {
-        switchLang(s.settings.language);
-      } else {
-        i18n.instance.on('languageChanged', setLang);
+  // 判断注水后，用户本地持久化偏好语言是否被切换
+  const isHydrated = useRef(false);
+  useEffect(() => {
+    const putLang = (lng: string) => {
+      if (isHydrated.current) {
+        return (isHydrated.current = false);
       }
-    },
-    [defaultLang],
-  );
+      setLang(lng);
+    };
+    i18n.instance.on('languageChanged', putLang);
+    return () => i18n.instance.off('languageChanged', putLang);
+  }, []);
+
+  useOnFinishHydrationGlobal((s) => {
+    if (s.settings.language !== defaultLang) {
+      isHydrated.current = true;
+      switchLang(s.settings.language);
+    }
+  }, []);
 
   return <ConfigProvider locale={locale}>{children}</ConfigProvider>;
 });
