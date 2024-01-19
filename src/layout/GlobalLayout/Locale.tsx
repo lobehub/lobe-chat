c965da6db711401e831da7445c64f613d6b514d7
红 <wxh1220@gmail.com>
2024-01-19 20:50:03 +0800

    :bug: fix: antd i18n miss error (#1083)
    * 📝 docs(bot): Auto sync agents & plugin to readme
    
    * :bug: fix: antd i18n miss error
    
    * :bug: fix: update logic
    
    * :beers: chore: update
    
    ---------
    
    Co-authored-by: lobehubbot <i@lobehub.com>

--- src/layout/GlobalLayout/Locale.tsx
+++ src/layout/GlobalLayout/Locale.tsx
@@ -1,40 +1,61 @@
 import { ConfigProvider } from 'antd';
-import { PropsWithChildren, memo, useState } from 'react';
+import { PropsWithChildren, memo, useEffect, useRef, useState } from 'react';
 import useSWR from 'swr';
 
+import { DEFAULT_LANG } from '@/const/locale';
 import { createI18nNext } from '@/locales/create';
 import { useOnFinishHydrationGlobal } from '@/store/global';
 import { isOnServerSide } from '@/utils/env';
 import { switchLang } from '@/utils/switchLang';
 
 interface LocaleLayoutProps extends PropsWithChildren {
-  lang?: string;
+  defaultLang?: string;
 }
 
-const Locale = memo<LocaleLayoutProps>(({ children, lang }) => {
+const FETCH_ANTD_LOCALE = 'fetch-antd-locale';
+
+const Locale = memo<LocaleLayoutProps>(({ children, defaultLang }) => {
+  const [lang, setLang] = useState(defaultLang ?? DEFAULT_LANG);
+  const [i18n] = useState(createI18nNext(defaultLang));
+
   const { data: locale } = useSWR(
-    lang,
-    async () =>
-      await import(`antd/locale/${lang?.includes('-') ? lang?.replace('-', '_') : 'en_US'}.js`),
+    [FETCH_ANTD_LOCALE, lang],
+    async ([, l]) => {
+      const localeName = l?.includes('-') ? l?.replace('-', '_') : 'en_US';
+      const antdLocale = await import(`antd/locale/${localeName}.js`);
+
+      return antdLocale.default ?? antdLocale;
+    },
     { revalidateOnFocus: false },
   );
-  const [i18n] = useState(createI18nNext(lang));
 
   // if run on server side, init i18n instance everytime
   if (isOnServerSide) {
     i18n.init();
   } else {
     // if on browser side, init i18n instance only once
-    if (!i18n.instance.isInitialized)
-      // console.debug('locale', lang);
-      i18n.init().then(() => {
-        // console.debug('inited.');
-      });
+    if (!i18n.instance.isInitialized) {
+      i18n.init();
+    }
   }
 
+  // 判断注水后，用户本地持久化偏好语言是否被切换
+  const isHydrated = useRef(false);
+  useEffect(() => {
+    const putLang = (lng: string) => {
+      if (isHydrated.current) {
+        return (isHydrated.current = false);
+      }
+      setLang(lng);
+    };
+    i18n.instance.on('languageChanged', putLang);
+    return () => i18n.instance.off('languageChanged', putLang);
+  }, []);
+
   useOnFinishHydrationGlobal((s) => {
-    if (s.settings.language === 'auto') {
-      switchLang('auto');
+    if (s.settings.language !== defaultLang) {
+      isHydrated.current = true;
+      switchLang(s.settings.language);
     }
   }, []);
 
