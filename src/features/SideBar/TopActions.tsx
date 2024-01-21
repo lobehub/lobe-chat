c4ac4f1bcbe70d3d535a4cf82d92748775a6a91d
arvinxx <arvinx@foxmail.com>
2024-01-21 14:18:35 +0800

    💄 style: improve discover icon

--- src/features/SideBar/TopActions.tsx
+++ src/features/SideBar/TopActions.tsx
@@ -1,5 +1,5 @@
 import { ActionIcon } from '@lobehub/ui';
-import { Bot, MessageSquare } from 'lucide-react';
+import { Compass, MessageSquare } from 'lucide-react';
 import Link from 'next/link';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -38,7 +38,7 @@ const TopActions = memo<TopActionProps>(({ tab, setTab }) => {
       <Link href={'/market'}>
         <ActionIcon
           active={tab === SidebarTabKey.Market}
-          icon={Bot}
+          icon={Compass}
           onClick={() => {
             setTab(SidebarTabKey.Market);
           }}
