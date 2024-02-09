da7beba421cc522dc6fcc73226c1c09c55664aa3
Bryan <ImSingee@users.noreply.github.com>
2024-02-09 10:14:06 +0800

    ✨ feat: Support umami analytics (#1267)
    * :sparkles: feat(analytics): support umami
    
    * 📝 docs: add docs

--- src/config/client.ts
+++ src/config/client.ts
@@ -24,6 +24,10 @@ declare global {
       NEXT_PUBLIC_POSTHOG_HOST: string;
       NEXT_PUBLIC_POSTHOG_DEBUG: string;
 
+      NEXT_PUBLIC_ANALYTICS_UMAMI?: string;
+      NEXT_PUBLIC_UMAMI_SCRIPT_URL?: string;
+      NEXT_PUBLIC_UMAMI_WEBSITE_ID?: string;
+
       NEXT_PUBLIC_I18N_DEBUG: string;
       NEXT_PUBLIC_I18N_DEBUG_BROWSER: string;
       NEXT_PUBLIC_I18N_DEBUG_SERVER: string;
@@ -49,6 +53,12 @@ export const getClientConfig = () => ({
   POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
   POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   POSTHOG_DEBUG: process.env.NEXT_PUBLIC_POSTHOG_DEBUG === '1',
+
+  // Umami Analytics
+  ANALYTICS_UMAMI: process.env.NEXT_PUBLIC_ANALYTICS_UMAMI === '1',
+  UMAMI_SCRIPT_URL:
+    process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL || 'https://analytics.umami.is/script.js',
+  UMAMI_WEBSITE_ID: process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID,
 
   // i18n debug mode
   I18N_DEBUG: process.env.NEXT_PUBLIC_I18N_DEBUG === '1',
