81791cd30b30bd5a92da6d5c50d4523f2ccfc44b
arvinxx <arvinx@foxmail.com>
2024-02-09 16:06:48 +0800

    🐛 fix: fix api base path

--- src/services/_url.ts
+++ src/services/_url.ts
@@ -1,24 +1,39 @@
 /* eslint-disable sort-keys-fix/sort-keys-fix */
-export const API_ENDPOINTS = {
-  config: 'api/config',
-  proxy: 'api/proxy',
+import { transform } from 'lodash-es';
+
+import { withBasePath } from '@/utils/basePath';
+
+const mapWithBasePath = <T extends object>(apis: T): T => {
+  return transform(apis, (result, value, key) => {
+    if (typeof value === 'string') {
+      // @ts-ignore
+      result[key] = withBasePath(value);
+    } else {
+      result[key] = value;
+    }
+  });
+};
+
+export const API_ENDPOINTS = mapWithBasePath({
+  config: '/api/config',
+  proxy: '/api/proxy',
 
   // agent markets
-  market: 'api/market',
+  market: '/api/market',
 
   // plugins
-  gateway: 'api/plugin/gateway',
-  pluginStore: 'api/plugin/store',
+  gateway: '/api/plugin/gateway',
+  pluginStore: '/api/plugin/store',
 
   // chat
-  chat: (provider: string) => `api/chat/${provider}`,
+  chat: (provider: string) => withBasePath(`/api/chat/${provider}`),
 
   // image
-  images: 'api/openai/images',
+  images: '/api/openai/images',
 
   // TTS & STT
-  stt: 'api/openai/stt',
-  tts: 'api/openai/tts',
-  edge: 'api/tts/edge-speech',
-  microsoft: 'api/tts/microsoft-speech',
-};
+  stt: '/api/openai/stt',
+  tts: '/api/openai/tts',
+  edge: '/api/tts/edge-speech',
+  microsoft: '/api/tts/microsoft-speech',
+});
