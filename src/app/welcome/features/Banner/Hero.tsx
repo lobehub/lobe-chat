c1e19e0a2a9e7315ddab98901071aef919d021c6
ABYSSTENSOR <125164235+ABYSSTENSOR@users.noreply.github.com>
2024-02-11 19:13:22 +0100

    Update Hero.tsx

--- src/app/welcome/features/Banner/Hero.tsx
+++ src/app/welcome/features/Banner/Hero.tsx
@@ -33,7 +33,8 @@ const Hero = memo<{ mobile?: boolean; width: number }>(({ width, mobile }) => {
           position: 'relative',
         }}
       >
-        {mobile ? <LogoThree size={size.logo} /> : <LogoSpline height={'100%'} width={'100%'} />}
+        {/* Remplacer le logo par votre GIF */}
+        {mobile ? <img src="https://i.gifer.com/79uk.gif" alt="GIF" style={{ width: size.logo, height: size.logo }} /> : <LogoSpline height={'100%'} width={'100%'} />}
       </Flexbox>
       <div className={styles.title} style={{ fontSize: size.title }}>
         <strong style={mobile ? { fontSize: '1.2em' } : {}}>Ai Fensch Tech</strong>
