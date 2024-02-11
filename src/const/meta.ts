e87b4e17284d31b7229f06201513d81edde1b592
ABYSSTENSOR <125164235+ABYSSTENSOR@users.noreply.github.com>
2024-02-11 18:40:34 +0100

    Update meta.ts

--- src/const/meta.ts
+++ src/const/meta.ts
@@ -4,6 +4,6 @@ export const DEFAULT_AVATAR = '🤖';
 export const DEFAULT_USER_AVATAR = '😀';
 export const DEFAULT_BACKGROUND_COLOR = 'rgba(0,0,0,0)';
 export const DEFAULT_AGENT_META: MetaData = {};
-export const DEFAULT_INBOX_AVATAR = '🤯';
+export const DEFAULT_INBOX_AVATAR = '🧠';
 export const DEFAULT_USER_AVATAR_URL =
   'https://i.gifer.com/79uk.gif';
