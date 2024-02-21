46b8524d305c1ca3f66a9be98188987810f1d1e3
cy948 <948190767@qq.com>
2024-02-21 14:53:29 +0800

    :bug: fix: Could not get `user_id` from session

--- src/app/api/auth/next-auth.ts
+++ src/app/api/auth/next-auth.ts
@@ -7,9 +7,30 @@ const { AUTH0_CLIENT_ID, ENABLE_OAUTH_SSO, AUTH0_CLIENT_SECRET, AUTH0_ISSUER, NE
   getServerConfig();
 
 const nextAuth = NextAuth({
+  callbacks: {
+    async jwt({ token, profile }) {
+      // Override with the attributes from the Auth0 Profile.
+      // Default values for profile.sub: 'auth0|USER-ID'.
+      // ref: https://auth0.com/docs/get-started/apis/scopes/sample-use-cases-scopes-and-claims#authenticate-a-user-and-request-standard-claims
+      if (profile) {
+        token.sub = profile.sub ?? token.sub;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      // Pick userid from token
+      if (session.user) {
+        session.user.id = token.sub ?? session.user.id;
+      }
+      return session;
+    },
+  },
   providers: ENABLE_OAUTH_SSO
     ? [
         Auth0({
+          // Specify auth scope, at least include 'openid email'
+          // all scopes in Auth0 ref: https://auth0.com/docs/get-started/apis/scopes/openid-connect-scopes#standard-claims
+          authorization: { params: { scope: 'openid email profile' } },
           clientId: AUTH0_CLIENT_ID,
           clientSecret: AUTH0_CLIENT_SECRET,
           issuer: AUTH0_ISSUER,
