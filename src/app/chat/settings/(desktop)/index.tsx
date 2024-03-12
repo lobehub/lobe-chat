9f1635e0c154bbbbb3866ed5c3d40f91c6c17c5b
Arvin Xu <arvinx@foxmail.com>
2024-03-12 11:57:31 +0800

    ⚡️ perf: refactor the responsive layout to improve performance (#1541)

--- src/app/chat/settings/(desktop)/index.tsx
+++ src/app/chat/settings/(desktop)/index.tsx
@@ -4,20 +4,24 @@ import dynamic from 'next/dynamic';
 import { FC, memo } from 'react';
 
 import SessionHydration from '@/app/chat/components/SessionHydration';
-import ResponsiveIndex from '@/components/ResponsiveIndex';
+import ResponsiveContainer from '@/components/ResponsiveContainer';
+import MobileSwitchLoading from '@/features/MobileSwitchLoading';
 
 import EditPage from '../features/EditPage';
 import Layout from './layout.desktop';
 
-const Mobile: FC = dynamic(() => import('../(mobile)'), { ssr: false }) as FC;
+const Mobile: FC = dynamic(() => import('../(mobile)'), {
+  loading: MobileSwitchLoading,
+  ssr: false,
+}) as FC;
 
 const ChatSettings = memo(() => (
   <>
-    <ResponsiveIndex Mobile={Mobile}>
+    <ResponsiveContainer Mobile={Mobile}>
       <Layout>
         <EditPage />
       </Layout>
-    </ResponsiveIndex>
+    </ResponsiveContainer>
     <SessionHydration />
   </>
 ));
