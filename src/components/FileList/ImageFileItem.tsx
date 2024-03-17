19afbf355e7a245a18076264dfba77fe364e87ba
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-03-17 15:31:26 +0800

    💄 style: make uploading files more beautiful

--- src/components/FileList/ImageFileItem.tsx
+++ src/components/FileList/ImageFileItem.tsx
@@ -34,6 +34,16 @@ interface FileItemProps {
   onClick?: () => void;
   style?: CSSProperties;
 }
+const truncateString = (filename: any, maxChars: any) => {
+  if (!filename) return '';
+  if (filename.length > maxChars) {
+    const extension = filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2);
+    // 使用 slice() 函数替代 substring() 函数
+    return filename.slice(0, maxChars) + "." + extension;
+  }
+  return filename;
+}
+
 const ImageFileItem = memo<FileItemProps>(({ editable, id, alwaysShowClose }) => {
   const [useFetchFile, removeFile] = useFileStore((s) => [s.useFetchFile, s.removeFile]);
   const IMAGE_SIZE = editable ? MIN_IMAGE_SIZE : '100%';
@@ -49,29 +59,94 @@ const ImageFileItem = memo<FileItemProps>(({ editable, id, alwaysShowClose }) =>
     [id],
   );
 
-  return (
-    <Image
-      actions={
-        editable && (
-          <ActionIcon
-            className={styles.deleteButton}
-            glass
-            icon={Trash}
-            onClick={handleRemoveFile}
-            size={'small'}
-          />
-        )
-      }
-      alt={data?.name || id || ''}
-      alwaysShowActions={alwaysShowClose}
-      height={isSafari ? 'auto' : '100%'}
-      isLoading={isLoading}
-      size={IMAGE_SIZE as any}
-      src={data?.url}
-      style={{ height: isSafari ? 'auto' : '100%' }}
-      wrapperClassName={cx(styles.image, editable && styles.editableImage)}
-    />
-  );
+  const imageFileExtensions = new Set([
+    '.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.ico', '.webp',
+    '.svg', '.eps', '.raw', '.heif', '.heic', '.xbm', '.tif',
+    '.pjp', '.apng', '.svgz', '.jfif', '.pjpeg', '.avif'
+  ]);
+
+  const isImage = (filename: string) => {
+    const extension = filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2);
+    return imageFileExtensions.has(`.${extension.toLowerCase()}`);
+  };
+
+  let isShowImage = true;
+  if (data && data.name) {
+    isShowImage = isImage(data.name);
+  }
+
+  const temFileName = truncateString(data?.name, 4);
+
+  if (isShowImage) {
+    return (
+      <Image
+        actions={
+          editable && (
+            <ActionIcon
+              className={styles.deleteButton}
+              glass
+              icon={Trash}
+              onClick={handleRemoveFile}
+              size={'small'}
+            />
+          )
+        }
+        alt={data?.name || id || ''}
+        alwaysShowActions={alwaysShowClose}
+        height={isSafari ? 'auto' : '100%'}
+        isLoading={isLoading}
+        size={IMAGE_SIZE as any}
+        src={data?.url}
+        style={{ height: isSafari ? 'auto' : '100%' }}
+        wrapperClassName={cx(styles.image, editable && styles.editableImage)}
+      />
+    );
+  } else {
+    return (
+      <div style={{
+        display: 'inline-block',
+        fontOpticalSizing: 'auto', fontSize: 'revert-layer', position: 'relative'
+      }}>
+        <Image
+          actions={
+            editable && (
+              <ActionIcon
+                className={styles.deleteButton}
+                glass
+                icon={Trash}
+                onClick={handleRemoveFile}
+                size={'small'}
+              />
+            )
+          }
+          alt={data?.name || id || ''}
+          alwaysShowActions={alwaysShowClose}
+          height={isSafari ? 'auto' : '100%'}
+          isLoading={isLoading}
+          size={IMAGE_SIZE as any}
+          src={"/images/updateFile.jpg"}
+          style={{
+            alignItems: 'center',
+            backgroundColor: 'white',
+            display: 'flex',
+            height: isSafari ? 'auto' : '100%',
+            justifyContent: 'center'
+          }}
+          wrapperClassName={cx(styles.image, editable && styles.editableImage)}
+        />
+        <div style={{ color: '#ffffff', fontWeight: 'bold', left: '50%', position: 'absolute', textAlign: 'center', top: '55%', transform: 'translate(-50%, -50%)', width: '90%' }}>
+          <a
+            href={data?.url}
+            rel="noreferrer noopener"
+            style={{ color: '#ffffff', textDecoration: 'none' }}
+            target="_blank"
+          >
+            {temFileName}
+          </a>
+        </div>
+      </div>
+    );
+  }
 });
 
 export default ImageFileItem;
