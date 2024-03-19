6e2e9069399859183b4a12035b31df38dadb6548
Arvin Xu <arvinx@foxmail.com>
2024-03-19 10:50:56 +0800

    Update global.ts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,39 +1,33 @@
 import { css } from 'antd-style';
 
-import { isInStandaloneMode } from '@/utils/matchMedia';
+export default ({ prefixCls }: { prefixCls: string }) => css`
+  html,
+  body,
+  #__next,
+  .${prefixCls}-app {
+    position: relative;
+    overscroll-behavior: none;
+    height: 100% !important;
+    max-height: 100dvh !important;
 
-export default ({ prefixCls }: { prefixCls: string }) => {
-  // 当水合前也判断为 PWA，防止界面跳动
-  const isPWA = typeof window === 'undefined' || isInStandaloneMode();
-  return css`
-    html,
-    body,
-    #__next,
-    .${prefixCls}-app {
-      position: relative;
-      overscroll-behavior: none;
-      height: ${isPWA ? '100vh' : '100%'} !important;
-      max-height: ${isPWA ? '100vh' : '100dvh'} !important;
+    ::-webkit-scrollbar {
+      display: none;
+      width: 0;
+      height: 0;
+    }
+  }
 
+  p {
+    margin-bottom: 0;
+  }
+
+  @media (max-width: 575px) {
+    * {
       ::-webkit-scrollbar {
         display: none;
         width: 0;
         height: 0;
       }
     }
-
-    p {
-      margin-bottom: 0;
-    }
-
-    @media (max-width: 575px) {
-      * {
-        ::-webkit-scrollbar {
-          display: none;
-          width: 0;
-          height: 0;
-        }
-      }
-    }
-  `;
-};
+  }
+`;
