6b2aea34cd93bedcfbfa8fb9be43052137984a42
Leo Q <LeoQuote@users.noreply.github.com>
2024-03-20 06:11:10 +0000

    :sparkles: feat: support configuring multiple plugin index

--- src/app/api/plugin/store/route.ts
+++ src/app/api/plugin/store/route.ts
@@ -1,3 +1,7 @@
+import { LobeChatPluginsMarketIndex } from '@lobehub/chat-plugin-sdk';
+import { NextResponse } from 'next/server';
+
+import { getServerConfig } from '@/config/server';
 import { DEFAULT_LANG } from '@/const/locale';
 
 import { PluginStore } from './Store';
@@ -7,15 +11,27 @@ export const runtime = 'edge';
 export const GET = async (req: Request) => {
   const locale = new URL(req.url).searchParams.get('locale');
 
-  const pluginStore = new PluginStore();
+  const pluginBaseUrls = getServerConfig().PLUGINS_INDEX_URL.split(',');
+  let pluginIndex: LobeChatPluginsMarketIndex = {
+    plugins: [],
+    schemaVersion: 1,
+  };
+  for (const baseUrl of pluginBaseUrls) {
+    const pluginStore = new PluginStore(baseUrl);
+    let res: Response;
+    res = await fetch(pluginStore.getPluginIndexUrl(locale as any));
 
-  let res: Response;
+    if (res.status === 404) {
+      res = await fetch(pluginStore.getPluginIndexUrl(DEFAULT_LANG));
+    }
 
-  res = await fetch(pluginStore.getPluginIndexUrl(locale as any), { next: { revalidate: 3600 } });
+    if (res.status !== 200) {
+      continue;
+    }
 
-  if (res.status === 404) {
-    res = await fetch(pluginStore.getPluginIndexUrl(DEFAULT_LANG));
+    const data: LobeChatPluginsMarketIndex = await res.json();
+    pluginIndex.plugins = pluginIndex.plugins.concat(data.plugins);
   }
 
-  return res;
+  return NextResponse.json(pluginIndex);
 };
