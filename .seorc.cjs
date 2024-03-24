773ef04d223ad33c399c2ae79b2465a54317a1d4
canisminor1990 <i@canisminor.cc>
2024-03-24 16:47:08 +0800

    🔧 chore: Update cli model config

--- .seorc.cjs
+++ .seorc.cjs
@@ -2,7 +2,7 @@ const { defineConfig } = require('@lobehub/seo-cli');
 
 module.exports = defineConfig({
   entry: ['./docs/**/*.mdx'],
-  modelName: 'gpt-3.5-turbo-1106',
+  modelName: 'gpt-3.5-turbo-0125',
   tagStringify: true,
   experimental: {
     jsonMode: true,
