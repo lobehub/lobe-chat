bd96dd15a9095c24fc8c2de1f97c3cdfefbe664f
Jason <78479629+imjason@users.noreply.github.com>
2024-04-09 18:40:50 +0800

    Update index.tsx

--- src/app/settings/features/SettingList/index.tsx
+++ src/app/settings/features/SettingList/index.tsx
@@ -21,7 +21,6 @@ const SettingList = memo<SettingListProps>(({ activeTab, mobile }) => {
     { icon: Webhook, label: t('tab.llm'), value: SettingsTabs.LLM },
     { icon: Mic2, label: t('tab.tts'), value: SettingsTabs.TTS },
     { icon: Bot, label: t('tab.agent'), value: SettingsTabs.Agent },
-    { icon: Info, label: t('tab.about'), value: SettingsTabs.About },
   ];
 
   return items.map(({ value, icon, label }) => (
