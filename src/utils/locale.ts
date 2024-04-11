44b4190800e236eda18fbc37a0eafb7c0bfb044b
Arvin Xu <arvinx@foxmail.com>
2024-04-11 10:55:00 +0800

    Update locale.ts

--- src/utils/locale.ts
+++ src/utils/locale.ts
@@ -9,7 +9,7 @@ export const getAntdLocale = async (lang?: string) => {
   // And we don't want to handle it in `normalizeLocale` function
   // because of other locale files are all `ar` not `ar-EG`
   if (normalLang === 'ar') normalLang = 'ar-EG';
-  
+
   const { default: locale } = await import(`antd/locale/${normalLang.replace('-', '_')}.js`);
 
   return locale;
