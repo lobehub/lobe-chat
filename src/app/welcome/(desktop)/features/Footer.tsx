ab96a767a7e670a35fbb7e5051e8cbbed374ec7f
Jason <78479629+imjason@users.noreply.github.com>
2024-04-13 23:16:56 +0800

    Update Footer.tsx

--- src/app/welcome/(desktop)/features/Footer.tsx
+++ src/app/welcome/(desktop)/features/Footer.tsx
@@ -16,7 +16,7 @@ const Footer = memo(() => {
   return (
     <Flexbox align={'center'} horizontal justify={'space-between'} style={{ padding: 16 }}>
       <span style={{ color: theme.colorTextDescription }}>
-        ©{new Date().getFullYear()} LobeHub
+        ©{new Date().getFullYear()} EDUGPT
       </span>
       <Flexbox horizontal>
         <ActionIcon
