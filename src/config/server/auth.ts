ba9eb293ab89ad61c8967c9a1469091906a338f9
arvinxx <arvinx@foxmail.com>
2024-04-15 00:50:10 +0800

    ♻️ refactor: refactor the auth code

--- src/config/server/auth.ts
+++ src/config/server/auth.ts
@@ -4,13 +4,32 @@ declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace NodeJS {
     interface ProcessEnv {
-      // Vercel Analytics
-      ENABLE_VERCEL_ANALYTICS?: string;
-      DEBUG_VERCEL_ANALYTICS?: string;
+      ENABLE_OAUTH_SSO?: string;
+      SSO_PROVIDERS?: string;
 
-      // Google Analytics
-      ENABLE_GOOGLE_ANALYTICS?: string;
-      GOOGLE_ANALYTICS_MEASUREMENT_ID?: string;
+      AUTH0_CLIENT_ID?: string;
+      AUTH0_CLIENT_SECRET?: string;
+      AUTH0_ISSUER?: string;
+
+      // Github
+      GITHUB_CLIENT_ID?: string;
+      GITHUB_CLIENT_SECRET?: string;
+
+      // Azure AD
+      AZURE_AD_CLIENT_ID?: string;
+      AZURE_AD_CLIENT_SECRET?: string;
+      AZURE_AD_TENANT_ID?: string;
+
+      // AUTHENTIK
+      AUTHENTIK_CLIENT_ID?: string;
+      AUTHENTIK_CLIENT_SECRET?: string;
+      AUTHENTIK_ISSUER?: string;
+
+      // ZITADEL
+      ZITADEL_CLIENT_ID?: string;
+      ZITADEL_CLIENT_SECRET?: string;
+      ZITADEL_ISSUER?: string;
+      NEXTAUTH_SECRET?: string;
     }
   }
 }
