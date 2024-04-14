d0972ce3d38d1781a4b58b81675998bccc33f598
Jason <78479629+imjason@users.noreply.github.com>
2024-04-14 11:24:18 +0800

    Update BottomActions.tsx

--- src/layout/GlobalLayout/Desktop/SideBar/BottomActions.tsx
+++ src/layout/GlobalLayout/Desktop/SideBar/BottomActions.tsx
@@ -1,13 +1,10 @@
 import { ActionIcon, DiscordIcon, Icon } from '@lobehub/ui';
 import { Badge, ConfigProvider, Dropdown, MenuProps } from 'antd';
 import {
-  Book,
   Feather,
   FileClock,
-  Github,
   HardDriveDownload,
   HardDriveUpload,
-  Heart,
   Settings,
   Settings2,
 } from 'lucide-react';
@@ -16,8 +13,6 @@ import { useRouter } from 'next/navigation';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Flexbox } from 'react-layout-kit';
-
-import { ABOUT, CHANGELOG, DISCORD, DOCUMENTS, FEEDBACK, GITHUB } from '@/const/url';
 import DataImporter from '@/features/DataImporter';
 import { configService } from '@/services/config';
 import { GlobalStore, useGlobalStore } from '@/store/global';
@@ -78,33 +73,6 @@ const BottomActions = memo<BottomActionProps>(({ tab }) => {
       type: 'divider',
     },
     {
-      icon: <Icon icon={Feather} />,
-      key: 'feedback',
-      label: t('feedback'),
-      onClick: () => window.open(FEEDBACK, '__blank'),
-    },
-    {
-      icon: <Icon icon={FileClock} />,
-      key: 'changelog',
-      label: t('changelog'),
-      onClick: () => window.open(CHANGELOG, '__blank'),
-    },
-    {
-      icon: <Icon icon={DiscordIcon} />,
-      key: 'wiki',
-      label: 'Discord',
-      onClick: () => window.open(DISCORD, '__blank'),
-    },
-    {
-      icon: <Icon icon={Heart} />,
-      key: 'about',
-      label: t('about'),
-      onClick: () => window.open(ABOUT, '__blank'),
-    },
-    {
-      type: 'divider',
-    },
-    {
       icon: <Icon icon={Settings} />,
       key: 'setting',
       label: (
@@ -120,12 +88,6 @@ const BottomActions = memo<BottomActionProps>(({ tab }) => {
 
   return (
     <>
-      <Link aria-label={'GitHub'} href={GITHUB} target={'_blank'}>
-        <ActionIcon icon={Github} placement={'right'} title={'GitHub'} />
-      </Link>
-      <Link aria-label={t('document')} href={DOCUMENTS} target={'_blank'}>
-        <ActionIcon icon={Book} placement={'right'} title={t('document')} />
-      </Link>
       <Dropdown arrow={false} menu={{ items }} trigger={['click']}>
         {hasNewVersion ? (
           <Flexbox>
