216dc1cd4f422cc4e7ef80cac3ba237fc52050ba
Jason <78479629+imjason@users.noreply.github.com>
2024-04-14 10:58:11 +0800

    Update Footer.tsx

--- src/app/welcome/(desktop)/features/Footer.tsx
+++ src/app/welcome/(desktop)/features/Footer.tsx
@@ -2,12 +2,10 @@
 
 import { useTheme } from 'antd-style';
 import { memo } from 'react';
-import { useTranslation } from 'react-i18next';
 import { Flexbox } from 'react-layout-kit';
 
 const Footer = memo(() => {
   const theme = useTheme();
-  const { t } = useTranslation('common');
 
   return (
     <Flexbox align={'center'} horizontal justify={'space-between'} style={{ padding: 16 }}>
