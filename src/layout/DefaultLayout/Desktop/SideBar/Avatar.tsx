ec1d3f92d20da5219ab4175148e2b838e4d17651
arvinxx <arvinx@foxmail.com>
2024-04-15 00:52:09 +0800

    ♻️ refactor: refactor the auth code

--- src/layout/DefaultLayout/Desktop/SideBar/Avatar.tsx
+++ src/layout/DefaultLayout/Desktop/SideBar/Avatar.tsx
@@ -1,24 +1,9 @@
-import { UserButton, useUser } from '@clerk/nextjs';
-import Image from 'next/image';
 import { memo } from 'react';
 
-import { DEFAULT_USER_AVATAR_URL } from '@/const/meta';
+import AvatarWithUpload from '@/features/AvatarWithUpload';
 
 const Avatar = memo(() => {
-  const { isSignedIn, isLoaded } = useUser();
-
-  console.log(isLoaded);
-  return isSignedIn ? (
-    <UserButton
-      appearance={{
-        elements: {
-          avatarBox: { height: 40, width: 40 },
-        },
-      }}
-    />
-  ) : (
-    <Image alt={'LobeChat'} height={40} src={DEFAULT_USER_AVATAR_URL} unoptimized width={40} />
-  );
+  return <AvatarWithUpload id={'avatar'} />;
 });
 
 Avatar.displayName = 'Avatar';
