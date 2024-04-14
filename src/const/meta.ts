d1c0212af7720d0f460c5c3ecb247a02a87c8e58
h7ml <h7ml@qq.com>
2024-04-14 11:56:24 +0800

    ⭐ feat: Customize user metadata and avatar information(#1122)

--- src/const/meta.ts
+++ src/const/meta.ts
@@ -1,9 +1,25 @@
-import { MetaData } from '@/types/meta';
+import { MetaData as OriginalMetaData } from '@/types/meta';
 
-export const DEFAULT_AVATAR = '🤖';
-export const DEFAULT_USER_AVATAR = '😀';
-export const DEFAULT_BACKGROUND_COLOR = 'rgba(0,0,0,0)';
-export const DEFAULT_AGENT_META: MetaData = {};
-export const DEFAULT_INBOX_AVATAR = '🤯';
+interface ProcessEnvMeta {
+  DEFAULT_AVATAR?: string;
+  DEFAULT_BACKGROUND_COLOR?: string;
+  DEFAULT_INBOX_AVATAR?: string;
+  DEFAULT_USER_AVATAR?: string;
+}
+
+const envMeta: ProcessEnvMeta = process.env.META ? JSON.parse(process.env.META) : {};
+
+const {
+  DEFAULT_AVATAR = '🤖',
+  DEFAULT_USER_AVATAR = '😀',
+  DEFAULT_BACKGROUND_COLOR = 'rgba(0,0,0,0)',
+  DEFAULT_INBOX_AVATAR = '🤯',
+} = envMeta;
+
+export { DEFAULT_AVATAR, DEFAULT_BACKGROUND_COLOR, DEFAULT_INBOX_AVATAR, DEFAULT_USER_AVATAR };
+
+export type MetaData = OriginalMetaData;
+
 export const DEFAULT_USER_AVATAR_URL =
+  process.env.DEFAULT_USER_AVATAR_URL ??
   'https://registry.npmmirror.com/@lobehub/assets-logo/1.2.0/files/assets/logo-3d.webp';
