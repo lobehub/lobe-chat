fec2ca7b086641445e65870e43a7b28583e38224
Jason <78479629+imjason@users.noreply.github.com>
2024-04-14 21:40:42 +0800

    Update Header.tsx

--- src/app/market/_layout/Desktop/Header.tsx
+++ src/app/market/_layout/Desktop/Header.tsx
@@ -3,8 +3,6 @@ import { createStyles } from 'antd-style';
 import Link from 'next/link';
 import { memo } from 'react';
 
-import ShareAgentButton from '../../features/ShareAgentButton';
-
 export const useStyles = createStyles(({ css, token }) => ({
   logo: css`
     color: ${token.colorText};
@@ -22,7 +20,6 @@ const Header = memo(() => {
           <Logo className={styles.logo} extra={'Discover'} size={36} type={'text'} />
         </Link>
       }
-      right={<ShareAgentButton />}
     />
   );
 });
