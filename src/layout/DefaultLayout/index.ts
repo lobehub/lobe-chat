8104a7525c7335b302c4135d53668ddf69ef6781
arvinxx <arvinx@foxmail.com>
2024-04-15 00:54:02 +0800

    🎨 refactor: improve code

--- src/layout/DefaultLayout/index.ts
+++ src/layout/DefaultLayout/index.ts
@@ -1,2 +1,2 @@
-export {default as DefaultLayoutDesktop} from './Desktop'
-export {default as DefaultLayoutMobile} from './Mobile'
+export { default as DefaultLayoutDesktop } from './Desktop';
+export { default as DefaultLayoutMobile } from './Mobile';
