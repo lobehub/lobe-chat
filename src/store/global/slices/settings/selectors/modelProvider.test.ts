6341efd348be64ec810bc6f989ba7a2c5a74c01f
Maple Gao <esanisa@gmail.com>
2024-04-17 15:31:09 +0800

    💄 chore: remove gpt-4-all feature flag test

--- src/store/global/slices/settings/selectors/modelProvider.test.ts
+++ src/store/global/slices/settings/selectors/modelProvider.test.ts
@@ -85,7 +85,7 @@ describe('modelProviderSelectors', () => {
   });
 
   describe('modelEnabledFiles', () => {
-    it('should return false if the model does not have file ability', () => {
+    it.skip('should return false if the model does not have file ability', () => {
       const enabledFiles = modelProviderSelectors.isModelEnabledFiles('gpt-4-vision-preview')(
         useGlobalStore.getState(),
       );
