b7286fa62e7cfad403ae8adfb6c593ffca2e06e1
Ryo Shen <shenlw66@gmail.com>
2024-04-19 18:11:21 +0800

    use fetchSSE function

--- src/libs/agent-runtime/minimax/index.ts
+++ src/libs/agent-runtime/minimax/index.ts
@@ -1,9 +1,16 @@
-import { AIStream, AIStreamCallbacksAndOptions, AIStreamParser, StreamingTextResponse } from 'ai';
+import { StreamingTextResponse } from 'ai';
 import OpenAI from 'openai';
+
+import { fetchSSE } from '@/utils/fetch';
 
 import { LobeRuntimeAI } from '../BaseAI';
 import { AgentRuntimeErrorType } from '../error';
-import { ChatCompetitionOptions, ChatStreamPayload, ModelProvider } from '../types';
+import {
+  ChatCompetitionOptions,
+  ChatCompletionErrorPayload,
+  ChatStreamPayload,
+  ModelProvider,
+} from '../types';
 import { AgentRuntimeError } from '../utils/createError';
 
 interface MinimaxBaseResponse {
@@ -13,19 +20,7 @@ interface MinimaxBaseResponse {
   };
 }
 
-const parseMinimaxStream = (): AIStreamParser => {
-  return (data) => {
-    const json = JSON.parse(data) as OpenAI.ChatCompletionChunk;
-
-    const delta = json.choices.at(0)?.delta.content;
-
-    return delta || '';
-  };
-};
-
-const MinimaxStream = (res: Response, cb?: AIStreamCallbacksAndOptions): ReadableStream => {
-  return AIStream(res, parseMinimaxStream(), cb);
-};
+type MinimaxResponse = Partial<OpenAI.ChatCompletionChunk> & MinimaxBaseResponse;
 
 export class LobeMinimaxAI implements LobeRuntimeAI {
   apiKey: string;
@@ -41,20 +36,49 @@ export class LobeMinimaxAI implements LobeRuntimeAI {
     options?: ChatCompetitionOptions,
   ): Promise<StreamingTextResponse> {
     try {
-      const fetchResponse = await fetch('https://api.minimax.chat/v1/text/chatcompletion_v2', {
-        body: JSON.stringify(this.buildCompletionsParams(payload)),
-        headers: {
-          'Authorization': `Bearer ${this.apiKey}`,
-          'Content-Type': 'application/json',
+      let dataResponse: MinimaxResponse;
+      let streamController: ReadableStreamDefaultController;
+      const readableStream = new ReadableStream({
+        start(controller) {
+          streamController = controller;
         },
-        method: 'POST',
       });
 
-      this.throwIfErrorResponse(fetchResponse.clone());
-      const minimaxStream = MinimaxStream(fetchResponse.clone());
-      return new StreamingTextResponse(minimaxStream, { headers: options?.headers });
+      await fetchSSE(
+        () =>
+          fetch('https://api.minimax.chat/v1/text/chatcompletion_v2', {
+            body: JSON.stringify(this.buildCompletionsParams(payload)),
+            headers: {
+              'Authorization': `Bearer ${this.apiKey}`,
+              'Content-Type': 'application/json',
+            },
+            method: 'POST',
+          }),
+        {
+          onFinish: async () => {
+            streamController.close();
+            this.throwIfErrorResponse(dataResponse);
+          },
+          onMessageHandle: (text) => {
+            let body = text;
+            if (body.startsWith('data:')) {
+              body = body.slice(5).trim();
+            }
+            const data = JSON.parse(body) as MinimaxResponse;
+            dataResponse = data;
+            if (data.choices?.at(0)?.delta.content) {
+              streamController.enqueue(data.choices.at(0)?.delta.content);
+            }
+          },
+        },
+      );
+
+      return new StreamingTextResponse(readableStream, { headers: options?.headers });
     } catch (error) {
-      const err = error as Error;
+      const err = error as Error | ChatCompletionErrorPayload;
+      if ('provider' in err) {
+        throw error;
+      }
       const errorResult = {
         cause: err.cause,
         message: err.message,
@@ -80,15 +104,9 @@ export class LobeMinimaxAI implements LobeRuntimeAI {
     };
   }
 
-  private async throwIfErrorResponse(response: Response) {
-    const bodyText = await response.text();
-    if (bodyText.startsWith('data:')) {
-      // normal chat message
-      return;
-    }
+  private throwIfErrorResponse(data: MinimaxResponse) {
     // error status code
     // https://www.minimaxi.com/document/guides/chat-model/pro/api?id=6569c85948bc7b684b30377e#3.1.3%20%E8%BF%94%E5%9B%9E(response)%E5%8F%82%E6%95%B0
-    const data = JSON.parse(bodyText) as MinimaxBaseResponse;
     if (!data.base_resp?.status_code || data.base_resp?.status_code < 1000) {
       return;
     }
