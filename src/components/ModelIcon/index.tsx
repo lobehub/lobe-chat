dfed806971206c636441781c9aaa095ac244309e
Maple Gao <esanisa@gmail.com>
2024-04-20 03:31:23 +0800

    Revert "✨ new: support more model Icons: dbrx, command-r, openchat, rwkv, Bert-vits2, Stable Diffusion, WizardLM, adobe firefly, skylark"
    This reverts commit 1a6ada28fe38a10806e4aeb8de4b77702b5489be.

--- src/components/ModelIcon/index.tsx
+++ src/components/ModelIcon/index.tsx
@@ -1,14 +1,9 @@
 import {
-  AdobeFirefly,
   Aws,
-  Azure,
   Baichuan,
-  ByteDance,
   ChatGLM,
   Claude,
   Cohere,
-  Dbrx,
-  FishAudio,
   Gemini,
   Gemma,
   Hunyuan,
@@ -18,12 +13,9 @@ import {
   Mistral,
   Moonshot,
   OpenAI,
-  OpenChat,
   OpenRouter,
   Perplexity,
-  Rwkv,
   Spark,
-  Stability,
   Tongyi,
   Wenxin,
   Yi,
@@ -40,57 +32,34 @@ const ModelIcon = memo<ModelProviderIconProps>(({ model: originModel, size = 12 
 
   // lower case the origin model so to better match more model id case
   const model = originModel.toLowerCase();
-  const modelToComponentMap = {
-    'abab': <Minimax.Avatar size={size} />,
-    'baichuan': <Baichuan.Avatar background={Baichuan.colorPrimary} size={size} />,
-    'chatglm': <ChatGLM.Avatar size={size} />,
-    'claude': <Claude.Avatar size={size} />,
-    'command': <Cohere.Avatar size={size} />,
-    'dbrx': <Dbrx.Avatar size={size} />,
-    'ernie': <Wenxin.Avatar size={size} />,
-    'firefly': <AdobeFirefly.Avatar size={size} />,
-    'gemini': <Gemini.Avatar size={size} />,
-    'gemma': <Gemma.Avatar size={size} />,
-    'glm': <ChatGLM.Avatar size={size} />,
-    'gpt-3': <OpenAI.Avatar size={size} type={'gpt3'} />,
-    'gpt-4': <OpenAI.Avatar size={size} type={'gpt4'} />,
-    'hunyuan': <Hunyuan.Avatar size={size} />,
-    'llama': <Meta.Avatar size={size} />,
-    'llava': <LLaVA.Avatar size={size} />,
-    'minmax': <Minimax.Avatar size={size} />,
-    'mistral': <Mistral.Avatar size={size} />,
-    'mixtral': <Mistral.Avatar size={size} />,
-    'moonshot': <Moonshot.Avatar size={size} />,
-    'openchat': <OpenChat.Avatar size={size} />,
-    'pplx': <Perplexity.Avatar size={size} />,
-    'qwen': <Tongyi.Avatar background={Tongyi.colorPrimary} size={size} />,
-    'rwkv': <Rwkv.Avatar size={size} />,
-    'sdxl': <Stability.Avatar background={Stability.colorPrimary} size={size} />,
-    'skylark': <ByteDance.Avatar size={size} />,
-    'sonar': <Perplexity.Avatar size={size} />,
-    'spark': <Spark.Avatar size={size} />,
-    'stable-': <Stability.Avatar background={Stability.colorPrimary} size={size} />,
-    'stable-diffusion': <Stability.Avatar background={Stability.colorPrimary} size={size} />,
-    'stable-video': <Stability.Avatar background={Stability.colorPrimary} size={size} />,
-    'stablelm': <Stability.Avatar background={Stability.colorPrimary} size={size} />,
-    'titan': <Aws.Avatar size={size} />,
-    'wizardlm': <Azure.Avatar size={size} />,
-    'yi-': <Yi.Avatar size={size} />,
-  };
 
-  // find the component based on the model name
-  for (const [key, component] of Object.entries(modelToComponentMap)) {
-    if (model.includes(key)) {
-      return component;
-    }
-  }
+  if (model.includes('gpt-3')) return <OpenAI.Avatar size={size} type={'gpt3'} />;
+  if (model.includes('gpt-4')) return <OpenAI.Avatar size={size} type={'gpt4'} />;
+  if (model.startsWith('glm') || model.includes('chatglm')) return <ChatGLM.Avatar size={size} />;
+  if (model.includes('claude')) return <Claude.Avatar size={size} />;
+  if (model.includes('titan')) return <Aws.Avatar size={size} />;
+  if (model.includes('llama')) return <Meta.Avatar size={size} />;
+  if (model.includes('llava')) return <LLaVA.Avatar size={size} />;
+  if (model.includes('gemini')) return <Gemini.Avatar size={size} />;
+  if (model.includes('gemma')) return <Gemma.Avatar size={size} />;
+  if (model.includes('qwen')) return <Tongyi.Avatar background={Tongyi.colorPrimary} size={size} />;
+  if (model.includes('minmax')) return <Minimax.Avatar size={size} />;
+  if (model.includes('moonshot')) return <Moonshot.Avatar size={size} />;
+  if (model.includes('baichuan'))
+    return <Baichuan.Avatar background={Baichuan.colorPrimary} size={size} />;
 
-  // handle special case with startsWith
-  if (model.startsWith('openrouter')) return <OpenRouter.Avatar size={size} />;
-  if (model.startsWith('stable-') || model.startsWith('sd3'))
-    return <Stability.Avatar size={size} />;
-  if (model.startsWith('d_') || model.startsWith('g_') || model.startsWith('wd_'))
-    return <FishAudio.Avatar size={size} />;
+  if (model.includes('mistral') || model.includes('mixtral')) return <Mistral.Avatar size={size} />;
+
+  if (model.includes('pplx') || model.includes('sonar')) return <Perplexity.Avatar size={size} />;
+
+  if (model.includes('yi-')) return <Yi.Avatar size={size} />;
+  if (model.includes('openrouter')) return <OpenRouter.Avatar size={size} />;
+  if (model.includes('command')) return <Cohere.Color size={size} />;
+
+  if (model.includes('ernie')) return <Wenxin.Avatar size={size} />;
+  if (model.includes('spark')) return <Spark.Avatar size={size} />;
+  if (model.includes('hunyuan')) return <Hunyuan.Avatar size={size} />;
+  if (model.includes('abab')) return <Minimax.Avatar size={size} />;
 });
 
 export default ModelIcon;
--- src/components/ModelTag/ModelIcon.tsx
+++ src/components/ModelTag/ModelIcon.tsx
@@ -1,30 +1,18 @@
 import {
-  AdobeFirefly,
   Aws,
-  Azure,
   Baichuan,
-  ByteDance,
   ChatGLM,
   Claude,
-  Cohere,
-  Dbrx,
-  FishAudio,
   Gemini,
   Gemma,
-  Hunyuan,
   LLaVA,
   Meta,
   Minimax,
   Mistral,
   Moonshot,
   OpenAI,
-  OpenRouter,
   Perplexity,
-  Rwkv,
-  Spark,
-  Stability,
   Tongyi,
-  Wenxin,
   ZeroOne,
 } from '@lobehub/icons';
 import { memo } from 'react';
@@ -37,9 +25,8 @@ interface ModelIconProps {
 const ModelIcon = memo<ModelIconProps>(({ model, size = 12 }) => {
   if (!model) return;
 
-  // currently supported models, maybe not in its own provider
   if (model.startsWith('gpt')) return <OpenAI size={size} />;
-  if (model.startsWith('glm') || model.includes('chatglm')) return <ChatGLM size={size} />;
+  if (model.startsWith('glm')) return <ChatGLM size={size} />;
   if (model.includes('claude')) return <Claude size={size} />;
   if (model.includes('titan')) return <Aws size={size} />;
   if (model.includes('llama')) return <Meta size={size} />;
@@ -49,34 +36,10 @@ const ModelIcon = memo<ModelIconProps>(({ model, size = 12 }) => {
   if (model.includes('moonshot')) return <Moonshot size={size} />;
   if (model.includes('qwen')) return <Tongyi size={size} />;
   if (model.includes('minmax')) return <Minimax size={size} />;
-  if (model.includes('mistral') || model.includes('mixtral')) return <Mistral size={size} />;
-  if (model.includes('pplx') || model.includes('sonar')) return <Perplexity size={size} />;
-  if (model.startsWith('yi-')) return <ZeroOne size={size} />;
-  if (model.startsWith('openrouter')) return <OpenRouter size={size} />; // only for Cinematika and Auto
-  if (model.includes('command')) return <Cohere size={size} />;
-  if (model.includes('dbrx')) return <Dbrx size={size} />;
-  if (model.includes('openchat')) return <Dbrx size={size} />;
-
-  // below: To be supported in providers, move up if supported
   if (model.includes('baichuan')) return <Baichuan size={size} />;
-  if (model.includes('rwkv')) return <Rwkv size={size} />;
-  if (model.includes('ernie')) return <Wenxin size={size} />;
-  if (model.includes('spark')) return <Spark size={size} />;
-  if (model.includes('hunyuan')) return <Hunyuan size={size} />;
-  if (model.startsWith('d_') || model.startsWith('g_') || model.startsWith('wd_'))
-    return <FishAudio size={size} />; // ref https://github.com/fishaudio/Bert-VITS2/blob/master/train_ms.py#L702
-  if (model.includes('skylark')) return <ByteDance size={size} />;
-  if (
-    model.includes('stable-diffusion') ||
-    model.includes('stable-video') ||
-    model.includes('sdxl') ||
-    model.includes('stablelm') ||
-    model.startsWith('stable-') ||
-    model.startsWith('sd3')
-  )
-    return <Stability size={size} />;
-  if (model.includes('wizardlm')) return <Azure size={size} />;
-  if (model.includes('firefly')) return <AdobeFirefly size={size} />;
+  if (model.includes('mistral') || model.includes('mixtral')) return <Mistral size={size} />;
+  if (model.includes('pplx')) return <Perplexity size={size} />;
+  if (model.startsWith('yi-')) return <ZeroOne size={size} />;
 });
 
 export default ModelIcon;
