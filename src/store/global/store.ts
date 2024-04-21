98c844bf55626b7835c550c892dea778a8511e53
Arvin Xu <arvinx@foxmail.com>
2024-04-22 02:51:21 +0800

    🐛 fix: fix model list menu not display correctly (#2133)
    * ♻️ refactor: remove preference hydration
    
    * 🐛 fix: fix model-list menu not display correctly
    
    * 🐛 fix: fix azure not show the deployment name
    
    * ♻️ refactor: rename the StoreHydration

--- src/store/global/store.ts
+++ src/store/global/store.ts
@@ -1,11 +1,10 @@
-import { PersistOptions, devtools, persist, subscribeWithSelector } from 'zustand/middleware';
+import { devtools, subscribeWithSelector } from 'zustand/middleware';
 import { shallow } from 'zustand/shallow';
 import { createWithEqualityFn } from 'zustand/traditional';
 import { StateCreator } from 'zustand/vanilla';
 
 import { isDev } from '@/utils/env';
 
-import { createHyperStorage } from '../middleware/createHyperStorage';
 import { type GlobalState, initialState } from './initialState';
 import { type CommonAction, createCommonSlice } from './slices/common/action';
 import { type PreferenceAction, createPreferenceSlice } from './slices/preference/action';
@@ -22,32 +21,13 @@ const createStore: StateCreator<GlobalStore, [['zustand/devtools', never]]> = (.
   ...createPreferenceSlice(...parameters),
 });
 
-//  ===============  persist 本地缓存中间件配置 ============ //
-type GlobalPersist = Pick<GlobalStore, 'preference' | 'settings'>;
-
-const persistOptions: PersistOptions<GlobalStore, GlobalPersist> = {
-  name: 'LOBE_GLOBAL',
-
-  skipHydration: true,
-
-  storage: createHyperStorage({
-    localStorage: {
-      dbName: 'LobeHub',
-      selectors: ['preference'],
-    },
-  }),
-};
-
 //  ===============  实装 useStore ============ //
 
 export const useGlobalStore = createWithEqualityFn<GlobalStore>()(
-  persist(
-    subscribeWithSelector(
-      devtools(createStore, {
-        name: 'LobeChat_Global' + (isDev ? '_DEV' : ''),
-      }),
-    ),
-    persistOptions,
+  subscribeWithSelector(
+    devtools(createStore, {
+      name: 'LobeChat_Global' + (isDev ? '_DEV' : ''),
+    }),
   ),
   shallow,
 );
