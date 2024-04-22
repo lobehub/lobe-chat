4268d8be22068e2f26b2638852e3ba34b6bf5011
Maple Gao <esanisa@gmail.com>
2024-04-22 20:14:35 +0800

    💄 style: support more model Icons: dbrx, command-r, openchat, rwkv, Bert-vits2, Stable Diffusion, WizardLM, adobe firefly, skylark (#2107)
    * ✨ new: support more model Icons: dbrx, command-r, openchat, rwkv, Bert-vits2, Stable Diffusion, WizardLM, adobe firefly, skylark
    
    * 💄 add: model Stable Cascade
    
    * 💄 add: Jamba and OpenChat
    
    * ♻️ refactor: support more model Icons: dbrx, command-r, openchat, rwkv, Bert-vits2, Stable Diffusion, WizardLM, adobe firefly, skylark and so on
    
    * 💄 fix: remove unused Zhipu from import
    
    * 💄 fix: 01.AI not show Icons in other providers.

--- src/components/ModelIcon/index.tsx
+++ src/components/ModelIcon/index.tsx
@@ -1,9 +1,15 @@
 import {
+  Adobe,
+  Ai21,
   Aws,
+  Azure,
   Baichuan,
+  ByteDance,
   ChatGLM,
   Claude,
   Cohere,
+  Dbrx,
+  FishAudio,
   Gemini,
   Gemma,
   Hunyuan,
@@ -13,9 +19,12 @@ import {
   Mistral,
   Moonshot,
   OpenAI,
+  OpenChat,
   OpenRouter,
   Perplexity,
+  Rwkv,
   Spark,
+  Stability,
   Tongyi,
   Wenxin,
   Yi,
@@ -33,33 +42,63 @@ const ModelIcon = memo<ModelProviderIconProps>(({ model: originModel, size = 12 
   // lower case the origin model so to better match more model id case
   const model = originModel.toLowerCase();
 
+  // currently supported models, maybe not in its own provider
   if (model.includes('gpt-3')) return <OpenAI.Avatar size={size} type={'gpt3'} />;
   if (model.includes('gpt-4')) return <OpenAI.Avatar size={size} type={'gpt4'} />;
-  if (model.startsWith('glm') || model.includes('chatglm')) return <ChatGLM.Avatar size={size} />;
+  if (model.startsWith('glm') ||
+    model.includes('chatglm'))
+    return <ChatGLM.Avatar size={size} />;
   if (model.includes('claude')) return <Claude.Avatar size={size} />;
   if (model.includes('titan')) return <Aws.Avatar size={size} />;
   if (model.includes('llama')) return <Meta.Avatar size={size} />;
   if (model.includes('llava')) return <LLaVA.Avatar size={size} />;
   if (model.includes('gemini')) return <Gemini.Avatar size={size} />;
   if (model.includes('gemma')) return <Gemma.Avatar size={size} />;
+  if (model.includes('moonshot')) return <Moonshot.Avatar size={size} />;
   if (model.includes('qwen')) return <Tongyi.Avatar background={Tongyi.colorPrimary} size={size} />;
   if (model.includes('minmax')) return <Minimax.Avatar size={size} />;
-  if (model.includes('moonshot')) return <Moonshot.Avatar size={size} />;
+  if (model.includes('mistral') ||
+    model.includes('mixtral'))
+    return <Mistral.Avatar size={size} />;
+  if (model.includes('pplx') ||
+    model.includes('sonar'))
+    return <Perplexity.Avatar size={size} />;
+  if (model.includes('yi-')) return <Yi.Avatar size={size} />;
+  if (model.startsWith('openrouter')) return <OpenRouter.Avatar size={size} />; // only for Cinematika and Auto
+  if (model.startsWith('openchat')) return <OpenChat.Avatar size={size} />;
+  if (model.includes('command')) return <Cohere.Avatar size={size} />;
+  if (model.includes('dbrx')) return <Dbrx.Avatar size={size} />;
+
+  // below: To be supported in providers, move up if supported
   if (model.includes('baichuan'))
     return <Baichuan.Avatar background={Baichuan.colorPrimary} size={size} />;
-
-  if (model.includes('mistral') || model.includes('mixtral')) return <Mistral.Avatar size={size} />;
-
-  if (model.includes('pplx') || model.includes('sonar')) return <Perplexity.Avatar size={size} />;
-
-  if (model.includes('yi-')) return <Yi.Avatar size={size} />;
-  if (model.includes('openrouter')) return <OpenRouter.Avatar size={size} />;
-  if (model.includes('command')) return <Cohere.Color size={size} />;
-
-  if (model.includes('ernie')) return <Wenxin.Avatar size={size} />;
+  if (model.includes('rwkv')) return <Rwkv.Avatar size={size} />;
+  if (model.includes('ernie'))
+    return <Wenxin.Avatar size={size} />;
   if (model.includes('spark')) return <Spark.Avatar size={size} />;
   if (model.includes('hunyuan')) return <Hunyuan.Avatar size={size} />;
-  if (model.includes('abab')) return <Minimax.Avatar size={size} />;
+  // ref https://github.com/fishaudio/Bert-VITS2/blob/master/train_ms.py#L702
+  if (model.startsWith('d_') ||
+    model.startsWith('g_') || model.startsWith('wd_'))
+    return <FishAudio.Avatar size={size} />;
+  if (model.includes('skylark')) return <ByteDance.Avatar size={size} />;
+
+  if (
+    model.includes('stable-diffusion') ||
+    model.includes('stable-video') ||
+    model.includes('stable-cascade') ||
+    model.includes('sdxl') ||
+    model.includes('stablelm') ||
+    model.startsWith('stable-') ||
+    model.startsWith('sd3')
+  )
+    return <Stability.Avatar size={size} />;
+
+  if (model.includes('wizardlm')) return <Azure.Avatar size={size} />;
+  if (model.includes('firefly')) return <Adobe.Avatar size={size} />;
+  if (model.includes('jamba') ||
+    model.includes('j2-'))
+    return <Ai21.Avatar size={size} />;
 });
 
 export default ModelIcon;
