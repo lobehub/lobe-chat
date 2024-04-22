10d2b9beaa167237704473cbf994f4389bb72326
Arvin Xu <arvinx@foxmail.com>
2024-04-22 04:24:22 +0000

    💄 style: support together ai model fetch

--- src/libs/agent-runtime/togetherai/index.ts
+++ src/libs/agent-runtime/togetherai/index.ts
@@ -1,9 +1,12 @@
+import { TogetherAIModel } from '@/libs/agent-runtime/togetherai/type';
+
 import { AgentRuntimeErrorType } from '../error';
 import { ModelProvider } from '../types';
 import { LobeOpenAICompatibleFactory } from '../utils/openaiCompatibleFactory';
 
+const baseURL = 'https://api.together.xyz';
 export const LobeTogetherAI = LobeOpenAICompatibleFactory({
-  baseURL: 'https://api.together.xyz/v1',
+  baseURL: `${baseURL}/v1`,
   constructorOptions: {
     defaultHeaders: {
       'HTTP-Referer': 'https://chat-preview.lobehub.com',
@@ -16,6 +19,31 @@ export const LobeTogetherAI = LobeOpenAICompatibleFactory({
   errorType: {
     bizError: AgentRuntimeErrorType.TogetherAIBizError,
     invalidAPIKey: AgentRuntimeErrorType.InvalidTogetherAIAPIKey,
+  },
+  models: async ({ apiKey }) => {
+    const data = await fetch(`${baseURL}/api/models`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+    });
+    if (!data.ok) {
+      throw new Error(`Together Fetch Error: ${data.statusText || data.status}`);
+    }
+
+    const models: TogetherAIModel[] = await data.json();
+
+    return models.map((model) => {
+      return {
+        description: model.description,
+        displayName: model.display_name,
+        enabled: true,
+        functionCall: model.description.includes('function calling'),
+        id: model.name,
+        maxOutput: model.context_length,
+        tokens: model.context_length,
+        vision: model.description.includes('vision') || model.name.includes('vision'),
+      };
+    });
   },
   provider: ModelProvider.TogetherAI,
 });
