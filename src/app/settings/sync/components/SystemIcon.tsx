9cdcf88c827340db353203ccc1c6f8fce072cc8b
Maple Gao <esanisa@gmail.com>
2024-04-22 13:06:22 +0800

    💄 fix: Windows Phone, iPadOS, BlackBerry OS, Linux OS and Chrome OS sync icons.

--- src/app/settings/sync/components/SystemIcon.tsx
+++ src/app/settings/sync/components/SystemIcon.tsx
@@ -1,4 +1,4 @@
-import { SiAndroid, SiApple, SiWindows11 } from '@icons-pack/react-simple-icons';
+import {SiAndroid, SiApple, SiBlackberry, SiGooglechrome, SiLinux, SiWindows11} from '@icons-pack/react-simple-icons';
 import { memo } from 'react';
 
 // TODO: 等 simple icons 修复类型，移除 ignore
@@ -7,13 +7,22 @@ const SystemIcon = memo<{ title?: string }>(({ title }) => {
   if (!title) return;
 
   // @ts-ignore
-  if (['Mac OS', 'iOS'].includes(title)) return <SiApple size={32} />;
+  if (['Mac OS', 'iOS', 'iPadOS'].includes(title)) return <SiApple size={32} />;
 
   // @ts-ignore
-  if (title === 'Windows') return <SiWindows11 size={32} />;
+  if (['Windows'].includes(title))return <SiWindows11 size={32} />;
 
   // @ts-ignore
   if (title === 'Android') return <SiAndroid size={32} />;
+
+  // @ts-ignore
+  if (['BlackBerry'].includes(title))return <SiBlackberry size={32} />;
+
+  // @ts-ignore
+  if (title === 'Linux') return <SiLinux size={32} />;
+
+  // @ts-ignore
+  if (title === 'Chrome OS') return <SiGooglechrome size={32} />;
 
   return null;
 });
