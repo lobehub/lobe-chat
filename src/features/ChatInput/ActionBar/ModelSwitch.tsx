ff4f3568a350335331f9a0ad918a1a983dbd4e52
arvinxx <arvinx@foxmail.com>
2024-04-26 01:00:03 +0800

    🎨 chore: clear code

--- src/features/ChatInput/ActionBar/ModelSwitch.tsx
+++ src/features/ChatInput/ActionBar/ModelSwitch.tsx
@@ -1,5 +1,4 @@
 import { ActionIcon } from '@lobehub/ui';
-import { useWhyDidYouUpdate } from 'ahooks';
 import { BrainCog } from 'lucide-react';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -7,9 +6,8 @@ import { useTranslation } from 'react-i18next';
 import ModelSwitchPanel from '@/features/ModelSwitchPanel';
 
 const ModelSwitch = memo(() => {
-  const { t, ...res } = useTranslation('chat');
+  const { t } = useTranslation('chat');
 
-  useWhyDidYouUpdate('Trans', { res, t });
   return (
     <ModelSwitchPanel>
       <ActionIcon icon={BrainCog} placement={'bottom'} title={t('ModelSwitch.title')} />
