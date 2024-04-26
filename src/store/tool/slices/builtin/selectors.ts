b375e7eea273cb9fc5a53155553c037b32967ba8
arvinxx <arvinx@foxmail.com>
2024-04-27 00:09:31 +0800

    ✨ feat: support hide dalle

--- src/store/tool/slices/builtin/selectors.ts
+++ src/store/tool/slices/builtin/selectors.ts
@@ -1,3 +1,4 @@
+import { DalleManifest } from '@/tools/dalle';
 import { LobeToolMeta } from '@/types/tool/tool';
 
 import { pluginHelpers } from '../../helpers';
@@ -6,13 +7,17 @@ import type { ToolStoreState } from '../../initialState';
 const isBuiltinTool = (id: string) => (s: ToolStoreState) =>
   pluginHelpers.isCustomPlugin(id, s.installedPlugins);
 
-const metaList = (s: ToolStoreState): LobeToolMeta[] =>
-  s.builtinTools.map((t) => ({
-    author: 'LobeHub',
-    identifier: t.identifier,
-    meta: t.manifest.meta,
-    type: 'builtin',
-  }));
+const metaList =
+  (showDalle?: boolean) =>
+  (s: ToolStoreState): LobeToolMeta[] =>
+    s.builtinTools
+      .filter((item) => (!showDalle ? item.identifier !== DalleManifest.identifier : true))
+      .map((t) => ({
+        author: 'LobeHub',
+        identifier: t.identifier,
+        meta: t.manifest.meta,
+        type: 'builtin',
+      }));
 
 export const builtinToolSelectors = {
   isBuiltinTool,
