465665a735556669ee30446c7ea9049a20cc7c37
Arvin Xu <arvinx@foxmail.com>
2024-04-28 17:03:58 +0800

    🐛 fix: fix `/api/proxy` internal proxy attack (#2255)

--- src/app/api/proxy/route.ts
+++ src/app/api/proxy/route.ts
@@ -1,12 +1,34 @@
-export const runtime = 'edge';
+import { isPrivate } from 'ip';
+import { NextResponse } from 'next/server';
+import dns from 'node:dns';
+import { promisify } from 'node:util';
+
+const lookupAsync = promisify(dns.lookup);
+
+export const runtime = 'nodejs';
 
 /**
  * just for a proxy
  */
 export const POST = async (req: Request) => {
-  const url = await req.text();
+  const url = new URL(await req.text());
+  let address;
 
-  const res = await fetch(url);
+  try {
+    const lookupResult = await lookupAsync(url.hostname);
+    address = lookupResult.address;
+  } catch (err) {
+    console.error(`${url.hostname} DNS parser error:`, err);
+
+    return NextResponse.json({ error: 'DNS parser error' }, { status: 504 });
+  }
+
+  const isInternalHost = isPrivate(address);
+
+  if (isInternalHost)
+    return NextResponse.json({ error: 'Not support internal host proxy' }, { status: 400 });
+
+  const res = await fetch(url.toString());
 
   return new Response(res.body, { headers: res.headers });
 };
