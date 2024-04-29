03bcb06ef4873648951acb23df0b06ad073d93e6
sxjeru <sxjeru@gmail.com>
2024-04-29 23:48:49 +0800

    💄 style: patching models info (#2269)
    * update models config
    
    * patch
    
    * context window
    
    * [Request] Add MiniMax new models #22802280
    
    * pass test

--- src/config/modelProviders/minimax.ts
+++ src/config/modelProviders/minimax.ts
@@ -1,7 +1,22 @@
 import { ModelProviderCard } from '@/types/llm';
 
+// ref https://www.minimaxi.com/document/guides/chat-model/pro/api
 const Minimax: ModelProviderCard = {
   chatModels: [
+    {
+      description: '复杂场景，例如应用题计算、科学计算等场景',
+      displayName: 'abab6.5',
+      enabled: true,
+      id: 'abab6.5-chat',
+      tokens: 8192,
+    },
+    {
+      description: '通用场景',
+      displayName: 'abab6.5s',
+      enabled: true,
+      id: 'abab6.5s-chat',
+      tokens: 245_760,
+    },
     {
       description: '更复杂的格式化文本生成',
       displayName: 'abab6',
