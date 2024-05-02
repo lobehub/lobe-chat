6ac2cbb4d2b493542c0eeec0206fa4891974bdc8
sxjeru <sxjeru@gmail.com>
2024-05-02 23:14:22 +0800

    const Kibi = 1024;

--- src/app/(main)/settings/llm/components/ProviderModelList/MaxTokenSlider.tsx
+++ src/app/(main)/settings/llm/components/ProviderModelList/MaxTokenSlider.tsx
@@ -4,9 +4,11 @@ import { useTranslation } from 'react-i18next';
 import { Flexbox } from 'react-layout-kit';
 import useMergeState from 'use-merge-value';
 
+const Kibi = 1024;
+
 const exponent = (num: number) => Math.log2(num);
 const getRealValue = (num: number) => Math.round(Math.pow(2, num));
-const power1024 = (num: number) => Math.round(Math.pow(2, num) * 1024);
+const powerKibi = (num: number) => Math.round(Math.pow(2, num) * Kibi);
 
 const isSmallScreen = typeof window !== 'undefined' ? window.innerWidth < 475 : false;
 
@@ -20,8 +22,8 @@ const marks: SliderSingleProps['marks'] = {
   [exponent(64)]: isSmallScreen ? '64' : '64K',
   // [exponent(128)]: isSmallScreen ? '128' : '128K',
   // [exponent(256)]: isSmallScreen ? '256' : '256K',
-  [exponent((128 / 1024) * 1000)]: ' ', // hide tick mark
-  [exponent((200 / 1024) * 1000)]: isSmallScreen ? '200' : '200k', // 200,000
+  [exponent((128 / Kibi) * 1000)]: ' ', // hide tick mark
+  [exponent((200 / Kibi) * 1000)]: isSmallScreen ? '200' : '200k', // 200,000
   [exponent(1024)]: isSmallScreen ? '1024' : '1M',
 };
 
@@ -42,19 +44,19 @@ const MaxTokenSlider = memo<MaxTokenSliderProps>(({ value, onChange, defaultValu
 
   const [powValue, setPowValue] = useMergeState(0, {
     defaultValue: exponent(typeof defaultValue === 'undefined' ? 0 : defaultValue / 1024),
-    value: exponent(typeof value === 'undefined' ? 0 : value / 1024),
+    value: exponent(typeof value === 'undefined' ? 0 : value / Kibi),
   });
 
   const updateWithPowValue = (value: number) => {
     setPowValue(value);
 
-    setTokens(getRealValue(value) === 1 ? 0 : power1024(value));
+    setTokens(getRealValue(value) === 1 ? 0 : powerKibi(value));
   };
 
   const updateWithRealValue = (value: number) => {
     setTokens(Math.round(value));
 
-    setPowValue(exponent(value / 1024));
+    setPowValue(exponent(value / Kibi));
   };
 
   return (
@@ -73,8 +75,8 @@ const MaxTokenSlider = memo<MaxTokenSliderProps>(({ value, onChange, defaultValu
 
               let value = getRealValue(x);
               if (value < 125) return value.toFixed(0) + 'K';
-              else if (value < 1024) return ((value * 1024) / 1000).toFixed(0) + 'k';
-              return (value / 1024).toFixed(0) + 'M';
+              else if (value < Kibi) return ((value * Kibi) / 1000).toFixed(0) + 'k';
+              return (value / Kibi).toFixed(0) + 'M';
             },
           }}
           value={powValue}
