5047890a150f6c835d045922d344ef76fac318e2
canisminor1990 <i@canisminor.cc>
2024-05-02 22:51:54 +0800

    💄 style: Update styles and i18n

--- src/app/(main)/settings/_layout/Desktop/index.tsx
+++ src/app/(main)/settings/_layout/Desktop/index.tsx
@@ -35,7 +35,7 @@ const Layout = memo<LayoutProps>(({ children, category }) => {
           gap={64}
           style={{
             maxWidth: 1024,
-            padding: mobile ? undefined : '24px 20px 24px 28px',
+            padding: mobile ? undefined : '32px 24px',
           }}
           width={'100%'}
         >
