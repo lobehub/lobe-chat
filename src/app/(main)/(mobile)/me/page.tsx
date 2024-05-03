79b8115cf8e472f93df9fca5045f34e32bca0669
CanisMinor <i@canisminor.cc>
2024-05-03 17:35:26 +0800

    💄 style: improve market layout styles and mobile style (#2347)
    * 💄 style: Layout styles patch
    
    * 💄 style: Update style

--- src/app/(main)/(mobile)/me/page.tsx
+++ src/app/(main)/(mobile)/me/page.tsx
@@ -5,7 +5,7 @@ import BrandWatermark from '@/components/BrandWatermark';
 import Avatar from '@/features/AvatarWithUpload';
 import { isMobileDevice } from '@/utils/responsive';
 
-import AvatarBanner from './features/AvatarBanner';
+import AvatarBanner, { AVATAR_SIZE } from './features/AvatarBanner';
 import Cate from './features/Cate';
 import ExtraCate from './features/ExtraCate';
 
@@ -17,7 +17,7 @@ const Page = () => {
   return (
     <>
       <AvatarBanner>
-        <Avatar size={88} />
+        <Avatar size={AVATAR_SIZE} />
       </AvatarBanner>
       <Cate />
       <ExtraCate />
