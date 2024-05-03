5cecee096eb91c3bbf8b0bec7ebf33713a418a8d
Arvin Xu <arvinx@foxmail.com>
2024-05-03 20:52:22 +0800

    💄 style: add user panel and refactor the next-auth (#2349)
    * ♻️ refactor: refactor the next-auth
    
    * ✨ feat: Add User Panel
    
    * 💄 style: Update User Avatar on mobile
    
    * 🚸 style: fix data importer hot zone
    
    * 🚸 style: add migration guide
    
    * 🎨 chore:  clean code
    
    * ✅ test: add test
    
    * 🌐 chore: update locale
    
    * 💄 style: improve style
    
    * ✅ test: fix test
    
    * 💄 style: improve locale switch
    
    * ♻️ refactor: use middleware redirect instead of page
    
    ---------
    
    Co-authored-by: canisminor1990 <i@canisminor.cc>

--- src/layout/AuthProvider/NextAuth/index.tsx
+++ src/layout/AuthProvider/NextAuth/index.tsx
@@ -3,8 +3,15 @@ import { PropsWithChildren } from 'react';
 
 import { API_ENDPOINTS } from '@/services/_url';
 
+import UserUpdater from './UserUpdater';
+
 const NextAuth = ({ children }: PropsWithChildren) => {
-  return <SessionProvider basePath={API_ENDPOINTS.oauth}>{children}</SessionProvider>;
+  return (
+    <SessionProvider basePath={API_ENDPOINTS.oauth}>
+      {children}
+      <UserUpdater />
+    </SessionProvider>
+  );
 };
 
 export default NextAuth;
--- src/store/user/slices/auth/selectors.ts
+++ src/store/user/slices/auth/selectors.ts
@@ -1,6 +1,9 @@
 import { UserStore } from '@/store/user';
+import { LobeUser } from '@/types/user';
 
 export const userProfileSelectors = {
-  userAvatar: (s: UserStore): string => s.avatar || '',
+  userAvatar: (s: UserStore): string => s.user?.avatar || s.avatar || '',
   userId: (s: UserStore) => s.userId,
+  userProfile: (s: UserStore): LobeUser | null | undefined => s.user,
+  username: (s: UserStore): string | null | undefined => s.user?.username,
 };
