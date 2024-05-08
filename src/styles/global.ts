909ec1c7d87dbd2e72cfaf0646965eef4436cd81
CanisMinor <i@canisminor.cc>
2024-05-08 05:49:48 +0000

    🐛 fix: Fix lint

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,14 +1,14 @@
 import { Theme, css } from 'antd-style';
 
+// fix ios input keyboard
+// overflow: hidden;
+// ref: https://zhuanlan.zhihu.com/p/113855026
 export default ({ token }: { prefixCls: string; token: Theme }) => css`
   html,
   body,
   #__next {
     position: relative;
 
-    // fix ios input keyboard
-    // overflow: hidden;
-    // ref: https://zhuanlan.zhihu.com/p/113855026
     overscroll-behavior: none;
 
     height: 100%;
