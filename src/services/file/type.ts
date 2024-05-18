882ebbe940b827f39c88f6e9b14bab3aeafdcbb8
Arvin Xu <arvinx@foxmail.com>
2024-05-18 22:30:48 +0800

    ✨ feat: support upload images to s3  (#2547)
    * ♻️ refactor: refactor file by file and upload for upload to s3
    
    * ✨ feat: support upload file to s3
    
    * ✅ test: fix test
    
    * 🛂 feat: add auth for the upload path
    
    * ✅ test: fix circular deps
    
    * ✅ test: add more tests
    
    * ✅ test: add more tests
    
    * ✅ test: add more tests
    
    * ✅ test: add more tests

--- src/services/file/type.ts
+++ src/services/file/type.ts
@@ -3,8 +3,7 @@ import { DB_File } from '@/database/client/schemas/files';
 import { FilePreview } from '@/types/files';
 
 export interface IFileService {
-  uploadFile(file: DB_File): Promise<any>;
-  uploadImageByUrl(url: string, file: Pick<DB_File, 'name' | 'metadata'>): Promise<any>;
+  createFile(file: DB_File): Promise<any>;
   removeFile(id: string): Promise<any>;
   removeAllFiles(): Promise<any>;
   getFile(id: string): Promise<FilePreview>;
