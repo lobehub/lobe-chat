4536a6aee4401beeed599bf26ff28743ce7607db
Arvin Xu <arvinx@foxmail.com>
2024-05-20 05:51:19 +0000

    ♻️ refactor: refactor analytics env

--- src/config/client.ts
+++ src/config/client.ts
@@ -8,23 +8,6 @@ declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace NodeJS {
     interface ProcessEnv {
-      NEXT_PUBLIC_ANALYTICS_MIXPANEL?: string;
-      NEXT_PUBLIC_MIXPANEL_PROJECT_TOKEN?: string;
-      NEXT_PUBLIC_MIXPANEL_DEBUG?: string;
-
-      NEXT_PUBLIC_ANALYTICS_PLAUSIBLE?: string;
-      NEXT_PUBLIC_PLAUSIBLE_DOMAIN?: string;
-      NEXT_PUBLIC_PLAUSIBLE_SCRIPT_BASE_URL?: string;
-
-      NEXT_PUBLIC_ANALYTICS_POSTHOG: string;
-      NEXT_PUBLIC_POSTHOG_KEY: string;
-      NEXT_PUBLIC_POSTHOG_HOST: string;
-      NEXT_PUBLIC_POSTHOG_DEBUG: string;
-
-      NEXT_PUBLIC_ANALYTICS_UMAMI?: string;
-      NEXT_PUBLIC_UMAMI_SCRIPT_URL?: string;
-      NEXT_PUBLIC_UMAMI_WEBSITE_ID?: string;
-
       NEXT_PUBLIC_I18N_DEBUG: string;
       NEXT_PUBLIC_I18N_DEBUG_BROWSER: string;
       NEXT_PUBLIC_I18N_DEBUG_SERVER: string;
@@ -40,24 +23,6 @@ export const getClientConfig = () => ({
   ENABLED_SERVER_SERVICE: process.env.NEXT_PUBLIC_SERVICE_MODE === 'server',
 
   BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH || '',
-
-  // Plausible Analytics
-  ANALYTICS_PLAUSIBLE: process.env.NEXT_PUBLIC_ANALYTICS_PLAUSIBLE === '1',
-  PLAUSIBLE_DOMAIN: process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN,
-  PLAUSIBLE_SCRIPT_BASE_URL:
-    process.env.NEXT_PUBLIC_PLAUSIBLE_SCRIPT_BASE_URL || 'https://plausible.io',
-
-  // Posthog Analytics
-  ANALYTICS_POSTHOG: process.env.NEXT_PUBLIC_ANALYTICS_POSTHOG === '1',
-  POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
-  POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  POSTHOG_DEBUG: process.env.NEXT_PUBLIC_POSTHOG_DEBUG === '1',
-
-  // Umami Analytics
-  ANALYTICS_UMAMI: process.env.NEXT_PUBLIC_ANALYTICS_UMAMI === '1',
-  UMAMI_SCRIPT_URL:
-    process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL || 'https://analytics.umami.is/script.js',
-  UMAMI_WEBSITE_ID: process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID,
 
   // Sentry
   ENABLE_SENTRY: !!process.env.NEXT_PUBLIC_SENTRY_DSN,
