8729a0ef91cc468e7e99662f06a004ab079d5eab
arvinxx <arvinx@foxmail.com>
2024-05-22 02:18:41 +0800

    ♻️ refactor: refactor the llm env

--- src/app/api/config.ts
+++ src/app/api/config.ts
@@ -1,13 +1,12 @@
-import { getServerConfig } from '@/config/server';
+import { llmEnv } from '@/config/llm';
 
 export const getPreferredRegion = (region: string | string[] = 'auto') => {
   try {
-    const cfg = getServerConfig();
-    if (cfg.OPENAI_FUNCTION_REGIONS.length <= 0) {
+    if (llmEnv.OPENAI_FUNCTION_REGIONS.length <= 0) {
       return region;
     }
 
-    return cfg.OPENAI_FUNCTION_REGIONS;
+    return llmEnv.OPENAI_FUNCTION_REGIONS;
   } catch (error) {
     console.error('get server config failed, error:', error);
     return region;
--- src/config/server/index.ts
+++ src/config/server/index.ts
@@ -1,13 +1,9 @@
 import { getAppConfig } from './app';
-import { getProviderConfig } from './provider';
 
 export const getServerConfig = () => {
   if (typeof process === 'undefined') {
     throw new Error('[Server Config] you are importing a server-only module outside of server');
   }
 
-  const provider = getProviderConfig();
-  const app = getAppConfig();
-
-  return { ...provider, ...app };
+  return getAppConfig();
 };
