6e78edab78608ebbbf3ad994b9228fa1157e7d4c
arvinxx <arvinx@foxmail.com>
2024-05-23 00:27:01 +0800

    ⚡️ perf: improve session performance

--- src/app/(main)/chat/@session/default.tsx
+++ src/app/(main)/chat/@session/default.tsx
@@ -1,9 +1,13 @@
+import { Suspense, lazy } from 'react';
+
 import ServerLayout from '@/components/server/ServerLayout';
 
 import Desktop from './_layout/Desktop';
 import Mobile from './_layout/Mobile';
 import SessionHydration from './features/SessionHydration';
-import SessionListContent from './features/SessionListContent';
+import SkeletonList from './features/SkeletonList';
+
+const SessionListContent = lazy(() => import('./features/SessionListContent'));
 
 const Layout = ServerLayout({ Desktop, Mobile });
 
@@ -11,7 +15,9 @@ const Session = () => {
   return (
     <>
       <Layout>
-        <SessionListContent />
+        <Suspense fallback={<SkeletonList />}>
+          <SessionListContent />
+        </Suspense>
       </Layout>
       <SessionHydration />
     </>
