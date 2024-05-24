b1fa6bd0384f2b5e118fb054921c54143cbc76ac
canisminor1990 <i@canisminor.cc>
2024-05-24 23:11:57 +0800

    💄 style: Add title

--- src/features/ChatInput/ActionBar/Token/TokenTag.tsx
+++ src/features/ChatInput/ActionBar/Token/TokenTag.tsx
@@ -1,9 +1,10 @@
-import { TokenTag } from '@lobehub/ui';
+import { TokenTag, Tooltip } from '@lobehub/ui';
 import { Popover } from 'antd';
 import { useTheme } from 'antd-style';
+import numeral from 'numeral';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Flexbox } from 'react-layout-kit';
+import { Center, Flexbox } from 'react-layout-kit';
 
 import { useTokenCount } from '@/hooks/useTokenCount';
 import { useAgentStore } from '@/store/agent';
@@ -18,7 +19,7 @@ import { modelProviderSelectors } from '@/store/user/selectors';
 import TokenProgress from './TokenProgress';
 
 const Token = memo(() => {
-  const { t } = useTranslation('chat');
+  const { t } = useTranslation(['chat', 'components']);
   const theme = useTheme();
 
   const [input, messageString] = useChatStore((s) => [
@@ -57,57 +58,79 @@ const Token = memo(() => {
 
   // Total token
   const totalToken = systemRoleToken + toolsToken + chatsToken;
+
+  const content = (
+    <Flexbox gap={12} style={{ minWidth: 200 }}>
+      <Flexbox align={'center'} gap={4} horizontal justify={'space-between'} width={'100%'}>
+        <div style={{ color: theme.colorTextDescription }}>{t('tokenDetails.title')}</div>
+        <Tooltip
+          overlayStyle={{ maxWidth: 'unset', pointerEvents: 'none' }}
+          title={t('ModelSelect.featureTag.tokens', {
+            ns: 'components',
+            tokens: numeral(maxTokens).format('0,0'),
+          })}
+        >
+          <Center
+            height={20}
+            paddingInline={4}
+            style={{
+              background: theme.colorFillTertiary,
+              borderRadius: 4,
+              color: theme.colorTextSecondary,
+              fontFamily: theme.fontFamilyCode,
+              fontSize: 11,
+            }}
+          >
+            TOKEN
+          </Center>
+        </Tooltip>
+      </Flexbox>
+      <TokenProgress
+        data={[
+          {
+            color: theme.magenta,
+            id: 'systemRole',
+            title: t('tokenDetails.systemRole'),
+            value: systemRoleToken,
+          },
+          {
+            color: theme.geekblue,
+            id: 'tools',
+            title: t('tokenDetails.tools'),
+            value: toolsToken,
+          },
+          {
+            color: theme.gold,
+            id: 'chats',
+            title: t('tokenDetails.chats'),
+            value: chatsToken,
+          },
+        ]}
+        showIcon
+      />
+      <TokenProgress
+        data={[
+          {
+            color: theme.colorSuccess,
+            id: 'used',
+            title: t('tokenDetails.used'),
+            value: totalToken,
+          },
+          {
+            color: theme.colorFill,
+            id: 'rest',
+            title: t('tokenDetails.rest'),
+            value: maxTokens - totalToken,
+          },
+        ]}
+        showIcon
+        showTotal={t('tokenDetails.total')}
+      />
+    </Flexbox>
+  );
+
   return (
-    <Popover
-      arrow={false}
-      content={
-        <Flexbox gap={12} style={{ minWidth: 160 }}>
-          <TokenProgress
-            data={[
-              {
-                color: theme.magenta,
-                id: 'systemRole',
-                title: t('tokenDetails.systemRole'),
-                value: systemRoleToken,
-              },
-              {
-                color: theme.geekblue,
-                id: 'tools',
-                title: t('tokenDetails.tools'),
-                value: toolsToken,
-              },
-              {
-                color: theme.gold,
-                id: 'chats',
-                title: t('tokenDetails.chats'),
-                value: chatsToken,
-              },
-            ]}
-            showIcon
-          />
-          <TokenProgress
-            data={[
-              {
-                color: theme.colorSuccess,
-                id: 'used',
-                title: t('tokenDetails.used'),
-                value: totalToken,
-              },
-              {
-                color: theme.colorFill,
-                id: 'rest',
-                title: t('tokenDetails.rest'),
-                value: maxTokens - totalToken,
-              },
-            ]}
-            showIcon
-            showTotal={t('tokenDetails.total')}
-          />
-        </Flexbox>
-      }
-      placement={'top'}
-      trigger={['hover', 'click']}
-    >
+    <Popover arrow={false} content={content} placement={'top'} trigger={['hover', 'click']}>
       <TokenTag
         displayMode={'used'}
         maxValue={maxTokens}
