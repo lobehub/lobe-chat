55346defb833bf158e4f430199acd7514e635639
cy948@ultra7 <cy948@vip.qq.com>
2024-06-12 01:10:26 +0800

    :test_tube: test: isProviderApiKeyNotEmpty

--- src/store/user/slices/modelList/selectors/keyVaults.test.ts
+++ src/store/user/slices/modelList/selectors/keyVaults.test.ts
@@ -70,7 +70,7 @@ describe('keyVaultsConfigSelectors', () => {
     });
 
     describe('AWSBedrockKeyVault', () => {
-      it('should return true if provider endpoint is not empty for AWSBedrockKeyVault', () => {
+      it('should return true if provider region is not empty for AWSBedrockKeyVault', () => {
         const s = merge(initialSettingsState, {
           settings: {
             keyVaults: {
@@ -83,7 +83,7 @@ describe('keyVaultsConfigSelectors', () => {
         expect(keyVaultsConfigSelectors.isProviderEndpointNotEmpty('bedrock')(s)).toBe(true);
       });
 
-      it('should return false if provider endpoint is empty for AWSBedrockKeyVault', () => {
+      it('should return false if provider region is empty for AWSBedrockKeyVault', () => {
         const s = merge(initialSettingsState, {
           settings: {
             keyVaults: {
@@ -94,6 +94,106 @@ describe('keyVaultsConfigSelectors', () => {
           },
         }) as unknown as UserStore;
         expect(keyVaultsConfigSelectors.isProviderEndpointNotEmpty('bedrock')(s)).toBe(false);
+      });
+    });
+  });
+
+  describe('isProviderApiKeyNotEmpty', () => {
+    describe('OpenAICompatibleKeyVault', () => {
+      it('should return true if provider apikey is not empty', () => {
+        const s = merge(initialSettingsState, {
+          settings: {
+            keyVaults: {
+              openai: {
+                apiKey: 'apikey',
+              } as OpenAICompatibleKeyVault,
+            },
+          },
+        }) as unknown as UserStore;
+        expect(keyVaultsConfigSelectors.isProviderApiKeyNotEmpty('openai')(s)).toBe(true);
+      });
+
+      it('should return false if provider apikey is empty', () => {
+        const s = merge(initialSettingsState, {
+          settings: {
+            keyVaults: {
+              openai: {
+                apiKey: undefined,
+              } as OpenAICompatibleKeyVault,
+            },
+          },
+        }) as unknown as UserStore;
+        expect(keyVaultsConfigSelectors.isProviderApiKeyNotEmpty('openai')(s)).toBe(false);
+      });
+    });
+
+    describe('AzureOpenAIKeyVault', () => {
+      it('should return true if provider apikey is not empty', () => {
+        const s = merge(initialSettingsState, {
+          settings: {
+            keyVaults: {
+              azure: {
+                apiKey: 'apikey',
+              } as AzureOpenAIKeyVault,
+            },
+          },
+        }) as unknown as UserStore;
+        expect(keyVaultsConfigSelectors.isProviderApiKeyNotEmpty('azure')(s)).toBe(true);
+      });
+
+      it('should return false if provider apikey is empty', () => {
+        const s = merge(initialSettingsState, {
+          settings: {
+            keyVaults: {
+              azure: {
+                apiKey: undefined,
+              } as AzureOpenAIKeyVault,
+            },
+          },
+        }) as unknown as UserStore;
+        expect(keyVaultsConfigSelectors.isProviderApiKeyNotEmpty('azure')(s)).toBe(false);
+      });
+    });
+
+    describe('AWSBedrockKeyVault', () => {
+      it('should return true if provider accessKeyId is not empty for AWSBedrockKeyVault', () => {
+        const s = merge(initialSettingsState, {
+          settings: {
+            keyVaults: {
+              bedrock: {
+                accessKeyId: 'accessKeyId',
+              } as AWSBedrockKeyVault,
+            },
+          },
+        }) as unknown as UserStore;
+        expect(keyVaultsConfigSelectors.isProviderApiKeyNotEmpty('bedrock')(s)).toBe(true);
+      });
+
+      it('should return true if provider secretAccessKey is not empty for AWSBedrockKeyVault', () => {
+        const s = merge(initialSettingsState, {
+          settings: {
+            keyVaults: {
+              bedrock: {
+                secretAccessKey: 'secretAccessKey',
+              } as AWSBedrockKeyVault,
+            },
+          },
+        }) as unknown as UserStore;
+        expect(keyVaultsConfigSelectors.isProviderApiKeyNotEmpty('bedrock')(s)).toBe(true);
+      });
+
+      it('should return false if provider accessKeyId and secretAccessKey are both empty for AWSBedrockKeyVault', () => {
+        const s = merge(initialSettingsState, {
+          settings: {
+            keyVaults: {
+              bedrock: {
+                accessKeyId: undefined,
+                secretAccessKey: undefined,
+              } as AWSBedrockKeyVault,
+            },
+          },
+        }) as unknown as UserStore;
+        expect(keyVaultsConfigSelectors.isProviderApiKeyNotEmpty('bedrock')(s)).toBe(false);
       });
     });
   });
