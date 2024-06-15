9a97c78dd11ffcea5f6eb05c206ab742ac688d0a
sxjeru <sxjeru@gmail.com>
2024-06-15 19:33:16 +0800

    Update index.ts

--- src/libs/agent-runtime/qwen/index.ts
+++ src/libs/agent-runtime/qwen/index.ts
@@ -7,8 +7,16 @@ export const LobeQwenAI = LobeOpenAICompatibleFactory({
   baseURL: 'https://dashscope.aliyuncs.com/compatible-mode/v1',
   chatCompletion: {
     handlePayload: (payload) => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { temperature, ...restPayload } = payload;
       const top_p = payload.top_p;
-      return {
+      const model = payload.model;
+      
+      return model.includes('-vl-') ? {
+        ...restPayload,
+        stream: restPayload.stream ?? true,
+        top_p: top_p && top_p >= 1 ? 0.9999 : top_p,
+      } as OpenAI.ChatCompletionCreateParamsStreaming : {
         ...payload,
         stream: payload.stream ?? true,
         top_p: top_p && top_p >= 1 ? 0.9999 : top_p,
