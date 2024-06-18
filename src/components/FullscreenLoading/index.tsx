2b54caeda18497cee3cd07a3ad29945c881247f6
Arvin Xu <arvinx@foxmail.com>
2024-06-18 21:00:17 +0800

    💄 style: update brand and improve docs (#2917)
    * 💄 style: update logo style
    
    * ♻️ refactor: hide webrtc sync by default
    
    * 🔧 chore: improve db migration script
    
    * 💄 style: rename to assistant
    
    * 💄 style: update brand
    
    * 💄 style: update brand
    
    * ⚡️ perf: dynamic pwa install
    
    * ✅ test: fix tests
    
    * 📝 docs: update docs
    
    * 📝 docs: update env
    
    * 📝 docs: update docs

--- src/components/FullscreenLoading/index.tsx
+++ src/components/FullscreenLoading/index.tsx
@@ -1,4 +1,5 @@
-import { Icon, Logo } from '@lobehub/ui';
+import { Icon } from '@lobehub/ui';
+import { LobeChat } from '@lobehub/ui/brand';
 import { Loader2 } from 'lucide-react';
 import { memo } from 'react';
 import { Center, Flexbox } from 'react-layout-kit';
@@ -7,7 +8,7 @@ const FullscreenLoading = memo<{ title?: string }>(({ title }) => {
   return (
     <Flexbox height={'100%'} style={{ userSelect: 'none' }} width={'100%'}>
       <Center flex={1} gap={12} width={'100%'}>
-        <Logo extra={'Chat'} size={48} type={'combine'} />
+        <LobeChat size={48} type={'combine'} />
         <Center gap={16} horizontal>
           <Icon icon={Loader2} spin />
           {title}
