fa23ba43af2331504a9bba8105bccd562e8c0f0d
sxjeru <sxjeru@gmail.com>
2024-06-21 22:49:35 +0800

    Update cloudflare.ts

--- src/config/modelProviders/cloudflare.ts
+++ src/config/modelProviders/cloudflare.ts
@@ -5,14 +5,77 @@ import { ModelProviderCard } from '@/types/llm';
 const Cloudflare: ModelProviderCard = {
   chatModels: [
     {
-      displayName: 'LLaMA2-7B-chat',
+      displayName: 'deepseek-coder-6.7b-instruct-awq',
       enabled: true,
-    //   functionCall: true,
-      id: '@cf/meta/llama-2-7b-chat-fp16',
-      tokens: 3072,
+      id: '@hf/thebloke/deepseek-coder-6.7b-instruct-awq',
+      tokens: 16384,
+    },
+    {
+      displayName: 'deepseek-math-7b-instruct',
+      enabled: true,
+      id: '@hf/thebloke/deepseek-math-7b-instruct',
+      tokens: 4096,
+    },
+    {
+      displayName: 'gemma-7b-it',
+      enabled: true,
+      id: '@hf/google/gemma-7b-it',
+      tokens: 2048,
+    },
+    {
+      displayName: 'hermes-2-pro-mistral-7b',
+      enabled: true,
+      // functionCall: true,
+      id: '@hf/nousresearch/hermes-2-pro-mistral-7b',
+      tokens: 4096,
+    },
+    {
+      displayName: 'llama-3-8b-instruct-awq',
+      id: '@cf/meta/llama-3-8b-instruct-awq',
+      tokens: 8192,
+    },
+    {
+      displayName: 'mistral-7b-instruct-v0.2',
+      id: '@hf/mistral/mistral-7b-instruct-v0.2',
+      tokens: 4096,
+    },
+    {
+      displayName: 'neural-chat-7b-v3-1-awq',
+      enabled: true,
+      id: '@hf/thebloke/neural-chat-7b-v3-1-awq',
+      tokens: 32768,
+    },
+    {
+      displayName: 'openchat-3.5-0106',
+      id: '@cf/openchat/openchat-3.5-0106',
+      tokens: 8192,
+    },
+    {
+      displayName: 'openhermes-2.5-mistral-7b-awq',
+      enabled: true,
+      id: '@hf/thebloke/openhermes-2.5-mistral-7b-awq',
+      tokens: 32768,
+    },
+    {
+      displayName: 'qwen1.5-14b-chat-awq',
+      enabled: true,
+      id: '@cf/qwen/qwen1.5-14b-chat-awq',
+      tokens: 32768,
+    },
+    {
+      displayName: 'starling-lm-7b-beta',
+      enabled: true,
+      id: '@hf/nexusflow/starling-lm-7b-beta',
+      tokens: 4096,
+    },
+    {
+      displayName: 'zephyr-7b-beta-awq',
+      enabled: true,
+      id: '@hf/thebloke/zephyr-7b-beta-awq',
+      tokens: 32768,
     },
   ],
-  checkModel: '@cf/meta/llama-2-7b-chat-fp16',
+  checkModel: '@hf/thebloke/deepseek-coder-6.7b-instruct-awq',
   id: 'cloudflare',
   name: 'Cloudflare Workers AI',
 };
