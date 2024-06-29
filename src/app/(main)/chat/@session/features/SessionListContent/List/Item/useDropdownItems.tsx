29440f4e3751499ace201315c8d8cfc7bb2741a4
sxjeru <sxjeru@gmail.com>
2024-06-29 16:28:27 +0800

    rebase

--- src/app/(main)/chat/@session/features/SessionListContent/List/Item/useDropdownItems.tsx
+++ src/app/(main)/chat/@session/features/SessionListContent/List/Item/useDropdownItems.tsx
@@ -2,6 +2,7 @@
 import { Icon } from '@lobehub/ui';
 import { App, MenuProps } from 'antd';
 import { createStyles } from 'antd-style';
+import { ItemType } from 'antd/es/menu/interface';
 import isEqual from 'fast-deep-equal';
 import {
   Check,
@@ -16,6 +17,7 @@ import {
 import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import { isServerMode } from '@/const/version';
 import { configService } from '@/services/config';
 import { useSessionStore } from '@/store/session';
 import { sessionHelpers } from '@/store/session/helpers';
@@ -42,6 +44,7 @@ export const useDropdownItems = (
 ): MenuProps['items'] => {
   const { styles } = useStyles();
   const { t } = useTranslation('chat');
+
   const sessionCustomGroups = useSessionStore(sessionGroupSelectors.sessionGroupItems, isEqual);
   const [pin, removeSession, pinSession, duplicateSession, updateSessionGroup] = useSessionStore(
     (s) => {
@@ -61,107 +64,112 @@ export const useDropdownItems = (
   const isDefault = group === SessionDefaultGroup.Default;
 
   return useMemo(
-    () => [
-      {
-        icon: <Icon icon={pin ? PinOff : Pin} />,
-        key: 'pin',
-        label: t(pin ? 'pinOff' : 'pin'),
-        onClick: () => {
-          pinSession(id, !pin);
-        },
-      },
-      {
-        icon: <Icon icon={LucideCopy} />,
-        key: 'duplicate',
-        label: t('duplicate', { ns: 'common' }),
-        onClick: ({ domEvent }) => {
-          domEvent.stopPropagation();
-
-          duplicateSession(id);
-        },
-      },
-      {
-        type: 'divider',
-      },
-      {
-        children: [
-          ...sessionCustomGroups.map(({ id: groupId, name }) => ({
-            icon: group === groupId ? <Icon icon={Check} /> : <div />,
-            key: groupId,
-            label: name,
-            onClick: () => {
-              updateSessionGroup(id, groupId);
-            },
-          })),
+    () =>
+      (
+        [
           {
-            icon: isDefault ? <Icon icon={Check} /> : <div />,
-            key: 'defaultList',
-            label: t('defaultList'),
+            icon: <Icon icon={pin ? PinOff : Pin} />,
+            key: 'pin',
+            label: t(pin ? 'pinOff' : 'pin'),
             onClick: () => {
-              updateSessionGroup(id, SessionDefaultGroup.Default);
+              pinSession(id, !pin);
+            },
+          },
+          {
+            icon: <Icon icon={LucideCopy} />,
+            key: 'duplicate',
+            label: t('duplicate', { ns: 'common' }),
+            onClick: ({ domEvent }) => {
+              domEvent.stopPropagation();
+
+              duplicateSession(id);
             },
           },
           {
             type: 'divider',
           },
           {
-            icon: <Icon icon={LucidePlus} />,
-            key: 'createGroup',
-            label: <div>{t('sessionGroup.createGroup')}</div>,
+            children: [
+              ...sessionCustomGroups.map(({ id: groupId, name }) => ({
+                icon: group === groupId ? <Icon icon={Check} /> : <div />,
+                key: groupId,
+                label: name,
+                onClick: () => {
+                  updateSessionGroup(id, groupId);
+                },
+              })),
+              {
+                icon: isDefault ? <Icon icon={Check} /> : <div />,
+                key: 'defaultList',
+                label: t('defaultList'),
+                onClick: () => {
+                  updateSessionGroup(id, SessionDefaultGroup.Default);
+                },
+              },
+              {
+                type: 'divider',
+              },
+              {
+                icon: <Icon icon={LucidePlus} />,
+                key: 'createGroup',
+                label: <div>{t('sessionGroup.createGroup')}</div>,
+                onClick: ({ domEvent }) => {
+                  domEvent.stopPropagation();
+                  openCreateGroupModal();
+                },
+              },
+            ],
+            icon: <Icon icon={ListTree} />,
+            key: 'moveGroup',
+            label: t('sessionGroup.moveGroup'),
+          },
+          {
+            type: 'divider',
+          },
+          isServerMode
+            ? undefined
+            : {
+                children: [
+                  {
+                    key: 'agent',
+                    label: t('exportType.agent', { ns: 'common' }),
+                    onClick: () => {
+                      configService.exportSingleAgent(id);
+                    },
+                  },
+                  {
+                    key: 'agentWithMessage',
+                    label: t('exportType.agentWithMessage', { ns: 'common' }),
+                    onClick: () => {
+                      configService.exportSingleSession(id);
+                    },
+                  },
+                ],
+                icon: <Icon icon={HardDriveDownload} />,
+                key: 'export',
+                label: t('export', { ns: 'common' }),
+              },
+          {
+            danger: true,
+            icon: <Icon icon={Trash} />,
+            key: 'delete',
+            label: t('delete', { ns: 'common' }),
             onClick: ({ domEvent }) => {
               domEvent.stopPropagation();
-              openCreateGroupModal();
+              modal.confirm({
+                centered: true,
+                okButtonProps: { danger: true },
+                onOk: async () => {
+                  await removeSession(id);
+                  message.success(t('confirmRemoveSessionSuccess'));
+                },
+                rootClassName: styles.modalRoot,
+                title: t('confirmRemoveSessionItemAlert'),
+              });
             },
           },
-        ],
-        icon: <Icon icon={ListTree} />,
-        key: 'moveGroup',
-        label: t('sessionGroup.moveGroup'),
-      },
-      {
-        type: 'divider',
-      },
-      {
-        children: [
-          {
-            key: 'agent',
-            label: t('exportType.agent', { ns: 'common' }),
-            onClick: () => {
-              configService.exportSingleAgent(id);
-            },
-          },
-          {
-            key: 'agentWithMessage',
-            label: t('exportType.agentWithMessage', { ns: 'common' }),
-            onClick: () => {
-              configService.exportSingleSession(id);
-            },
-          },
-        ],
-        icon: <Icon icon={HardDriveDownload} />,
-        key: 'export',
-        label: t('export', { ns: 'common' }),
-      },
-      {
-        danger: true,
-        icon: <Icon icon={Trash} />,
-        key: 'delete',
-        label: t('delete', { ns: 'common' }),
-        onClick: ({ domEvent }) => {
-          domEvent.stopPropagation();
-          modal.confirm({
-            centered: true,
-            okButtonProps: { danger: true },
-            onOk: async () => {
-              await removeSession(id);
-              message.success(t('confirmRemoveSessionSuccess'));
-            },
-            rootClassName: styles.modalRoot,
-            title: t('confirmRemoveSessionItemAlert'),
-          });
-        },
-      },
-    ],
+        ] as ItemType[]
+      ).filter(Boolean),
     [id, pin],
   );
 };
