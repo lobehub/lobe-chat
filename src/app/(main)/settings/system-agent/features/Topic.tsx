23f0f836540af36b9efbd7c6765789fb3691817d
Zhijie He <hezhijie0327@hotmail.com>
2024-07-01 21:30:25 +0800

    🔨 chore: update useSyncSystemAgent function

--- src/app/(main)/settings/system-agent/features/Topic.tsx
+++ src/app/(main)/settings/system-agent/features/Topic.tsx
@@ -3,7 +3,7 @@
 import { Form, type ItemGroup } from '@lobehub/ui';
 import { Form as AntForm } from 'antd';
 import isEqual from 'fast-deep-equal';
-import { memo, useEffect } from 'react';
+import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { FORM_STYLE } from '@/const/layoutTokens';
@@ -23,9 +23,6 @@ const Topic = memo(() => {
   const [updateSystemAgent] = useUserStore((s) => [s.updateSystemAgent]);
 
   const [form] = AntForm.useForm();
-  useEffect(() => {
-    form.setFieldsValue(settings);
-  }, [settings]);
 
   const systemAgentSettings: SettingItemGroup = {
     children: [
@@ -46,7 +43,7 @@ const Topic = memo(() => {
     title: t(`systemAgent.${systemAgentKey}.title`),
   };
 
-  useSyncSystemAgent(form);
+  useSyncSystemAgent(form, settings);
 
   return (
     <Form
