df40f6c50ff16b8c4b3a5f34dee764284224c0b5
arvinxx <arvinx@foxmail.com>
2024-07-09 21:47:38 +0800

    🐛 fix: fix assistant meta change race issue

--- src/features/AgentSetting/AgentMeta/AutoGenerateInput.tsx
+++ src/features/AgentSetting/AgentMeta/AutoGenerateInput.tsx
@@ -2,19 +2,34 @@ import { ActionIcon } from '@lobehub/ui';
 import { Input, InputProps } from 'antd';
 import { useTheme } from 'antd-style';
 import { Wand2 } from 'lucide-react';
-import { memo } from 'react';
+import { memo, useCallback, useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-export interface AutoGenerateInputProps extends InputProps {
+export interface AutoGenerateInputProps extends Omit<InputProps, 'onChange'> {
   canAutoGenerate?: boolean;
   loading?: boolean;
+  onChange?: (value: string) => void;
   onGenerate?: () => void;
+  value?: string | any;
 }
 
 const AutoGenerateInput = memo<AutoGenerateInputProps>(
-  ({ loading, onGenerate, canAutoGenerate, ...props }) => {
+  ({ loading, value, onChange, onGenerate, canAutoGenerate, ...props }) => {
     const { t } = useTranslation('common');
     const theme = useTheme();
+
+    const [input, setInput] = useState<string>(value || '');
+
+    const isChineseInput = useRef(false);
+    const isFocusing = useRef(false);
+
+    const updateValue = useCallback(() => {
+      onChange?.(input);
+    }, [input]);
+
+    useEffect(() => {
+      if (value !== undefined) setInput(value);
+    }, [value]);
 
     return (
       <Input
@@ -37,6 +52,29 @@ const AutoGenerateInput = memo<AutoGenerateInputProps>(
         }
         type="block"
         {...props}
+        onBlur={() => {
+          isFocusing.current = false;
+        }}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+        onCompositionEnd={() => {
+          isChineseInput.current = false;
+        }}
+        onCompositionStart={() => {
+          isChineseInput.current = true;
+        }}
+        onFocus={() => {
+          isFocusing.current = true;
+        }}
+        onPressEnter={(e) => {
+          if (!e.shiftKey && !isChineseInput.current) {
+            e.preventDefault();
+            updateValue();
+            isFocusing.current = false;
+          }
+        }}
+        value={input}
       />
     );
   },
