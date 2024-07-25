ec20fd0c1069d52429f625e9d9c8983d02b3da7c
Star_dusted <zhaotianxion@gmail.com>
2024-07-26 00:07:43 +0800

    💄 style: add new models to groq which are llama 3.1 (#3301)
    * feat: Add new models for groq
    
    * fix: modify the id of model
    
    * chore: Update chatModels in Groq model providers, which is LLaMA3.1

--- src/config/modelProviders/groq.ts
+++ src/config/modelProviders/groq.ts
@@ -4,6 +4,27 @@ import { ModelProviderCard } from '@/types/llm';
 const Groq: ModelProviderCard = {
   chatModels: [
     {
+      displayName: 'LLaMA3.1 405B (Preview)',
+      enabled: false,
+      functionCall: true,
+      id: 'llama-3.1-405b-reasoning',
+      tokens: 16_000,
+    },
+    {
+      displayName: 'LLaMA3.1 70B (Preview)',
+      enabled: true,
+      functionCall: true,
+      id: 'llama-3.1-70b-versatile',
+      tokens: 8000,
+    },
+    {
+      displayName: 'LLaMA3.1 8B (Preview)',
+      enabled: true,
+      functionCall: true,
+      id: 'llama-3.1-8b-instant',
+      tokens: 8000,
+    },
+    {
       displayName: 'LLaMA3 70B',
       enabled: true,
       functionCall: true,
