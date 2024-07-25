e13fb83c4134cc2dcf43f387b94354f647bf1970
sxjeru <sxjeru@gmail.com>
2024-07-25 23:16:49 +0800

    Update index.ts

--- src/libs/agent-runtime/siliconcloud/index.ts
+++ src/libs/agent-runtime/siliconcloud/index.ts
@@ -4,7 +4,7 @@ import { LobeOpenAICompatibleFactory } from '../utils/openaiCompatibleFactory';
 export const LobeSiliconCloudAI = LobeOpenAICompatibleFactory({
   baseURL: 'https://api.siliconflow.cn/v1',
   debug: {
-    chatCompletion: () => process.env.DEBUG_STEPFUN_CHAT_COMPLETION === '1',
+    chatCompletion: () => process.env.DEBUG_SILICONCLOUD_CHAT_COMPLETION === '1',
   },
   provider: ModelProvider.SiliconCloud,
 });
