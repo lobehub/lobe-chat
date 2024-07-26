e429fdd1ce06d00bf89fc6c547cebb1e4c01f6b5
sxjeru <sxjeru@gmail.com>
2024-07-26 10:11:47 +0800

    Update siliconcloud.ts

--- src/config/modelProviders/siliconcloud.ts
+++ src/config/modelProviders/siliconcloud.ts
@@ -4,15 +4,16 @@ import { ModelProviderCard } from '@/types/llm';
 const SiliconCloud: ModelProviderCard = {
   chatModels: [
     {
+      enabled: true,
       id: 'Qwen/Qwen2-72B-Instruct',
       tokens: 32_768,
     },
     {
+      enabled: true,
       id: 'Qwen/Qwen2-57B-A14B-Instruct',
       tokens: 32_768,
     },
     {
-      enabled: true,
       id: 'Qwen/Qwen2-7B-Instruct',
       tokens: 32_768,
     },
@@ -41,6 +42,7 @@ const SiliconCloud: ModelProviderCard = {
       tokens: 32_768,
     },
     {
+      enabled: true,
       id: 'deepseek-ai/DeepSeek-V2-Chat',
       tokens: 32_768,
     },
@@ -57,6 +59,7 @@ const SiliconCloud: ModelProviderCard = {
       tokens: 32_768,
     },
     {
+      enabled: true,
       id: '01-ai/Yi-1.5-34B-Chat-16K',
       tokens: 16_384,
     },
@@ -67,6 +70,34 @@ const SiliconCloud: ModelProviderCard = {
     {
       id: '01-ai/Yi-1.5-6B-Chat',
       tokens: 4096,
+    },
+    {
+      id: 'internlm/internlm2_5-7b-chat',
+      tokens: 32_768,
+    },
+    {
+      id: 'google/gemma-2-9b-it',
+      tokens: 8192,
+    },
+    {
+      id: 'google/gemma-2-27b-it',
+      tokens: 8192,
+    },
+    {
+      id: 'meta-llama/Meta-Llama-3.1-8B-Instruct',
+      tokens: 32_768,
+    },
+    {
+      id: 'meta-llama/Meta-Llama-3-70B-Instruct',
+      tokens: 8192,
+    },
+    {
+      id: 'mistralai/Mistral-7B-Instruct-v0.2',
+      tokens: 32_768,
+    },
+    {
+      id: 'mistralai/Mixtral-8x7B-Instruct-v0.1',
+      tokens: 32_768,
     },
   ],
   checkModel: 'Qwen/Qwen2-1.5B-Instruct',
