381fafe598000cbf75f48f7577d7c5190f149662
Zhijie He <hezhijie0327@hotmail.com>
2024-08-02 08:46:30 +0800

    💄 style: update ollama model info

--- src/config/modelProviders/ollama.ts
+++ src/config/modelProviders/ollama.ts
@@ -4,8 +4,23 @@ import { ModelProviderCard } from '@/types/llm';
 const Ollama: ModelProviderCard = {
   chatModels: [
     {
-      displayName: 'Llama3 8B',
+      displayName: 'Llama3.1 8B',
       enabled: true,
+      id: 'llama3.1',
+      tokens: 128_000,
+    },
+    {
+      displayName: 'Llama3.1 70B',
+      id: 'llama3.1:70b',
+      tokens: 128_000,
+    },
+    {
+      displayName: 'Llama3.1 405B',
+      id: 'llama3.1:405b',
+      tokens: 128_000,
+    },
+    {
+      displayName: 'Llama3 8B',
       id: 'llama3',
       tokens: 8000, // https://huggingface.co/blog/zh/llama3#llama-3-的新进展
     },
@@ -15,48 +30,45 @@ const Ollama: ModelProviderCard = {
       tokens: 8000,
     },
     {
-      displayName: 'Phi-3 3.8B',
+      displayName: 'Llama2 Chat 7B',
+      id: 'llama2', // https://llama.meta.com/llama2/
+      tokens: 4096,
+    },
+    {
+      displayName: 'Llama2 Chat 13B',
+      id: 'llama2:13b',
+      tokens: 4096,
+    },
+    {
+      displayName: 'Llama2 Chat 70B',
+      id: 'llama2:70b',
+      tokens: 4096,
+    },
+    {
+      displayName: 'Code Llama 7B',
       enabled: true,
-      id: 'phi3',
-      tokens: 128_000,
+      id: 'codellama',
+      tokens: 16_384, // https://huggingface.co/codellama/CodeLlama-7b-hf/blob/main/config.json
     },
     {
-      displayName: 'Phi-3 14B',
-      id: 'phi3:14b',
-      tokens: 128_000,
+      displayName: 'Code Llama 13B',
+      id: 'codellama:13b',
+      tokens: 16_384,
     },
     {
-      displayName: 'Aya 23 8B',
-      enabled: true,
-      id: 'aya',
-      tokens: 8192, // https://cohere.com/research/papers/aya-command-23-8b-and-35b-technical-report-2024-05-23
+      displayName: 'Code Llama 34B',
+      id: 'codellama:34b',
+      tokens: 16_384,
     },
     {
-      displayName: 'Aya 23 35B',
-      id: 'aya:35b',
+      displayName: 'Code Llama 70B',
+      id: 'codellama:70b',
+      tokens: 16_384,
+    },
+    {
+      displayName: 'Gemma2 2B',
+      id: 'gemma2:2b',
       tokens: 8192,
-    },
-    {
-      displayName: 'Qwen2 7B',
-      enabled: true,
-      id: 'qwen2',
-      tokens: 128_000,
-    },
-    {
-      displayName: 'Qwen2 72B',
-      id: 'qwen2:72b',
-      tokens: 128_000,
-    },
-    {
-      displayName: 'Command R 35B',
-      enabled: true,
-      id: 'command-r',
-      tokens: 131_072, // https://huggingface.co/CohereForAI/c4ai-command-r-v01/blob/main/config.json
-    },
-    {
-      displayName: 'Command R+ 104B (Q2_K)',
-      id: 'command-r-plus:104b-q2_K',
-      tokens: 131_072, // https://huggingface.co/CohereForAI/c4ai-command-r-plus/blob/main/config.json
     },
     {
       displayName: 'Gemma2 9B',
@@ -77,6 +89,73 @@ const Ollama: ModelProviderCard = {
     {
       displayName: 'Gemma 2B',
       id: 'gemma:2b',
+      tokens: 8192,
+    },
+    {
+      displayName: 'Phi-3 3.8B',
+      enabled: true,
+      id: 'phi3',
+      tokens: 128_000,
+    },
+    {
+      displayName: 'Phi-3 14B',
+      id: 'phi3:14b',
+      tokens: 128_000,
+    },
+    {
+      displayName: 'WizardLM 2 7B',
+      enabled: true,
+      id: 'wizardlm2',
+      tokens: 65_536,
+    },
+    {
+      displayName: 'WizardLM 2 8x22B',
+      id: 'wizardlm2:8x22b',
+      tokens: 65_536,
+    },
+    {
+      displayName: 'MathΣtral',
+      enabled: true,
+      id: 'mathstral',
+      tokens: 32_000, // https://huggingface.co/mistralai/mathstral-7B-v0.1
+    },
+    {
+      displayName: 'Mixtral 8x7B',
+      enabled: true,
+      id: 'mixtral',
+      tokens: 32_768,
+    },
+    {
+      displayName: 'Mixtral 8x22B',
+      id: 'mixtral:8x22b',
+      tokens: 65_536, // https://huggingface.co/mistralai/Mixtral-8x22B-v0.1/blob/main/config.json
+    },
+    {
+      displayName: 'Mixtral Nemo',
+      enabled: true,
+      id: 'mistral-nemo',
+      tokens: 128_000,
+    },
+    {
+      displayName: 'Command R 35B',
+      enabled: true,
+      id: 'command-r',
+      tokens: 131_072, // https://huggingface.co/CohereForAI/c4ai-command-r-v01/blob/main/config.json
+    },
+    {
+      displayName: 'Command R+ 104B (Q2_K)',
+      id: 'command-r-plus:104b-q2_K',
+      tokens: 131_072,
+    },
+    {
+      displayName: 'Aya 23 8B',
+      enabled: true,
+      id: 'aya',
+      tokens: 8192, // https://cohere.com/research/papers/aya-command-23-8b-and-35b-technical-report-2024-05-23
+    },
+    {
+      displayName: 'Aya 23 35B',
+      id: 'aya:35b',
       tokens: 8192,
     },
     {
@@ -102,96 +181,59 @@ const Ollama: ModelProviderCard = {
       tokens: 128_000,
     },
     {
-      displayName: 'Llama2 Chat 13B',
-      id: 'llama2:13b',
-      tokens: 4096, // https://llama.meta.com/llama2/
+      displayName: 'Qwen2 0.5B',
+      id: 'qwen2:0.5b',
+      tokens: 128_000,
     },
     {
-      displayName: 'Llama2 Chat 7B',
-      id: 'llama2',
-      tokens: 4096,
+      displayName: 'Qwen2 1.5B',
+      id: 'qwen2:1.5b',
+      tokens: 128_000,
     },
     {
-      displayName: 'Llama2 Chat 70B',
-      id: 'llama2:70b',
-      tokens: 4096,
-    },
-    {
-      displayName: 'Llama2 CN 13B',
-      id: 'llama2-chinese:13b',
-      tokens: 4096,
-    },
-    {
-      displayName: 'Llama2 CN 7B',
-      id: 'llama2-chinese',
-      tokens: 4096,
-    },
-    {
-      displayName: 'WizardLM 2 7B',
+      displayName: 'Qwen2 7B',
       enabled: true,
-      id: 'wizardlm2',
-      tokens: 65_536,
+      id: 'qwen2',
+      tokens: 128_000,
     },
     {
-      displayName: 'WizardLM 2 8x22B',
-      id: 'wizardlm2:8x22b',
-      tokens: 65_536,
+      displayName: 'Qwen2 72B',
+      id: 'qwen2:72b',
+      tokens: 128_000,
     },
     {
-      displayName: 'Code Llama 7B',
-      id: 'codellama',
-      tokens: 16_384, // https://huggingface.co/codellama/CodeLlama-7b-hf/blob/main/config.json
-    },
-    {
-      displayName: 'Code Llama 34B',
-      id: 'codellama:34b',
-      tokens: 16_384,
-    },
-    {
-      displayName: 'Code Llama 70B',
-      id: 'codellama:70b',
-      tokens: 16_384,
-    },
-    {
-      displayName: 'Code Llama 7B (Python)',
-      id: 'codellama:python',
-      tokens: 16_384,
-    },
-    {
-      displayName: 'MathΣtral',
-      enabled: true,
-      id: 'mathstral',
-      tokens: 32_000, // https://huggingface.co/mistralai/mathstral-7B-v0.1
-    },
-    {
-      displayName: 'Mixtral 8x7B',
-      enabled: true,
-      id: 'mixtral',
+      displayName: 'Qwen1.5 0.5B',
+      id: 'qwen:0.5b',
       tokens: 32_768,
     },
     {
-      displayName: 'Mixtral 8x22B',
-      id: 'mixtral:8x22b',
-      tokens: 65_536, // https://huggingface.co/mistralai/Mixtral-8x22B-v0.1/blob/main/config.json
+      displayName: 'Qwen1.5 1.8B',
+      id: 'qwen:1.8b',
+      tokens: 32_768,
     },
     {
-      displayName: 'Qwen Chat 4B',
+      displayName: 'Qwen1.5 4B',
       id: 'qwen',
       tokens: 32_768,
     },
     {
-      displayName: 'Qwen Chat 7B',
+      displayName: 'Qwen1.5 7B',
       id: 'qwen:7b',
       tokens: 32_768,
     },
     {
-      displayName: 'Qwen Chat 14B',
+      displayName: 'Qwen1.5 14B',
       id: 'qwen:14b',
       tokens: 32_768,
     },
     {
-      displayName: 'Qwen Chat 72B',
+      displayName: 'Qwen1.5 72B',
       id: 'qwen:72b',
+      tokens: 32_768,
+    },
+    {
+      displayName: 'Qwen1.5 110B',
+      id: 'qwen:110b',
       tokens: 32_768,
     },
     {
