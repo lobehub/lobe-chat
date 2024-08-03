abf3519c69e9ffa2322b8f2d35bfbbd67e6b3406
sxjeru <sxjeru@gmail.com>
2024-08-03 20:51:11 +0800

    Update siliconcloud.ts

--- src/config/modelProviders/siliconcloud.ts
+++ src/config/modelProviders/siliconcloud.ts
@@ -88,7 +88,12 @@ const SiliconCloud: ModelProviderCard = {
       tokens: 32_768,
     },
     {
+      enabled: true,
       id: 'meta-llama/Meta-Llama-3.1-70B-Instruct',
+      tokens: 32_768,
+    },
+    {
+      id: 'meta-llama/Meta-Llama-3.1-405B-Instruct',
       tokens: 32_768,
     },
     {
