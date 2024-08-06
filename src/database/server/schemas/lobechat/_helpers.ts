e9989f4ae563fa8a4d5348c1f0d408d7b66d5ae6
Arvin Xu <arvinx@foxmail.com>
2024-08-06 03:22:31 +0000

    fix lint

--- src/database/server/schemas/lobechat/_helpers.ts
+++ src/database/server/schemas/lobechat/_helpers.ts
@@ -1,27 +1,5 @@
 /* eslint-disable sort-keys-fix/sort-keys-fix  */
-import { LobeChatPluginManifest } from '@lobehub/chat-plugin-sdk';
-import { relations } from 'drizzle-orm';
-import {
-  boolean,
-  index,
-  integer,
-  jsonb,
-  pgTable,
-  primaryKey,
-  serial,
-  text,
-  timestamp,
-  unique,
-  uniqueIndex,
-  varchar,
-} from 'drizzle-orm/pg-core';
-import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
-
-import { DEFAULT_PREFERENCE } from '@/const/user';
-import { LobeAgentChatConfig, LobeAgentTTSConfig } from '@/types/agent';
-import { CustomPluginParams } from '@/types/tool/plugin';
-
-import { idGenerator, randomSlug } from '../../utils/idGenerator';
+import { timestamp } from 'drizzle-orm/pg-core';
 
 export const timestamptz = (name: string) => timestamp(name, { withTimezone: true });
 
