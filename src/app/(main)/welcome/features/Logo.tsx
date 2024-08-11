f592690b568c3f7be30a710f96c7d8ed6c07455c
chokiproai <Dogaopc13@gmail.com>
2024-08-11 20:48:29 +0700

    [Fix]

--- src/app/(main)/welcome/features/Logo.tsx
+++ src/app/(main)/welcome/features/Logo.tsx
@@ -2,12 +2,24 @@
 
 import { memo } from 'react';
 import { Center } from 'react-layout-kit';
+import Image from 'next/image'; // Import the Image component
 
 const LogoThree = () => (
-  <img src="https://github.com/user-attachments/assets/aa547c58-6045-4b9f-9c1b-bef3ea3536a8" alt="Logo Three" />
+  <Image
+    src="https://github.com/user-attachments/assets/aa547c58-6045-4b9f-9c1b-bef3ea3536a8"
+    alt="Logo Three"
+    width={240} // Specify width
+    height={240} // Specify height
+  />
 );
+
 const LogoSpline = () => (
-  <img src="https://github.com/user-attachments/assets/aa547c58-6045-4b9f-9c1b-bef3ea3536a8" alt="Logo Spline" />
+  <Image
+    src="https://github.com/user-attachments/assets/aa547c58-6045-4b9f-9c1b-bef3ea3536a8"
+    alt="Logo Spline"
+    width={976} // Specify width
+    height={482} // Specify height
+  />
 );
 
 const Logo = memo<{ mobile?: boolean }>(({ mobile }) => {
