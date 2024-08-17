2a3e1c9fe063cdca4bab2184f654950c77fcab9d
arvinxx <arvinx@foxmail.com>
2024-08-17 19:36:08 +0800

    🎨 chore: pre commit to reduce code update

--- next.config.mjs
+++ next.config.mjs
@@ -167,107 +167,39 @@ const withSentry =
         withSentryConfig(
           c,
           {
-            
-            
-
-            
             org: process.env.SENTRY_ORG,
-            
 
-
-project: process.env.SENTRY_PROJECT,
+            project: process.env.SENTRY_PROJECT,
             // For all available options, see:
-// https://github.com/getsentry/sentry-webpack-plugin#options
-// Suppresses source map uploading logs during build
-silent: true,
+            // https://github.com/getsentry/sentry-webpack-plugin#options
+            // Suppresses source map uploading logs during build
+            silent: true,
           },
           {
-            
-            
+            // Enables automatic instrumentation of Vercel Cron Monitors.
+            // See the following for more information:
+            // https://docs.sentry.io/product/crons/
+            // https://vercel.com/docs/cron-jobs
+            automaticVercelMonitors: true,
 
-            
-            
+            // Automatically tree-shake Sentry logger statements to reduce bundle size
+            disableLogger: true,
 
-// Enables automatic instrumentation of Vercel Cron Monitors.
-// See the following for more information:
-// https://docs.sentry.io/product/crons/
-// https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
+            // Hides source maps from generated client bundles
+            hideSourceMaps: true,
 
-            
-            
+            // Transpiles SDK to be compatible with IE11 (increases bundle size)
+            transpileClientSDK: true,
 
+            // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers. (increases server load)
+            // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+            // side errors will fail.
+            tunnelRoute: '/monitoring',
 
-
-
-
-
-
-
-
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
-
-            
-            
-            
-            
-
-
-
-
-
-
-
-
-
-// Hides source maps from generated client bundles
-hideSourceMaps: true,
-
-            
-            
-
-
-
-
-
-
-
-
-
-
-// Transpiles SDK to be compatible with IE11 (increases bundle size)
-transpileClientSDK: true,
-
-            
-            
-
-
-
-
-
-
-
-
-
-
-// Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers. (increases server load)
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-tunnelRoute: '/monitoring',
-
-            
-            
-            
-            
-            
-
-
-
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
+            // For all available options, see:
+            // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+            // Upload a larger set of source maps for prettier stack traces (increases build time)
+            widenClientFileUpload: true,
           },
         )
     : noWrapper;
