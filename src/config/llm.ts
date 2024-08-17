49b0fff2d9b926f4a4277931c6aef3e196e303b4
Zhijie He <hezhijie0327@hotmail.com>
2024-08-18 00:35:18 +0800

    ✨ feat: add baseURL support

--- src/config/llm.ts
+++ src/config/llm.ts
@@ -91,6 +91,7 @@ export const getLLMConfig = () => {
       ENABLED_SILICONCLOUD: z.boolean(),
       SILICONCLOUD_API_KEY: z.string().optional(),
       SILICONCLOUD_MODEL_LIST: z.string().optional(),
+      SILICONCLOUD_PROXY_URL: z.string().optional(),
     },
     runtimeEnv: {
       API_KEY_SELECT_MODE: process.env.API_KEY_SELECT_MODE,
@@ -179,6 +180,7 @@ export const getLLMConfig = () => {
       ENABLED_SILICONCLOUD: !!process.env.SILICONCLOUD_API_KEY,
       SILICONCLOUD_API_KEY: process.env.SILICONCLOUD_API_KEY,
       SILICONCLOUD_MODEL_LIST: process.env.SILICONCLOUD_MODEL_LIST,
+      SILICONCLOUD_PROXY_URL: process.env.SILICONCLOUD_PROXY_URL,
     },
   });
 };
