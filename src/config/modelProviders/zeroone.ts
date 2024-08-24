c4a7f703b89e905920d9d674311efff48f7a071d
sxjeru <sxjeru@gmail.com>
2024-08-24 13:24:12 +0800

    💄 style: Update 01.AI models (#3586)

--- src/config/modelProviders/zeroone.ts
+++ src/config/modelProviders/zeroone.ts
@@ -11,15 +11,6 @@ const ZeroOne: ModelProviderCard = {
       tokens: 32_768,
     },
     {
-      description:
-        '在 yi-large 模型的基础上支持并强化了工具调用的能力，适用于各种需要搭建 agent 或 workflow 的业务场景。',
-      displayName: 'Yi Large FC',
-      enabled: true,
-      functionCall: true,
-      id: 'yi-large-fc',
-      tokens: 32_768,
-    },
-    {
       description: '中型尺寸模型升级微调，能力均衡，性价比高。深度优化指令遵循能力。',
       displayName: 'Yi Medium',
       enabled: true,
@@ -32,6 +23,7 @@ const ZeroOne: ModelProviderCard = {
       enabled: true,
       id: 'yi-vision',
       tokens: 16_384,
+      vision: true,
     },
     {
       description: '200K 超长上下文窗口，提供长文本深度理解和生成能力。',
@@ -43,16 +35,25 @@ const ZeroOne: ModelProviderCard = {
     {
       description: '小而精悍，轻量极速模型。提供强化数学运算和代码编写能力。',
       displayName: 'Yi Spark',
-      enabled: true,
       id: 'yi-spark',
       tokens: 16_384,
     },
     {
       description:
-        '基于Yi-Large超强模型的高阶服务，结合检索与生成技术提供精准答案，支持客⼾私有知识库（请联系客服申请）。',
+        '基于 yi-large 超强模型的高阶服务，结合检索与生成技术提供精准答案，实时全网检索信息服务。',
       displayName: 'Yi Large RAG',
+      enabled: true,
       id: 'yi-large-rag',
       tokens: 16_384,
+    },
+    {
+      description:
+        '在 yi-large 模型的基础上支持并强化了工具调用的能力，适用于各种需要搭建 agent 或 workflow 的业务场景。',
+      displayName: 'Yi Large FC',
+      enabled: true,
+      functionCall: true,
+      id: 'yi-large-fc',
+      tokens: 32_768,
     },
     {
       description: '超高性价比、卓越性能。根据性能和推理速度、成本，进行平衡性高精度调优。',
@@ -62,15 +63,13 @@ const ZeroOne: ModelProviderCard = {
       tokens: 16_384,
     },
     {
-      description: '「兼容版本模型」文本推理能力增强。',
+      description: '初期版本，推荐使用 yi-large（新版本）',
       displayName: 'Yi Large Preview',
-      enabled: true,
       id: 'yi-large-preview',
       tokens: 16_384,
     },
   ],
   checkModel: 'yi-large',
-  disableBrowserRequest: true,
   id: 'zeroone',
   name: '01.AI',
 };
