b1690f9de305b4cb874e7ad4310a6d23559cc2d4
Ivan Li <ivanli2048@gmail.com>
2024-08-26 10:08:29 +0800

    feat(next-auth): support Authelia.

---
+++ src/libs/next-auth/sso-providers/authelia.ts
@@ -0,0 +1,22 @@
+import type { OIDCConfig } from '@auth/core/providers';
+
+import { authEnv } from '@/config/auth';
+
+import { CommonProviderConfig } from './sso.config';
+
+const provider = {
+  id: 'authelia',
+  provider: {
+    ...CommonProviderConfig,
+    authorization: { params: { scope: 'openid email profile' } },
+    checks: ['state', 'pkce'],
+    clientId: authEnv.AUTHELIA_CLIENT_ID,
+    clientSecret: authEnv.AUTHELIA_CLIENT_SECRET,
+    id: 'authelia',
+    issuer: authEnv.AUTHELIA_ISSUER,
+    name: 'Authentik',
+    type: 'oidc',
+  } satisfies OIDCConfig<unknown>,
+};
+
+export default provider;
