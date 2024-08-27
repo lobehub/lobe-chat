47dfd000feba52d04ab0b4247e6d48d6ec029f7a
cy948@ultra7 <cy948@vip.qq.com>
2024-08-27 18:32:26 +0800

    :art: style: format code

--- src/libs/next-auth/sso-providers/logto.ts
+++ src/libs/next-auth/sso-providers/logto.ts
@@ -36,14 +36,12 @@ function LobeLogtoProvider(config: OIDCUserConfig<LogtoProfile>): OIDCConfig<Log
 const provider = {
   id: 'logto',
   provider: LobeLogtoProvider({
-    
-    
     authorization: {
       params: { scope: 'openid offline_access profile email' },
     },
     // You can get the issuer value from the Logto Application Details page,
-// in the field "Issuer endpoint"
-clientId: authEnv.LOGTO_CLIENT_ID,
+    // in the field "Issuer endpoint"
+    clientId: authEnv.LOGTO_CLIENT_ID,
     clientSecret: authEnv.LOGTO_CLIENT_SECRET,
     issuer: authEnv.LOGTO_ISSUER,
   }),
