a4b870e2d1b368ef763a077c74e6e8fed427cfe6
Zhijie He <hezhijie0327@hotmail.com>
2024-08-27 20:20:13 +0800

    ♻️ refactor: cleanup

--- src/libs/next-auth/sso-providers/cloudflare-zero-trust.ts
+++ src/libs/next-auth/sso-providers/cloudflare-zero-trust.ts
@@ -5,14 +5,9 @@ import { authEnv } from '@/config/auth';
 import { CommonProviderConfig } from './sso.config';
 
 export type CloudflareZeroTrustProfile = {
-  // The users display name
-  email: string; 
-  // The users email
-  groups: string[]; 
-  // The username the user used to login with
-  name: string; 
-  preferred_username: string; // The users groups
-  sub: string; // The users id
+  email: string;
+  name: string;
+  sub: string;
 };
 
 const provider = {
@@ -20,7 +15,6 @@ const provider = {
   provider: {
     ...CommonProviderConfig,
     authorization: { params: { scope: 'openid email profile' } },
-    checks: ['state', 'pkce'],
     clientId: authEnv.CLOUDFLARE_ZERO_TRUST_CLIENT_ID,
     clientSecret: authEnv.CLOUDFLARE_ZERO_TRUST_CLIENT_SECRET,
     id: 'cloudflare-zero-trust',
