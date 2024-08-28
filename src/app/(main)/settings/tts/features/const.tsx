5c7e17a68f92f405feed7df8fc365f35ee433d55
CanisMinor <i@canisminor.cc>
2024-08-28 11:27:59 +0800

    💄 style: Move model and provider icon components to `@lobehub/icons` (#3634)
    * 💄 style: Move model and provider icon components to @lobehub/icons
    
    * 🐛 fix: Fix review problem
    
    * 🔧 chore: Update deps
    
    * 💄 style: Rm Google Avatar in Browser options

--- src/app/(main)/settings/tts/features/const.tsx
+++ src/app/(main)/settings/tts/features/const.tsx
@@ -1,19 +1,22 @@
+import { OpenAI } from '@lobehub/icons';
 import { SelectProps } from 'antd';
+
+import { LabelRenderer } from '@/components/ModelSelect';
 
 export const opeanaiTTSOptions: SelectProps['options'] = [
   {
-    label: 'tts-1',
+    label: <LabelRenderer Icon={OpenAI.Avatar} label={'tts-1'} />,
     value: 'tts-1',
   },
   {
-    label: 'tts-1-hd',
+    label: <LabelRenderer Icon={OpenAI.Avatar} label={'tts-1-hd'} />,
     value: 'tts-1-hd',
   },
 ];
 
 export const opeanaiSTTOptions: SelectProps['options'] = [
   {
-    label: 'whisper-1',
+    label: <LabelRenderer Icon={OpenAI.Avatar} label={'stt-1'} />,
     value: 'whisper-1',
   },
 ];
--- src/const/url.ts
+++ src/const/url.ts
@@ -69,3 +69,4 @@ export const RELEASES_URL = urlJoin(GITHUB, 'releases');
 export const mailTo = (email: string) => `mailto:${email}`;
 
 export const AES_GCM_URL = 'https://datatracker.ietf.org/doc/html/draft-ietf-avt-srtp-aes-gcm-01';
+export const BASE_PROVIDER_DOC_URL = 'https://lobehub.com/docs/usage/providers';
