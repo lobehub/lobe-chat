322cd36422045b31cd61413078adc06a89deab89
Zhijie He <hezhijie0327@hotmail.com>
2024-09-02 10:42:59 +0800

    💄 style: Update Bedrock model list

--- src/config/modelProviders/bedrock.ts
+++ src/config/modelProviders/bedrock.ts
@@ -6,56 +6,20 @@ const Bedrock: ModelProviderCard = {
   chatModels: [
     {
       displayName: 'Titan Text G1 - Lite',
-      id: 'amazon.titan-text-lite-v1:0:4k',
+      id: 'amazon.titan-text-lite-v1',
       tokens: 4000,
     },
     {
-      description:
-        'Amazon Titan Text G1 - Express v1，上下文长度可达 8000 个 token，适合广泛的用途。',
       displayName: 'Titan Text G1 - Express',
-      id: 'amazon.titan-text-express-v1:0:8k',
+      id: 'amazon.titan-text-express-v1',
       tokens: 8000,
     },
     {
       displayName: 'Titan Text Premier',
-      id: 'amazon.titan-text-premier-v1:0:32K',
+      id: 'amazon.titan-text-premier-v1:0',
       tokens: 32_000,
     },
     {
-      displayName: 'Jurassic-2 Mid',
-      enabled: true,
-      id: 'ai21.j2-mid-v1',
-      tokens: 8192,
-    },
-    {
-      displayName: 'Jurassic-2 Ultra',
-      enabled: true,
-      id: 'ai21.j2-ultra-v1',
-      tokens: 8192,
-    },
-    {
-      description:
-        'Claude 3 Opus 是 Anthropic 最强大的人工智能模型，在处理高度复杂的任务方面具备顶尖性能。该模型能够以非凡的流畅性和类似人类的理解能力引导开放式的提示和未可见的场景。Claude 3 Opus 向我们展示生成式人工智能的美好前景。 Claude 3 Opus 可以处理图像和返回文本输出，并且提供 200K 上下文窗口。',
-      displayName: 'Claude 3 Opus',
-      enabled: true,
-      functionCall: true,
-      id: 'anthropic.claude-3-opus-20240229-v1:0',
-      tokens: 200_000,
-      vision: true,
-    },
-    {
-      description:
-        'Anthropic 推出的 Claude 3 Sonnet 模型在智能和速度之间取得理想的平衡，尤其是在处理企业工作负载方面。该模型提供最大的效用，同时价格低于竞争产品，并且其经过精心设计，是大规模部署人工智能的可信赖、高耐久性骨干模型。 Claude 3 Sonnet 可以处理图像和返回文本输出，并且提供 200K 上下文窗口。',
-      displayName: 'Claude 3 Sonnet',
-      enabled: true,
-      functionCall: true,
-      id: 'anthropic.claude-3-sonnet-20240229-v1:0',
-      tokens: 200_000,
-      vision: true,
-    },
-    {
-      description:
-        'Claude 3.5 Sonnet 提高了行业的智能标准, 在广泛的基准测试中超越了竞争对手模型以及 Claude 3 Opus, 以中端模型的速度和成本，展现出卓越性能。 Claude 3.5 Sonnet 可以处理图像和返回文本输出，并且提供 200K 上下文窗口。',
       displayName: 'Claude 3.5 Sonnet',
       enabled: true,
       functionCall: true,
@@ -64,8 +28,22 @@ const Bedrock: ModelProviderCard = {
       vision: true,
     },
     {
-      description:
-        'Claude 3 Haiku 是 Anthropic 最快速、最紧凑的模型，具有近乎即时的响应能力。该模型可以快速回答简单的查询和请求。客户将能够构建模仿人类交互的无缝人工智能体验。 Claude 3 Haiku 可以处理图像和返回文本输出，并且提供 200K 上下文窗口。',
+      displayName: 'Claude 3 Sonnet',
+      enabled: true,
+      functionCall: true,
+      id: 'anthropic.claude-3-sonnet-20240229-v1:0',
+      tokens: 200_000,
+      vision: true,
+    },
+    {
+      displayName: 'Claude 3 Opus',
+      enabled: true,
+      functionCall: true,
+      id: 'anthropic.claude-3-opus-20240229-v1:0',
+      tokens: 200_000,
+      vision: true,
+    },
+    {
       displayName: 'Claude 3 Haiku',
       enabled: true,
       functionCall: true,
@@ -74,50 +52,140 @@ const Bedrock: ModelProviderCard = {
       vision: true,
     },
     {
-      description:
-        'Claude 2.1 v2.1，上下文大小等于 200k。Claude 2 的更新版本，采用双倍的上下文窗口，并在长文档和 RAG 上下文中提高可靠性、幻觉率和循证准确性。',
       displayName: 'Claude 2.1',
       id: 'anthropic.claude-v2:1',
       tokens: 200_000,
     },
     {
-      description:
-        'Claude Instant 1.2 v1.2，上下文大小等于 100k。一种更快速、更实惠但仍然非常强大的模型，它可以处理一系列任务，包括随意对话、文本分析、摘要和文档问题回答。',
-      displayName: 'Claude Instant 1.2',
+      displayName: 'Claude 2.0',
+      id: 'anthropic.claude-v2',
+      tokens: 100_000,
+    },
+    {
+      displayName: 'Claude Instant',
       id: 'anthropic.claude-instant-v1',
       tokens: 100_000,
     },
     {
-      description: 'Mistral Large 2 128k',
-      displayName: 'Mistral Large 2',
+      displayName: 'Llama 3.1 8B Instruct',
+      enabled: true,
+      functionCall: true,
+      id: 'meta.llama3-1-8b-instruct-v1:0',
+      tokens: 128_000,
+    },
+    {
+      displayName: 'Llama 3.1 70B Instruct',
+      enabled: true,
+      functionCall: true,
+      id: 'meta.llama3-1-70b-instruct-v1:0',
+      tokens: 128_000,
+    },
+    {
+      displayName: 'Llama 3.1 405B Instruct',
+      enabled: true,
+      functionCall: true,
+      id: 'meta.llama3-1-405b-instruct-v1:0',
+      tokens: 128_000,
+    },
+    {
+      displayName: 'Llama 3 8B Instruct',
+      id: 'meta.llama3-8b-instruct-v1:0',
+      tokens: 8192,
+    },
+    {
+      displayName: 'Llama 3 70B Instruct',
+      id: 'meta.llama3-70b-instruct-v1:0',
+      tokens: 8192,
+    },
+    {
+      displayName: 'Llama 2 Chat 13B',
+      id: 'meta.llama2-13b-chat-v1',
+      tokens: 4096,
+    },
+    {
+      displayName: 'Llama 2 Chat 70B',
+      id: 'meta.llama2-70b-chat-v1',
+      tokens: 4096,
+    },
+    {
+      displayName: 'Mistral 7B Instruct',
+      enabled: true,
+      id: 'mistral.mistral-7b-instruct-v0:2',
+      tokens: 8192,
+    },
+    {
+      displayName: 'Mixtral 8X7B Instruct',
+      enabled: true,
+      id: 'mistral.mixtral-8x7b-instruct-v0:1',
+      tokens: 4096,
+    },
+    {
+      displayName: 'Mistral Small',
+      functionCall: true,
+      id: 'mistral.mistral-small-2402-v1:0',
+      tokens: 8192,
+    },
+    {
+      displayName: 'Mistral Large 2 (24.07)',
       enabled: true,
       functionCall: true,
       id: 'mistral.mistral-large-2407-v1:0',
       tokens: 128_000,
     },
     {
-      description: 'Llama 3.1 405B Instruct',
-      displayName: 'Llama 3.1 405B Instruct',
+      displayName: 'Mistral Large',
       enabled: true,
-      id: 'meta.llama3-1-405b-instruct-v1:0',
+      functionCall: true,
+      id: 'mistral.mistral-large-2402-v1:0',
+      tokens: 8192,
+    },
+    {
+      displayName: 'Command R+',
+      enabled: true,
+      functionCall: true,
+      id: 'cohere.command-r-plus-v1:0',
       tokens: 128_000,
     },
     {
-      description: 'Llama 3.1 70B Instruct',
-      displayName: 'Llama 3.1 70B Instruct',
+      displayName: 'Command R',
       enabled: true,
-      id: 'meta.llama3-1-70b-instruct-v1:0',
+      functionCall: true,
+      id: 'cohere.command-r-v1:0',
       tokens: 128_000,
+    },
+/*
+    // Cohere Command (Text) and AI21 Labs Jurassic-2 (Text) don't support chat with the Converse API
+    {
+      displayName: 'Command',
+      id: 'cohere.command-text-v14',
+      tokens: 4096,
     },
     {
-      description: 'Llama 3.1 8B Instruct',
-      displayName: 'Llama 3.1 8B Instruct',
-      enabled: true,
-      id: 'meta.llama3-1-8b-instruct-v1:0',
-      tokens: 128_000,
+      displayName: 'Command Light',
+      id: 'cohere.command-light-text-v14',
+      tokens: 4096,
     },
+*/
+    {
+      displayName: 'Jamba-Instruct',
+      id: 'ai21.jamba-instruct-v1:0',
+      tokens: 4096,
+    },
+/*
+    // Cohere Command (Text) and AI21 Labs Jurassic-2 (Text) don't support chat with the Converse API
+    {
+      displayName: 'Jurassic-2 Mid',
+      id: 'ai21.j2-mid-v1',
+      tokens: 8192,
+    },
+    {
+      displayName: 'Jurassic-2 Ultra',
+      id: 'ai21.j2-ultra-v1',
+      tokens: 8192,
+    },
+*/
   ],
-  checkModel: 'anthropic.claude-instant-v1',
+  checkModel: 'amazon.titan-text-lite-v1',
   id: 'bedrock',
   name: 'Bedrock',
 };
