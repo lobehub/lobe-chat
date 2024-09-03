d0d3e5656dcd503dc9f16fddb398f13dbfdeb6db
arvinxx <arvinx@foxmail.com>
2024-09-04 00:47:12 +0800

    ✅ test: add test for tokenizer edge runtime

--- src/server/routers/edge/index.ts
+++ src/server/routers/edge/index.ts
@@ -2,9 +2,9 @@
  * This file contains the edge router of Lobe Chat tRPC-backend
  */
 import { publicProcedure, router } from '@/libs/trpc';
-import { tokenizerRouter } from '@/server/routers/edge/tokenizer';
 
 import { configRouter } from './config';
+import { tokenizerRouter } from './tokenizer';
 import { uploadRouter } from './upload';
 
 export const edgeRouter = router({
