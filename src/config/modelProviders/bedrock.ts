c4fab194f65875489cfa52698655601647b71caf
Zhijie He <hezhijie0327@hotmail.com>
2024-09-05 16:03:51 +0800

    💄 style: rollback & update model description

--- src/config/modelProviders/bedrock.ts
+++ src/config/modelProviders/bedrock.ts
@@ -7,22 +7,26 @@ const Bedrock: ModelProviderCard = {
 /*
     // TODO: Not support for now
     {
+      description: 'Amazon Titan Text G1 - Lite is a light weight efficient model, ideal for fine-tuning of English-language tasks, including like summarizations and copy writing, where customers want a smaller, more cost-effective model that is also highly customizable.',
       displayName: 'Titan Text G1 - Lite',
       id: 'amazon.titan-text-lite-v1',
       tokens: 4000,
     },
     {
+      description: 'Amazon Titan Text G1 - Express is a large language model for text generation. It is useful for a wide range of advanced, general language tasks such as open-ended text generation and conversational chat, as well as support within Retrieval Augmented Generation (RAG). At launch, the model is optimized for English, with multilingual support for more than 30 additional languages available in preview.',
       displayName: 'Titan Text G1 - Express',
       id: 'amazon.titan-text-express-v1',
       tokens: 8000,
     },
     {
+      description: 'Amazon Titan Text G1 - Premier is a large language model for text generation. It is useful for a wide range of tasks including open-ended and context-based question answering, code generation, and summarization. This model is integrated with Amazon Bedrock Knowledge Base and Amazon Bedrock Agents. The model also supports Custom Finetuning in preview.',
       displayName: 'Titan Text Premier',
       id: 'amazon.titan-text-premier-v1:0',
       tokens: 32_000,
     },
 */
     {
+      description: 'Claude 3.5 Sonnet 提高了行业的智能标准, 在广泛的基准测试中超越了竞争对手模型以及 Claude 3 Opus, 以中端模型的速度和成本，展现出卓越性能。 Claude 3.5 Sonnet 可以处理图像和返回文本输出，并且提供 200K 上下文窗口。',
       displayName: 'Claude 3.5 Sonnet',
       enabled: true,
       functionCall: true,
@@ -31,6 +35,7 @@ const Bedrock: ModelProviderCard = {
       vision: true,
     },
     {
+      description: 'Anthropic 推出的 Claude 3 Sonnet 模型在智能和速度之间取得理想的平衡，尤其是在处理企业工作负载方面。该模型提供最大的效用，同时价格低于竞争产品，并且其经过精心设计，是大规模部署人工智能的可信赖、高耐久性骨干模型。 Claude 3 Sonnet 可以处理图像和返回文本输出，并且提供 200K 上下文窗口。',
       displayName: 'Claude 3 Sonnet',
       enabled: true,
       functionCall: true,
@@ -39,6 +44,7 @@ const Bedrock: ModelProviderCard = {
       vision: true,
     },
     {
+      description: 'Claude 3 Opus 是 Anthropic 最强大的人工智能模型，在处理高度复杂的任务方面具备顶尖性能。该模型能够以非凡的流畅性和类似人类的理解能力引导开放式的提示和未可见的场景。Claude 3 Opus 向我们展示生成式人工智能的美好前景。 Claude 3 Opus 可以处理图像和返回文本输出，并且提供 200K 上下文窗口。',
       displayName: 'Claude 3 Opus',
       enabled: true,
       functionCall: true,
@@ -47,6 +53,7 @@ const Bedrock: ModelProviderCard = {
       vision: true,
     },
     {
+      description: 'Claude 3 Haiku 是 Anthropic 最快速、最紧凑的模型，具有近乎即时的响应能力。该模型可以快速回答简单的查询和请求。客户将能够构建模仿人类交互的无缝人工智能体验。 Claude 3 Haiku 可以处理图像和返回文本输出，并且提供 200K 上下文窗口。',
       displayName: 'Claude 3 Haiku',
       enabled: true,
       functionCall: true,
@@ -55,6 +62,7 @@ const Bedrock: ModelProviderCard = {
       vision: true,
     },
     {
+      description: 'Claude 2.1 v2.1，上下文大小等于 200k。Claude 2 的更新版本，采用双倍的上下文窗口，并在长文档和 RAG 上下文中提高可靠性、幻觉率和循证准确性。',
       displayName: 'Claude 2.1',
       id: 'anthropic.claude-v2:1',
       tokens: 200_000,
@@ -65,6 +73,7 @@ const Bedrock: ModelProviderCard = {
       tokens: 100_000,
     },
     {
+      description: 'Claude Instant 1.2 v1.2，上下文大小等于 100k。一种更快速、更实惠但仍然非常强大的模型，它可以处理一系列任务，包括随意对话、文本分析、摘要和文档问题回答。',
       displayName: 'Claude Instant',
       id: 'anthropic.claude-instant-v1',
       tokens: 100_000,
