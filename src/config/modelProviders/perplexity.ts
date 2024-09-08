21a37431b01f4c8c7309796b2fbdc3472923e37c
LovelyGuYiMeng <76251800+LovelyGuYiMeng@users.noreply.github.com>
2024-09-08 13:44:37 +0800

    Update perplexity.ts

--- src/config/modelProviders/perplexity.ts
+++ src/config/modelProviders/perplexity.ts
@@ -4,36 +4,36 @@ import { ModelProviderCard } from '@/types/llm';
 const Perplexity: ModelProviderCard = {
   chatModels: [
     {
-      displayName: 'Llama3.1 Sonar Small Chat',
+      displayName: 'Llama 3.1 Sonar Small Chat',
       enabled: true,
       id: 'llama-3.1-sonar-small-128k-chat',
       tokens: 128_000,
     },
     {
-      displayName: 'Llama3.1 Sonar Large Chat',
+      displayName: 'Llama 3.1 Sonar Large Chat',
       enabled: true,
       id: 'llama-3.1-sonar-large-128k-chat',
       tokens: 128_000,
     },
     {
-      displayName: 'Llama3.1 Sonar Small Online',
+      displayName: 'Llama 3.1 Sonar Small Online',
       enabled: true,
       id: 'llama-3.1-sonar-small-128k-online',
       tokens: 128_000,
     },
     {
-      displayName: 'Llama3.1 Sonar Large Online',
+      displayName: 'Llama 3.1 Sonar Large Online',
       enabled: true,
       id: 'llama-3.1-sonar-large-128k-online',
       tokens: 128_000,
     },
     {
-      displayName: 'Llama3.1 8B Instruct',
+      displayName: 'Llama 3.1 8B Instruct',
       id: 'llama-3.1-8b-instruct',
       tokens: 128_000,
     },
     {
-      displayName: 'Llama3.1 70B Instruct',
+      displayName: 'Llama 3.1 70B Instruct',
       id: 'llama-3.1-70b-instruct',
       tokens: 128_000,
     },
