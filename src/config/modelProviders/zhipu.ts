a4b70eb0ba45fba6d6b181b28ab4952196df287c
LovelyGuYiMeng <76251800+LovelyGuYiMeng@users.noreply.github.com>
2024-09-08 13:26:06 +0800

    Update zhipu.ts

--- src/config/modelProviders/zhipu.ts
+++ src/config/modelProviders/zhipu.ts
@@ -8,7 +8,7 @@ const ZhiPu: ModelProviderCard = {
     {
       description:
         'GLM-4-AllTools 是专门为支持智能体和相关任务而进一步优化的模型版本。它能够自主理解用户的意图，规划复杂的指令，并能够调用一个或多个工具（例如网络浏览器、代码解释器和文本生图像）以完成复杂的任务。',
-      displayName: 'GLM-4-AllTools',
+      displayName: 'GLM-4 AllTools',
       enabled: true,
       functionCall: true,
       id: 'glm-4-alltools',
@@ -16,7 +16,7 @@ const ZhiPu: ModelProviderCard = {
     },
     {
       description: '高智能旗舰：性能全面提升，长文本和复杂任务能力显著增强',
-      displayName: 'GLM-4-Plus',
+      displayName: 'GLM 4 Plus',
       enabled: true,
       functionCall: true,
       id: 'glm-4-plus',
@@ -24,7 +24,7 @@ const ZhiPu: ModelProviderCard = {
     },
     {
       description: '高智能模型：适用于处理高度复杂和多样化的任务',
-      displayName: 'GLM-4-0520',
+      displayName: 'GLM 4 0520',
       enabled: true,
       functionCall: true,
       id: 'glm-4-0520',
@@ -32,14 +32,14 @@ const ZhiPu: ModelProviderCard = {
     },
     {
       description: '旧版旗舰：发布于2024年1月16日，目前已被 GLM-4-0520 取代', // deprecated on 2025-06
-      displayName: 'GLM-4',
+      displayName: 'GLM 4',
       functionCall: true,
       id: 'glm-4',
       tokens: 128_000,
     },
     {
       description: '性价比最高的版本，综合性能接近GLM-4，速度快，价格实惠',
-      displayName: 'GLM-4-Air',
+      displayName: 'GLM 4 Air',
       enabled: true,
       functionCall: true,
       id: 'glm-4-air',
@@ -47,14 +47,14 @@ const ZhiPu: ModelProviderCard = {
     },
     {
       description: 'GLM-4-Air 的高性能版本，效果不变，推理速度达到其2.6倍',
-      displayName: 'GLM-4-AirX',
+      displayName: 'GLM 4 AirX',
       functionCall: true,
       id: 'glm-4-airx',
       tokens: 8192,
     },
     {
       description: '超长输入：专为处理超长文本和记忆型任务设计',
-      displayName: 'GLM-4-Long',
+      displayName: 'GLM 4 Long',
       enabled: true,
       functionCall: true,
       id: 'glm-4-long',
@@ -62,7 +62,7 @@ const ZhiPu: ModelProviderCard = {
     },
     {
       description: '适用简单任务，速度最快，价格最实惠的版本',
-      displayName: 'GLM-4-Flash',
+      displayName: 'GLM 4 Flash',
       enabled: true,
       functionCall: true,
       id: 'glm-4-flash',
@@ -71,7 +71,7 @@ const ZhiPu: ModelProviderCard = {
     {
       description:
         '视频和图像理解：具备视频内容和多图片的理解能力',
-      displayName: 'GLM-4V-Plus',
+      displayName: 'GLM 4V Plus',
       enabled: true,
       id: 'glm-4v-plus',
       tokens: 8192,
@@ -80,7 +80,7 @@ const ZhiPu: ModelProviderCard = {
     {
       description:
         '图像理解：具备图像理解能力和推理能力',
-      displayName: 'GLM-4V',
+      displayName: 'GLM 4V',
       enabled: true,
       id: 'glm-4v',
       tokens: 2048,
@@ -89,14 +89,14 @@ const ZhiPu: ModelProviderCard = {
     {
       description:
         'CodeGeeX是一款强大的AI编程助手，提供智能问答和代码补全功能，支持多种编程语言，帮助开发者提高编程效率。',
-      displayName: 'CodeGeeX-4',
+      displayName: 'CodeGeeX 4',
       id: 'codegeex-4',
       tokens: 128_000,
     },
     {
       description:
         '支持基于人设的角色扮演、超长多轮的记忆、千人千面的角色对话，广泛应用于情感陪伴、游戏智能NPC、网红/明星/影视剧IP分身、数字人/虚拟主播、文字冒险游戏等拟人对话或游戏场景。',
-      displayName: 'CharGLM-3',
+      displayName: 'CharGLM 3',
       id: 'charglm-3',
       tokens: 4096,
     },
