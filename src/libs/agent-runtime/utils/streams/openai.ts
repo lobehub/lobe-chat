8c9eb74f12ac99badcfe985d32e8151155058999
Zhijie He <hezhijie0327@hotmail.com>
2024-09-10 19:02:35 +0800

    🔨 chore: rollback changes

--- src/libs/agent-runtime/utils/streams/openai.ts
+++ src/libs/agent-runtime/utils/streams/openai.ts
@@ -8,13 +8,17 @@ import { ChatStreamCallbacks } from '../../types';
 import {
   StreamProtocolChunk,
   StreamProtocolToolCallChunk,
+  StreamStack,
   StreamToolCallChunkData,
   createCallbacksTransformer,
   createSSEProtocolTransformer,
   generateToolCallId,
 } from './protocol';
 
-export const transformOpenAIStream = (chunk: OpenAI.ChatCompletionChunk): StreamProtocolChunk => {
+export const transformOpenAIStream = (
+  chunk: OpenAI.ChatCompletionChunk,
+  stack?: StreamStack,
+): StreamProtocolChunk => {
   // maybe need another structure to add support for multiple choices
 
   try {
@@ -23,15 +27,23 @@ export const transformOpenAIStream = (chunk: OpenAI.ChatCompletionChunk): Stream
       return { data: chunk, id: chunk.id, type: 'data' };
     }
 
+    if (typeof item.delta?.content === 'string' && !item.finish_reason && !item.delta?.tool_calls) {
+      return { data: item.delta.content, id: chunk.id, type: 'text' };
+    }
+
     if (item.delta?.tool_calls) {
       return {
-        data: item.delta.tool_calls.map(
-          (value, index): StreamToolCallChunkData => ({
+        data: item.delta.tool_calls.map((value, index): StreamToolCallChunkData => {
+          if (stack && !stack.tool) {
+            stack.tool = { id: value.id!, index: value.index, name: value.function!.name! };
+          }
+
+          return {
             function: {
               arguments: value.function?.arguments ?? '',
               name: value.function?.name ?? null,
             },
-            id: value.id || generateToolCallId(index, value.function?.name),
+            id: value.id || stack?.tool?.id || generateToolCallId(index, value.function?.name),
 
             // mistral's tool calling don't have index and function field, it's data like:
             // [{"id":"xbhnmTtY7","function":{"name":"lobe-image-designer____text2image____builtin","arguments":"{\"prompts\": [\"A photo of a small, fluffy dog with a playful expression and wagging tail.\", \"A watercolor painting of a small, energetic dog with a glossy coat and bright eyes.\", \"A vector illustration of a small, adorable dog with a short snout and perky ears.\", \"A drawing of a small, scruffy dog with a mischievous grin and a wagging tail.\"], \"quality\": \"standard\", \"seeds\": [123456, 654321, 111222, 333444], \"size\": \"1024x1024\", \"style\": \"vivid\"}"}}]
@@ -42,8 +54,8 @@ export const transformOpenAIStream = (chunk: OpenAI.ChatCompletionChunk): Stream
             // so we need to add these default values
             index: typeof value.index !== 'undefined' ? value.index : index,
             type: value.type || 'function',
-          }),
-        ),
+          };
+        }),
         id: chunk.id,
         type: 'tool_calls',
       } as StreamProtocolToolCallChunk;
@@ -52,10 +64,6 @@ export const transformOpenAIStream = (chunk: OpenAI.ChatCompletionChunk): Stream
     // 给定结束原因
     if (item.finish_reason) {
       return { data: item.finish_reason, id: chunk.id, type: 'stop' };
-    }
-
-    if (typeof item.delta?.content === 'string') {
-      return { data: item.delta.content, id: chunk.id, type: 'text' };
     }
 
     if (item.delta?.content === null) {
@@ -100,10 +108,12 @@ export const OpenAIStream = (
   stream: Stream<OpenAI.ChatCompletionChunk> | ReadableStream,
   callbacks?: ChatStreamCallbacks,
 ) => {
+  const streamStack: StreamStack = { id: '' };
+
   const readableStream =
     stream instanceof ReadableStream ? stream : readableFromAsyncIterable(chatStreamable(stream));
 
   return readableStream
-    .pipeThrough(createSSEProtocolTransformer(transformOpenAIStream))
+    .pipeThrough(createSSEProtocolTransformer(transformOpenAIStream, streamStack))
     .pipeThrough(createCallbacksTransformer(callbacks));
 };
