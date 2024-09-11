53d005ad9229f82413e6f7595e54abb58d3abba5
cy948@ultra7 <cy948@vip.qq.com>
2024-09-11 16:23:36 +0800

    :globe_with_meridians: i18n: support zh_CN in setup.sh

--- docker-compose/local/setup.sh
+++ docker-compose/local/setup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# ==================
+# == Env settings ==
+# ==================
+# Set locale to UTF-8 to support Chinese characters
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+# ===============
+# == Variables ==
+# ===============
 SOURCE_URL="https://raw.githubusercontent.com/cy948/lobe-chat/run/casdoor"
 SUB_DIR=docker-compose/local
 FILES=(
@@ -8,21 +19,89 @@ FILES=(
   "$SUB_DIR/s3_data.tar.gz"
 )
 
-# Default value for force download
+# ======================
+# == Process the args ==
+# ======================
+
+# Arg: -f 
+# Determine force download asserts, default is not
 FORCE_DOWNLOAD=false
 
+# Arg: -l or --lang
+# Determine the language to show, default is en
+LANGUAGE="en_US"
+
 # Parse script arguments
-while getopts "f" opt; do
+while getopts "fl:-:" opt; do
   case $opt in
     f)
       FORCE_DOWNLOAD=true
       ;;
+    l)
+      LANGUAGE=$OPTARG
+      ;;
+    -)
+      case "${OPTARG}" in
+        lang)
+          LANGUAGE="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          ;;
+        *)
+          echo "Usage: $0 [-f] [-l language|--lang language]" >&2
+          exit 1
+          ;;
+      esac
+      ;;
     *)
-      echo "Usage: $0 [-f]"
+      echo "Usage: $0 [-f] [-l language|--lang language]" >&2
       exit 1
       ;;
   esac
 done
+
+# Supported languages and messages
+declare -A MESSAGES
+# en_US, zh_CN are Supported
+MESSAGES["en_US"]="en_US"
+MESSAGES["zh_CN"]="中文"
+# Declare messages
+MESSAGES["downloading_en_US"]="Downloading files..."
+MESSAGES["downloading_zh_CN"]="正在下载文件..."
+
+MESSAGES["downloaded_en_US"]=" already exists, skipping download."
+MESSAGES["downloaded_zh_CN"]=" 已经存在，跳过下载。"
+
+MESSAGES["extracted_success_en_US"]=" extracted successfully to directory: "
+MESSAGES["extracted_success_zh_CN"]=" 解压成功到目录："
+
+MESSAGES["extracted_failed_en_US"]=" extraction failed."
+MESSAGES["extracted_failed_zh_CN"]=" 解压失败。"
+
+MESSAGES["file_not_exists_en_US"]=" does not exist."
+MESSAGES["file_not_exists_zh_CN"]=" 不存在。"
+
+MESSAGES["tips_run_command_en_US"]="You have completed downloading all configuration files. Please run this command to start LobeChat:"
+MESSAGES["tips_run_command_zh_CN"]="您已经完成了所有配置文件的下载。请运行以下命令启动LobeChat："
+
+MESSAGES["tips_show_documentation_en_US"]="Full environment variables in the '.env' can be found at the documentation on "
+MESSAGES["tips_show_documentation_zh_CN"]="完整的环境变量在'.env'中可以在文档中找到："
+
+MESSAGES["tips_show_documentation_url_en_US"]="https://lobehub.com/docs/self-hosting/environment-variables"
+MESSAGES["tips_show_documentation_url_zh_CN"]="https://lobehub.com/zh/docs/self-hosting/environment-variables"
+
+MESSAGES["tips_warning_en_US"]="Warning: do not use this demo application in production!!!"
+MESSAGES["tips_warning_zh_CN"]="警告：不要在生产环境中使用此演示应用程序！！！"
+
+# Arg: -l --lang
+# If the language is not supported, default to English
+if [[ -z "${MESSAGES[$LANGUAGE]}" ]]; then
+  LANGUAGE="en_US"
+fi
+
+# Function to show messages
+show_message() {
+  local message_key="$1"
+  echo "${MESSAGES["$message_key"_"$LANGUAGE"]}"
+}
 
 # Function to download files
 download_file() {
@@ -30,7 +109,7 @@ download_file() {
   local local_file="$2"
 
   if [ "$FORCE_DOWNLOAD" = false ] && [ -e "$local_file" ]; then
-    echo "$local_file already exists, skipping download."
+    echo "$local_file" $(show_message "downloaded")
     return 0
   fi
 
@@ -44,13 +123,13 @@ extract_file() {
   if [ -e "$file_name" ]; then
     tar -zxvf "$file_name" -C "$target_dir" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
-      echo "$file_name extracted successfully to $target_dir."
+      echo "$file_name" $(show_message "extracted_success") "$target_dir"
     else
-      echo "$file_name extraction failed."
+      echo "$file_name" $(show_message "extracted_failed") 
       exit 1
     fi
   else
-    echo "$file_name does not exist."
+    echo "$file_name" $(show_message "file_not_exists")
     exit 1
   fi
 }
@@ -98,8 +177,8 @@ extract_file "s3_data.tar.gz" "."
 extract_file "init_data.json.tar.gz" "."
 
 # Display final message
-echo -e "\nYou have completed downloading all configuration files. Next steps:"
-echo -e "1. Edit the environment variables in the '.env' file if needed. You can find the documentation on: "
-print_centered "https://lobehub.com/docs/self-hosting/environment-variables" "blue"
-echo -e "2. Run:"
+echo -e "\n" $(show_message "tips_run_command") "\n"
 print_centered "docker compose up" "green"
+printf $(show_message "tips_show_documentation")
+printf "\e[34m%s\e[0m\n" $(show_message "tips_show_documentation_url")
+printf "\n\e[33m%s\e[0m\n" $(show_message "tips_warning")
