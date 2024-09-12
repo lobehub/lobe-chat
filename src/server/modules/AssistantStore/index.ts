ce28003a04f33952ac852b24283cb42d74aafde2
canisminor1990 <i@canisminor.cc>
2024-09-12 21:13:08 +0800

    fix

--- src/server/modules/AssistantStore/index.ts
+++ src/server/modules/AssistantStore/index.ts
@@ -4,6 +4,10 @@ import { appEnv } from '@/config/app';
 import { DEFAULT_LANG, isLocaleNotSupport } from '@/const/locale';
 import { Locales, normalizeLocale } from '@/locales/resources';
 
+const checkSupportLocale = (lang: Locales) => {
+  return isLocaleNotSupport(lang) || normalizeLocale(lang) !== 'zh-CN';
+};
+
 export class AssistantStore {
   private readonly baseUrl: string;
 
@@ -12,14 +16,13 @@ export class AssistantStore {
   }
 
   getAgentIndexUrl = (lang: Locales = DEFAULT_LANG) => {
-    if (isLocaleNotSupport(lang) || lang !== 'zh-CN') return this.baseUrl;
+    if (checkSupportLocale(lang)) return this.baseUrl;
 
     return urlJoin(this.baseUrl, `index.${normalizeLocale(lang)}.json`);
   };
 
   getAgentUrl = (identifier: string, lang: Locales = DEFAULT_LANG) => {
-    if (isLocaleNotSupport(lang) || lang !== 'zh-CN')
-      return urlJoin(this.baseUrl, `${identifier}.json`);
+    if (checkSupportLocale(lang)) return urlJoin(this.baseUrl, `${identifier}.json`);
 
     return urlJoin(this.baseUrl, `${identifier}.${normalizeLocale(lang)}.json`);
   };
