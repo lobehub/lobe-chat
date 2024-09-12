b956755f12b1cb45ffba5f52c1ccf1b84f8e5c79
Cai Yao <67412196+cy948@users.noreply.github.com>
2024-09-12 21:35:33 +0800

    ♻️ refactor: Support Environment Variable Inference For NextAuth (#3701)
    * :recycle: refactor: `AUTH_SECRET`&`AUTH_TRUST_HOST`
    
    * :arrow_up: chore: update nextauth & @auth/core version
    
    * :recycle: refactor: env infer for `auth0`
    
    * :arrow_up: chore: always use latest `next-auth` & `@auth/core`
    
    * :recycle: refactor: align `authelia`
    
    * :recycle: refactor: align `authentik`
    
    * :recycle: align `github`
    
    * :recycle: refactor: align `azure_ad`
    
    * :recycle: refactor: align `cloudflare zero trust`
    
    * :recycle: refactor: align `generic-oidc`
    
    * :recycle: refactor: align `logto`
    
    * :recycle: refactor: align `zitadel`
    
    * :recycle: refactor: add deprecate tips
    
    * :recycle: refactor: add warning for `azure_ad`
    
    * :lipstick: style: reformat codes
    
    * :bug: fix: azure warning
    
    * :bug: fix: warning for cloudfalre zero turst
    
    * :bug: fix: warning for generic oidc
    
    * :rewind: revert: revert changes to `NEXT_AUTH_SECRET`
    
    * :recycle: refactor: add redirectProxy url
    
    * :rewind: revert: unmodify ENABLE_NEXT_AUTH
    
    * :test_tube: test: should show env warning

--- src/libs/next-auth/sso-providers/azure-ad.ts
+++ src/libs/next-auth/sso-providers/azure-ad.ts
@@ -11,9 +11,11 @@ const provider = {
     // Specify auth scope, at least include 'openid email'
     // all scopes in Azure AD ref: https://learn.microsoft.com/en-us/entra/identity-platform/scopes-oidc#openid-connect-scopes
     authorization: { params: { scope: 'openid email profile' } },
-    clientId: authEnv.AZURE_AD_CLIENT_ID,
-    clientSecret: authEnv.AZURE_AD_CLIENT_SECRET,
-    tenantId: authEnv.AZURE_AD_TENANT_ID,
+    // TODO(NextAuth ENVs Migration): Remove once nextauth envs migration time end
+    clientId: authEnv.AZURE_AD_CLIENT_ID ?? process.env.AUTH_AZURE_AD_ID,
+    clientSecret: authEnv.AZURE_AD_CLIENT_SECRET ?? process.env.AUTH_AZURE_AD_SECRET,
+    tenantId: authEnv.AZURE_AD_TENANT_ID ?? process.env.AUTH_AZURE_AD_TENANT_ID,
+    // Remove end
     // TODO(NextAuth): map unique user id to `providerAccountId` field
     // profile(profile) {
     //   return {
