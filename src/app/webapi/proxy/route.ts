73983cf6e4e31e2edf7a343b675f69041e80f05c
arvinxx <arvinx@foxmail.com>
2024-09-20 22:56:35 +0800

    🐛 fix: fix error

--- src/app/webapi/proxy/route.ts
+++ src/app/webapi/proxy/route.ts
@@ -9,7 +9,9 @@ export const POST = async (req: Request) => {
   const url = await req.text();
 
   try {
-    return await fetch(url, { agent: ssrfAgent(url) });
+    const res = await fetch(url, { agent: ssrfAgent(url) });
+
+    return new Response(await res.arrayBuffer(), { headers: { ...res.headers } });
   } catch (err) {
     console.error(err); // DNS lookup 127.0.0.1(family:4, host:127.0.0.1.nip.io) is not allowed. Because, It is private IP address.
     return NextResponse.json({ error: 'Not support internal host proxy' }, { status: 400 });
