8afb4e29812f761b3f77f27e1235e59a4a0efa4d
CanisMinor <i@canisminor.cc>
2024-09-21 14:58:13 +0800

    ⚡ perf: Use nextjs pwa feat to gen manifest file (#4042)
    * ⚡ perf: Use nextjs pwa feat to gen manifest file
    
    * 🐛 fix: Fix review problem
    
    * ✅ test: Add test
    
    * 💄 style: Update next config
    
    * 🐛 fix: Fix test config
    
    * 💄 style: Update layout

--- next.config.mjs
+++ next.config.mjs
@@ -101,6 +101,19 @@ const nextConfig = {
         ],
         source: '/apple-touch-icon.png',
       },
+      {
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/javascript; charset=utf-8',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self'",
+          },
+        ],
+        source: '/sw.js',
+      },
     ];
   },
 
@@ -113,9 +126,41 @@ const nextConfig = {
       source: '/sitemap.xml',
     },
     {
-      destination: '/discover',
+      destination: '/manifest.webmanifest',
+      permanent: true,
+      source: '/manifest.json',
+    },
+    {
+      destination: '/discover/assistant/:slug',
+      has: [
+        {
+          key: 'agent',
+          type: 'query',
+          value: '(?<slug>.*)',
+        },
+      ],
       permanent: true,
       source: '/market',
+    },
+    {
+      destination: '/discover/assistants',
+      permanent: true,
+      source: '/discover/assistant',
+    },
+    {
+      destination: '/discover/models',
+      permanent: true,
+      source: '/discover/model',
+    },
+    {
+      destination: '/discover/plugins',
+      permanent: true,
+      source: '/discover/plugin',
+    },
+    {
+      destination: '/discover/providers',
+      permanent: true,
+      source: '/discover/provider',
     },
     {
       destination: '/settings/common',
