5b2f749cec03efcb280ba08c07e5f19ca9947e04
sxjeru <sxjeru@gmail.com>
2024-09-23 14:29:28 +0000

    fix

--- src/libs/agent-runtime/utils/streams/minimax.ts
+++ src/libs/agent-runtime/utils/streams/minimax.ts
@@ -12,15 +12,18 @@ const unit8ArrayToJSONChunk = (unit8Array: Uint8Array): OpenAI.ChatCompletionChu
   // chunkValue example:
   // data: {"id":"028a65377137d57aaceeffddf48ae99f","choices":[{"finish_reason":"tool_calls","index":0,"delta":{"role":"assistant","tool_calls":[{"id":"call_function_7371372822","type":"function","function":{"name":"realtime-weather____fetchCurrentWeather","arguments":"{\"city\": [\"杭州\", \"北京\"]}"}}]}}],"created":155511,"model":"abab6.5s-chat","object":"chat.completion.chunk"}
 
-  const dataIdPattern = /data: {"id":"/g;
+  const dataIdPattern = /data: {"id":/g;
   const matchCount = (chunkValue.match(dataIdPattern) || []).length;
   if (matchCount === 2) {
-    const secondDataIdIndex = chunkValue.indexOf('data: {"id":', chunkValue.indexOf('data: {"id":') + 1);
+    const secondDataIdIndex = chunkValue.indexOf(
+      'data: {"id":',
+      chunkValue.indexOf('data: {"id":') + 1,
+    );
     if (secondDataIdIndex !== -1) {
       chunkValue = chunkValue.slice(0, secondDataIdIndex).trim();
     }
   }
-  
+
   // so we need to remove `data:` prefix and then parse it as JSON
   if (chunkValue.startsWith('data:')) {
     chunkValue = chunkValue.slice(5).trim();
