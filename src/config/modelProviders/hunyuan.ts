2be45bb5804da2e8ab6578a9e07ea9a9820d5409
Zhijie He <hezhijie0327@hotmail.com>
2024-09-26 22:54:05 +0800

    💄 style: update model tokens info

--- src/config/modelProviders/hunyuan.ts
+++ src/config/modelProviders/hunyuan.ts
@@ -9,7 +9,7 @@ const Hunyuan: ModelProviderCard = {
       enabled: true,
       id: 'hunyuan-lite',
       maxOutput: 6000,
-      tokens: 250_000,
+      tokens: 256_000,
     },
     {
       description: '采用更优的路由策略，同时缓解了负载均衡和专家趋同的问题。长文方面，大海捞针指标达到99.9%。MOE-32K 性价比相对更高，在平衡效果、价格的同时，可对实现对长文本输入的处理。',
@@ -22,7 +22,7 @@ const Hunyuan: ModelProviderCard = {
         input: 4.5,
         output: 5,
       },
-      tokens: 30_000,
+      tokens: 32_000,
     },
     {
       description: '采用更优的路由策略，同时缓解了负载均衡和专家趋同的问题。长文方面，大海捞针指标达到99.9%。MOE-256K 在长度和效果上进一步突破，极大的扩展了可输入长度。',
@@ -35,7 +35,7 @@ const Hunyuan: ModelProviderCard = {
         input: 15,
         output: 60,
       },
-      tokens: 250_000,
+      tokens: 256_000,
     },
     {
       description: '混元全新一代大语言模型的预览版，采用全新的混合专家模型（MoE）结构，相比hunyuan-pro推理效率更快，效果表现更强。',
@@ -48,7 +48,7 @@ const Hunyuan: ModelProviderCard = {
         input: 15,
         output: 50,
       },
-      tokens: 28_000,
+      tokens: 32_000,
     },
     {
       description: '万亿级参数规模 MOE-32K 长文模型。在各种 benchmark 上达到绝对领先的水平，复杂指令和推理，具备复杂数学能力，支持 functioncall，在多语言翻译、金融法律医疗等领域应用重点优化。',
@@ -62,7 +62,7 @@ const Hunyuan: ModelProviderCard = {
         input: 30,
         output: 100,
       },
-      tokens: 28_000,
+      tokens: 32_000,
     },
     {
       description: '混元最新代码生成模型，经过 200B 高质量代码数据增训基座模型，迭代半年高质量 SFT 数据训练，上下文长窗口长度增大到 8K，五大语言代码生成自动评测指标上位居前列；五大语言10项考量各方面综合代码任务人工高质量评测上，性能处于第一梯队',
@@ -75,7 +75,7 @@ const Hunyuan: ModelProviderCard = {
         input: 4,
         output: 8,
       },
-      tokens: 4000,
+      tokens: 8000,
     },
     {
       description: '混元最新多模态模型，支持图片+文本输入生成文本内容。',
@@ -88,7 +88,7 @@ const Hunyuan: ModelProviderCard = {
         input: 18,
         output: 18,
       },
-      tokens: 4000,
+      tokens: 8000,
       vision: true,
     },
     {
@@ -102,7 +102,7 @@ const Hunyuan: ModelProviderCard = {
         input: 4,
         output: 8,
       },
-      tokens: 28_000,
+      tokens: 32_000,
     },
     {
       description: '混元最新版角色扮演模型，混元官方精调训练推出的角色扮演模型，基于混元模型结合角色扮演场景数据集进行增训，在角色扮演场景具有更好的基础效果。',
@@ -114,7 +114,7 @@ const Hunyuan: ModelProviderCard = {
         input: 4,
         output: 8,
       },
-      tokens: 4000,
+      tokens: 8000,
     },
   ],
   checkModel: 'hunyuan-lite',
