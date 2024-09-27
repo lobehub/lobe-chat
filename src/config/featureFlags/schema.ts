380497a18df44b3f629a9bf4c8fe785f9d64629a
Duy Bui <khanhduyvt0101@gmail.com>
2024-09-27 13:33:08 +0700

    fix

--- src/config/featureFlags/schema.ts
+++ src/config/featureFlags/schema.ts
@@ -38,6 +38,7 @@ export const FeatureFlagsSchema = z.object({
   // please contact us for more information: hello@lobehub.com
   commercial_hide_github: z.boolean().optional(),
   commercial_hide_docs: z.boolean().optional(),
+  commercial_branding: z.boolean().optional(),
 });
 
 export type IFeatureFlags = z.infer<typeof FeatureFlagsSchema>;
@@ -73,6 +74,7 @@ export const DEFAULT_FEATURE_FLAGS: IFeatureFlags = {
   // please contact us for more information: hello@lobehub.com
   commercial_hide_github: false,
   commercial_hide_docs: false,
+  commercial_branding: false,
 };
 
 export const mapFeatureFlagsEnvToState = (config: IFeatureFlags) => {
@@ -103,5 +105,6 @@ export const mapFeatureFlagsEnvToState = (config: IFeatureFlags) => {
 
     hideGitHub: config.commercial_hide_github,
     hideDocs: config.commercial_hide_docs,
+    enableCommercialBranding: config.commercial_branding,
   };
 };
