28ad1aa14cb82dcaddb1d510472114234cd4e0cd
LovelyGuYiMeng <76251800+LovelyGuYiMeng@users.noreply.github.com>
2024-09-27 19:14:20 +0800

    Update zhipu.ts

--- src/config/modelProviders/zhipu.ts
+++ src/config/modelProviders/zhipu.ts
@@ -46,18 +46,6 @@ const ZhiPu: ModelProviderCard = {
       tokens: 128_000,
     },
     {
-      description: 'GLM-4 是发布于2024年1月的旧旗舰版本，目前已被更强的 GLM-4-0520 取代。',
-      displayName: 'GLM-4',
-      functionCall: true,
-      id: 'glm-4',
-      pricing: {
-        currency: 'CNY',
-        input: 100,
-        output: 100,
-      },
-      tokens: 128_000,
-    },
-    {
       description: 'GLM-4-AirX 提供 GLM-4-Air 的高效版本，推理速度可达其2.6倍。',
       displayName: 'GLM-4-AirX',
       enabled: true,
@@ -97,7 +85,20 @@ const ZhiPu: ModelProviderCard = {
       tokens: 1_024_000,
     },
     {
-      description: 'GLM-4-Flash 是处理简单任务的理想选择，速度最快且价格最优惠。',
+      description: 'GLM-4-FlashX 是Flash的增强版本，超快推理速度。',
+      displayName: 'GLM-4-FlashX',
+      enabled: true,
+      functionCall: true,
+      id: 'glm-4-flashx',
+      pricing: {
+        currency: 'CNY',
+        input: 0.1,
+        output: 0.1,
+      },
+      tokens: 128_000,
+    },
+    {
+      description: 'GLM-4-Flash 是处理简单任务的理想选择，速度最快且免费。',
       displayName: 'GLM-4-Flash',
       enabled: true,
       functionCall: true,
