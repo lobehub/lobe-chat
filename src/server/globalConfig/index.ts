e3b227aab6f4b0d51b2905704bea6ebb343e8798
Henry <henry@yearning.io>
2024-09-30 18:29:29 +0800

    Not finished changing

--- src/server/globalConfig/index.ts
+++ src/server/globalConfig/index.ts
@@ -1,6 +1,7 @@
 import { appEnv, getAppConfig } from '@/config/app';
 import { authEnv } from '@/config/auth';
 import { fileEnv } from '@/config/file';
+import { knowledgeEnv } from '@/config/knowledge';
 import { langfuseEnv } from '@/config/langfuse';
 import { getLLMConfig } from '@/config/llm';
 import {
@@ -108,6 +109,7 @@ export const getServerGlobalConfig = () => {
     defaultAgent: {
       config: parseAgentConfig(DEFAULT_AGENT_CONFIG),
     },
+    defaultEmbed: parseAgentConfig(knowledgeEnv.DEFAULT_FILES_CONFIG),
     enableUploadFileToServer: !!fileEnv.S3_SECRET_ACCESS_KEY,
     enabledAccessCode: ACCESS_CODES?.length > 0,
     enabledOAuthSSO: enableNextAuth,
