0dceb24a3e3f5dc413da82ccba92bbc39e3352da
Arvin Xu <arvinx@foxmail.com>
2024-10-12 06:36:30 +0000

    fix

--- src/config/modelProviders/huggingface.ts
+++ src/config/modelProviders/huggingface.ts
@@ -37,7 +37,7 @@ const HuggingFace: ModelProviderCard = {
       tokens: 4096,
     },
   ],
-  checkModel: 'meta-llama/Llama-3.1-8B-Instruct',
+  checkModel: 'mistralai/Mistral-7B-Instruct-v0.2',
   description:
     'HuggingFace Inference API 提供了一种快速且免费的方式，让您可以探索成千上万种模型，适用于各种任务。无论您是在为新应用程序进行原型设计，还是在尝试机器学习的功能，这个 API 都能让您即时访问多个领域的高性能模型。',
   disableBrowserRequest: true,
