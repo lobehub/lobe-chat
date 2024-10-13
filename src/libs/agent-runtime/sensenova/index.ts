0592472133e71e45573c91f14614103ef7d38b4e
Zhijie He <hezhijie0327@hotmail.com>
2024-10-13 14:01:40 +0800

    ♻️ refactor: replace `crypto-js` to `jose`, only support ENV now (Part 1)

--- src/libs/agent-runtime/sensenova/index.ts
+++ src/libs/agent-runtime/sensenova/index.ts
@@ -1,23 +1,17 @@
+import { SignJWT } from 'jose'; 
 import OpenAI from 'openai';
-import CryptoJS from 'crypto-js';
 
 import { ChatStreamPayload, ModelProvider } from '../types';
 import { LobeOpenAICompatibleFactory } from '../utils/openaiCompatibleFactory';
 
-// Helper function for base64 URL encoding
-const base64UrlEncode = (obj: object) => {
-  return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(JSON.stringify(obj)))
-    .replaceAll('=', '')
-    .replaceAll('+', '-')
-    .replaceAll('/', '_');
-};
-
-// Function to generate JWT token
-const generateJwtTokenSenseNova = (accessKeyID: string = '', accessKeySecret: string = '', expiredAfter: number = 1800, notBefore: number = 5) => {
-  const headers = {
-    alg: 'HS256',
-    typ: 'JWT',
-  };
+// https://console.sensecore.cn/help/docs/model-as-a-service/nova/overview/Authorization
+const generateJwtTokenSenseNova = async (
+  accessKeyID: string = '',
+  accessKeySecret: string = '',
+  expiredAfter: number = 1800,
+  notBefore: number = 5
+): Promise<string> => {
+  const encoder = new TextEncoder();
 
   const payload = {
     exp: Math.floor(Date.now() / 1000) + expiredAfter,
@@ -25,22 +19,14 @@ const generateJwtTokenSenseNova = (accessKeyID: string = '', accessKeySecret: st
     nbf: Math.floor(Date.now() / 1000) - notBefore,
   };
 
-  const data = `${base64UrlEncode(headers)}.${base64UrlEncode(payload)}`;
+  const jwt = await new SignJWT(payload)
+    .setProtectedHeader({ alg: 'HS256', typ: 'JWT' })
+    .sign(encoder.encode(accessKeySecret));
 
-  const signature = CryptoJS.HmacSHA256(data, accessKeySecret)
-    .toString(CryptoJS.enc.Base64)
-    .replaceAll('=', '')
-    .replaceAll('+', '-')
-    .replaceAll('/', '_');
-
-  const apiKey = `${data}.${signature}`;
-
-  return apiKey;
+  return jwt;
 };
 
-// LobeSenseNovaAI setup
 export const LobeSenseNovaAI = (() => {
-  // Create the factory instance using LobeOpenAICompatibleFactory
   const factory = LobeOpenAICompatibleFactory({
     baseURL: 'https://api.sensenova.cn/compatible-mode/v1',
     chatCompletion: {
@@ -61,10 +47,16 @@ export const LobeSenseNovaAI = (() => {
     provider: ModelProvider.SenseNova,
   });
 
-  // Use Object.assign to add the generateJWTToken method
   return Object.assign(factory, {
-    generateJWTToken: (ak: string, sk: string, expiredAfter: number = 1800, notBefore: number = 5) => {
-      return generateJwtTokenSenseNova(ak, sk, expiredAfter, notBefore);
+    generateJWTToken: async (
+      ak: string,
+      sk: string,
+      expiredAfter: number = 1800,
+      notBefore: number = 5
+    ) => {
+      const apiKey = await generateJwtTokenSenseNova(ak, sk, expiredAfter, notBefore);
+
+      return apiKey;
     },
   });
 })();
