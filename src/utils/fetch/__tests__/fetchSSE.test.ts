f60b6f6d26d46125a68ceee5975548cdd4c254c5
Henry <henry@yearning.io>
2024-10-15 15:10:37 +0800

    fix: ci file

--- src/utils/fetch/__tests__/fetchSSE.test.ts
+++ src/utils/fetch/__tests__/fetchSSE.test.ts
@@ -437,7 +437,7 @@ describe('fetchSSE', () => {
           context: {
             chunk: 'abc',
             error: {
-              message: `Unexpected token 'a', \"abc\" is not valid JSON`,
+              message: 'Unexpected token a in JSON at position 0',
               name: 'SyntaxError',
             },
           },
