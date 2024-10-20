2ba5b2e53599501abd36d0002ca2af34ff8f302f
LovelyGuYiMeng <76251800+LovelyGuYiMeng@users.noreply.github.com>
2024-10-20 22:46:58 +0800

    Update mistral.ts

--- src/config/modelProviders/mistral.ts
+++ src/config/modelProviders/mistral.ts
@@ -135,7 +135,7 @@ const Mistral: ModelProviderCard = {
       tokens: 256_000,
     },
   ],
-  checkModel: 'open-mistral-7b',
+  checkModel: 'ministral-3b-latest',
   description:
     'Mistral 提供先进的通用、专业和研究型模型，广泛应用于复杂推理、多语言任务、代码生成等领域，通过功能调用接口，用户可以集成自定义功能，实现特定应用。',
   id: 'mistral',
