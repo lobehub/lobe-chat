1a5c7ae84e218d09f34de2b2ee3906d13c4ce4b3
Arvin Xu <arvinx@foxmail.com>
2024-10-26 18:48:05 +0800

    ✨ feat: support ZEN mode (#4496)
    * ✨ feat: add zen mode
    
    * 💄 style: improve style
    
    * 💄 style: 新增 Zen Mode
    
    * 💄 style: 新增 Zen Mode
    
    * update locale
    
    * add tests
    
    * 💄 style: fix text

--- src/const/hotkeys.ts
+++ src/const/hotkeys.ts
@@ -2,4 +2,10 @@ export const ALT_KEY = 'alt';
 export const META_KEY = 'mod';
 export const SAVE_TOPIC_KEY = 'n';
 export const CLEAN_MESSAGE_KEY = 'backspace';
-export const REGENERATE_KEY = 'r';
+
+export const HOTKEYS = {
+  clear: 'mod+alt+backspace',
+  regenerate: 'alt+r',
+  saveTopic: 'alt+n',
+  zenMode: 'mod+\\',
+};
