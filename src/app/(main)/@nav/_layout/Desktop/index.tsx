6e55865135e465ef8d6fd6942c3e712224e8b112
Arvin Xu <arvinx@foxmail.com>
2024-10-27 17:39:42 +0800

    ✨ feat:  experimentally support to pin assistant to sidebar (#4514)
    * ✨ feat: support pin assistant at side
    
    * ✨ feat: add a feature flag
    
    * ✅ test: fix tests
    
    * 💄 style: fix avtar meta
    
    * 💄 style: fix pin mode style
    
    * 💄 style: add hotkeys

--- src/app/(main)/@nav/_layout/Desktop/index.tsx
+++ src/app/(main)/@nav/_layout/Desktop/index.tsx
@@ -6,22 +6,29 @@ import { memo } from 'react';
 import { useActiveTabKey } from '@/hooks/useActiveTabKey';
 import { useGlobalStore } from '@/store/global';
 import { systemStatusSelectors } from '@/store/global/selectors';
+import { featureFlagsSelectors, useServerConfigStore } from '@/store/serverConfig';
 
 import Avatar from './Avatar';
 import BottomActions from './BottomActions';
+import PinList from './PinList';
 import TopActions from './TopActions';
 
 const Nav = memo(() => {
   const sidebarKey = useActiveTabKey();
   const inZenMode = useGlobalStore(systemStatusSelectors.inZenMode);
-
+  const { showPinList } = useServerConfigStore(featureFlagsSelectors);
   return (
     !inZenMode && (
       <SideNav
         avatar={<Avatar />}
         bottomActions={<BottomActions />}
         style={{ height: '100%', zIndex: 100 }}
-        topActions={<TopActions tab={sidebarKey} />}
+        topActions={
+          <>
+            <TopActions tab={sidebarKey} />
+            {showPinList && <PinList />}
+          </>
+        }
       />
     )
   );
