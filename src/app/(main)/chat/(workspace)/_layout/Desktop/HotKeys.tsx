584ca8396cee7a282f6870bb4061cec11e984b60
mushan0x0 <mushan0x0@gmail.com>
2024-10-28 16:38:28 +0800

    🔨 chore: clean up unused methods

--- src/app/(main)/chat/(workspace)/_layout/Desktop/HotKeys.tsx
+++ src/app/(main)/chat/(workspace)/_layout/Desktop/HotKeys.tsx
@@ -1,19 +1,15 @@
 'use client';
 
 import isEqual from 'fast-deep-equal';
-import { useCallback } from 'react';
 import { useHotkeys } from 'react-hotkeys-hook';
 
 import { HOTKEYS } from '@/const/hotkeys';
 import { useChatStore } from '@/store/chat';
 import { chatSelectors } from '@/store/chat/selectors';
-import { useFileStore } from '@/store/file';
 import { useGlobalStore } from '@/store/global';
 
 const HotKeys = () => {
-  const [regenerateMessage] = useChatStore((s) => [
-    s.regenerateMessage,
-  ]);
+  const [regenerateMessage] = useChatStore((s) => [s.regenerateMessage]);
   const lastMessage = useChatStore(chatSelectors.latestMessage, isEqual);
 
   const toggleZenMode = useGlobalStore((s) => s.toggleZenMode);
