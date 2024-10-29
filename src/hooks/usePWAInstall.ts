53f0a476836bcb3440d2e7210b489cb9199cc7fc
canisminor1990 <i@canisminor.cc>
2024-10-29 14:46:39 +0800

    ✨ feat: Add release

--- src/hooks/usePWAInstall.ts
+++ src/hooks/usePWAInstall.ts
@@ -19,12 +19,10 @@ export const usePWAInstall = () => {
   }, []);
 
   const installCheck = () => {
-    // 当在 PWA 中时，不显示安装按钮
-    if (isPWA) return false;
-    // 其他情况下，根据是否可以安装来显示安装按钮 (如已经安装则不显示)
-    if (isSupportInstallPWA) return canInstall;
+    // 当在 PWA 或不支持 PWA 的环境中时，不显示安装按钮
+    if (isPWA || isSupportInstallPWA) return false;
     // 当在不支持 PWA 的环境中时，安装按钮 (此时为安装教程)
-    return true;
+    return canInstall;
   };
 
   return {
