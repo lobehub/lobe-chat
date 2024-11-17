30e2a95ef38f7bdb9834582587ed5b52e2b47f52
Zhijie He <hezhijie0327@hotmail.com>
2024-11-17 10:43:07 +0800

    💄 style: update model list

--- src/libs/agent-runtime/gitee/index.ts
+++ src/libs/agent-runtime/gitee/index.ts
@@ -1,22 +1,8 @@
-import OpenAI from 'openai';
-
-import { ChatStreamPayload, ModelProvider } from '../types';
+import { ModelProvider } from '../types';
 import { LobeOpenAICompatibleFactory } from '../utils/openaiCompatibleFactory';
 
 export const LobeGiteeAI = LobeOpenAICompatibleFactory({
   baseURL: 'https://ai.gitee.com/v1',
-  chatCompletion: {
-    handlePayload: ({ model, top_p, ...payload }: ChatStreamPayload) =>
-      ({
-        ...payload,
-        model,
-        ...(model === "code-raccoon-v1" ? {
-          top_p: (top_p !== undefined && top_p > 0 && top_p < 1) ? top_p : undefined,
-        } : {
-          top_p,
-        }),
-      }) as OpenAI.ChatCompletionCreateParamsStreaming,
-  },
   debug: {
     chatCompletion: () => process.env.DEBUG_GITEE_CHAT_COMPLETION === '1',
   },
