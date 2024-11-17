b43fb5449c5edd19f1ae2f1288b380e1aa087426
Zhijie He <hezhijie0327@hotmail.com>
2024-11-17 10:18:02 +0800

    🐛 fix: fix `code-raccoon-v1` topP issue

--- src/libs/agent-runtime/gitee/index.ts
+++ src/libs/agent-runtime/gitee/index.ts
@@ -1,8 +1,22 @@
-import { ModelProvider } from '../types';
+import OpenAI from 'openai';
+
+import { ChatStreamPayload, ModelProvider } from '../types';
 import { LobeOpenAICompatibleFactory } from '../utils/openaiCompatibleFactory';
 
 export const LobeGiteeAI = LobeOpenAICompatibleFactory({
   baseURL: 'https://ai.gitee.com/v1',
+  chatCompletion: {
+    handlePayload: ({ model, top_p, ...payload }: ChatStreamPayload) =>
+      ({
+        ...payload,
+        model,
+        ...(model === "code-raccoon-v1" ? {
+          top_p: (top_p !== undefined && top_p > 0 && top_p < 1) ? top_p : undefined,
+        } : {
+          top_p,
+        }),
+      }) as OpenAI.ChatCompletionCreateParamsStreaming,
+  },
   debug: {
     chatCompletion: () => process.env.DEBUG_GITEE_CHAT_COMPLETION === '1',
   },
