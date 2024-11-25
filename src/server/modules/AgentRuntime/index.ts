bca1ae9cab263d8262d8619d0ab71f6ef019ce6e
Zhijie He <hezhijie0327@hotmail.com>
2024-11-25 21:48:55 +0800

    🐛 fix: fix typo

--- src/server/modules/AgentRuntime/index.ts
+++ src/server/modules/AgentRuntime/index.ts
@@ -44,7 +44,7 @@ const getLlmOptionsFromPayload = (provider: string, payload: JWTPayload) => {
       const apiKey = apiKeyManager.pick(payload?.apiKey || AZURE_API_KEY);
       const endpoint = payload?.endpoint || AZURE_ENDPOINT;
       const apiVersion = payload?.azureApiVersion || AZURE_API_VERSION;
-      return { apikey, apiVersion, endpoint };
+      return { apiKey, apiVersion, endpoint };
     }
 
     case ModelProvider.Bedrock: {
