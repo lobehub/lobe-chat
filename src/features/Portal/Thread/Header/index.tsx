8bec6e5a8ae12d10e90769be0dd7817458bb3ce8
arvinxx <arvinx@foxmail.com>
2024-11-28 02:43:58 +0800

    💄 style: add switch portal thread

--- src/features/Portal/Thread/Header/index.tsx
+++ src/features/Portal/Thread/Header/index.tsx
@@ -1,20 +1,43 @@
 import { ActionIcon } from '@lobehub/ui';
-import { XIcon } from 'lucide-react';
+import { ArrowLeftRight, XIcon } from 'lucide-react';
+import { memo } from 'react';
+import { Flexbox } from 'react-layout-kit';
 
 import SidebarHeader from '@/components/SidebarHeader';
 import { useChatStore } from '@/store/chat';
 
 import Title from './Title';
 
-const Header = () => {
-  const closeThreadPortal = useChatStore((s) => s.closeThreadPortal);
+const Header = memo(() => {
+  const [hasPortal, portalThreadId, closeThreadPortal, switchThread] = useChatStore((s) => [
+    !!s.portalThreadId,
+    s.portalThreadId,
+    s.closeThreadPortal,
+    s.switchThread,
+  ]);
+
   return (
     <SidebarHeader
-      actions={<ActionIcon icon={XIcon} onClick={closeThreadPortal} />}
+      actions={
+        <Flexbox horizontal>
+          {hasPortal && (
+            <ActionIcon
+              icon={ArrowLeftRight}
+              onClick={() => {
+                if (!portalThreadId) return;
+
+                switchThread(portalThreadId);
+                closeThreadPortal();
+              }}
+            />
+          )}
+          <ActionIcon icon={XIcon} onClick={closeThreadPortal} />
+        </Flexbox>
+      }
       style={{ paddingBlock: 8, paddingInline: 8 }}
       title={<Title />}
     />
   );
-};
+});
 
 export default Header;
