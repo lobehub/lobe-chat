3da1704f29f32488a108429c9b491dab39c6378b
Arvin Xu <arvinx@foxmail.com>
2024-11-27 12:26:06 +0800

    💄 style: improve thread i18n locale (#4807)

--- src/locales/default/thread.ts
+++ src/locales/default/thread.ts
@@ -2,4 +2,6 @@ export default {
   actions: {
     confirmRemoveThread: '即将删除该子话题，删除后将不可恢复，请谨慎操作。',
   },
+  newPortalThread: { includeContext: '包含话题上下文', title: '开启新的子话题' },
+  notSupportMultiModals: '子话题暂不支持文件/图片上传，如有需求，欢迎留言：<1>💬 讨论区</1>',
 };
