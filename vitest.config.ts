4627640d81e0c76d7848b79efa43da620e07fb24
arvinxx <arvinx@foxmail.com>
2024-12-10 22:36:16 +0800

    🔨 chore: improve script

--- vitest.config.ts
+++ vitest.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
       '**/dist/**',
       '**/build/**',
       'src/database/server/**/**',
-      'src/server/services/!(discover|subscription|budget|user)/**/**',
+      'src/server/services/dataImporter/**/**',
     ],
     globals: true,
     server: {
