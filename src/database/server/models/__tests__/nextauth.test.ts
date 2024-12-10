593164bd394c4894e8763ea1d5e9e490d9fd2713
cy948 <cy948@vip.qq.com>
2024-12-10 22:25:40 +0800

    :white_check_mark: test: pass tests

--- src/database/server/models/__tests__/nextauth.test.ts
+++ src/database/server/models/__tests__/nextauth.test.ts
@@ -147,7 +147,7 @@ describe('LobeNextAuthDbAdapter', () => {
         ).toHaveLength(1);
       });
 
-      it('should create a user if id not exist and email is undefined', async () => {
+      it('should create a user if id not exist even email is invalid type', async () => {
         // In previous version, it will link the account to the existing user if the email is null
         // issue: https://github.com/lobehub/lobe-chat/issues/4918
         expect(nextAuthAdapter).toBeDefined();
@@ -156,13 +156,12 @@ describe('LobeNextAuthDbAdapter', () => {
         const existUserId = 'user-db-1';
         const existUserName = 'John Doe 1';
         // @ts-expect-error: createUser is defined
-        const createUser = nextAuthAdapter.createUser({
+        await nextAuthAdapter.createUser({
           ...user,
           id: existUserId,
           name: existUserName,
+          email: Object({}), // assign a non-string value
         });
-
-        expect(await createUser).not.toThrowError();
 
         const anotherUserId = 'user-db-2';
         const anotherUserName = 'John Doe 2';
@@ -171,6 +170,8 @@ describe('LobeNextAuthDbAdapter', () => {
           ...user,
           id: anotherUserId,
           name: anotherUserName,
+          // @ts-expect-error: try to assign undefined value
+          email: undefined,
         });
 
         // Should create a new user if id not exists and email is null
