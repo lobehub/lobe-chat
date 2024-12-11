ba5c064f66352d9adc5b973e981f927517f45f17
Arvin Xu <arvinx@foxmail.com>
2024-12-11 07:30:24 +0000

    fix tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
       '**/dist/**',
       '**/build/**',
       'src/database/server/**/**',
-      'src/server/services/dataImporter/**/**',
+      'src/database/repositories/dataImporter/**/**',
     ],
     globals: true,
     server: {
