bf902418c71f419ae7cc1d8e601b89dec355e619
Arvin Xu <arvinx@foxmail.com>
2024-12-13 00:03:20 +0800

    Update index.ts

--- src/utils/tokenizer/index.ts
+++ src/utils/tokenizer/index.ts
@@ -2,7 +2,7 @@ export const encodeAsync = async (str: string): Promise<number> => {
   if (str.length === 0) return 0;
 
   // 30_000 is the limit of the client
-  // if the string is longer than 300_000, we will use the server
+  // if the string is longer than 30_000, we will use the server
   if (str.length <= 30_000) {
     const { clientEncodeAsync } = await import('./client');
 
