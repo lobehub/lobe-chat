0df395a7a72280ba834dce36a77c33ccbfc49f38
Arvin Xu <arvinx@foxmail.com>
2024-12-14 23:51:09 +0800

    Delete src/database/queries/fileUrl.ts

--- src/database/queries/fileUrl.ts
+++
@@ -1,9 +0,0 @@
-import { sql } from 'drizzle-orm';
-
-import { files } from '../schemas';
-
-export const fileUrl = (domain: string = '') => {
-  const prefix = domain.endsWith('/') ? domain : `${domain}/`;
-
-  return sql<string>`${prefix} || ${files.url}`;
-};
