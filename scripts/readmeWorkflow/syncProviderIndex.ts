4220633e9c1e0ab15a854824d905e47b8dd06fb8
canisminor1990 <i@canisminor.cc>
2024-12-18 18:11:36 +0800

    🔧 chore: Update Readme Sync Workflow

---
+++ scripts/readmeWorkflow/syncProviderIndex.ts
@@ -0,0 +1,39 @@
+import { consola } from 'consola';
+import { readJSONSync } from 'fs-extra';
+import { resolve } from 'node:path';
+import urlJoin from 'url-join';
+
+import { PROVIDER_LIST, PROVIDER_SPLIT, root } from './const';
+import { genLink, readReadme, updateReadme, writeReadme } from './utlis';
+
+const genProviderTable = (data: { desc?: string; id: string; name: string }) => {
+  const title = genLink(data.name, urlJoin('https://lobechat.com/discover/provider', data.id));
+
+  return ['-', `**${title}**:`, data.desc].join(' ');
+};
+
+const runProviderTable = async (lang?: string) => {
+  const md = readReadme(lang);
+  const desc = readJSONSync(resolve(root, 'locales', lang || 'en-US', 'providers.json'));
+  const newMd = updateReadme(
+    PROVIDER_SPLIT,
+    md,
+    [
+      PROVIDER_LIST.map((item) =>
+        genProviderTable({
+          ...item,
+          desc: desc?.[item.id]?.description,
+        }),
+      ).join('\n'),
+      `> 📊 Total providers: ${genLink(`<kbd>**${PROVIDER_LIST.length}**</kbd>`, 'https://lobechat.com/discover/providers')}`,
+    ].join('\n\n'),
+  );
+  writeReadme(newMd, lang);
+  consola.success(`Sync ${lang || 'en-US'} provider index success!`);
+};
+
+export default async () => {
+  await runProviderTable();
+  await runProviderTable('zh-CN');
+  await runProviderTable('ja-JP');
+};
