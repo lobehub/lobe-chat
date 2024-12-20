6a2be39d0ff75c46ab1083a8c07efae38e6ea4cd
Arvin Xu <arvinx@foxmail.com>
2024-12-21 01:36:57 +0800

    Update ReactScan.tsx

--- src/components/Analytics/ReactScan.tsx
+++ src/components/Analytics/ReactScan.tsx
@@ -1,4 +1,4 @@
-import { MonitoringInner as Monitoring } from 'react-scan/monitoring/next';
+import { Monitoring } from 'react-scan/monitoring/next';
 
 interface ReactScanProps {
   apiKey: string;
@@ -7,8 +7,6 @@ interface ReactScanProps {
 const ReactScan = ({ apiKey }: ReactScanProps) => (
   <Monitoring
     apiKey={apiKey}
-    branch={process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF}
-    commit={process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA}
     url="https://monitoring.react-scan.com/api/v1/ingest"
   />
 );
