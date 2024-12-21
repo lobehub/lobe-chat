3821729341876dfa50f8920769c84d873432ac04
arvinxx <arvinx@foxmail.com>
2024-12-22 00:01:27 +0800

    update model

--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -7,3 +7,5 @@ const clientService =
 
 export const userService =
   process.env.NEXT_PUBLIC_SERVICE_MODE === 'server' ? new ServerService() : clientService;
+
+export const userClientService = clientService;
