bbbe7e407aaf1dce4893eb51d72ee3abc04e5e7f
arvinxx <arvinx@foxmail.com>
2024-12-31 02:30:24 +0800

    test cache key

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextRequest, NextResponse } from 'next/server';
+import { UAParser } from 'ua-parser-js';
 
 import { authEnv } from '@/config/auth';
 import { DEFAULT_LANG, LOBE_LOCALE_COOKIE } from '@/const/locale';
@@ -27,11 +28,12 @@ const defaultMiddleware = (request: NextRequest) => {
   const theme = request.cookies.get(LOBE_THEME_APPEARANCE)?.value || 'light';
   const language = request.cookies.get(LOBE_LOCALE_COOKIE)?.value || DEFAULT_LANG;
 
+  const ua = request.headers.get('user-agent');
+
+  const device = new UAParser(ua || '').getDevice();
+
   // 2. 创建规范化的偏好值
-  const preference = JSON.stringify({
-    language,
-    theme,
-  });
+  const preference = JSON.stringify({ device: device.type ?? undefined, language, theme });
 
   const response = NextResponse.next();
 
@@ -41,18 +43,16 @@ const defaultMiddleware = (request: NextRequest) => {
   // 5. 使用自定义头作为缓存变体的依据
   response.headers.set('Vary', 'x-user-preference');
 
-  response.headers.set(
-    'Vercel-CDN-cache-control',
-    'public, s-maxage=3600, stale-while-revalidate=86400',
-  );
+  response.headers.set('CDN-cache-control', 'public, s-maxage=3600, stale-while-revalidate=86400');
 
   return response;
 };
 
 // Initialize an Edge compatible NextAuth middleware
 const nextAuthMiddleware = NextAuthEdge.auth((req) => {
+  const response = defaultMiddleware(req);
   // skip the '/' route
-  if (req.nextUrl.pathname === '/') return NextResponse.next();
+  if (req.nextUrl.pathname === '/') return response;
 
   // Just check if session exists
   const session = req.auth;
@@ -62,15 +62,12 @@ const nextAuthMiddleware = NextAuthEdge.auth((req) => {
   const isLoggedIn = !!session?.expires;
 
   // Remove & amend OAuth authorized header
-  const requestHeaders = new Headers(req.headers);
-  requestHeaders.delete(OAUTH_AUTHORIZED);
-  if (isLoggedIn) requestHeaders.set(OAUTH_AUTHORIZED, 'true');
+  response.headers.delete(OAUTH_AUTHORIZED);
+  if (isLoggedIn) {
+    response.headers.set(OAUTH_AUTHORIZED, 'true');
+  }
 
-  return NextResponse.next({
-    request: {
-      headers: requestHeaders,
-    },
-  });
+  return response;
 });
 
 const isProtectedRoute = createRouteMatcher([
