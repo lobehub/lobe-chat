042dd6fd0187e6b8b04ce4eab96b04cf806c3bf2
arvinxx <arvinx@foxmail.com>
2025-01-07 11:09:51 +0800

    test remove global.crypto

--- tests/setup-db.ts
+++ tests/setup-db.ts
@@ -1,7 +1,7 @@
 // import env
-import { Crypto } from '@peculiar/webcrypto';
+// import { Crypto } from '@peculiar/webcrypto';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-global.crypto = new Crypto();
+// global.crypto = new Crypto();
