3125a55545db7d8d34a5aa38c12fdc25433acc02
arvinxx <arvinx@foxmail.com>
2025-01-09 13:57:32 +0800

    fix github

--- src/app/(main)/settings/provider/(detail)/github/page.tsx
+++ src/app/(main)/settings/provider/(detail)/github/page.tsx
@@ -43,15 +43,15 @@ const useProviderCard = (): ProviderItem => {
         ) : (
           <Input.Password
             autoComplete={'new-password'}
-            placeholder={t(`${providerKey}.personalAccessToken.placeholder`)}
+            placeholder={t(`github.personalAccessToken.placeholder`)}
           />
         ),
         desc: (
           <Markdown className={styles.markdown} fontSize={12} variant={'chat'}>
-            {t(`${providerKey}.personalAccessToken.desc`)}
+            {t(`github.personalAccessToken.desc`)}
           </Markdown>
         ),
-        label: t(`${providerKey}.personalAccessToken.title`),
+        label: t(`github.personalAccessToken.title`),
         name: [KeyVaultsConfigKey, LLMProviderApiTokenKey],
       },
     ],
