0ac5802d1e1c2761afbba59a7ce30ddce3ad699e
Arvin Xu <arvinx@foxmail.com>
2025-01-15 11:16:24 +0800

    ✨ feat: add lm studio provider (#4483)
    * 添加 LM Studio
    
    * Update index.ts
    
    * update desc

--- src/const/settings/llm.ts
+++ src/const/settings/llm.ts
@@ -2,6 +2,9 @@ import { ModelProvider } from '@/libs/agent-runtime';
 import { genUserLLMConfig } from '@/utils/genUserLLMConfig';
 
 export const DEFAULT_LLM_CONFIG = genUserLLMConfig({
+  lmstudio: {
+    fetchOnClient: true,
+  },
   ollama: {
     enabled: true,
     fetchOnClient: true,
