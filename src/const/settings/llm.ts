f61b40a14b488f7bc44d6b10fa24e68097d2aa7a
sijinhui <sijinhui@qq.com>
2025-01-18 00:44:03 +0800

    修改一些自用的配置

--- src/const/settings/llm.ts
+++ src/const/settings/llm.ts
@@ -14,13 +14,13 @@ export const DEFAULT_LLM_CONFIG = genUserLLMConfig({
   },
 });
 
-export const DEFAULT_MODEL = 'gpt-4o-mini';
+export const DEFAULT_MODEL = 'gpt-4o';
 
-export const DEFAULT_EMBEDDING_MODEL = 'text-embedding-3-small';
-export const DEFAULT_EMBEDDING_PROVIDER = ModelProvider.OpenAI;
+export const DEFAULT_EMBEDDING_MODEL = 'text-embedding-3-large';
+export const DEFAULT_EMBEDDING_PROVIDER = ModelProvider.Azure;
 
 export const DEFAULT_RERANK_MODEL = 'rerank-english-v3.0';
 export const DEFAULT_RERANK_PROVIDER = 'cohere';
 export const DEFAULT_RERANK_QUERY_MODE = 'full_text';
 
-export const DEFAULT_PROVIDER = ModelProvider.OpenAI;
+export const DEFAULT_PROVIDER = ModelProvider.Azure;
