f2dede3d786ffa8af60c6d40b83f38a0cfea2c21
arvinxx <arvinx@foxmail.com>
2025-01-17 20:27:46 +0800

    fix client fetch

--- src/store/aiInfra/slices/aiProvider/selectors.ts
+++ src/store/aiInfra/slices/aiProvider/selectors.ts
@@ -37,6 +37,17 @@ const isActiveProviderApiKeyNotEmpty = (s: AIProviderStoreState) => {
   return !!vault?.apiKey || !!vault?.accessKeyId || !!vault?.secretAccessKey;
 };
 
+const providerConfigById =
+  (id: string) =>
+  (s: AIProviderStoreState): AiProviderRuntimeConfig | undefined => {
+    if (!id) return undefined;
+
+    return s.aiProviderRuntimeConfig?.[id];
+  };
+
+const isProviderConfigUpdating = (id: string) => (s: AIProviderStoreState) =>
+  s.aiProviderConfigUpdatingIds.includes(id);
+
 /**
  * @description The conditions to enable client fetch
  * 1. If no baseUrl and apikey input, force on Server.
@@ -46,7 +57,7 @@ const isActiveProviderApiKeyNotEmpty = (s: AIProviderStoreState) => {
  */
 const isProviderFetchOnClient =
   (provider: GlobalLLMProviderKey | string) => (s: AIProviderStoreState) => {
-    const config = activeProviderConfig(s);
+    const config = providerConfigById(provider)(s);
 
     // If the provider already disable broswer request in model config, force on Server.
     if (isProviderDisableBroswerRequest(provider)) return false;
@@ -56,8 +67,8 @@ const isProviderFetchOnClient =
       return config?.fetchOnClient;
 
     // 1. If no baseUrl and apikey input, force on Server.
-    const isProviderEndpointNotEmpty = isActiveProviderEndpointNotEmpty(s);
-    const isProviderApiKeyNotEmpty = isActiveProviderApiKeyNotEmpty(s);
+    const isProviderEndpointNotEmpty = !!config?.keyVaults.baseURL;
+    const isProviderApiKeyNotEmpty = !!config?.keyVaults.apiKey;
     if (!isProviderEndpointNotEmpty && !isProviderApiKeyNotEmpty) return false;
 
     // 2. If only contains baseUrl, force on Client
@@ -75,17 +86,6 @@ const providerKeyVaults = (provider: string | undefined) => (s: AIProviderStoreS
 
   return s.aiProviderRuntimeConfig?.[provider]?.keyVaults;
 };
-
-const providerConfigById =
-  (id: string) =>
-  (s: AIProviderStoreState): AiProviderRuntimeConfig | undefined => {
-    if (!id) return undefined;
-
-    return s.aiProviderRuntimeConfig?.[id];
-  };
-
-const isProviderConfigUpdating = (id: string) => (s: AIProviderStoreState) =>
-  s.aiProviderConfigUpdatingIds.includes(id);
 
 export const aiProviderSelectors = {
   activeProviderConfig,
