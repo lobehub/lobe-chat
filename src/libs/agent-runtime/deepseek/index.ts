5f9139a2659b2c2db3508ecbbd9b01a83056df25
sxjeru <sxjeru@gmail.com>
2025-01-20 20:44:38 +0800

    Update index.ts

--- src/libs/agent-runtime/deepseek/index.ts
+++ src/libs/agent-runtime/deepseek/index.ts
@@ -3,6 +3,16 @@ import { LobeOpenAICompatibleFactory } from '../utils/openaiCompatibleFactory';
 
 export const LobeDeepSeekAI = LobeOpenAICompatibleFactory({
   baseURL: 'https://api.deepseek.com/v1',
+  chatCompletion: {
+    handlePayload: ({ model, presence_penalty, ...payload }: ChatStreamPayload) =>
+      ({
+        ...payload,
+        model,
+        ...(model === 'deepseek-reasoner'
+          ? { presence_penalty: undefined }
+          : { presence_penalty }),
+      }) as OpenAI.ChatCompletionCreateParamsStreaming,
+  },
   debug: {
     chatCompletion: () => process.env.DEBUG_DEEPSEEK_CHAT_COMPLETION === '1',
   },
