e23cf442adfe4682d18fe45dc6a1b8ca3474fe4e
Arvin Xu <arvinx@foxmail.com>
2025-01-22 04:58:54 +0000

    improve code

--- src/features/DevPanel/PostgresViewer/usePgTable.ts
+++ src/features/DevPanel/PostgresViewer/usePgTable.ts
@@ -4,18 +4,20 @@ import { tableViewerService } from '@/services/tableViewer';
 import { useGlobalStore } from '@/store/global';
 import { systemStatusSelectors } from '@/store/global/selectors';
 
+const FETCH_TABLES = 'fetch-tables';
+const FETCH_TABLE_COLUMN_KEY = (tableName: string) => ['fetch-table-columns', tableName];
 export const FETCH_TABLE_DATA_KEY = (tableName: string) => ['fetch-table-data', tableName];
 
 export const useFetchTables = () => {
   const isDBInited = useGlobalStore(systemStatusSelectors.isDBInited);
 
-  return useSWR(isDBInited ? 'fetch-tables' : null, () => tableViewerService.getAllTables());
+  return useSWR(isDBInited ? FETCH_TABLES : null, () => tableViewerService.getAllTables());
 };
 
 export const useTableColumns = (tableName?: string) => {
   const isDBInited = useGlobalStore(systemStatusSelectors.isDBInited);
 
-  return useSWR(isDBInited && tableName ? ['fetch-table-columns', tableName] : null, ([, table]) =>
+  return useSWR(isDBInited && tableName ? FETCH_TABLE_COLUMN_KEY(tableName) : null, ([, table]) =>
     tableViewerService.getTableDetails(table),
   );
 };
