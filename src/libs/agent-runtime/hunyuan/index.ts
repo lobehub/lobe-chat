24a49464d00007edc962cd43668e8eb4f12a8036
Zhijie He <hezhijie0327@hotmail.com>
2025-01-23 21:59:06 +0800

    💄 style: fix model fetch match tag error & add model fetch support for Hunyuan

--- src/libs/agent-runtime/hunyuan/index.ts
+++ src/libs/agent-runtime/hunyuan/index.ts
@@ -6,5 +6,22 @@ export const LobeHunyuanAI = LobeOpenAICompatibleFactory({
   debug: {
     chatCompletion: () => process.env.DEBUG_HUNYUAN_CHAT_COMPLETION === '1',
   },
+  models: {
+    transformModel: (m) => {
+      const functionCallKeywords = [
+        'hunyuan-turbo',
+        'hunyuan-pro',
+      ];
+
+      const model = m as unknown as QwenModelCard;
+
+      return {
+        enabled: LOBE_DEFAULT_MODEL_LIST.find((m) => model.id.endsWith(m.id))?.enabled || false,
+        functionCall: functionCallKeywords.some(keyword => model.id.toLowerCase().includes(keyword)) || model.modelCode.toLowerCase().includes('functioncall'),
+        id: model.id,
+        vision: model.modelCode.toLowerCase().includes('vision'),
+      };
+    },
+  },
   provider: ModelProvider.Hunyuan,
 });
