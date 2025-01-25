a12e1a153050ccfbc37db9ebe47c06abdded700d
cy948@ultra7 <cy948@vip.qq.com>
2025-01-25 12:31:29 +0800

    :lipstick: style: use Avatar wrap AuthIcons

--- src/app/(main)/profile/(home)/features/SSOProvidersList/AuthIcons.tsx
+++ src/app/(main)/profile/(home)/features/SSOProvidersList/AuthIcons.tsx
@@ -1,26 +1,38 @@
+import { Avatar } from '@lobehub/ui';
+import {
+  Auth0,
+  Authelia,
+  Authentik,
+  Casdoor,
+  Cloudflare,
+  Github,
+  Logto,
+  MicrosoftEntra,
+  NextAuth,
+  Zitadel,
+} from '@lobehub/ui/icons';
 import React from 'react';
-import { Auth0, Authelia, Authentik, Casdoor, Cloudflare, Github, Logto, MicrosoftEntra, Zitadel, NextAuth } from '@lobehub/ui/icons';
 
 const iconProps = {
-    size: 32,
+  size: 32,
 };
 
 const iconComponents: { [key: string]: React.ElementType } = {
-    auth0: Auth0,
-    authelia: Authelia.Color,
-    authentik: Authentik.Color,
-    casdoor: Casdoor.Color,
-    cloudflare: Cloudflare.Color,
-    github: Github,
-    logto: Logto.Color,
-    'microsoft-entra': MicrosoftEntra.Color,
-    zitadel: Zitadel.Color,
-    default: NextAuth.Color,
+  'auth0': Auth0,
+  'authelia': Authelia.Color,
+  'authentik': Authentik.Color,
+  'casdoor': Casdoor.Color,
+  'cloudflare': Cloudflare.Color,
+  'default': NextAuth.Color,
+  'github': Github,
+  'logto': Logto.Color,
+  'microsoft-entra': MicrosoftEntra.Color,
+  'zitadel': Zitadel.Color,
 };
 
 const AuthIcons = (id: string) => {
-    const IconComponent = iconComponents[id] || iconComponents.default;
-    return <IconComponent {...iconProps} />;
+  const IconComponent = iconComponents[id] || iconComponents.default;
+  return <Avatar avatar={<IconComponent {...iconProps} />} />;
 };
 
 export default AuthIcons;
