8ab4a30b3f8c2082111e66e76c64a3e04c1f9a07
sxjeru <sxjeru@gmail.com>
2025-02-02 19:12:21 +0800

    💄 style: Check siliconcloud API-Key Balance  (#5667)
    * Update siliconcloud.ts
    
    * Update index.ts
    
    * Update index.ts
    
    * Update index.ts
    
    * Update index.ts

--- src/libs/agent-runtime/siliconcloud/index.ts
+++ src/libs/agent-runtime/siliconcloud/index.ts
@@ -1,4 +1,5 @@
-import { ModelProvider } from '../types';
+import { AgentRuntimeErrorType } from '../error';
+import { ChatCompletionErrorPayload, ModelProvider } from '../types';
 import { LobeOpenAICompatibleFactory } from '../utils/openaiCompatibleFactory';
 
 import { LOBE_DEFAULT_MODEL_LIST } from '@/config/aiModels';
@@ -10,6 +11,33 @@ export interface SiliconCloudModelCard {
 export const LobeSiliconCloudAI = LobeOpenAICompatibleFactory({
   baseURL: 'https://api.siliconflow.cn/v1',
   chatCompletion: {
+    handleError: (error: any): Omit<ChatCompletionErrorPayload, 'provider'> | undefined => {
+      let errorResponse: Response | undefined;
+      if (error instanceof Response) {
+        errorResponse = error;
+      } else if ('status' in (error as any)) {
+        errorResponse = error as Response;
+      }
+      if (errorResponse) {
+        if (errorResponse.status === 401) {
+          return {
+            error: errorResponse.status,
+            errorType: AgentRuntimeErrorType.InvalidProviderAPIKey,
+          };
+        }
+
+        if (errorResponse.status === 403) {
+          return {
+            error: errorResponse.status,
+            errorType: AgentRuntimeErrorType.ProviderBizError,
+            message: '请检查 API Key 余额是否充足，或者是否在用未实名的 API Key 访问需要实名的模型。',
+          };
+        }
+      }
+      return {
+        error,
+      };
+    },
     handlePayload: (payload) => {
       return {
         ...payload,
@@ -19,6 +47,10 @@ export const LobeSiliconCloudAI = LobeOpenAICompatibleFactory({
   },
   debug: {
     chatCompletion: () => process.env.DEBUG_SILICONCLOUD_CHAT_COMPLETION === '1',
+  },
+  errorType: {
+    bizError: AgentRuntimeErrorType.ProviderBizError,
+    invalidAPIKey: AgentRuntimeErrorType.InvalidProviderAPIKey,
   },
   models: {
     transformModel: (m) => {
