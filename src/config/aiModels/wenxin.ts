9c28a583d9e552bb69809e6aa437a88994bf1a11
Zhijie He <hezhijie0327@hotmail.com>
2025-02-04 21:15:32 +0800

    💄 style: update model list

--- src/config/aiModels/wenxin.ts
+++ src/config/aiModels/wenxin.ts
@@ -2,6 +2,9 @@ import { AIChatModelCard } from '@/types/aiModel';
 
 const wenxinChatModels: AIChatModelCard[] = [
   {
+    abilities: {
+      functionCall: true,
+    },
     contextWindowTokens: 8192,
     description:
       '百度自研的旗舰级大规模⼤语⾔模型，覆盖海量中英文语料，具有强大的通用能力，可满足绝大部分对话问答、创作生成、插件应用场景要求；支持自动对接百度搜索插件，保障问答信息时效。',
@@ -16,6 +19,9 @@ const wenxinChatModels: AIChatModelCard[] = [
     type: 'chat',
   },
   {
+    abilities: {
+      functionCall: true,
+    },
     contextWindowTokens: 8192,
     description:
       '百度自研的旗舰级大规模⼤语⾔模型，覆盖海量中英文语料，具有强大的通用能力，可满足绝大部分对话问答、创作生成、插件应用场景要求；支持自动对接百度搜索插件，保障问答信息时效。',
@@ -29,6 +35,9 @@ const wenxinChatModels: AIChatModelCard[] = [
     type: 'chat',
   },
   {
+    abilities: {
+      functionCall: true,
+    },
     contextWindowTokens: 128_000,
     description:
       '百度自研的旗舰级大规模⼤语⾔模型，覆盖海量中英文语料，具有强大的通用能力，可满足绝大部分对话问答、创作生成、插件应用场景要求；支持自动对接百度搜索插件，保障问答信息时效。',
@@ -43,6 +52,9 @@ const wenxinChatModels: AIChatModelCard[] = [
     type: 'chat',
   },
   {
+    abilities: {
+      functionCall: true,
+    },
     contextWindowTokens: 8192,
     description:
       '百度自研的旗舰级超大规模⼤语⾔模型，相较ERNIE 3.5实现了模型能力全面升级，广泛适用于各领域复杂任务场景；支持自动对接百度搜索插件，保障问答信息时效。',
@@ -57,6 +69,9 @@ const wenxinChatModels: AIChatModelCard[] = [
     type: 'chat',
   },
   {
+    abilities: {
+      functionCall: true,
+    },
     contextWindowTokens: 8192,
     description:
       '百度自研的旗舰级超大规模⼤语⾔模型，相较ERNIE 3.5实现了模型能力全面升级，广泛适用于各领域复杂任务场景；支持自动对接百度搜索插件，保障问答信息时效。',
@@ -70,6 +85,9 @@ const wenxinChatModels: AIChatModelCard[] = [
     type: 'chat',
   },
   {
+    abilities: {
+      functionCall: true,
+    },
     contextWindowTokens: 8192,
     description:
       '百度自研的旗舰级超大规模⼤语⾔模型，综合效果表现出色，广泛适用于各领域复杂任务场景；支持自动对接百度搜索插件，保障问答信息时效。相较于ERNIE 4.0在性能表现上更优秀',
@@ -84,6 +102,9 @@ const wenxinChatModels: AIChatModelCard[] = [
     type: 'chat',
   },
   {
+    abilities: {
+      functionCall: true,
+    },
     contextWindowTokens: 128_000,
     description:
       '百度自研的旗舰级超大规模⼤语⾔模型，综合效果表现出色，广泛适用于各领域复杂任务场景；支持自动对接百度搜索插件，保障问答信息时效。相较于ERNIE 4.0在性能表现上更优秀',
@@ -98,6 +119,9 @@ const wenxinChatModels: AIChatModelCard[] = [
     type: 'chat',
   },
   {
+    abilities: {
+      functionCall: true,
+    },
     contextWindowTokens: 8192,
     description:
       '百度自研的旗舰级超大规模⼤语⾔模型，综合效果表现出色，广泛适用于各领域复杂任务场景；支持自动对接百度搜索插件，保障问答信息时效。相较于ERNIE 4.0在性能表现上更优秀',
@@ -111,6 +135,23 @@ const wenxinChatModels: AIChatModelCard[] = [
     type: 'chat',
   },
   {
+    contextWindowTokens: 8192,
+    description:
+      'ERNIE Lite是百度自研的轻量级大语言模型，兼顾优异的模型效果与推理性能，适合低算力AI加速卡推理使用。',
+    displayName: 'ERNIE Lite 8K',
+    enabled: true,
+    id: 'ernie-lite-8k',
+    pricing: {
+      currency: 'CNY',
+      input: 0,
+      output: 0,
+    },
+    type: 'chat',
+  },
+  {
+    abilities: {
+      functionCall: true,
+    },
     contextWindowTokens: 128_000,
     description:
       '百度自研的轻量级大语言模型，兼顾优异的模型效果与推理性能，效果比ERNIE Lite更优，适合低算力AI加速卡推理使用。',
@@ -121,6 +162,20 @@ const wenxinChatModels: AIChatModelCard[] = [
       currency: 'CNY',
       input: 0.2,
       output: 0.4,
+    },
+    type: 'chat',
+  },
+  {
+    contextWindowTokens: 8192,
+    description:
+      'ERNIE Tiny是百度自研的超高性能大语言模型，部署与精调成本在文心系列模型中最低。',
+    displayName: 'ERNIE Tiny 8K',
+    enabled: true,
+    id: 'ernie-tiny-8k',
+    pricing: {
+      currency: 'CNY',
+      input: 0,
+      output: 0,
     },
     type: 'chat',
   },
@@ -160,6 +215,48 @@ const wenxinChatModels: AIChatModelCard[] = [
     pricing: {
       currency: 'CNY',
       input: 4,
+      output: 8,
+    },
+    type: 'chat',
+  },
+  {
+    contextWindowTokens: 8192,
+    description:
+      '2024年5月23日发布的版本，支持8K上下文长度，在情节演绎和规则化文本等场景下指令遵循能力更强',
+    displayName: 'ERNIE Character Fiction 8K',
+    id: 'ernie-char-fiction-8k',
+    pricing: {
+      currency: 'CNY',
+      input: 4,
+      output: 8,
+    },
+    type: 'chat',
+  },
+  {
+    contextWindowTokens: 65_536,
+    description:
+      'DeepSeek-V3 为杭州深度求索人工智能基础技术研究有限公司自研的 MoE 模型，其多项评测成绩突出，在主流榜单中位列开源模型榜首。V3 相比 V2.5 模型生成速度实现 3 倍提升，为用户带来更加迅速流畅的使用体验。',
+    displayName: 'DeepSeek V3',
+    id: 'deepseek-v3',
+    pricing: {
+      currency: 'CNY',
+      input: 0.8,
+      output: 1.6,
+    },
+    type: 'chat',
+  },
+  {
+    abilities: {
+      reasoning: true,
+    },
+    contextWindowTokens: 65_536,
+    description:
+      'DeepSeek-R1 在后训练阶段大规模使用了强化学习技术，在仅有极少标注数据的情况下，极大提升了模型推理能力。在数学、代码、自然语言推理等任务上，性能比肩 OpenAI o1 正式版。',
+    displayName: 'DeepSeek R1',
+    id: 'deepseek-r1',
+    pricing: {
+      currency: 'CNY',
+      input: 2,
       output: 8,
     },
     type: 'chat',
