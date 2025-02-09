9fa0f257e06e5843b512cb38a0859c19eb88538c
arvinxx <arvinx@foxmail.com>
2025-02-09 11:12:01 +0800

    clean log

--- src/utils/server/geo.ts
+++ src/utils/server/geo.ts
@@ -5,7 +5,6 @@ import { NextRequest } from 'next/server';
 export const parseDefaultThemeFromCountry = (request: NextRequest) => {
   // 1. 从请求头中获取国家代码
   const geo = geolocation(request);
-  console.log('vercel geo', geo);
 
   const countryCode =
     geo?.country ||
@@ -23,7 +22,7 @@ export const parseDefaultThemeFromCountry = (request: NextRequest) => {
   // 如果找不到国家信息或该国家没有时区信息，返回 light 主题
   if (!country?.timezones?.length) return 'light';
 
-  // 3. 获取该国家的第一个时区下的当前时间
+  // 3. 获取该国家的第一个 时区下的当前时间
   const localTime = new Date().toLocaleString('en-US', {
     hour: 'numeric',
     hour12: false,
@@ -32,9 +31,9 @@ export const parseDefaultThemeFromCountry = (request: NextRequest) => {
 
   // 4. 解析小时数并确定主题
   const localHour = parseInt(localTime);
-  console.log(
-    `[theme] Country: ${countryCode}, Timezone: ${country.timezones[0]}, LocalHour: ${localHour}`,
-  );
+  // console.log(
+  //   `[theme] Country: ${countryCode}, Timezone: ${country.timezones[0]}, LocalHour: ${localHour}`,
+  // );
 
   return localHour >= 6 && localHour < 18 ? 'light' : 'dark';
 };
