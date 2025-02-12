de935e785c5c572b3683221b257ccb7ec8a828e7
Zhijie He <hezhijie0327@hotmail.com>
2025-02-12 21:21:57 +0800

    ✨ feat: add model fetch support for Tencent Cloud

--- src/libs/agent-runtime/tencentcloud/index.ts
+++ src/libs/agent-runtime/tencentcloud/index.ts
@@ -1,10 +1,46 @@
 import { ModelProvider } from '../types';
 import { LobeOpenAICompatibleFactory } from '../utils/openaiCompatibleFactory';
 
+import { LOBE_DEFAULT_MODEL_LIST } from '@/config/aiModels';
+
+export interface TencentCloudModelCard {
+  id: string;
+}
+
 export const LobeTencentCloudAI = LobeOpenAICompatibleFactory({
   baseURL: 'https://api.lkeap.cloud.tencent.com/v1',
   debug: {
     chatCompletion: () => process.env.DEBUG_TENCENT_CLOUD_CHAT_COMPLETION === '1',
+  },
+  models: {
+    transformModel: (m) => {
+      const functionCallKeywords = [
+        'deepseek-v3',
+      ];
+
+      const reasoningKeywords = [
+        'deepseek-r1',
+      ];
+
+      const model = m as unknown as TencentCloudModelCard;
+
+      const knownModel = LOBE_DEFAULT_MODEL_LIST.find((m) => model.id === m.id);
+
+      return {
+        contextWindowTokens: knownModel?.contextWindowTokens ?? undefined,
+        displayName: knownModel?.displayName ?? undefined,
+        enabled: knownModel?.enabled || false,
+        functionCall:
+          functionCallKeywords.some(keyword => model.id.toLowerCase().includes(keyword))
+          || knownModel?.abilities?.functionCall
+          || false,
+        id: model.id,
+        reasoning:
+          reasoningKeywords.some(keyword => model.id.toLowerCase().includes(keyword))
+          || knownModel?.abilities?.reasoning
+          || false,
+      };
+    },
   },
   provider: ModelProvider.TencentCloud,
 });
