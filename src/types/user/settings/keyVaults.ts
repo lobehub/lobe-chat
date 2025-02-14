4f827cd1cc9dccd26cd2b461d80fd40f240467d9
Zhijie He <hezhijie0327@hotmail.com>
2025-02-14 21:48:36 +0800

    ✨ feat: add Nvidia NIM provider support (#6142)
    * ✨ feat: add Nvidia NIM provider support
    
    * 🐛 fix: fix build error
    
    * 🔨 chore: sort code
    
    * 💄 style: update model list
    
    * 🐛 fix: fix ci error

--- src/types/user/settings/keyVaults.ts
+++ src/types/user/settings/keyVaults.ts
@@ -50,6 +50,7 @@ export interface UserKeyVaults {
   mistral?: OpenAICompatibleKeyVault;
   moonshot?: OpenAICompatibleKeyVault;
   novita?: OpenAICompatibleKeyVault;
+  nvidia?: OpenAICompatibleKeyVault;
   ollama?: OpenAICompatibleKeyVault;
   openai?: OpenAICompatibleKeyVault;
   openrouter?: OpenAICompatibleKeyVault;
