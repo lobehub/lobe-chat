f994aaa8c213945b67f81a5e46c5976044d7bcb0
sijinhui <62741773+sijinhui@users.noreply.github.com>
2025-02-14 14:43:45 +0800

    merge

--- src/server/utils/url.ts
+++ src/server/utils/url.ts
@@ -4,6 +4,6 @@ const isVercelPreview = process.env.VERCEL === '1' && process.env.VERCEL_ENV !==
 
 const vercelPreviewUrl = `https://${process.env.VERCEL_URL}`;
 
-const siteUrl = isVercelPreview ? vercelPreviewUrl : 'https://lobechat.com';
+const siteUrl = isVercelPreview ? vercelPreviewUrl : 'https://si.icu';
 
 export const getCanonicalUrl = (...paths: string[]) => urlJoin(siteUrl, ...paths);
