2b8c841245d31cb7ea09338ccc186fb7608fabf3
Arvin Xu <arvinx@foxmail.com>
2025-02-16 16:39:28 +0800

    💄 style: fix mobile agent settings not show correctly (#6203)
    * fix agent setting mobile
    
    * improve code
    
    * improve style for desktop
    
    * fix inbox issue
    
    * fix chat page issue
    
    * fix desktop style
    
    * refactor the store code
    
    * fix navigation issue

--- src/app/[variants]/(main)/chat/(workspace)/@conversation/features/ChatInput/Mobile/index.tsx
+++ src/app/[variants]/(main)/chat/(workspace)/@conversation/features/ChatInput/Mobile/index.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import { Skeleton } from 'antd';
 import { useTheme } from 'antd-style';
 import { TextAreaRef } from 'antd/es/input/TextArea';
 import { memo, useRef, useState } from 'react';
+import { Flexbox } from 'react-layout-kit';
 
 import ActionBar from '@/features/ChatInput/ActionBar';
 import { ActionKeys } from '@/features/ChatInput/ActionBar/config';
@@ -12,6 +14,7 @@ import { useSendMessage } from '@/features/ChatInput/useSend';
 import { useChatStore } from '@/store/chat';
 import { chatSelectors } from '@/store/chat/selectors';
 
+import { useInitAgentConfig } from '../../../../_layout/useInitAgentConfig';
 import Files from './Files';
 import InputArea from './InputArea';
 import SendButton from './Send';
@@ -33,6 +36,7 @@ const MobileChatInput = memo(() => {
   const ref = useRef<TextAreaRef>(null);
   const [expand, setExpand] = useState<boolean>(false);
   const { send: sendMessage, canSend } = useSendMessage();
+  const { isLoading } = useInitAgentConfig();
 
   const [loading, value, onInput, onStop] = useChatStore((s) => [
     chatSelectors.isAIGenerating(s),
@@ -63,15 +67,21 @@ const MobileChatInput = memo(() => {
         <SendButton disabled={!canSend} loading={loading} onSend={sendMessage} onStop={onStop} />
       }
       topAddons={
-        <>
-          <Files />
-          <ActionBar
-            leftActions={defaultLeftActions}
-            padding={'0 8px'}
-            rightActions={defaultRightActions}
-            rightAreaStartRender={<SaveTopic mobile />}
-          />
-        </>
+        isLoading ? (
+          <Flexbox paddingInline={8}>
+            <Skeleton.Button active block size={'small'} />
+          </Flexbox>
+        ) : (
+          <>
+            <Files />
+            <ActionBar
+              leftActions={defaultLeftActions}
+              padding={'0 8px'}
+              rightActions={defaultRightActions}
+              rightAreaStartRender={<SaveTopic mobile />}
+            />
+          </>
+        )
       }
       value={value}
     />
--- src/app/[variants]/(main)/chat/settings/page.tsx
+++ src/app/[variants]/(main)/chat/settings/page.tsx
@@ -1,7 +1,97 @@
-import EditPage from './features/EditPage';
+'use client';
 
-const Page = () => {
-  return <EditPage />;
-};
+import { TabsNav } from '@lobehub/ui';
+import isEqual from 'fast-deep-equal';
+import { memo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
-export default Page;
+import PageTitle from '@/components/PageTitle';
+import { INBOX_SESSION_ID } from '@/const/session';
+import { AgentSettingsProvider } from '@/features/AgentSetting';
+import AgentChat from '@/features/AgentSetting/AgentChat';
+import AgentMeta from '@/features/AgentSetting/AgentMeta';
+import AgentModal from '@/features/AgentSetting/AgentModal';
+import AgentPlugin from '@/features/AgentSetting/AgentPlugin';
+import AgentPrompt from '@/features/AgentSetting/AgentPrompt';
+import AgentTTS from '@/features/AgentSetting/AgentTTS';
+import { useAgentStore } from '@/store/agent';
+import { agentSelectors } from '@/store/agent/selectors';
+import { ChatSettingsTabs } from '@/store/global/initialState';
+import { featureFlagsSelectors, useServerConfigStore } from '@/store/serverConfig';
+import { useSessionStore } from '@/store/session';
+import { sessionMetaSelectors } from '@/store/session/selectors';
+
+const EditPage = memo(() => {
+  const { t } = useTranslation('setting');
+  const [tab, setTab] = useState(ChatSettingsTabs.Prompt);
+
+  const [id, updateAgentMeta, title] = useSessionStore((s) => [
+    s.activeId,
+    s.updateSessionMeta,
+    sessionMetaSelectors.currentAgentTitle(s),
+  ]);
+  const [useFetchAgentConfig, updateAgentConfig] = useAgentStore((s) => [
+    s.useFetchAgentConfig,
+    s.updateAgentConfig,
+  ]);
+
+  const config = useAgentStore(agentSelectors.currentAgentConfig, isEqual);
+  const meta = useSessionStore(sessionMetaSelectors.currentAgentMeta, isEqual);
+
+  const { isLoading } = useFetchAgentConfig(id);
+  const { enablePlugins } = useServerConfigStore(featureFlagsSelectors);
+
+  return (
+    <>
+      <PageTitle title={t('header.sessionWithName', { name: title })} />
+
+      <TabsNav
+        items={[
+          {
+            key: ChatSettingsTabs.Prompt,
+            label: t('settingAgent.prompt.title'),
+          },
+          (id !== INBOX_SESSION_ID && {
+            key: ChatSettingsTabs.Meta,
+            label: t('settingAgent.title'),
+          }) as any,
+          {
+            key: ChatSettingsTabs.Chat,
+            label: t('settingChat.title'),
+          },
+          {
+            key: ChatSettingsTabs.Modal,
+            label: t('settingModel.title'),
+          },
+          {
+            key: ChatSettingsTabs.TTS,
+            label: t('settingTTS.title'),
+          },
+          (enablePlugins && {
+            key: ChatSettingsTabs.Plugin,
+            label: t('settingPlugin.title'),
+          }) as any,
+        ]}
+        onChange={(value) => setTab(value as ChatSettingsTabs)}
+        variant={'compact'}
+      />
+      <AgentSettingsProvider
+        config={config}
+        id={id}
+        loading={isLoading}
+        meta={meta}
+        onConfigChange={updateAgentConfig}
+        onMetaChange={updateAgentMeta}
+      >
+        {tab === ChatSettingsTabs.Prompt && <AgentPrompt modal />}
+        {tab === ChatSettingsTabs.Meta && <AgentMeta />}
+        {tab === ChatSettingsTabs.Chat && <AgentChat />}
+        {tab === ChatSettingsTabs.Modal && <AgentModal />}
+        {tab === ChatSettingsTabs.TTS && <AgentTTS />}
+        {tab === ChatSettingsTabs.Plugin && <AgentPlugin />}
+      </AgentSettingsProvider>
+    </>
+  );
+});
+
+export default EditPage;
