620e82e99745872d44bbc4aad337d8fe4a302cd5
cy948@ultra7 <cy948@vip.qq.com>
2025-02-16 15:27:09 +0800

    :sparkles: feat: support backend token refresh

--- src/libs/next-auth/auth.config.ts
+++ src/libs/next-auth/auth.config.ts
@@ -6,7 +6,7 @@ import { ssoProviders } from './sso-providers';
 
 export const initSSOProviders = () => {
   return authEnv.NEXT_PUBLIC_ENABLE_NEXT_AUTH
-    ? authEnv.NEXT_AUTH_SSO_PROVIDERS.split(/[,，]/).map((provider) => {
+    ? authEnv.NEXT_AUTH_SSO_PROVIDERS.split(/[,，]/).map((provider: string) => {
         const validProvider = ssoProviders.find((item) => item.id === provider.trim());
 
         if (validProvider) return validProvider.provider;
@@ -20,11 +20,70 @@ export const initSSOProviders = () => {
 export default {
   callbacks: {
     // Note: Data processing order of callback: authorize --> jwt --> session
-    async jwt({ token, user }) {
+    async jwt({ token, user, account, profile }) {
       // ref: https://authjs.dev/guides/extending-the-session#with-jwt
       if (user?.id) {
         token.userId = user?.id;
       }
+
+      // If the refresh token is enabled, try refresh token to the JWT
+      // Only Casdoor & logto are supported
+      // ref: https://authjs.dev/guides/refresh-token-rotation
+      if (authEnv?.NEXT_AUTH_SSO_ENABLE_REFRESH_TOKEN) {
+        if (account) {
+          // First-time login, save the `access_token`, its expiry and the `refresh_token`
+          return {
+            ...token,
+            access_token: account.access_token,
+            expires_at: account.expires_at,
+            iss: profile?.iss,
+            provider: account.provider,
+            refresh_token: account.refresh_token,
+          };
+          // @ts-ignore: type infer error
+        } else if (token?.expires_at && Date.now() < token.expires_at * 1000) {
+          // Subsequent logins, but the `access_token` is still valid
+          return token;
+        } else {
+          // Subsequent logins, but the `access_token` has expired, try to refresh it
+          if (!token?.refresh_token) throw new TypeError('Missing refresh_token');
+          if (!token?.iss) throw new TypeError('Missing issuer');
+          if (!token?.provider) throw new TypeError('Missing provider');
+
+          try {
+            const { default: p } = await import(`./sso-providers/${token.provider}`);
+
+            if (!p?.refreshToken)
+              throw new TypeError(
+                `Provider ${token.provider} does not support backend token refresh`,
+              );
+
+            const newTokens = (await p.refreshToken(token.iss, token.refresh_token)) as {
+              access_token: string;
+              expires_in: number;
+              refresh_token?: string;
+            };
+
+            return {
+              ...token,
+              access_token: newTokens.access_token,
+              expires_at: Math.floor(Date.now() / 1000 + newTokens.expires_in),
+              // Preserve the refresh token for two reasons:
+              // - Some providers (e.g. logto) only issue refresh tokens once,
+              //   so preserve if we did not get a new one
+              // - If user enable token rotation,
+              //   we need to use the new token
+              refresh_token: newTokens?.refresh_token ?? token.refresh_token,
+            };
+          } catch (error) {
+            console.error('Error refreshing access_token', error);
+            // If we fail to refresh the token, return an error so we can handle it on the page
+            token.error = 'RefreshTokenError';
+            return token;
+          }
+        }
+      }
+
       return token;
     },
     async session({ session, token, user }) {
@@ -35,6 +94,10 @@ export default {
         } else {
           session.user.id = (token.userId ?? session.user.id) as string;
         }
+      }
+      if (token.error) {
+        // @ts-ignore: type infer error
+        session.error = token.error;
       }
       return session;
     },
--- src/server/context.ts
+++ src/server/context.ts
@@ -57,6 +57,12 @@ export const createContext = async (request: NextRequest): Promise<Context> => {
       const { default: NextAuthEdge } = await import('@/libs/next-auth/edge');
 
       const session = await NextAuthEdge.auth();
+
+      if (session?.error === 'RefreshTokenError') {
+        // await NextAuthEdge.signIn()
+        throw new Error('RefreshTokenError');
+      }
+
       if (session && session?.user?.id) {
         auth = session.user;
         userId = session.user.id;
