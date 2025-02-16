d77e885a95defe4f1a58ce3fe943d6863ec85b44
arvinxx <arvinx@foxmail.com>
2025-02-16 23:57:45 +0800

    add ability

--- src/config/aiModels/volcengine.ts
+++ src/config/aiModels/volcengine.ts
@@ -2,6 +2,9 @@ import { AIChatModelCard } from '@/types/aiModel';
 
 const doubaoChatModels: AIChatModelCard[] = [
   {
+    abilities: {
+      reasoning: true,
+    },
     contextWindowTokens: 65_536,
     description:
       '拥有极致的响应速度，更好的性价比，为客户不同场景提供更灵活的选择。支持 4k 上下文窗口的推理和精调。',
@@ -11,6 +14,9 @@ const doubaoChatModels: AIChatModelCard[] = [
     type: 'chat',
   },
   {
+    abilities: {
+      functionCall: true,
+    },
     contextWindowTokens: 65_536,
     description:
       'DeepSeek-V3 是一款由深度求索公司自研的MoE模型。DeepSeek-V3 多项评测成绩超越了 Qwen2.5-72B 和 Llama-3.1-405B 等其他开源模型，并在性能上和世界顶尖的闭源模型 GPT-4o 以及 Claude-3.5-Sonnet 不分伯仲。',
