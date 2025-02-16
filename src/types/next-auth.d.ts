1bc0df728460b358eb8f4815c1d491dc0855b46b
cy948@ultra7 <cy948@vip.qq.com>
2025-02-16 15:27:09 +0800

    :sparkles: feat: support backend token refresh

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -5,6 +5,7 @@ declare module 'next-auth' {
    * Returned by `useSession`, `auth`, contains information about the active session.
    */
   interface Session {
+    error?: 'RefreshTokenError';
     user: {
       firstName?: string;
     } & DefaultSession['user'];
@@ -21,6 +22,12 @@ declare module 'next-auth' {
 declare module '@auth/core/jwt' {
   /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
   interface JWT {
+    access_token?: string;
+    error?: 'RefreshTokenError';
+    expires?: number;
+    iss?: string;
+    provider?: string;
+    refresh_token?: string;
     userId: string;
   }
 }
