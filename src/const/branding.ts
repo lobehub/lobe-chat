a49fdad20eb3c81b8fb7c71a3146478b21bd6567
sijinhui <sijinhui@qq.com>
2025-02-18 23:38:49 +0800

    优化资源

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -5,7 +5,7 @@
 export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
 
 export const BRANDING_NAME = 'LobeChat';
-export const BRANDING_LOGO_URL = '';
+export const BRANDING_LOGO_URL = '/favicon.ico';
 
 export const ORG_NAME = 'LobeHub';
 
