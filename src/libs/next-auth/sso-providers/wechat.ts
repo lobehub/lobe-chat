323cfbfdaa307fd7bf6139b60fbe9c454b9b09a9
xiangnan <280145668@qq.com>
2025-02-21 10:09:59 +0800

    fix: wechat text/plain error

--- src/libs/next-auth/sso-providers/wechat.ts
+++ src/libs/next-auth/sso-providers/wechat.ts
@@ -9,6 +9,17 @@ const provider = {
     clientId: process.env.AUTH_WECHAT_ID,
     clientSecret: process.env.AUTH_WECHAT_SECRET,
     platformType: 'WebsiteApp',
+    token: {
+      url: "https://api.weixin.qq.com/sns/oauth2/access_token",
+      params: { appid: clientId, secret: clientSecret },
+      async conform(response) {
+        const data = await response.json()
+        console.log('wechat data:',data)
+        return new Response( JSON.stringify({ ...data, token_type: "bearer" }), {
+          headers: { 'Content-Type': 'application/json' }
+        })
+      },
+    },
     profile: (profile) => {
       return {
         email: null,
