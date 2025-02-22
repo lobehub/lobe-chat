7b3ba453add8826754210a9f0f271931c26e80d7
Verber <hrfcmj@sina.com>
2025-02-22 14:09:10 +0800

    fix fetch on client check status display, and sort props

--- src/app/[variants]/(main)/settings/provider/features/ProviderConfig/index.tsx
+++ src/app/[variants]/(main)/settings/provider/features/ProviderConfig/index.tsx
@@ -258,7 +258,7 @@ const ProviderConfig = memo<ProviderConfigProps>(
       children: isLoading ? (
         <Skeleton.Button active className={styles.switchLoading} />
       ) : (
-        <Switch disabled={configUpdating} checked={isFetchOnClient} />
+        <Switch checked={isFetchOnClient} disabled={configUpdating} />
       ),
       desc: t('providerModels.config.fetchOnClient.desc'),
       label: t('providerModels.config.fetchOnClient.title'),
