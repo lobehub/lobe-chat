b00353fadc3078529f311a0f12509c56b6eaa244
sijinhui <sijinhui@qq.com>
2025-02-26 22:14:04 +0800

    merge

--- src/libs/agent-runtime/openai/index.ts
+++ src/libs/agent-runtime/openai/index.ts
@@ -1,7 +1,7 @@
-import type { ChatModelCard } from '@/types/llm';
-
 import { ChatStreamPayload, ModelProvider, OpenAIChatMessage } from '../types';
 import { LobeOpenAICompatibleFactory } from '../utils/openaiCompatibleFactory';
+
+import type { ChatModelCard } from '@/types/llm';
 
 export interface OpenAIModelCard {
   id: string;
@@ -9,7 +9,10 @@ export interface OpenAIModelCard {
 
 export const pruneReasoningPayload = (payload: ChatStreamPayload) => {
   // TODO: 临时写法，后续要重构成 model card 展示配置
-  const disableStreamModels = new Set(['o1', 'o1-2024-12-17']);
+  const disableStreamModels = new Set([
+    'o1',
+    'o1-2024-12-17'
+  ]);
   const systemToUserModels = new Set([
     'o1-preview',
     'o1-preview-2024-09-12',
@@ -46,11 +49,6 @@ export const LobeOpenAI = LobeOpenAICompatibleFactory({
         return pruneReasoningPayload(payload) as any;
       }
 
-      // oneapi 下的claude也需要设置temperature为1
-      if (model.startsWith('claude')) {
-        return { ...payload, temperature: 1, top_p: null }
-      }
-
       return { ...payload, stream: payload.stream ?? true };
     },
   },
@@ -60,40 +58,46 @@ export const LobeOpenAI = LobeOpenAICompatibleFactory({
   models: async ({ client }) => {
     const { LOBE_DEFAULT_MODEL_LIST } = await import('@/config/aiModels');
 
-    const functionCallKeywords = ['gpt-4', 'gpt-3.5', 'o3-mini'];
+    const functionCallKeywords = [
+      'gpt-4',
+      'gpt-3.5',
+      'o3-mini',
+    ];
 
-    const visionKeywords = ['gpt-4o', 'vision'];
+    const visionKeywords = [
+      'gpt-4o',
+      'vision',
+    ];
 
-    const reasoningKeywords = ['o1', 'o3'];
+    const reasoningKeywords = [
+      'o1',
+      'o3',
+    ];
 
-    const modelsPage = (await client.models.list()) as any;
+    const modelsPage = await client.models.list() as any;
     const modelList: OpenAIModelCard[] = modelsPage.data;
 
     return modelList
       .map((model) => {
-        const knownModel = LOBE_DEFAULT_MODEL_LIST.find(
-          (m) => model.id.toLowerCase() === m.id.toLowerCase(),
-        );
+        const knownModel = LOBE_DEFAULT_MODEL_LIST.find((m) => model.id.toLowerCase() === m.id.toLowerCase());
 
         return {
           contextWindowTokens: knownModel?.contextWindowTokens ?? undefined,
           displayName: knownModel?.displayName ?? undefined,
           enabled: knownModel?.enabled || false,
           functionCall:
-            (functionCallKeywords.some((keyword) => model.id.toLowerCase().includes(keyword)) &&
-              !model.id.toLowerCase().includes('audio')) ||
-            knownModel?.abilities?.functionCall ||
-            false,
+            functionCallKeywords.some(keyword => model.id.toLowerCase().includes(keyword)) && !model.id.toLowerCase().includes('audio')
+            || knownModel?.abilities?.functionCall
+            || false,
           id: model.id,
           reasoning:
-            reasoningKeywords.some((keyword) => model.id.toLowerCase().includes(keyword)) ||
-            knownModel?.abilities?.reasoning ||
-            false,
+            reasoningKeywords.some(keyword => model.id.toLowerCase().includes(keyword))
+            || knownModel?.abilities?.reasoning
+            || false,
           vision:
-            (visionKeywords.some((keyword) => model.id.toLowerCase().includes(keyword)) &&
-              !model.id.toLowerCase().includes('audio')) ||
-            knownModel?.abilities?.vision ||
-            false,
+            visionKeywords.some(keyword => model.id.toLowerCase().includes(keyword)) && !model.id.toLowerCase().includes('audio')
+            || knownModel?.abilities?.vision
+            || false,
         };
       })
       .filter(Boolean) as ChatModelCard[];
