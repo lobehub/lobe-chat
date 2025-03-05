e35d373c3a103cdb17dee4be385b440da24b12bf
Jamie Stivala <jamiestivala@gmail.com>
2025-03-05 11:56:55 +0100

    Removed NPM Releases

--- .releaserc.js
+++ .releaserc.js
@@ -1,10 +1,15 @@
 const config = require('@lobehub/lint').semanticRelease;
 
-config.plugins.push([
-  '@semantic-release/exec',
-  {
-    prepareCmd: 'npm run workflow:changelog',
-  },
-]);
+// Remove NPM publishing by excluding "@semantic-release/npm" plugin
+// Add GitHub Releases plugin if not already included
+config.plugins.push(
+  [
+    '@semantic-release/exec',
+    {
+      prepareCmd: 'npm run workflow:changelog',
+    },
+  ],
+  '@semantic-release/github', // Ensure GitHub releases are enabled
+);
 
 module.exports = config;
