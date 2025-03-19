b355aa0db185d60eeab32a1232dd7b18dccb840f
PandoCheng <107836498+PandoCheng@users.noreply.github.com>
2025-03-20 02:58:24 +0800

    Update branding.ts

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -1,13 +1,9 @@
-// the code below can only be modified with commercial license
-// if you want to use it in the commercial usage
-// please contact us for more information: hello@lobehub.com
+export const LOBE_CHAT_CLOUD = 'DoDoChat Cloud';
 
-export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
-
-export const BRANDING_NAME = 'LobeChat';
+export const BRANDING_NAME = 'DoDoChat';
 export const BRANDING_LOGO_URL = '';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = 'PandoRun';
 
 export const BRANDING_URL = {
   help: undefined,
