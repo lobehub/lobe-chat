da928c78dcb42e9096edd2e0b8f1b0be5732a53f
Jamie Stivala <jamiestivala@gmail.com>
2025-03-20 14:21:16 +0100

    Updated branding to reflect JA

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -2,15 +2,15 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
+export const LOBE_CHAT_CLOUD = 'OneJA AI';
 
-export const BRANDING_NAME = 'LobeChat';
+export const BRANDING_NAME = 'OneJA AI';
 export const BRANDING_LOGO_URL = '';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = 'JA Worldwide';
 
 export const BRANDING_URL = {
-  help: undefined,
-  privacy: undefined,
-  terms: undefined,
+  help: 'mailto:support@jaworldwide.org',
+  privacy: 'https://www.jaworldwide.org/terms',
+  terms: 'https://www.jaworldwide.org/terms',
 };
