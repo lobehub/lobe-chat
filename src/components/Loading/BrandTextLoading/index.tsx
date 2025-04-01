2a75492dd0da5b744c4afbec6f8c2cdbb6250542
arvinxx <arvinx@foxmail.com>
2025-04-01 14:21:19 +0800

    update branding loading

--- src/components/Loading/BrandTextLoading/index.tsx
+++ src/components/Loading/BrandTextLoading/index.tsx
@@ -1,4 +1,4 @@
-import { BrandLoading, LobeChatText } from '@lobehub/ui/brand';
+import { BrandLoading, LobeHubText } from '@lobehub/ui/brand';
 import { Center } from 'react-layout-kit';
 
 import { isCustomBranding } from '@/const/version';
@@ -10,7 +10,7 @@ export default () => {
 
   return (
     <Center height={'100%'} width={'100%'}>
-      <BrandLoading size={40} style={{ opacity: 0.6 }} text={LobeChatText} />
+      <BrandLoading size={40} style={{ opacity: 0.6 }} text={LobeHubText} />
     </Center>
   );
 };
