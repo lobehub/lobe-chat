c138eedb1ea00557605e848cbbf9977eb4094dc8
BrandonStudio <55647556+BrandonStudio@users.noreply.github.com>
2025-04-09 04:40:10 +0000

    Modify test config

--- tests/setup-db.ts
+++ tests/setup-db.ts
@@ -2,3 +2,12 @@
 import * as dotenv from 'dotenv';
 
 dotenv.config();
+
+process.env.TEST_SERVER_DB = '1';
+
+// process.env.DATABASE_TEST_URL = 'postgresql://postgres:postgres@localhost:5432/postgres';
+process.env.DATABASE_DRIVER = 'node';
+process.env.NEXT_PUBLIC_SERVICE_MODE = 'server';
+process.env.KEY_VAULTS_SECRET = 'LA7n9k3JdEcbSgml2sxfw+4TV1AzaaFU5+R176aQz4s=';
+process.env.S3_PUBLIC_DOMAIN = 'https://example.com';
+process.env.APP_URL = 'https://home.com';
--- tests/setup.ts
+++ tests/setup.ts
@@ -33,3 +33,13 @@ theme.defaultConfig.hashed = false;
 
 // 将 React 设置为全局变量，这样就不需要在每个测试文件中导入它了
 (global as any).React = React;
+
+// ======= Temporary env modification for test =======
+process.env.TEST_SERVER_DB = undefined;
+
+process.env.DATABASE_TEST_URL = undefined;
+process.env.DATABASE_DRIVER = undefined;
+process.env.NEXT_PUBLIC_SERVICE_MODE = undefined;
+process.env.KEY_VAULTS_SECRET = undefined;
+process.env.S3_PUBLIC_DOMAIN = undefined;
+process.env.APP_URL = undefined;
