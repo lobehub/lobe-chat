35699bfdedd0a29156560a0249901f3e70667f6d
sxjeru <sxjeru@gmail.com>
2025-04-10 10:52:45 +0800

    Update azureai.ts

--- src/config/aiModels/azureai.ts
+++ src/config/aiModels/azureai.ts
@@ -52,7 +52,7 @@ const azureChatModels: AIChatModelCard[] = [
     contextWindowTokens: 200_000,
     description:
       'o3-mini 是我们最新的小型推理模型，在与 o1-mini 相同的成本和延迟目标下提供高智能。',
-    displayName: 'OpenAI o3-mini',
+    displayName: 'o3-mini',
     id: 'o3-mini',
     pricing: {
       cachedInput: 0.55,
@@ -69,7 +69,7 @@ const azureChatModels: AIChatModelCard[] = [
     contextWindowTokens: 128_000,
     description:
       'o1-mini是一款针对编程、数学和科学应用场景而设计的快速、经济高效的推理模型。该模型具有128K上下文和2023年10月的知识截止日期。',
-    displayName: 'OpenAI o1-mini',
+    displayName: 'o1-mini',
     id: 'o1-mini',
     pricing: {
       cachedInput: 0.55,
@@ -86,7 +86,7 @@ const azureChatModels: AIChatModelCard[] = [
     contextWindowTokens: 200_000,
     description:
       'o1是OpenAI新的推理模型，支持图文输入并输出文本，适用于需要广泛通用知识的复杂任务。该模型具有200K上下文和2023年10月的知识截止日期。',
-    displayName: 'OpenAI o1',
+    displayName: 'o1',
     id: 'o1',
     pricing: {
       cachedInput: 7.5,
