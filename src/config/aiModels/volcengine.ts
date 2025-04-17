60c9ae2be95b8238769446f36c48ba7ebffffff2
Zhijie He <hezhijie0327@hotmail.com>
2025-04-17 23:00:58 +0800

    💄 style: update 360 AI & Taichu & AI21 model list (#7443)
    * 💄 style: update 360 AI & Taichu model list
    
    * 💄 style: update Github Models list
    
    * 💄 style: update Ai21 model list & fix ci error
    
    * 💄 style: update GitHub models
    
    * 🔨 chore: rollback changes for GitHub

--- src/config/aiModels/volcengine.ts
+++ src/config/aiModels/volcengine.ts
@@ -61,7 +61,6 @@ const doubaoChatModels: AIChatModelCard[] = [
     description:
       'DeepSeek-R1 在后训练阶段大规模使用了强化学习技术，在仅有极少标注数据的情况下，极大提升了模型推理能力。在数学、代码、自然语言推理等任务上，性能比肩 OpenAI o1 正式版。',
     displayName: 'DeepSeek R1',
-    enabled: true,
     id: 'deepseek-r1',
     maxOutput: 16_384,
     pricing: {
