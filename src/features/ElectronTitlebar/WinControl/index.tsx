c5f3d13c14636c0f902204987a19d351203ffef9
Arvin Xu <arvinx@foxmail.com>
2025-05-01 16:42:26 +0800

    💄 style: add windows control and tray (#7665)
    * add windows support
    
    * add windows close support
    
    * improve
    
    * fix
    
    * FIX
    
    * improve builder
    
    * improve windows icon

---
+++ src/features/ElectronTitlebar/WinControl/index.tsx
@@ -0,0 +1,85 @@
+import { Icon } from '@lobehub/ui';
+import { createStyles } from 'antd-style';
+import { Minus, Square, XIcon } from 'lucide-react';
+
+import { electronSystemService } from '@/services/electron/system';
+
+import { TITLE_BAR_HEIGHT } from '../const';
+
+const useStyles = createStyles(({ css, cx, token }) => {
+  const icon = css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 64px;
+    min-height: ${TITLE_BAR_HEIGHT}px;
+    transition: all ease-in-out 100ms;
+    -webkit-app-region: no-drag;
+
+    color: ${token.colorTextSecondary};
+
+    &:hover {
+      background: ${token.colorFillTertiary};
+    }
+    &:active {
+      background: ${token.colorFillSecondary};
+    }
+  `;
+  return {
+    close: cx(
+      icon,
+      css`
+        &:hover {
+          color: ${token.colorTextLightSolid};
+          /* win11 的色值，亮暗色均不变 */
+          background: #d33328;
+        }
+
+        &:active {
+          color: ${token.colorTextLightSolid};
+          /* win11 的色值 */
+          background: #8b2b25;
+        }
+      `,
+    ),
+    container: css`
+      display: flex;
+      cursor: pointer;
+    `,
+    icon,
+  };
+});
+
+const WinControl = () => {
+  const { styles } = useStyles();
+  return (
+    <div className={styles.container}>
+      <div
+        className={styles.icon}
+        onClick={() => {
+          electronSystemService.minimizeWindow();
+        }}
+      >
+        <Icon icon={Minus} style={{ fontSize: 18 }} />
+      </div>
+      <div
+        className={styles.icon}
+        onClick={() => {
+          electronSystemService.maximizeWindow();
+        }}
+      >
+        <Icon icon={Square} />
+      </div>
+      <div
+        className={styles.close}
+        onClick={() => {
+          electronSystemService.closeWindow();
+        }}
+      >
+        <Icon icon={XIcon} style={{ fontSize: 18 }} />
+      </div>
+    </div>
+  );
+};
+
+export default WinControl;
