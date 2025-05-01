e118e0fa7f83239a71b40a129e2a5b6d44e3bc08
Jamie Stivala <jamiestivala@gmail.com>
2025-05-02 01:02:31 +0700

    Update repository url on release to use JA Worldwide One-JA Bot

--- .releaserc.js
+++ .releaserc.js
@@ -12,4 +12,32 @@ config.plugins.push([
   },
 ]);
 
+// Override GitHub repository URL without modifying package.json
+// Make sure @semantic-release/github is present in the plugins
+if (!config.plugins.some(plugin => Array.isArray(plugin) ? plugin[0] === '@semantic-release/github' : plugin === '@semantic-release/github')) {
+  config.plugins.push([
+    '@semantic-release/github',
+    {
+      repositoryUrl: 'https://github.com/jaworldwideorg/OneJA-Bot.git'
+    }
+  ]);
+} else {
+  // Find and update the existing GitHub plugin configuration
+  config.plugins = config.plugins.map(plugin => {
+    if (Array.isArray(plugin) && plugin[0] === '@semantic-release/github') {
+      return [
+        '@semantic-release/github',
+        {
+          ...(plugin[1] || {}),
+          repositoryUrl: 'https://github.com/jaworldwideorg/OneJA-Bot.git'
+        }
+      ];
+    }
+    return plugin;
+  });
+}
+
+// Set repository URL in global config
+config.repositoryUrl = 'https://github.com/jaworldwideorg/OneJA-Bot.git';
+
 module.exports = config;
