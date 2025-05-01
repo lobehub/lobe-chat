edd2fbf2f464ee4e4f19aaed092f8ce1c6db340a
cy948 <cy948@qq.com>
2025-05-01 15:38:12 +0800

    :label: type: update types

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,4 +1,8 @@
 import { type DefaultSession } from 'next-auth';
+// eslint-disable-next-line unused-imports/no-unused-imports , @typescript-eslint/no-unused-vars
+import { JWT } from 'next-auth/jwt';
+
+// The IDE need to import this to get the type inference
 
 declare module 'next-auth' {
   /**
@@ -29,8 +33,17 @@ declare module 'next-auth/jwt' {
     access_token?: string;
     error?: 'RefreshTokenError';
     expires_at?: number;
+    /**
+     * The issuer while user logined
+     */
     iss?: string;
+    /**
+     * The provider name
+     */
     provider?: string;
+    /**
+     * The id from the provider
+     */
     providerAccountId?: string;
     refresh_token?: string;
     userId?: string;
