df81899d0cc6ec169bc90aac25f967de5f31a13f
arvinxx <arvinx@foxmail.com>
2025-05-09 20:51:41 +0800

    update tests

---
+++ packages/file-loaders/vitest.config.ts
@@ -0,0 +1,15 @@
+import { join } from 'node:path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    // coverage: {
+    //   all: false,
+    //   provider: 'v8',
+    //   reporter: ['text', 'json', 'lcov', 'text-summary'],
+    //   reportsDirectory: './coverage/app',
+    // },
+    environment: 'happy-dom',
+    setupFiles: join(__dirname, './test/setup.ts'),
+  },
+});
