a294fe260fff0c3923f6d6f003a3964274c0bd24
Eugeny Schibrikov <github@schibrikov.com>
2025-05-16 20:45:19 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -25,6 +25,7 @@ const nextConfig: NextConfig = {
   basePath,
   compress: isProd,
   experimental: {
+    webpackMemoryOptimizations: true,
     optimizePackageImports: [
       'emoji-mart',
       '@emoji-mart/react',
@@ -39,6 +40,20 @@ const nextConfig: NextConfig = {
     // refs: https://github.com/lobehub/lobe-chat/pull/7430
     serverMinification: false,
     webVitalsAttribution: ['CLS', 'LCP'],
+    serverSourceMaps: false
+  },
+  productionBrowserSourceMaps: false,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
   },
   async headers() {
     return [
@@ -200,6 +215,12 @@ const nextConfig: NextConfig = {
   transpilePackages: ['pdfjs-dist', 'mermaid'],
 
   webpack(config) {
+    if (config.cache && !dev) {
+      config.cache = Object.freeze({
+        type: 'memory',
+      })
+    }
+    
     config.experiments = {
       asyncWebAssembly: true,
       layers: true,
