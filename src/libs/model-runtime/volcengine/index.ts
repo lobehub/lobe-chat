a1161943340d0927b4bbccb89f33f27f950cfb94
sxjeru <sxjeru@gmail.com>
2025-05-21 20:10:15 +0800

    Update index.ts

--- src/libs/model-runtime/volcengine/index.ts
+++ src/libs/model-runtime/volcengine/index.ts
@@ -1,5 +1,11 @@
+import type { ChatModelCard } from '@/types/llm';
+
 import { ModelProvider } from '../types';
 import { LobeOpenAICompatibleFactory } from '../utils/openaiCompatibleFactory';
+
+export interface VolcengineModelCard {
+  id: string;
+}
 
 export const LobeVolcengineAI = LobeOpenAICompatibleFactory({
   baseURL: 'https://ark.cn-beijing.volces.com/api/v3',
@@ -23,6 +29,45 @@ export const LobeVolcengineAI = LobeOpenAICompatibleFactory({
   },
   debug: {
     chatCompletion: () => process.env.DEBUG_VOLCENGINE_CHAT_COMPLETION === '1',
+  },
+  models: async ({ client }) => {
+    const { LOBE_DEFAULT_MODEL_LIST } = await import('@/config/aiModels');
+
+    const functionCallKeywords = ['doubao-1.5'];
+
+    const visionKeywords = ['vision', '-m'];
+
+    const reasoningKeywords = ['thinking', '-r1'];
+
+    const modelsPage = (await client.models.list()) as any;
+    const modelList: VolcengineModelCard[] = modelsPage.data;
+
+    return modelList
+      .map((model) => {
+        const knownModel = LOBE_DEFAULT_MODEL_LIST.find(
+          (m) => model.id.toLowerCase() === m.id.toLowerCase(),
+        );
+
+        return {
+          contextWindowTokens: knownModel?.contextWindowTokens ?? undefined,
+          displayName: knownModel?.displayName ?? undefined,
+          enabled: knownModel?.enabled || false,
+          functionCall:
+            functionCallKeywords.some((keyword) => model.id.toLowerCase().includes(keyword)) ||
+            knownModel?.abilities?.functionCall ||
+            false,
+          id: model.id,
+          reasoning:
+            reasoningKeywords.some((keyword) => model.id.toLowerCase().includes(keyword)) ||
+            knownModel?.abilities?.reasoning ||
+            false,
+          vision:
+            visionKeywords.some((keyword) => model.id.toLowerCase().includes(keyword)) ||
+            knownModel?.abilities?.vision ||
+            false,
+        };
+      })
+      .filter(Boolean) as ChatModelCard[];
   },
   provider: ModelProvider.Volcengine,
 });
