cd84241709de4b3891d350c14d618ba716fc70f7
Arvin Xu <arvinx@foxmail.com>
2025-05-26 01:31:25 +0800

    🐛 fix: fix draggable issue with agent header (#7968)

--- src/app/[variants]/(main)/chat/(workspace)/_layout/Desktop/ChatHeader/index.tsx
+++ src/app/[variants]/(main)/chat/(workspace)/_layout/Desktop/ChatHeader/index.tsx
@@ -4,24 +4,11 @@ import { ChatHeader } from '@lobehub/ui/chat';
 
 import { useGlobalStore } from '@/store/global';
 import { systemStatusSelectors } from '@/store/global/selectors';
-import { electronStylish } from '@/styles/electron';
-
-import HeaderAction from './HeaderAction';
-import Main from './Main';
 
 const Header = () => {
   const showHeader = useGlobalStore(systemStatusSelectors.showChatHeader);
 
-  return (
-    showHeader && (
-      <ChatHeader
-        className={electronStylish.draggable}
-        left={<Main className={electronStylish.nodrag} />}
-        right={<HeaderAction className={electronStylish.nodrag} />}
-        style={{ paddingInline: 8, position: 'initial', zIndex: 11 }}
-      />
-    )
-  );
+  return showHeader && <ChatHeader style={{ paddingInline: 8, position: 'initial', zIndex: 11 }} />;
 };
 
 export default Header;
