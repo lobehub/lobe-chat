0e3e777eff2b09ffea63d0907741f7305274f45e
Zhijie He <hezhijie0327@hotmail.com>
2025-05-27 13:12:12 +0800

    🐛 fix: fix ci error

--- src/libs/model-runtime/utils/streams/openai.ts
+++ src/libs/model-runtime/utils/streams/openai.ts
@@ -131,6 +131,11 @@ export const transformOpenAIStream = (
         ];
       }
 
+      if (chunk.usage) {
+        const usage = chunk.usage;
+        return { data: convertUsage(usage), id: chunk.id, type: 'usage' };
+      }
+
       // xAI Live Search 功能返回引用源
       // {"id":"8721eebb-6465-4c47-ba2e-8e2ec0f97055","object":"chat.completion.chunk","created":1747809109,"model":"grok-3","choices":[{"index":0,"delta":{"role":"assistant"},"finish_reason":"stop"}],"system_fingerprint":"fp_1affcf9872","citations":["https://world.huanqiu.com/"]}
       if ((chunk as any).citations) {
@@ -151,11 +156,6 @@ export const transformOpenAIStream = (
             type: 'grounding',
           },
         ];
-      }
-
-      if (chunk.usage) {
-        const usage = chunk.usage;
-        return { data: convertUsage(usage), id: chunk.id, type: 'usage' };
       }
 
       return { data: item.finish_reason, id: chunk.id, type: 'stop' };
