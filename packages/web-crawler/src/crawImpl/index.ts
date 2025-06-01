7e4b8db59590e72ab65446b8ba180df92f68f62c
Zhijie He <hezhijie0327@hotmail.com>
2025-06-01 11:40:27 +0800

    ✨ feat: support tavily as crawl provider

--- packages/web-crawler/src/crawImpl/index.ts
+++ packages/web-crawler/src/crawImpl/index.ts
@@ -2,12 +2,14 @@ import { browserless } from './browserless';
 import { jina } from './jina';
 import { naive } from './naive';
 import { search1api } from './search1api';
+import { tavily } from './tavily';
 
 export const crawlImpls = {
   browserless,
   jina,
   naive,
   search1api,
+  tavily,
 };
 
 export type CrawlImplType = keyof typeof crawlImpls;
