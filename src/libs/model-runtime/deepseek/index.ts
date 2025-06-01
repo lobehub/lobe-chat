cf586289950d67d8fc04dadf5eb5d6301ad14d04
Arvin Xu <arvinx@foxmail.com>
2025-06-01 16:20:49 +0800

    🐛 fix: fix DeepSeek new R1 Search error (#8035)
    * fix DeepSeek R1 Search issue
    
    * fix tests

--- src/libs/model-runtime/deepseek/index.ts
+++ src/libs/model-runtime/deepseek/index.ts
@@ -1,8 +1,6 @@
-import OpenAI from 'openai';
-
 import type { ChatModelCard } from '@/types/llm';
 
-import { ChatStreamPayload, ModelProvider } from '../types';
+import { ModelProvider } from '../types';
 import { LobeOpenAICompatibleFactory } from '../utils/openaiCompatibleFactory';
 
 export interface DeepSeekModelCard {
@@ -11,59 +9,6 @@ export interface DeepSeekModelCard {
 
 export const LobeDeepSeekAI = LobeOpenAICompatibleFactory({
   baseURL: 'https://api.deepseek.com/v1',
-  chatCompletion: {
-    handlePayload: ({
-      frequency_penalty,
-      messages,
-      model,
-      presence_penalty,
-      temperature,
-      top_p,
-      ...payload
-    }: ChatStreamPayload) => {
-      // github.com/lobehub/lobe-chat/pull/5548
-      let filteredMessages = messages.filter((message) => message.role !== 'system');
-
-      if (filteredMessages.length > 0 && filteredMessages[0].role === 'assistant') {
-        filteredMessages.unshift({ content: '', role: 'user' });
-      }
-
-      let lastRole = '';
-      for (let i = 0; i < filteredMessages.length; i++) {
-        const message = filteredMessages[i];
-        if (message.role === lastRole) {
-          const newRole = lastRole === 'assistant' ? 'user' : 'assistant';
-          filteredMessages.splice(i, 0, { content: '', role: newRole });
-          i++;
-        }
-        lastRole = message.role;
-      }
-
-      if (messages.length > 0 && messages[0].role === 'system') {
-        filteredMessages.unshift(messages[0]);
-      }
-
-      return {
-        ...payload,
-        model,
-        ...(model === 'deepseek-reasoner'
-          ? {
-              frequency_penalty: undefined,
-              messages: filteredMessages,
-              presence_penalty: undefined,
-              temperature: undefined,
-              top_p: undefined,
-            }
-          : {
-              frequency_penalty,
-              messages,
-              presence_penalty,
-              temperature,
-              top_p,
-            }),
-      } as OpenAI.ChatCompletionCreateParamsStreaming;
-    },
-  },
   debug: {
     chatCompletion: () => process.env.DEBUG_DEEPSEEK_CHAT_COMPLETION === '1',
   },
