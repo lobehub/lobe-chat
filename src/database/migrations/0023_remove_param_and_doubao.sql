50075666201c594dc811db490e6ba38240454944
sxjeru <sxjeru@gmail.com>
2025-06-04 09:56:51 +0800

    revert

--- src/database/migrations/0023_remove_param_and_doubao.sql
+++ src/database/migrations/0023_remove_param_and_doubao.sql
@@ -1,3 +1,4 @@
+-- Custom SQL migration file, put your code below! --
 UPDATE agents SET chat_config = jsonb_set(chat_config, '{enableReasoningEffort}', 'false') WHERE chat_config ->> 'enableReasoningEffort' = 'true';
 --> statement-breakpoint
 UPDATE agents SET params = params - 'reasoning_effort' WHERE params ? 'reasoning_effort';
