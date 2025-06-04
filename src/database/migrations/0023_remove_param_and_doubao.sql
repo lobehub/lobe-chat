0cc193c59fb1c4f5f60edaf96fa043fe42c32e02
sxjeru <sxjeru@gmail.com>
2025-06-04 09:50:07 +0800

    Revert

---
+++ src/database/migrations/0023_remove_param_and_doubao.sql
@@ -0,0 +1,5 @@
+UPDATE agents SET chat_config = jsonb_set(chat_config, '{enableReasoningEffort}', 'false') WHERE chat_config ->> 'enableReasoningEffort' = 'true';
+--> statement-breakpoint
+UPDATE agents SET params = params - 'reasoning_effort' WHERE params ? 'reasoning_effort';
+--> statement-breakpoint
+DELETE FROM ai_providers WHERE id = 'doubao';
