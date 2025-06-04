c86d34b149fb7b65047af4947ec665e1f0433812
Daniele Sarnari <daniele.sarnari@beliven.com>
2025-06-04 19:23:21 +0200

    feat: add google sso

---
+++ src/libs/next-auth/sso-providers/google.ts
@@ -0,0 +1,23 @@
+import Google from 'next-auth/providers/google';
+
+import { authEnv } from '@/config/auth';
+
+import { CommonProviderConfig } from './sso.config';
+
+const provider = {
+  id: 'google',
+  provider: Google({
+    ...CommonProviderConfig,
+    authorization: {
+      params: {
+        scope:
+          'openid https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email openid',
+      },
+    },
+    // TODO(NextAuth ENVs Migration): Remove once nextauth envs migration time end
+    clientId: authEnv.AUTH_GOOGLE_CLIENT_ID ?? process.env.AUTH_GOOGLE_CLIENT_ID,
+    clientSecret: authEnv.AUTH_GOOGLE_CLIENT_SECRET ?? process.env.AUTH_GOOGLE_CLIENT_SECRET,
+  }),
+};
+
+export default provider;
