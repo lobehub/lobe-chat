5fa9ebaa863ea9deeab371494aaf27abb1da4b1d
Daniele Sarnari <daniele.sarnari@beliven.com>
2025-06-09 09:27:01 +0200

    chore: removed not used authEnv reference

--- src/libs/next-auth/sso-providers/google.ts
+++ src/libs/next-auth/sso-providers/google.ts
@@ -1,7 +1,5 @@
 import Google from 'next-auth/providers/google';
 
-import { authEnv } from '@/config/auth';
-
 import { CommonProviderConfig } from './sso.config';
 
 const provider = {
@@ -14,7 +12,6 @@ const provider = {
           'openid https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email openid',
       },
     },
-    // TODO(NextAuth ENVs Migration): Remove once nextauth envs migration time end
     clientId: process.env.AUTH_GOOGLE_CLIENT_ID,
     clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
   }),
