6ce07b21be33cf14009fcea7d478511d3c1a93ac
Jamie Stivala <jamiestivala@gmail.com>
2025-06-10 20:12:25 +0800

    Add Okta as a new SSO provider to `ssoProviders` array

--- src/libs/next-auth/sso-providers/index.ts
+++ src/libs/next-auth/sso-providers/index.ts
@@ -12,6 +12,7 @@ import Logto from './logto';
 import MicrosoftEntraID from './microsoft-entra-id';
 import WeChat from './wechat';
 import Zitadel from './zitadel';
+import Okta from './okta';
 
 export const ssoProviders = [
   Auth0,
@@ -28,4 +29,5 @@ export const ssoProviders = [
   WeChat,
   Keycloak,
   Google,
+  Okta
 ];
