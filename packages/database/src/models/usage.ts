6b212c3c9d179dcc890c2bf098671479c5c5c599
cy948 <cy948@qq.com>
2025-06-20 21:02:31 +0800

    :recycle: refactor: change layout

--- packages/database/src/models/usage.ts
+++ packages/database/src/models/usage.ts
@@ -3,6 +3,7 @@ import { NewSpendLog, SpendLogItem, spendLogs } from "../schemas/usage";
 import { LobeChatDatabase } from "../type";
 import { desc, eq } from "drizzle-orm";
 import { UsageLog } from "@/types/usage";
+import { formatDate } from "@/utils/format";
 
 export class UsageModel {
     private userId: string;
@@ -35,28 +36,27 @@ export class UsageModel {
             orderBy: desc(spendLogs.updatedAt),
         })
         // Clustering by time
-        let usages = new Map<number, SpendLogItem[]>()
+        let usages = new Map<string, { date: Date, logs: SpendLogItem[]}>()
         spends.forEach((spend) => {
-            const date = Math.floor(spend.createdAt.getTime() / 1000);
-            if (!usages.has(date)){
-                usages.set(date, [spend]);
+            if (!usages.has(formatDate(spend.createdAt))){
+                usages.set(formatDate(spend.createdAt), { date: spend.createdAt, logs: [spend] });
                 return;
             }
-            usages.get(date)?.push(spend);
+            usages.get(formatDate(spend.createdAt))?.logs.push(spend);
         })
         // Calculate usage
         let usageLogs: UsageLog[] = [];
         usages.forEach((spends, date) => {
-            const totalSpend = spends.reduce((acc, spend) => acc + spend.spend, 0);
-            const totalTokens = spends.reduce((acc, spend) => (spend.totalTokens || 0) + acc, 0);
-            const totalRequests = spends.length;
+            const totalSpend = spends.logs.reduce((acc, spend) => acc + spend.spend, 0);
+            const totalTokens = spends.logs.reduce((acc, spend) => (spend.totalTokens || 0) + acc, 0);
+            const totalRequests = spends.logs.length;
             console.log('date', date, 'totalSpend', totalSpend, 'totalTokens', totalTokens, 'totalRequests', totalRequests);
             usageLogs.push({
-                requestLogs: spends,
+                requestLogs: spends.logs,
                 totalSpend,
                 totalTokens,
                 totalRequests,
-                date,
+                date: spends.date.getTime() / 1000, // Convert to seconds
             });
         })
         return usageLogs;
