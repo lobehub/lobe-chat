383b1e338f82e94d01d9e2e47d8f0ffa07f628f2
cy948 <cy948@qq.com>
2025-06-20 21:02:31 +0800

    :recycle: refactor: change layout

---
+++ src/app/[variants]/(main)/profile/usage/features/TotalRequest.tsx
@@ -0,0 +1,50 @@
+import { LineChart, Heatmaps, HeatmapsProps } from '@lobehub/charts';
+import { FormGroup, Grid, Icon, Tag } from '@lobehub/ui';
+import { useTheme } from 'antd-style';
+import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Flexbox } from 'react-layout-kit';
+
+import { FORM_STYLE } from '@/const/layoutTokens';
+import { useClientDataSWR } from '@/libs/swr';
+import { usageService } from '@/services/usage';
+import { formatDate } from '@/utils/format';
+
+const AiRequest = memo<Omit<HeatmapsProps, 'data'> & { inShare?: boolean; mobile?: boolean }>(
+    ({ inShare, mobile, ...rest }) => {
+        const { t } = useTranslation('auth');
+        const theme = useTheme();
+        const { data, isLoading } = useClientDataSWR('stats-heatmaps', async () =>
+            usageService.getUsages()
+        );
+
+        const barChart = (
+            data &&
+            <LineChart 
+                categories={['totalTokens']}
+                data={data?.map(log => {
+                    console.log('log', log);
+                    return {
+                        totalTokens: log.totalTokens,
+                        date: formatDate(new Date(log.date * 1000)),
+                    }})}
+                index={'date'}
+                showLegend={false}
+            />
+        );
+
+        return (
+            <FormGroup
+                style={FORM_STYLE.style}
+                title={`Tokens ${data?.reduce((acc, log) => acc + log.totalTokens, 0) || 0}`}
+                variant={'borderless'}
+            >
+                <Flexbox paddingBlock={24}>
+                    {barChart}
+                </Flexbox>
+            </FormGroup>
+        );
+    },
+);
+
+export default AiRequest;
