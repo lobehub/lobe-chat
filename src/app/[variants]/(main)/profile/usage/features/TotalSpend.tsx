8c485dc4b397c5fd2c29a7cb76c754a43f7ba7f9
cy948 <cy948@vip.qq.com>
2025-06-21 20:48:37 +0800

    :lipstick: style: change charts

--- src/app/[variants]/(main)/profile/usage/features/TotalSpend.tsx
+++ src/app/[variants]/(main)/profile/usage/features/TotalSpend.tsx
@@ -1,41 +1,66 @@
-import { BarChart, Heatmaps, HeatmapsProps } from '@lobehub/charts';
-import { FormGroup, Grid, Icon, Tag } from '@lobehub/ui';
-import { useTheme } from 'antd-style';
+import { AreaChart, AreaChartProps } from '@lobehub/charts';
+import { FormGroup } from '@lobehub/ui';
 import { memo } from 'react';
-import { useTranslation } from 'react-i18next';
 import { Flexbox } from 'react-layout-kit';
 
 import { FORM_STYLE } from '@/const/layoutTokens';
-import { useClientDataSWR } from '@/libs/swr';
-import { usageService } from '@/services/usage';
 import { formatDate } from '@/utils/format';
+import { UsageLog } from '@/types/usage';
+import { UsageChartProps } from '../Client';
 
-const AiSpend = memo<Omit<HeatmapsProps, 'data'> & { inShare?: boolean; mobile?: boolean }>(
-    ({ inShare, mobile, ...rest }) => {
-        const { t } = useTranslation('auth');
-        const theme = useTheme();
-        const { data, isLoading } = useClientDataSWR('stats-heatmaps', async () =>
-            usageService.getUsages()
-        );
+const formatData = (data: UsageLog[]): { categories: string[], data: AreaChartProps['data'] } => {
+    if (!data || data.length === 0) return { categories: [], data: [] };
+    let formattedData: AreaChartProps['data'] = [];
+    let cateByProvider: Map<string, number> = data.reduce((acc, log) => {
+        if (log.requestLogs) {
+            for (const item of log.requestLogs) {
+                if (item.provider) {
+                    acc.set(item.provider, 0);
+                }
+            }
+        }
+        return acc;
+    }, new Map<string, number>());
+    const categories: string[] = Array.from(cateByProvider.keys());
+    categories.push('total');
+    formattedData = data.map((log) => {
+        const totalObj = {
+            date: formatDate(new Date(log.date * 1000)),
+            total: log.totalSpend,
+        }
+        let todayCateProvider = new Map<string, number>(cateByProvider);
+        for (const item of log.requestLogs) {
+            todayCateProvider.set(item.provider, (todayCateProvider.get(item.provider) || 0) + item.spend);
+        }
+        return {
+            ...totalObj,
+            ...Object.fromEntries(todayCateProvider.entries()),
+        };
+    });
+    return {
+        categories,
+        data: formattedData,
+    }
+}
+
+const AiSpend = memo<UsageChartProps>(
+    ({ isLoading, ...rest }) => {
+        const { data, categories } = formatData(rest?.data || []);
 
         const barChart = (
             data &&
-            <BarChart 
-                categories={['spend']}
-                data={data?.map(log => {
-                    console.log('log', log);
-                    return {
-                        'spend': log.totalSpend,
-                        'date': formatDate(new Date(log.date * 1000)),
-                    }})}
+            <AreaChart
+                categories={categories}
+                data={data}
                 index={'date'}
+                loading={isLoading || !data}
             />
         );
 
         return (
             <FormGroup
                 style={FORM_STYLE.style}
-                title={`Spend ${data?.reduce((acc, log) => acc + log.totalSpend, 0) || 0}`}
+                title={`Spend ${data?.reduce((acc, log) => acc + log.total, 0) || 0}`}
                 variant={'borderless'}
             >
                 <Flexbox paddingBlock={24}>
