84bdde9c622af2bfbc507ee1850d5598c128ffe0
cy948 <cy948@vip.qq.com>
2025-06-21 23:18:59 +0800

    :lip_stick: style: add cate charts

--- src/app/[variants]/(main)/profile/usage/Client.tsx
+++ src/app/[variants]/(main)/profile/usage/Client.tsx
@@ -1,18 +1,19 @@
 'use client';
 
+import { Col, Row } from 'antd';
 import { memo } from 'react';
 import { Flexbox } from 'react-layout-kit';
 
-import { Col, Row } from 'antd';
-
-import TotalSpend from './features/TotalSpend';
-import TotalRequest from './features/TotalRequest';
-import { UsageLog } from '@/types/usage';
 import { useClientDataSWR } from '@/libs/swr';
 import { usageService } from '@/services/usage';
+import { UsageLog } from '@/types/usage';
+
+import TotalRequest from './features/TotalRequest';
+import TotalSpend from './features/TotalSpend';
+import UsageCategories from './features/UsageCategories';
 
 export interface UsageChartProps {
-  data?: UsageLog[]
+  data?: UsageLog[];
   inShare?: boolean;
   isLoading?: boolean;
   mobile?: boolean;
@@ -20,14 +21,12 @@ export interface UsageChartProps {
 
 const Client = memo<{ mobile?: boolean }>(({ mobile }) => {
   const { data, isLoading } = useClientDataSWR('stats-heatmaps', async () =>
-    usageService.getUsages()
+    usageService.getUsages(),
   );
 
   return (
-    <Flexbox gap={mobile ? 0 : 24} >
-      <Flexbox>
-        Header
-      </Flexbox>
+    <Flexbox gap={mobile ? 0 : 24}>
+      <Flexbox>Header</Flexbox>
       <Row gutter={[16, 16]} style={{ padding: mobile ? '0 16px' : '0 24px' }}>
         <Col span={12}>
           {data && <TotalSpend data={data} isLoading={isLoading} mobile={mobile} />}
@@ -37,6 +36,9 @@ const Client = memo<{ mobile?: boolean }>(({ mobile }) => {
             {data && <TotalRequest data={data} isLoading={isLoading} mobile={mobile} />}
           </Flexbox>
         </Col>
+      </Row>
+      <Row>
+        <UsageCategories data={data} />
       </Row>
     </Flexbox>
   );
