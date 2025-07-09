1e2c0f8c5a703b97a64a786dd6b2d62607444c4f
YuTengjing <ytj2713151713@gmail.com>
2025-07-09 14:43:24 +0800

    fix: disable async router trpc batch feature

--- src/app/(backend)/trpc/async/[trpc]/route.ts
+++ src/app/(backend)/trpc/async/[trpc]/route.ts
@@ -9,6 +9,10 @@ export const maxDuration = 60;
 
 const handler = (req: NextRequest) =>
   fetchRequestHandler({
+    // 避免请求之间互相影响
+    // https://github.com/lobehub/lobe-chat/discussions/7442#discussioncomment-13658563
+    allowBatching: false,
+
     /**
      * @link https://trpc.io/docs/v11/context
      */
