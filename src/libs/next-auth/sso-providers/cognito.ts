24181422fb26ed522b2c99649fd713499ebdd1c4
arvinxx <arvinx@foxmail.com>
2025-07-10 19:10:38 +0800

    fix

--- src/libs/next-auth/sso-providers/cognito.ts
+++ src/libs/next-auth/sso-providers/cognito.ts
@@ -1,15 +1,12 @@
 import Cognito from 'next-auth/providers/cognito';
 
-import { authEnv } from '@/config/auth';
-
 const provider = {
   id: 'cognito',
   provider: Cognito({
-    clientId: authEnv.COGNITO_CLIENT_ID ?? process.env.AUTH_COGNITO_ID,
-    clientSecret: authEnv.COGNITO_CLIENT_SECRET ?? process.env.AUTH_COGNITO_SECRET,
-    issuer: authEnv.COGNITO_ISSUER ?? process.env.AUTH_COGNITO_ISSUER,
+    clientId: process.env.AUTH_COGNITO_ID,
+    clientSecret: process.env.AUTH_COGNITO_SECRET,
+    issuer: process.env.AUTH_COGNITO_ISSUER,
   }),
 };
-
 
 export default provider;
