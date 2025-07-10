d41ae2223cc22c5987c552131b81dd555ca73044
arvinxx <arvinx@foxmail.com>
2025-07-10 17:04:41 +0800

    repos and settings provider/id static

--- src/app/[variants]/(main)/settings/provider/(detail)/[id]/page.tsx
+++ src/app/[variants]/(main)/settings/provider/(detail)/[id]/page.tsx
@@ -1,14 +1,5 @@
-import { redirect } from 'next/navigation';
-
 import { DEFAULT_MODEL_PROVIDER_LIST } from '@/config/modelProviders';
-import { isDesktop, isServerMode } from '@/const/version';
-import { AiInfraRepos } from '@/database/repositories/aiInfra';
-import { serverDB } from '@/database/server';
-import { getServerGlobalConfig } from '@/server/globalConfig';
-import { KeyVaultsGateKeeper } from '@/server/modules/KeyVaultsEncrypt';
 import { PagePropsWithId } from '@/types/next';
-import { ProviderConfig } from '@/types/user/settings';
-import { getUserAuth } from '@/utils/server/auth';
 
 import ClientMode from './ClientMode';
 import ProviderDetail from './index';
@@ -19,27 +10,6 @@ const Page = async (props: PagePropsWithId) => {
   const builtinProviderCard = DEFAULT_MODEL_PROVIDER_LIST.find((v) => v.id === params.id);
   // if builtin provider
   if (!!builtinProviderCard) return <ProviderDetail source={'builtin'} {...builtinProviderCard} />;
-
-  // if user custom provider
-  if (!isDesktop && isServerMode) {
-    const { userId } = await getUserAuth();
-
-    const { aiProvider } = await getServerGlobalConfig();
-    const aiInfraRepos = new AiInfraRepos(
-      serverDB,
-      userId!,
-      aiProvider as Record<string, ProviderConfig>,
-    );
-
-    const userCard = await aiInfraRepos.getAiProviderDetail(
-      params.id,
-      KeyVaultsGateKeeper.getUserKeyVaults,
-    );
-
-    if (!userCard) return redirect('/settings/provider');
-
-    return <ProviderDetail {...userCard} />;
-  }
 
   return <ClientMode id={params.id} />;
 };
