0f5b1c6f82645b4f249a5ba749b5176ead91db9b
rdmclin2 <rdmclin2@gmail.com>
2025-08-01 12:48:08 +0800

    feat: migrate mobile trpc api

---
+++ src/server/routers/mobile/index.ts
@@ -0,0 +1,57 @@
+/**
+ * This file contains the root router of Lobe Chat tRPC-backend
+ */
+import { publicProcedure, router } from '@/libs/trpc/lambda';
+
+import { agentRouter } from './agent';
+import { aiModelRouter } from './aiModel';
+import { aiProviderRouter } from './aiProvider';
+import { chunkRouter } from './chunk';
+import { configRouter } from './config';
+import { documentRouter } from './document';
+import { exporterRouter } from './exporter';
+import { fileRouter } from './file';
+import { generationRouter } from './generation';
+import { generationBatchRouter } from './generationBatch';
+import { generationTopicRouter } from './generationTopic';
+import { imageRouter } from './image';
+import { importerRouter } from './importer';
+import { knowledgeBaseRouter } from './knowledgeBase';
+import { marketRouter } from './market';
+import { messageRouter } from './message';
+import { pluginRouter } from './plugin';
+import { ragEvalRouter } from './ragEval';
+import { sessionRouter } from './session';
+import { sessionGroupRouter } from './sessionGroup';
+import { threadRouter } from './thread';
+import { topicRouter } from './topic';
+import { userRouter } from './user';
+
+export const mobileRouter = router({
+  agent: agentRouter,
+  aiModel: aiModelRouter,
+  aiProvider: aiProviderRouter,
+  chunk: chunkRouter,
+  config: configRouter,
+  document: documentRouter,
+  exporter: exporterRouter,
+  file: fileRouter,
+  generation: generationRouter,
+  generationBatch: generationBatchRouter,
+  generationTopic: generationTopicRouter,
+  healthcheck: publicProcedure.query(() => "i'm live!"),
+  image: imageRouter,
+  importer: importerRouter,
+  knowledgeBase: knowledgeBaseRouter,
+  market: marketRouter,
+  message: messageRouter,
+  plugin: pluginRouter,
+  ragEval: ragEvalRouter,
+  session: sessionRouter,
+  sessionGroup: sessionGroupRouter,
+  thread: threadRouter,
+  topic: topicRouter,
+  user: userRouter,
+});
+
+export type MobileRouter = typeof mobileRouter;
---
+++ src/server/routers/mobile/plugin.ts
@@ -0,0 +1,106 @@
+import { z } from 'zod';
+
+import { PluginModel } from '@/database/models/plugin';
+import { getServerDB } from '@/database/server';
+import { authedProcedure, publicProcedure, router } from '@/libs/trpc/lambda';
+import { serverDatabase } from '@/libs/trpc/lambda/middleware';
+import { LobeTool } from '@/types/tool';
+
+const pluginProcedure = authedProcedure.use(serverDatabase).use(async (opts) => {
+  const { ctx } = opts;
+
+  return opts.next({
+    ctx: { pluginModel: new PluginModel(ctx.serverDB, ctx.userId) },
+  });
+});
+
+export const pluginRouter = router({
+  createOrInstallPlugin: pluginProcedure
+    .input(
+      z.object({
+        customParams: z.any(),
+        identifier: z.string(),
+        manifest: z.any(),
+        settings: z.any(),
+        type: z.enum(['plugin', 'customPlugin']),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const result = await ctx.pluginModel.findById(input.identifier);
+
+      // if not exist, we should create the plugin
+      if (!result) {
+        const data = await ctx.pluginModel.create({
+          customParams: input.customParams,
+          identifier: input.identifier,
+          manifest: input.manifest,
+          settings: input.settings,
+          type: input.type,
+        });
+
+        return data.identifier;
+      }
+
+      // or we can just update the plugin manifest
+      await ctx.pluginModel.update(input.identifier, { manifest: input.manifest });
+    }),
+
+  createPlugin: pluginProcedure
+    .input(
+      z.object({
+        customParams: z.any(),
+        identifier: z.string(),
+        manifest: z.any(),
+        type: z.enum(['plugin', 'customPlugin']),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const data = await ctx.pluginModel.create({
+        customParams: input.customParams,
+        identifier: input.identifier,
+        manifest: input.manifest,
+        type: input.type,
+      });
+
+      return data.identifier;
+    }),
+
+  // TODO: 未来这部分方法也需要使用 authedProcedure
+  getPlugins: publicProcedure.query(async ({ ctx }): Promise<LobeTool[]> => {
+    if (!ctx.userId) return [];
+
+    const serverDB = await getServerDB();
+    const pluginModel = new PluginModel(serverDB, ctx.userId);
+
+    return pluginModel.query();
+  }),
+
+  removeAllPlugins: pluginProcedure.mutation(async ({ ctx }) => {
+    return ctx.pluginModel.deleteAll();
+  }),
+
+  removePlugin: pluginProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      return ctx.pluginModel.delete(input.id);
+    }),
+
+  updatePlugin: pluginProcedure
+    .input(
+      z.object({
+        customParams: z.any().optional(),
+        id: z.string(),
+        manifest: z.any().optional(),
+        settings: z.any().optional(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      return ctx.pluginModel.update(input.id, {
+        customParams: input.customParams,
+        manifest: input.manifest,
+        settings: input.settings,
+      });
+    }),
+});
+
+export type PluginRouter = typeof pluginRouter;
