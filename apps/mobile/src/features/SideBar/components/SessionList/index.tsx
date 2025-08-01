dec99cf6687fd2209d8ec773202643141f601670
rdmclin2 <rdmclin2@gmail.com>
2025-08-01 16:43:10 +0800

    chore: migrate SIdeBar

---
+++ apps/mobile/src/features/SideBar/components/SessionList/index.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { ScrollView, TextInput, View } from 'react-native';
+
+import { useSessionStore } from '@/store/session';
+import { useStyles } from './style';
+
+import Inbox from './Inbox';
+import SessionItem from './SessionItem';
+
+export default function SideBar() {
+  const { t } = useTranslation(['chat']);
+  const [searchText, setSearchText] = useState('');
+  const { sessions, fetchSessions } = useSessionStore();
+  const { styles, token } = useStyles();
+
+  useEffect(() => {
+    fetchSessions();
+  }, []);
+
+  const filteredSessions =
+    sessions?.filter(
+      (session) =>
+        session.meta.title?.toLowerCase().includes(searchText.toLowerCase()) ||
+        session.meta.description?.toLowerCase().includes(searchText.toLowerCase()),
+    ) || [];
+
+  return (
+    <View style={styles.container}>
+      {/* 搜索栏 */}
+      <TextInput
+        onChangeText={setSearchText}
+        placeholder={t('session.search.placeholder', { ns: 'chat' })}
+        placeholderTextColor={token.colorTextPlaceholder}
+        style={styles.searchInput}
+        value={searchText}
+      />
+
+      {/* 会话列表 */}
+      <ScrollView style={styles.sessionList}>
+        <Inbox />
+
+        {filteredSessions.map((session) => (
+          <SessionItem id={session.id} key={session.id} />
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
--- apps/mobile/src/features/SideBar/index.tsx
+++ apps/mobile/src/features/SideBar/index.tsx
@@ -1,53 +1,20 @@
-import { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import { ScrollView, TextInput, View } from 'react-native';
+import {} from 'react-i18next';
+import { View } from 'react-native';
 
-import { useSessionStore } from '@/store/session';
-import { useStyles } from './styles';
+import { useStyles } from './style';
 
-import Inbox from './components/Inbox';
 import Header from './components/Header';
-import SessionItem from './components/SessionItem';
 import Footer from './components/Footer';
+import SessionList from './components/SessionList';
 
 export default function SideBar() {
-  const { t } = useTranslation(['chat']);
-  const [searchText, setSearchText] = useState('');
-  const { sessions, fetchSessions } = useSessionStore();
-  const { styles, token } = useStyles();
-
-  useEffect(() => {
-    fetchSessions();
-  }, []);
-
-  const filteredSessions =
-    sessions?.filter(
-      (session) =>
-        session.meta.title?.toLowerCase().includes(searchText.toLowerCase()) ||
-        session.meta.description?.toLowerCase().includes(searchText.toLowerCase()),
-    ) || [];
+  const { styles } = useStyles();
 
   return (
     <View style={styles.container}>
       <Header />
 
-      {/* 搜索栏 */}
-      <TextInput
-        onChangeText={setSearchText}
-        placeholder={t('session.search.placeholder', { ns: 'chat' })}
-        placeholderTextColor={token.colorTextPlaceholder}
-        style={styles.searchInput}
-        value={searchText}
-      />
-
-      {/* 会话列表 */}
-      <ScrollView style={styles.sessionList}>
-        <Inbox />
-
-        {filteredSessions.map((session) => (
-          <SessionItem id={session.id} key={session.id} />
-        ))}
-      </ScrollView>
+      <SessionList />
 
       <Footer />
     </View>
