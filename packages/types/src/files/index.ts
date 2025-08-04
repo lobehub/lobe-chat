a7f3eb9364fd2bd71f2f2d0b4a9339e0c0365f52
Shinji-Li <onlyyoulove3@gmail.com>
2025-08-04 17:29:49 +0800

    Revert "fix: when s3 files not exist , global files should delete" (#8661)

--- packages/types/src/files/index.ts
+++ packages/types/src/files/index.ts
@@ -7,13 +7,6 @@ export enum FilesTabs {
   Websites = 'websites',
 }
 
-export enum TRPCErrorMessage {
-  FileContentEmpty = 'File content is empty',
-  FileNotFound = 'File not found',
-  // files not find in origin
-  OrginFileNotFound = 'Origin File Not Found',
-}
-
 export enum FileSource {
   ImageGeneration = 'image_generation',
 }
