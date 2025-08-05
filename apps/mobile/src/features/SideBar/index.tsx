ee06cff06200821ce99b43658225490e10934dc2
Rdmclin2 <rdmclin2@gmail.com>
2025-08-05 11:15:22 +0800

    feat: sidebar implementation (#8658)
    * chore: migrate sidebar to features
    
    * chore: migrate SIdeBar
    
    * fix: add pnpm lockfile support
    
    * chore: update .npmrc
    
    * chore: optimize Session List
    
    * fix:  stoploading animation
    
    * chore: use mobile trpc
    
    * fix: optimize consent page for mobile
    
    * chore: optimize Consent page for mobile

---
+++ apps/mobile/src/features/SideBar/index.tsx
@@ -0,0 +1,22 @@
+import {} from 'react-i18next';
+import { View } from 'react-native';
+
+import { useStyles } from './style';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import SessionList from './components/SessionList';
+
+export default function SideBar() {
+  const { styles } = useStyles();
+
+  return (
+    <View style={styles.container}>
+      <Header />
+
+      <SessionList />
+
+      <Footer />
+    </View>
+  );
+}
