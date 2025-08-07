9a3a641958facbf8c54d17668a27ccdda944effa
cy948@ultra7 <cy948@vip.qq.com>
2025-08-07 16:52:33 +0800

    :recycle: refactor: move types

--- src/app/[variants]/(main)/profile/usage/Client.tsx
+++ src/app/[variants]/(main)/profile/usage/Client.tsx
@@ -1,19 +1,18 @@
 'use client';
+
+import { UsageLog } from '@lobechat/types/src/usage';
+import { Row , DatePicker, DatePickerProps } from 'antd';
 import dayjs from 'dayjs';
-import { Col, Row } from 'antd';
 import { memo, useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Flexbox } from 'react-layout-kit';
 
 import { useClientDataSWR } from '@/libs/swr';
 import { usageService } from '@/services/usage';
-import { UsageLog } from '@/types/usage';
 
 import TotalRequest from './features/TotalRequest';
 import TotalSpend from './features/TotalSpend';
 import UsageCategories from './features/UsageCategories';
-import { DatePicker, DatePickerProps } from 'antd';
-import { useTranslation } from 'react-i18next';
-
 
 export interface UsageChartProps {
   data?: UsageLog[];
@@ -23,9 +22,8 @@ export interface UsageChartProps {
 }
 
 const Client = memo<{ mobile?: boolean }>(({ mobile }) => {
-
-  const { i18n } = useTranslation()
-  dayjs.locale(i18n.language)
+  const { i18n } = useTranslation();
+  dayjs.locale(i18n.language);
 
   const [dateRange, setDateRange] = useState<dayjs.Dayjs>(dayjs(new Date()));
   const [dateStrings, setDateStrings] = useState<string>();
@@ -50,13 +48,11 @@ const Client = memo<{ mobile?: boolean }>(({ mobile }) => {
   return (
     <Flexbox gap={mobile ? 0 : 24}>
       <Flexbox>
-        <DatePicker picker="month" value={dateRange} onChange={handleDateChange} />
+        <DatePicker onChange={handleDateChange} picker="month" value={dateRange} />
       </Flexbox>
+      <Flexbox>{data && <TotalSpend data={data} isLoading={isLoading} mobile={mobile} />}</Flexbox>
       <Flexbox>
-          {data && <TotalSpend data={data} isLoading={isLoading} mobile={mobile} />}
-      </Flexbox>
-      <Flexbox>
-          {data && <TotalRequest data={data} isLoading={isLoading} mobile={mobile} />}
+        {data && <TotalRequest data={data} isLoading={isLoading} mobile={mobile} />}
       </Flexbox>
       <Row>
         <UsageCategories data={data} />
