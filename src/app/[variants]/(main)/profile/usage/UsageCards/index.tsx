bd437bb3f285468fe6aa83740c242472a44c2591
cy948@ultra7 <cy948@vip.qq.com>
2025-08-08 01:01:28 +0800

    :recycle: refactor: remove use client

--- src/app/[variants]/(main)/profile/usage/UsageCards/index.tsx
+++ src/app/[variants]/(main)/profile/usage/UsageCards/index.tsx
@@ -1,19 +1,8 @@
-import { useState, memo } from 'react';
+import { memo } from 'react';
 import { useTheme } from 'antd-style'
 import { Flexbox } from 'react-layout-kit';
-import dayjs from 'dayjs';
 
-import { UsageLog } from '@lobechat/types/src/usage';
-import { type BarChartProps } from '@lobehub/charts';
-import { Segmented } from '@lobehub/ui';
-
-
-import { formatNumber } from '@/utils/format';
-import StatisticCard from '@/components/StatisticCard';
-import Statistic from '@/components/Statistic';
-import TitleWithPercentage from '@/components/StatisticCard/TitleWithPercentage';
-
-import { UsageChartProps, GroupBy } from '../Client';
+import { UsageChartProps } from '../Client';
 import TodaySpend from './TodaySpend'
 import MonthSpend from './MonthSpend';
 import ActiveModels from './ActiveModels';
