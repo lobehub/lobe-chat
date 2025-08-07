bf7a55141d67f8d84cee0ab22169ea8ab0dc71a1
cy948@ultra7 <cy948@vip.qq.com>
2025-08-07 18:55:09 +0800

    :lipstick: style: add group by ui

--- src/app/[variants]/(main)/profile/usage/Client.tsx
+++ src/app/[variants]/(main)/profile/usage/Client.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { UsageLog } from '@lobechat/types/src/usage';
-import { Row , DatePicker, DatePickerProps } from 'antd';
+import { Icon, Segmented } from '@lobehub/ui';
+import { Col, DatePicker, DatePickerProps, Row } from 'antd';
 import dayjs from 'dayjs';
+import { Brain, Codesandbox } from 'lucide-react';
 import { memo, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Flexbox } from 'react-layout-kit';
@@ -10,21 +12,29 @@ import { Flexbox } from 'react-layout-kit';
 import { useClientDataSWR } from '@/libs/swr';
 import { usageService } from '@/services/usage';
 
+import Welcome from '../stats/features/Welcome';
 import TotalRequest from './features/TotalRequest';
 import TotalSpend from './features/TotalSpend';
 import UsageCategories from './features/UsageCategories';
 
 export interface UsageChartProps {
   data?: UsageLog[];
+  groupBy?: GroupBy;
   inShare?: boolean;
   isLoading?: boolean;
   mobile?: boolean;
+}
+
+enum GroupBy {
+  Model = 'model',
+  Provider = 'provider',
 }
 
 const Client = memo<{ mobile?: boolean }>(({ mobile }) => {
   const { i18n } = useTranslation();
   dayjs.locale(i18n.language);
 
+  const [groupBy, setGroupBy] = useState<GroupBy>(GroupBy.Model);
   const [dateRange, setDateRange] = useState<dayjs.Dayjs>(dayjs(new Date()));
   const [dateStrings, setDateStrings] = useState<string>();
 
@@ -48,7 +58,38 @@ const Client = memo<{ mobile?: boolean }>(({ mobile }) => {
   return (
     <Flexbox gap={mobile ? 0 : 24}>
       <Flexbox>
-        <DatePicker onChange={handleDateChange} picker="month" value={dateRange} />
+        <Row>
+          <Col span={16}>
+            {mobile ? (
+              <Welcome mobile />
+            ) : (
+              <Flexbox align={'flex-start'} gap={16} horizontal justify={'space-between'}>
+                <Welcome />
+              </Flexbox>
+            )}
+          </Col>
+          <Col span={8}>
+            <Flexbox gap={16} horizontal>
+              <Segmented
+                onChange={(v) => setGroupBy(v as GroupBy)}
+                options={[
+                  {
+                    icon: <Icon icon={Codesandbox} />,
+                    label: 'Model',
+                    value: GroupBy.Model,
+                  },
+                  {
+                    icon: <Icon icon={Brain} />,
+                    label: 'Provider',
+                    value: GroupBy.Provider,
+                  },
+                ]}
+                value={groupBy}
+              />
+              <DatePicker onChange={handleDateChange} picker="month" value={dateRange} />
+            </Flexbox>
+          </Col>
+        </Row>
       </Flexbox>
       <Flexbox>{data && <TotalSpend data={data} isLoading={isLoading} mobile={mobile} />}</Flexbox>
       <Flexbox>
