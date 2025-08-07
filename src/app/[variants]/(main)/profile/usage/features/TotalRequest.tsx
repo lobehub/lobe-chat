8f3de5ca0ffbfcc3927a7b7ef65c838126bfa949
cy948@ultra7 <cy948@vip.qq.com>
2025-08-07 16:52:33 +0800

    :recycle: refactor: move types

--- src/app/[variants]/(main)/profile/usage/features/TotalRequest.tsx
+++ src/app/[variants]/(main)/profile/usage/features/TotalRequest.tsx
@@ -1,71 +1,74 @@
+import { UsageLog } from '@lobechat/types/src/usage';
 import { type BarChartProps } from '@lobehub/charts';
 import { FormGroup } from '@lobehub/ui';
 import { memo } from 'react';
 import { Flexbox } from 'react-layout-kit';
 
 import { FORM_STYLE } from '@/const/layoutTokens';
-import { UsageLog } from '@/types/usage';
+
 import { UsageChartProps } from '../Client';
 import { UsageBarChart } from './components/UsageBarChart';
 
-const formatData = (data: UsageLog[]): { categories: string[], data: BarChartProps['data'] } => {
-    if (!data || data.length === 0) return { categories: [], data: [] };
-    let formattedData: BarChartProps['data'] = [];
-    let cateByProvider: Map<string, number> = data.reduce((acc, log) => {
-        if (log.requestLogs) {
-            for (const item of log.requestLogs) {
-                if (item.provider) {
-                    acc.set(item.provider, 0);
-                }
-            }
+const formatData = (data: UsageLog[]): { categories: string[]; data: BarChartProps['data'] } => {
+  if (!data || data.length === 0) return { categories: [], data: [] };
+  let formattedData: BarChartProps['data'] = [];
+  let cateByProvider: Map<string, number> = data.reduce((acc, log) => {
+    if (log.requestLogs) {
+      for (const item of log.requestLogs) {
+        if (item.provider) {
+          acc.set(item.provider, 0);
         }
-        return acc;
-    }, new Map<string, number>());
-    const categories: string[] = Array.from(cateByProvider.keys());
-    formattedData = data.map((log) => {
-        const totalObj = {
-            day: log.day,
-            total: log.totalTokens,
-        }
-        let todayCateProvider = new Map<string, number>(cateByProvider);
-        for (const item of log.requestLogs) {
-            todayCateProvider.set(item.provider, (todayCateProvider.get(item.provider) || 0) + (item.totalTokens || 0));
-        }
-        return {
-            ...totalObj,
-            ...Object.fromEntries(todayCateProvider.entries()),
-        };
-    });
-    return {
-        categories,
-        data: formattedData,
+      }
     }
-}
+    return acc;
+  }, new Map<string, number>());
+  const categories: string[] = Array.from(cateByProvider.keys());
+  formattedData = data.map((log) => {
+    const totalObj = {
+      day: log.day,
+      total: log.totalTokens,
+    };
+    let todayCateProvider = new Map<string, number>(cateByProvider);
+    for (const item of log.requestLogs) {
+      todayCateProvider.set(
+        item.provider,
+        (todayCateProvider.get(item.provider) || 0) + (item.totalTokens || 0),
+      );
+    }
+    return {
+      ...totalObj,
+      ...Object.fromEntries(todayCateProvider.entries()),
+    };
+  });
+  return {
+    categories,
+    data: formattedData,
+  };
+};
 
-const AiRequest = memo<UsageChartProps>(
-    ({ isLoading, ...rest }) => {
-        const { categories, data } = formatData(rest?.data || [])
-        return (
-            <FormGroup
-                style={FORM_STYLE.style}
-                title={`Tokens ${data?.reduce((acc, log) => acc + log.total, 0) || 0}`}
-                variant={'borderless'}
-            >
-                <Flexbox paddingBlock={24}>
-                    {data &&
-                        <UsageBarChart
-                            categories={categories}
-                            data={data}
-                            index={'day'}
-                            loading={isLoading || !data}
-                            style={{ maxHeight: 250 }}
-                            stack
-                            showTooltip={true}
-                        />}
-                </Flexbox>
-            </FormGroup>
-        );
-    },
-);
+const AiRequest = memo<UsageChartProps>(({ isLoading, ...rest }) => {
+  const { categories, data } = formatData(rest?.data || []);
+  return (
+    <FormGroup
+      style={FORM_STYLE.style}
+      title={`Tokens ${data?.reduce((acc, log) => acc + log.total, 0) || 0}`}
+      variant={'borderless'}
+    >
+      <Flexbox paddingBlock={24}>
+        {data && (
+          <UsageBarChart
+            categories={categories}
+            data={data}
+            index={'day'}
+            loading={isLoading || !data}
+            showTooltip={true}
+            stack
+            style={{ maxHeight: 250 }}
+          />
+        )}
+      </Flexbox>
+    </FormGroup>
+  );
+});
 
 export default AiRequest;
