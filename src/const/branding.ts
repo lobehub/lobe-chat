76eedda9dda1815f51dc4c00eb1cfe2ccb6fc36a
Jamie Stivala <jamiestivala@gmail.com>
2025-08-13 12:46:26 +0200

    Updated the branding to OneAI reflecting marketing changes

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -2,9 +2,9 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'OneJA AI';
+export const LOBE_CHAT_CLOUD = 'OneAI';
 
-export const BRANDING_NAME = 'OneJA AI';
+export const BRANDING_NAME = 'OneAI';
 export const BRANDING_LOGO_URL = '/icons/icon-512x512.png';
 
 export const ORG_NAME = 'JA Worldwide';
