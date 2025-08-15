8ff9892db0cd3f3f1f155eb7b6598f6c890e690c
YuTengjing <ytj2713151713@gmail.com>
2025-08-15 19:09:50 +0800

    chore: add more default aspect ratios

--- packages/const/src/image.ts
+++ packages/const/src/image.ts
@@ -3,4 +3,16 @@
  */
 export const DEFAULT_ASPECT_RATIO = '1:1';
 
-export const PRESET_ASPECT_RATIOS = [DEFAULT_ASPECT_RATIO, '16:9', '9:16', '4:3', '3:4'];
+export const PRESET_ASPECT_RATIOS = [
+  DEFAULT_ASPECT_RATIO, // '1:1' - 正方形，最常用
+  '16:9',   // 现代显示器/电视/视频标准
+  '9:16',   // 手机竖屏/短视频
+  '4:3',    // 传统显示器/照片
+  '3:4',    // 传统竖屏照片
+  '3:2',    // 经典照片比例横屏
+  '2:3',    // 经典照片比例竖屏
+  '4:5',    // Instagram 竖屏帖子
+  '5:4',    // Instagram 方形帖子变体/老式显示器
+  '21:9',   // 超宽显示器/电影
+  '9:21',   // 超窄竖屏
+];
