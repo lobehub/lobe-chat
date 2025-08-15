18104fe7b942f3c24783062f72928c50b658a64d
cy948 <cy948@qq.com>
2025-08-15 20:58:28 +0800

    :lipstick: style: add table

--- src/services/usage.ts
+++ src/services/usage.ts
@@ -4,6 +4,10 @@ class UsageService {
     getUsages = async (mo?: string) => {
         return lambdaClient.usage.getUsages.query({ mo });
     };
+
+    getRequests = async (mo?: string) => {
+        return lambdaClient.usage.getSpendLogs.query({ mo });
+    }
 }
 
 export const usageService = new UsageService();
