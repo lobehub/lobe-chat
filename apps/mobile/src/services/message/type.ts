b9376586313f61a431858e9118d73aaed4cc22b1
Tsuki <976499226@qq.com>
2025-08-18 13:46:19 +0800

    chore(mobile): update services

---
+++ apps/mobile/src/services/message/type.ts
@@ -0,0 +1,49 @@
+import type { HeatmapsProps } from '@lobehub/charts';
+
+import {
+  ChatMessage,
+  ChatMessageError,
+  ChatMessagePluginError,
+  ChatTTS,
+  ChatTranslate,
+  CreateMessageParams,
+  MessageItem,
+  ModelRankItem,
+  UpdateMessageParams,
+} from '@/types/message';
+
+/* eslint-disable typescript-sort-keys/interface */
+
+export interface IMessageService {
+  createMessage(data: CreateMessageParams): Promise<string>;
+  batchCreateMessages(messages: MessageItem[]): Promise<any>;
+
+  getMessages(sessionId: string, topicId?: string): Promise<ChatMessage[]>;
+  getAllMessages(): Promise<ChatMessage[]>;
+  getAllMessagesInSession(sessionId: string): Promise<ChatMessage[]>;
+  countMessages(params?: {
+    endDate?: string;
+    range?: [string, string];
+    startDate?: string;
+  }): Promise<number>;
+  countWords(params?: {
+    endDate?: string;
+    range?: [string, string];
+    startDate?: string;
+  }): Promise<number>;
+  rankModels(): Promise<ModelRankItem[]>;
+  getHeatmaps(): Promise<HeatmapsProps['data']>;
+  updateMessageError(id: string, error: ChatMessageError): Promise<any>;
+  updateMessage(id: string, message: Partial<UpdateMessageParams>): Promise<any>;
+  updateMessageTTS(id: string, tts: Partial<ChatTTS> | false): Promise<any>;
+  updateMessageTranslate(id: string, translate: Partial<ChatTranslate> | false): Promise<any>;
+  updateMessagePluginState(id: string, value: Record<string, any>): Promise<any>;
+  updateMessagePluginError(id: string, value: ChatMessagePluginError | null): Promise<any>;
+  updateMessagePluginArguments(id: string, value: string | Record<string, any>): Promise<any>;
+  removeMessage(id: string): Promise<any>;
+  removeMessages(ids: string[]): Promise<any>;
+  removeMessagesByAssistant(assistantId: string, topicId?: string): Promise<any>;
+  removeAllMessages(): Promise<any>;
+  messageCountToCheckTrace(): Promise<boolean>;
+  hasMessages(): Promise<boolean>;
+}
--- apps/mobile/src/services/session.ts
+++
@@ -1,6 +0,0 @@
-// // 重新导出新的 Session Service
-// export { sessionService } from './session';
-// export { sessionService as default } from './session';
-//
-// // 导出类型
-// export type { ISessionService } from './session/type';
