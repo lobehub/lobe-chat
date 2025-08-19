4d1631f6794d466f803e92954dda7ae7df4ac80a
DarinVerheijke <darinverheijke@hotmail.com>
2025-08-19 09:42:26 +0200

    fix linting issues

--- packages/types/src/user/settings/keyVaults.ts
+++ packages/types/src/user/settings/keyVaults.ts
@@ -49,8 +49,8 @@ export interface UserKeyVaults extends SearchEngineKeyVaults {
   cloudflare?: CloudflareKeyVault;
   cohere?: OpenAICompatibleKeyVault;
   deepseek?: OpenAICompatibleKeyVault;
-  featherlessai?: OpenAICompatibleKeyVault;
   fal?: FalKeyVault;
+  featherlessai?: OpenAICompatibleKeyVault;
   fireworksai?: OpenAICompatibleKeyVault;
   giteeai?: OpenAICompatibleKeyVault;
   github?: OpenAICompatibleKeyVault;
