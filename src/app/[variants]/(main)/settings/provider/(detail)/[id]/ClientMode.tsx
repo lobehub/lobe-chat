9ec3315595a8261051a1e74292b90792c5cba89a
YuTengjing <ytj2713151713@gmail.com>
2025-08-21 22:43:55 +0800

    🐛 fix: can't load custom provider config (#8880)

--- src/app/[variants]/(main)/settings/provider/(detail)/[id]/ClientMode.tsx
+++ src/app/[variants]/(main)/settings/provider/(detail)/[id]/ClientMode.tsx
@@ -7,11 +7,15 @@ import { Flexbox } from 'react-layout-kit';
 import Loading from '@/components/Loading/BrandTextLoading';
 import { useClientDataSWR } from '@/libs/swr';
 import { aiProviderService } from '@/services/aiProvider';
+import { useAiInfraStore } from '@/store/aiInfra';
 
 import ModelList from '../../features/ModelList';
 import ProviderConfig from '../../features/ProviderConfig';
 
 const ClientMode = memo<{ id: string }>(({ id }) => {
+  const useFetchAiProviderItem = useAiInfraStore((s) => s.useFetchAiProviderItem);
+  useFetchAiProviderItem(id);
+
   const { data, isLoading } = useClientDataSWR('get-client-provider', () =>
     aiProviderService.getAiProviderById(id),
   );
