7e5563f4f18e16ce02a787f79e5f8a7d910001d9
arvinxx <arvinx@foxmail.com>
2025-08-21 21:08:47 +0800

    refactor db

--- packages/database/vitest.config.ts
+++ packages/database/vitest.config.ts
@@ -1,5 +1,5 @@
 import { resolve } from 'node:path';
-import { defineConfig } from 'vitest/config';
+import { coverageConfigDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
@@ -11,6 +11,26 @@ export default defineConfig({
       '@': resolve(__dirname, '../../src'),
       /* eslint-enable */
     },
-    environment: 'happy-dom',
+    coverage: {
+      all: false,
+      exclude: [
+        // https://github.com/lobehub/lobe-chat/pull/7265
+        ...coverageConfigDefaults.exclude,
+        'src/server/core/dbForTest.ts',
+      ],
+      include: ['src/models/**/*.ts', 'src/server/**/*.ts'],
+      provider: 'v8',
+      reporter: ['text', 'json', 'lcov', 'text-summary'],
+    },
+    env: {
+      TEST_SERVER_DB: '1',
+    },
+    environment: 'node',
+    poolOptions: {
+      forks: {
+        singleFork: true,
+      },
+    },
+    setupFiles: './tests/setup-db.ts',
   },
 });
