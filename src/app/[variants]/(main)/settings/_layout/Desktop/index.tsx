655650e8aa540ddf240df5d8bacea3f4c4a6bf1b
ONLY-yours <1349021570@qq.com>
2025-08-21 18:10:07 +0800

    refactor: 剥离usePathname 在 client 端多次触发导致的 rerender 问题

--- src/app/[variants]/(main)/settings/_layout/Desktop/index.tsx
+++ src/app/[variants]/(main)/settings/_layout/Desktop/index.tsx
@@ -1,18 +1,13 @@
 'use client';
 
-import { Tag } from '@lobehub/ui';
 import { useResponsive, useTheme } from 'antd-style';
 import { usePathname } from 'next/navigation';
-import { memo, useRef } from 'react';
-import { useTranslation } from 'react-i18next';
+import { PropsWithChildren, memo, useEffect, useRef } from 'react';
 import { Flexbox } from 'react-layout-kit';
 
 import InitClientDB from '@/features/InitClientDB';
 import Footer from '@/features/Setting/Footer';
 import SettingContainer from '@/features/Setting/SettingContainer';
-import { useActiveSettingsKey } from '@/hooks/useActiveTabKey';
-import { useProviderName } from '@/hooks/useProviderName';
-import { SettingsTabs } from '@/store/global/initialState';
 
 import { LayoutProps } from '../type';
 import Header from './Header';
@@ -20,17 +15,25 @@ import SideBar from './SideBar';
 
 const SKIP_PATHS = ['/settings/provider', '/settings/agent'];
 
+const ContentContainer = memo<PropsWithChildren>(({ children }) => {
+  const pathname = usePathname();
+  const isSkip = SKIP_PATHS.some((path) => pathname.includes(path));
+
+  return isSkip ? (
+    children
+  ) : (
+    <SettingContainer addonAfter={<Footer />}>{children}</SettingContainer>
+  );
+});
+
 const Layout = memo<LayoutProps>(({ children, category }) => {
   const ref = useRef<any>(null);
   const { md = true } = useResponsive();
-  const { t } = useTranslation('setting');
-  const activeKey = useActiveSettingsKey();
   const theme = useTheme();
-  const pathname = usePathname();
 
-  const isSkip = SKIP_PATHS.some((path) => pathname.includes(path));
-  const isProvider = pathname.includes('/settings/provider/');
-  const providerName = useProviderName(activeKey);
+  useEffect(() => {
+    console.log('settings render');
+  });
 
   return (
     <Flexbox
@@ -42,19 +45,9 @@ const Layout = memo<LayoutProps>(({ children, category }) => {
       {md ? (
         <SideBar>{category}</SideBar>
       ) : (
-        <Header
-          getContainer={() => ref.current}
-          title={
-            <>
-              {isProvider ? providerName : t(`tab.${activeKey}`)}
-              {activeKey === SettingsTabs.Sync && <Tag color={'gold'}>{t('tab.experiment')}</Tag>}
-            </>
-          }
-        >
-          {category}
-        </Header>
+        <Header getContainer={() => ref.current!}>{category}</Header>
       )}
-      {isSkip ? children : <SettingContainer addonAfter={<Footer />}>{children}</SettingContainer>}
+      <ContentContainer>{children}</ContentContainer>
       <InitClientDB />
     </Flexbox>
   );
