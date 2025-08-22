209c407f55840cbfdf9d72792db649a62ec9de54
arvinxx <arvinx@foxmail.com>
2025-08-22 12:59:13 +0800

    fix tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       '@/libs/model-runtime': resolve(__dirname, './packages/model-runtime/src'),
       '@/database/_deprecated': resolve(__dirname, './src/database/_deprecated'),
       '@/database': resolve(__dirname, './packages/database/src'),
+      '@/utils/client': resolve(__dirname, './src/utils/client'),
       '@/utils/errorResponse': resolve(__dirname, './src/utils/errorResponse'),
       '@/utils': resolve(__dirname, './packages/utils/src'),
       '@/types': resolve(__dirname, './packages/types/src'),
