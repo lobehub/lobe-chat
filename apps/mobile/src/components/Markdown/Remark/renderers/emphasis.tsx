8fedf0c85589fe30b1e70124a0803ae0ce11d287
Rdmclin2 <rdmclin2@gmail.com>
2025-08-22 13:06:21 +0800

    Feat/mobile remark install error (#8891)
    * chore: modify Markdown styles and  Tag style
    
    * feat: Tag support color prop
    
    * feat: Tag Component support preset color
    
    * chore: 优化 playground Card style
    
    * feat: migrate to remark
    
    * feat: add Remark components
    
    * feat: Toast 支持静态方法
    
    * feat: animate loading message and simplify Toast props
    
    * chore: optimize Toast implementation

---
+++ apps/mobile/src/components/Markdown/Remark/renderers/emphasis.tsx
@@ -0,0 +1,19 @@
+import { Emphasis } from 'mdast';
+import { ReactNode } from 'react';
+import { Text } from 'react-native';
+
+import { useMarkdownContext } from '../context';
+import { RendererArgs } from './renderers';
+
+export const EmphasisRenderer = ({ node }: RendererArgs<Emphasis>): ReactNode => {
+  const { renderers, styles } = useMarkdownContext();
+  const { PhrasingContentRenderer } = renderers;
+
+  return (
+    <Text style={styles.emphasis}>
+      {node.children.map((child, idx) => (
+        <PhrasingContentRenderer index={idx} key={idx} node={child} parent={node} />
+      ))}
+    </Text>
+  );
+};
