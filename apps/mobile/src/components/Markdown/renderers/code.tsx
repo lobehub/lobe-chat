05e2a4cbb106a13a786e6d818e2125b4c0e6975b
Rdmclin2 <rdmclin2@gmail.com>
2025-08-31 14:49:09 +0800

    Feat/setting screens (#9014)
    * chore: modify Markdown styles and  Tag style
    
    * feat: Tag support color prop
    
    * feat: Tag Component support preset color
    
    * chore: 优化 playground Card style
    
    * feat: migrate to remark
    
    * feat: add Remark components
    
    * feat: Toast 支持静态方法
    
    * feat: animate loading message and simplify Toast props
    
    * chore: optimize Toast implementation
    
    * chore: remove unnecessary custtom renderers
    
    * chore: optimize Markdown Render and add Slider Component
    
    * chore: unified api with web version
    
    * chore: remove unnecessary props for slider
    
    * fix: accurately control step slider
    
    * feat: support marks props
    
    * fix: 修复 mark dot 位置
    
    * chore: optimize Slider token usage
    
    * feat: setting support fontSize
    
    * chore: Playground support fontSize setting
    
    * fix: Slider 样式
    
    * chore: prevent slider change
    
    * chore: update i18n keys
    
    * chore: optimize theme provider tokens
    
    * chore: remove unnecessary code
    
    * fix: slider demo
    
    * fix: theme-token type error
    
    * fix: auth expired handle
    
    * feat: handle auth expired redirect
    
    * feat: add developer setting
    
    * fix: login expired handle
    
    * feat: add SettingGroup to automatically add isLast prop and add prevew page
    
    * chore: adjust Preview component
    
    * feat: move theme settings to a seperate page
    
    * feat: 优化调整 Markdown 样式
    
    * fix: list style
    
    * chore: adjust markdown table style
    
    * fix: blockquoto style
    
    * chore: remove Markdown type
    
    * chore: 迁移 TokenDisplay
    
    * chore: optimize Highlight display
    
    * feat: support katex render

---
+++ apps/mobile/src/components/Markdown/renderers/code.tsx
@@ -0,0 +1,30 @@
+import { Code } from 'mdast';
+import { ReactNode } from 'react';
+
+import Highlighter from '@/components/Highlighter';
+
+import { useMarkdownContext } from '../context';
+import { RendererArgs } from './renderers';
+
+const countLines = (str: string): number => {
+  const regex = /\n/g;
+  const matches = str.match(regex);
+  return matches ? matches.length : 1;
+};
+
+export const CodeRenderer = ({ node }: RendererArgs<Code>): ReactNode => {
+  const { styles } = useMarkdownContext();
+  const code = node.value;
+  const isSingleLine = countLines(code) <= 1 && code.length <= 32;
+
+  return (
+    <Highlighter
+      code={node.value}
+      copyable={true}
+      fullFeatured={!isSingleLine}
+      lang={node.lang || 'text'}
+      showLanguage={true}
+      style={styles.blockCode}
+    />
+  );
+};
--- apps/mobile/src/theme/ThemeProvider/createStyles.ts
+++ apps/mobile/src/theme/ThemeProvider/createStyles.ts
@@ -2,15 +2,13 @@ import { useMemo } from 'react';
 import { StyleSheet, ViewStyle, TextStyle, ImageStyle } from 'react-native';
 
 import { useThemeToken, useTheme } from './context';
+import { AliasToken } from '../interface';
 
 // 定义样式类型
 export type NamedStyles<T> = { [P in keyof T]: ViewStyle | TextStyle | ImageStyle };
 
 // StyleCreator 类型，接收token和自定义参数，返回样式对象
-type StyleCreator<T, P extends any[] = []> = (
-  token: ReturnType<typeof useThemeToken>,
-  ...customParams: P
-) => T;
+type StyleCreator<T, P extends any[] = []> = (token: AliasToken, ...customParams: P) => T;
 
 /**
  * 创建样式钩子
@@ -18,7 +16,7 @@ type StyleCreator<T, P extends any[] = []> = (
  * @param creator 样式创建函数，接收 token 和用户参数
  * @returns 返回 useStyles 钩子函数
  */
-export const createStyles = <T extends NamedStyles<T>, P extends any[] = []>(
+export const createStyles = <T extends NamedStyles<T> | NamedStyles<any>, P extends any[] = []>(
   creator: StyleCreator<T, P>,
 ) => {
   // 返回一个hook函数，自动推断参数类型
