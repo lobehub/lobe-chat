c9f08540dddfd86228aa0121de7828888c7ce30c
rdmclin2 <rdmclin2@gmail.com>
2025-09-02 21:17:24 +0800

    feat: add fontSize Preview page

--- apps/mobile/app/(main)/setting/fontSize/index.tsx
+++ apps/mobile/app/(main)/setting/fontSize/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { ScrollView, Text } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 
 import Slider from '@/components/Slider';
 import { FONT_SIZE_STANDARD, FONT_SIZE_LARGE, FONT_SIZE_SMALL } from '@/const/common';
 import { useSettingStore } from '@/store/setting';
-import { SettingItem, SettingGroup } from '../(components)';
-import { useStyles } from '../styles';
+import { useStyles } from './styles';
+import Preview from './components/Preview';
 
 export default function FontSizeSettingScreen() {
   const { t } = useTranslation(['setting']);
@@ -15,32 +15,33 @@ export default function FontSizeSettingScreen() {
   const { fontSize, setFontSize } = useSettingStore();
 
   return (
-    <ScrollView style={styles.container}>
-      <SettingGroup>
-        <SettingItem
-          customContent={
-            <Slider
-              marks={{
-                [FONT_SIZE_LARGE]: { label: <Text style={styles.fontSizeLarge}>A</Text> },
-                [FONT_SIZE_SMALL]: { label: <Text style={styles.fontSizeSmall}>A</Text> },
-                [FONT_SIZE_STANDARD]: {
-                  label: (
-                    <Text style={styles.fontSizeStandard}>
-                      {t('fontSize.standard', { ns: 'setting' })}
-                    </Text>
-                  ),
-                },
-              }}
-              max={FONT_SIZE_LARGE}
-              min={FONT_SIZE_SMALL}
-              onChangeComplete={setFontSize}
-              step={1}
-              value={fontSize}
-            />
-          }
-          title={t('fontSize.title', { ns: 'setting' })}
+    <View style={{ flex: 1 }}>
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1, paddingBottom: 120 }}
+        style={styles.container}
+      >
+        <Preview />
+      </ScrollView>
+      <View style={styles.bottomBarWrapper}>
+        <Slider
+          marks={{
+            [FONT_SIZE_LARGE]: { label: <Text style={styles.fontSizeLarge}>A</Text> },
+            [FONT_SIZE_SMALL]: { label: <Text style={styles.fontSizeSmall}>A</Text> },
+            [FONT_SIZE_STANDARD]: {
+              label: (
+                <Text style={styles.fontSizeStandard}>
+                  {t('fontSize.standard', { ns: 'setting' })}
+                </Text>
+              ),
+            },
+          }}
+          max={FONT_SIZE_LARGE}
+          min={FONT_SIZE_SMALL}
+          onChangeComplete={setFontSize}
+          step={1}
+          value={fontSize}
         />
-      </SettingGroup>
-    </ScrollView>
+      </View>
+    </View>
   );
 }
--- apps/mobile/app/(main)/setting/styles.ts
+++ apps/mobile/app/(main)/setting/styles.ts
@@ -1,22 +1,9 @@
 import { createStyles } from '@/theme';
-import { FONT_SIZE_LARGE, FONT_SIZE_SMALL, FONT_SIZE_STANDARD } from '@/const/common';
 
 export const useStyles = createStyles((token) => ({
   container: {
     backgroundColor: token.colorBgLayout,
     flex: 1,
     padding: token.paddingContentHorizontal,
-  },
-  fontSizeLarge: {
-    color: token.colorText,
-    fontSize: FONT_SIZE_LARGE,
-  },
-  fontSizeSmall: {
-    color: token.colorText,
-    fontSize: FONT_SIZE_SMALL,
-  },
-  fontSizeStandard: {
-    color: token.colorText,
-    fontSize: FONT_SIZE_STANDARD,
   },
 }));
