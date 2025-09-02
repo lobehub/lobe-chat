b5ffbe12bb2797e43f63a3b3d7689d64bcb26076
bbbugg <daming20120101@163.com>
2025-09-02 21:31:33 +0800

    💄 style: update form items to support search and imageOutput abilities

--- src/app/[variants]/(main)/settings/provider/features/ModelList/CreateNewModelModal/Form.tsx
+++ src/app/[variants]/(main)/settings/provider/features/ModelList/CreateNewModelModal/Form.tsx
@@ -102,6 +102,23 @@ const ModelConfigForm = memo<ModelConfigFormProps>(
           >
             <Checkbox />
           </Form.Item>
+          <Form.Item
+            extra={t('providerModels.item.modelConfig.search.extra')}
+            label={t('providerModels.item.modelConfig.search.title')}
+            name={['abilities', 'search']}
+            valuePropName={'checked'}
+          >
+            <Checkbox />
+          </Form.Item>
+
+          <Form.Item
+            extra={t('providerModels.item.modelConfig.imageOutput.extra')}
+            label={t('providerModels.item.modelConfig.imageOutput.title')}
+            name={['abilities', 'imageOutput']}
+            valuePropName={'checked'}
+          >
+            <Checkbox />
+          </Form.Item>
           {/*<Form.Item*/}
           {/*  extra={t('providerModels.item.modelConfig.files.extra')}*/}
           {/*  label={t('providerModels.item.modelConfig.files.title')}*/}
