f0d27bf08769cd86cf686e5d5610095b50527ecb
ONLY-yours <1349021570@qq.com>
2025-09-02 14:23:37 +0800

    fix: delte useless pages in settings

--- src/app/[variants]/(main)/settings/proxy/index.tsx
+++ src/app/[variants]/(main)/settings/proxy/index.tsx
@@ -1,17 +1,11 @@
 'use client';
 
-import { useTranslation } from 'react-i18next';
-
-import PageTitle from '@/components/PageTitle';
-
 import ProxyForm from './features/ProxyForm';
 
 const ProxySettings = () => {
-  const { t } = useTranslation('setting');
 
   return (
     <div>
-      <PageTitle title={t('tab.proxy')} />
       <ProxyForm />
     </div>
   );
