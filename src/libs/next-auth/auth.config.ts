bde934c43a8a26da28a25453046eecbd0cc9d76d
cy948 <cy948@qq.com>
2025-09-04 19:48:18 +0800

    :bug: fix: only init db adapter in server service

--- src/libs/next-auth/auth.config.ts
+++ src/libs/next-auth/auth.config.ts
@@ -1,32 +1,36 @@
 import type { NextAuthConfig } from 'next-auth';
 
-import { ssoProviders } from './sso-providers';
-import { LobeNextAuthDbAdapter } from './adapter';
 import { getAuthConfig } from '@/config/auth';
+import { getServerDBConfig } from '@/config/db';
+
+import { LobeNextAuthDbAdapter } from './adapter';
+import { ssoProviders } from './sso-providers';
 
 const {
   NEXT_AUTH_DEBUG,
   NEXT_AUTH_SECRET,
   NEXT_AUTH_SSO_SESSION_STRATEGY,
   NEXT_AUTH_SSO_PROVIDERS,
-  NEXT_PUBLIC_ENABLE_NEXT_AUTH
+  NEXT_PUBLIC_ENABLE_NEXT_AUTH,
 } = getAuthConfig();
+
+const { NEXT_PUBLIC_ENABLED_SERVER_SERVICE } = getServerDBConfig();
 
 export const initSSOProviders = () => {
   return NEXT_PUBLIC_ENABLE_NEXT_AUTH
     ? NEXT_AUTH_SSO_PROVIDERS.split(/[,，]/).map((provider) => {
-      const validProvider = ssoProviders.find((item) => item.id === provider.trim());
+        const validProvider = ssoProviders.find((item) => item.id === provider.trim());
 
-      if (validProvider) return validProvider.provider;
+        if (validProvider) return validProvider.provider;
 
-      throw new Error(`[NextAuth] provider ${provider} is not supported`);
-    })
+        throw new Error(`[NextAuth] provider ${provider} is not supported`);
+      })
     : [];
 };
 
 // Notice this is only an object, not a full Auth.js instance
 export default {
-  adapter: LobeNextAuthDbAdapter(),
+  adapter: NEXT_PUBLIC_ENABLED_SERVER_SERVICE ? LobeNextAuthDbAdapter() : undefined,
   callbacks: {
     // Note: Data processing order of callback: authorize --> jwt --> session
     async jwt({ token, user }) {
@@ -56,7 +60,8 @@ export default {
   providers: initSSOProviders(),
   secret: NEXT_AUTH_SECRET,
   session: {
-    strategy: NEXT_AUTH_SSO_SESSION_STRATEGY,
+    // Force use JWT if server service is disabled
+    strategy: NEXT_PUBLIC_ENABLED_SERVER_SERVICE ? NEXT_AUTH_SSO_SESSION_STRATEGY : 'jwt',
   },
   trustHost: process.env?.AUTH_TRUST_HOST ? process.env.AUTH_TRUST_HOST === 'true' : true,
 } satisfies NextAuthConfig;
