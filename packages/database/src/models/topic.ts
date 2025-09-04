4813b6df273aa818412f03079743b8ed73963df2
Arvin Xu <arvinx@foxmail.com>
2025-09-04 12:53:55 +0800

    ✨ feat: refactor to speed up send message in server mode (#9046)
    * refactor send message
    
    * support message rag data
    
    * fix
    
    * fix tests
    
    * fix lint
    
    * add tests
    
    * fix bug

--- packages/database/src/models/topic.ts
+++ packages/database/src/models/topic.ts
@@ -1,23 +1,18 @@
 import { and, count, desc, eq, gt, ilike, inArray, isNull, sql } from 'drizzle-orm';
 
-import { LobeChatDatabase } from '../type';
-import {
-  genEndDateWhere,
-  genRangeWhere,
-  genStartDateWhere,
-  genWhere,
-} from '../utils/genWhere';
-import { idGenerator } from '../utils/idGenerator';
 import { MessageItem } from '@/types/message';
 import { TopicRankItem } from '@/types/topic';
 
 import { TopicItem, messages, topics } from '../schemas';
+import { LobeChatDatabase } from '../type';
+import { genEndDateWhere, genRangeWhere, genStartDateWhere, genWhere } from '../utils/genWhere';
+import { idGenerator } from '../utils/idGenerator';
 
 export interface CreateTopicParams {
   favorite?: boolean;
   messages?: string[];
   sessionId?: string | null;
-  title: string;
+  title?: string;
 }
 
 interface QueryTopicParams {
