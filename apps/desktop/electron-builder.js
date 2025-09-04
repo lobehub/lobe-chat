5e116babdcd1e690471510dd581e6d65c5ebd7f1
YuTengjing <ytj2713151713@gmail.com>
2025-09-04 18:08:03 +0800

    chore: test built intel version

--- apps/desktop/electron-builder.js
+++ apps/desktop/electron-builder.js
@@ -89,7 +89,10 @@ const config = {
     target:
       // 降低构建时间，nightly 只打 arm64
       isNightly
-        ? [{ arch: ['arm64'], target: 'dmg' }]
+        ? [
+            { arch: ['x64', 'arm64'], target: 'dmg' },
+            { arch: ['x64', 'arm64'], target: 'zip' },
+          ]
         : [
             { arch: ['x64', 'arm64'], target: 'dmg' },
             { arch: ['x64', 'arm64'], target: 'zip' },
