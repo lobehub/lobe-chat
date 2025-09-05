7b179248b403db36e0909570b91203fd93d73ad4
arvinxx <arvinx@foxmail.com>
2025-09-06 00:43:24 +0800

    fix mobile

--- src/app/[variants]/(main)/chat/(workspace)/@conversation/features/ChatInput/Mobile/index.tsx
+++ src/app/[variants]/(main)/chat/(workspace)/@conversation/features/ChatInput/Mobile/index.tsx
@@ -1,6 +1,9 @@
 'use client';
 
+import { Alert } from '@lobehub/ui';
 import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Flexbox } from 'react-layout-kit';
 
 import {
   type ActionKey,
@@ -8,6 +11,8 @@ import {
   MobileChatInput as ChatInput,
   ChatInputProvider,
 } from '@/features/ChatInput';
+import { useChatStore } from '@/store/chat';
+import { aiChatSelectors } from '@/store/chat/slices/aiChat/selectors';
 
 import { useSend } from '../useSend';
 
@@ -25,16 +30,38 @@ const leftActions: ActionKeys[] = [
 const rightActions: ActionKey[] = ['saveTopic'];
 
 const MobileChatInput = memo(() => {
+  const { t } = useTranslation('chat');
   const { send, disabled, generating, stop } = useSend();
 
+  const [mainInputSendErrorMsg, clearSendMessageError] = useChatStore((s) => [
+    aiChatSelectors.isCurrentSendMessageError(s),
+    s.clearSendMessageError,
+  ]);
   return (
     <ChatInputProvider
+      chatInputEditorRef={(instance) => {
+        if (!instance) return;
+        useChatStore.setState({ mainInputEditor: instance });
+      }}
       leftActions={leftActions}
       mobile
+      onMarkdownContentChange={(content) => {
+        useChatStore.setState({ inputMessage: content });
+      }}
       onSend={() => send()}
       rightActions={rightActions}
       sendButtonProps={{ disabled, generating, onStop: stop }}
     >
+      {mainInputSendErrorMsg && (
+        <Flexbox paddingBlock={'0 6px'} paddingInline={12}>
+          <Alert
+            closable
+            message={t('input.errorMsg', { errorMsg: mainInputSendErrorMsg })}
+            onClose={clearSendMessageError}
+            type={'warning'}
+          />
+        </Flexbox>
+      )}
       <ChatInput />
     </ChatInputProvider>
   );
