ed5a331051f20f604dea97d78b09d14d6816303c
Neko Ayaka <neko@ayaka.moe>
2025-09-09 15:10:25 +0800

    chore: instrument

--- src/instrumentation.node.ts
+++ src/instrumentation.node.ts
@@ -1,5 +1,8 @@
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { FetchInstrumentation } from '@opentelemetry/instrumentation-fetch';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { PgInstrumentation } from '@opentelemetry/instrumentation-pg';
 import { resourceFromAttributes } from '@opentelemetry/resources';
 import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { NodeSDK } from '@opentelemetry/sdk-node';
@@ -8,6 +11,11 @@ import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from '@opentelemetry/semantic
 import { version } from '../package.json';
 
 const sdk = new NodeSDK({
+  instrumentations: [
+    new PgInstrumentation(),
+    new FetchInstrumentation(),
+    new HttpInstrumentation(),
+  ],
   metricReader: new PeriodicExportingMetricReader({
     exporter: new OTLPMetricExporter(),
   }),
