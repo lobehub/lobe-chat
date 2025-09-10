874e1efa3878adad289286d6c4b49b4c7f8aa573
olakareemjuwon1992-eng <olakareemjuwon1992@gmail.com>
2025-09-11 00:05:59 +0100

    Update branding.ts

--- packages/const/src/branding.ts
+++ packages/const/src/branding.ts
@@ -2,12 +2,12 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
+export const LOBE_CHAT_CLOUD = 'ImoogleAI Cloud';
 
-export const BRANDING_NAME = 'LobeChat';
+export const BRANDING_NAME = 'ImoogleAI';
 export const BRANDING_LOGO_URL = '';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = 'Imoogle';
 
 export const BRANDING_URL = {
   help: undefined,
@@ -16,14 +16,14 @@ export const BRANDING_URL = {
 };
 
 export const SOCIAL_URL = {
-  discord: 'https://discord.gg/AYFPHvv2jT',
-  github: 'https://github.com/lobehub',
-  medium: 'https://medium.com/@lobehub',
-  x: 'https://x.com/lobehub',
-  youtube: 'https://www.youtube.com/@lobehub',
+  discord: 'https://discord.gg/',
+  github: 'https://github.com/',
+  medium: 'https://medium.com/',
+  x: 'https://x.com/',
+  youtube: 'https://www.youtube.com/',
 };
 
 export const BRANDING_EMAIL = {
-  business: 'hello@lobehub.com',
-  support: 'support@lobehub.com',
+  business: 'info@imoogleai.xyz',
+  support: 'support@imoogleai.xyz',
 };
