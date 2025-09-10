2a07edee7fb4045241f32d3f0a97056bc2816ebe
olakareemjuwon1992-eng <olakareemjuwon1992@gmail.com>
2025-09-11 00:08:26 +0100

    Update tts.ts

--- packages/const/src/settings/tts.ts
+++ packages/const/src/settings/tts.ts
@@ -2,7 +2,7 @@ import { UserTTSConfig } from '@lobechat/types';
 
 export const DEFAULT_TTS_CONFIG: UserTTSConfig = {
   openAI: {
-    sttModel: 'whisper-1',
+    sttModel: 'imoogle-nov',
     ttsModel: 'tts-1',
   },
   sttAutoStop: true,
