83e07fd5d1089e1108b825b4723f7eb1a664d4b8
olakareemjuwon1992-eng <olakareemjuwon1992@gmail.com>
2025-09-11 00:06:51 +0100

    Update common.ts

--- packages/const/src/settings/common.ts
+++ packages/const/src/settings/common.ts
@@ -3,7 +3,7 @@ import { UserGeneralConfig } from '@lobechat/types';
 export const DEFAULT_COMMON_SETTINGS: UserGeneralConfig = {
   animationMode: 'agile',
   fontSize: 14,
-  highlighterTheme: 'lobe-theme',
-  mermaidTheme: 'lobe-theme',
+  highlighterTheme: 'Imoogle-theme',
+  mermaidTheme: 'Imoogle-theme',
   transitionMode: 'fadeIn',
 };
