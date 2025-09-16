8f501c4b0aa590672cbf60d405baa8a31dfa67cb
arvinxx <arvinx@foxmail.com>
2025-09-16 11:37:59 +0800

    improve agent schema

--- packages/database/migrations/0031_add_agent_index.sql
+++ packages/database/migrations/0031_add_agent_index.sql
@@ -1,6 +1,6 @@
--- 将超过 2000 字符的 description 截断为 2000 字符
+-- 将超过 1000 字符的 description 截断为 1000 字符
 UPDATE agents
-SET description = LEFT(description, 2000)
-WHERE LENGTH(description) > 2000;--> statement-breakpoint
+SET description = LEFT(description, 1000)
+WHERE LENGTH(description) > 1000;--> statement-breakpoint
 CREATE INDEX IF NOT EXISTS "agents_title_idx" ON "agents" USING btree ("title");--> statement-breakpoint
 CREATE INDEX IF NOT EXISTS "agents_description_idx" ON "agents" USING btree ("description");
