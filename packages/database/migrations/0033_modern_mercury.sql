3cf4330afdddbf73c74fb2528c56558d2a4e7ecf
arvinxx <arvinx@foxmail.com>
2025-09-16 17:53:27 +0800

    improve db index

--- packages/database/migrations/0033_modern_mercury.sql
+++ packages/database/migrations/0033_modern_mercury.sql
@@ -3,7 +3,7 @@ CREATE INDEX IF NOT EXISTS "chunks_user_id_idx" ON "chunks" USING btree ("user_i
 
 -- 解决 topics 表批量删除慢查询
 CREATE INDEX IF NOT EXISTS "topics_user_id_idx" ON "topics" USING btree ("user_id");--> statement-breakpoint
-CREATE INDEX IF NOT EXISTS "topics_id_user_id_idx" ON "topics" USING btree ("id","user_id");
+CREATE INDEX IF NOT EXISTS "topics_id_user_id_idx" ON "topics" USING btree ("id","user_id");--> statement-breakpoint
 
 -- 解决 sessions 表删除慢查询
 CREATE INDEX IF NOT EXISTS "sessions_user_id_idx" ON "sessions" USING btree ("user_id");--> statement-breakpoint
