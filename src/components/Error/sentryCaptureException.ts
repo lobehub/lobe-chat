e51dc236832a40380114ea7da9450fee9c63c502
arvinxx <arvinx@foxmail.com>
2025-09-16 13:32:36 +0800

    fix

--- src/components/Error/sentryCaptureException.ts
+++
@@ -1,9 +0,0 @@
-import { appEnv } from '@/envs/app';
-
-export type ErrorType = Error & { digest?: string };
-
-export const sentryCaptureException = async (error: Error & { digest?: string }) => {
-  if (!appEnv.NEXT_PUBLIC_ENABLE_SENTRY) return;
-  const { captureException } = await import('@sentry/nextjs');
-  return captureException(error);
-};
