4859bdb5c5dac26a1f867b5467df6badbf1f843e
rdmclin2 <rdmclin2@gmail.com>
2025-09-18 15:46:06 +0800

    chore: adjust assistant skeleton style

--- apps/mobile/app/(discover)/assistant/detail/components/SkeletonDetail/index.tsx
+++ apps/mobile/app/(discover)/assistant/detail/components/SkeletonDetail/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { DimensionValue, View } from 'react-native';
 
 import { Skeleton } from '@/components';
-import { ICON_SIZE } from '@/const/common';
+import { AVATAR_SIZE_LARGE, ICON_SIZE } from '@/const/common';
 
 import { useStyles } from './styles';
 
 const TAG_WIDTHS = [64, 88, 72, 96];
-const SYSTEM_ROLE_WIDTHS: DimensionValue[] = ['100%', '95%', '90%', '100%', '85%', '75%'];
+const SYSTEM_ROLE_WIDTHS: DimensionValue[] = ['80%', '95%', '90%', '100%', '85%', '75%'];
+const SYSTEM_ROLE_WIDTHS2: DimensionValue[] = ['90%', '95%', '100%', '95%', '85%', '100%'];
+const SYSTEM_ROLE_WIDTHS3: DimensionValue[] = ['70%', '75%', '90%', '100%', '85%', '75%'];
 const DESCRIPTION_WIDTHS: DimensionValue[] = ['100%', '95%', '85%'];
 
 export const AssistantTitleSkeleton = () => {
@@ -21,13 +23,14 @@ export const AssistantTitleSkeleton = () => {
 };
 
 export const AssistantAuthorSkeleton = () => {
-  const { styles, token } = useStyles();
+  const { styles } = useStyles();
 
   return (
     <View style={styles.authorSection}>
-      <Skeleton.Avatar animated shape="circle" size={token.controlHeightSM} />
+      <Skeleton.Avatar animated shape="circle" size={AVATAR_SIZE_LARGE} />
       <View style={styles.authorInfo}>
         <Skeleton.Title animated style={styles.authorSkeleton} width="50%" />
+        <Skeleton.Paragraph animated rows={1} style={styles.authorSkeleton} width="80%" />
       </View>
     </View>
   );
@@ -79,7 +82,16 @@ export const AssistantSystemRoleSkeleton = () => {
 
       <View style={styles.systemRoleContent}>
         <Skeleton.Paragraph animated rows={SYSTEM_ROLE_WIDTHS.length} width={SYSTEM_ROLE_WIDTHS} />
-        <Skeleton.Paragraph animated rows={SYSTEM_ROLE_WIDTHS.length} width={SYSTEM_ROLE_WIDTHS} />
+        <Skeleton.Paragraph
+          animated
+          rows={SYSTEM_ROLE_WIDTHS2.length}
+          width={SYSTEM_ROLE_WIDTHS2}
+        />
+        <Skeleton.Paragraph
+          animated
+          rows={SYSTEM_ROLE_WIDTHS3.length}
+          width={SYSTEM_ROLE_WIDTHS3}
+        />
       </View>
     </View>
   );
