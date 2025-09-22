00d53817d1685bfe37465985757935a4898fa7d6
Neko Ayaka <neko@ayaka.moe>
2025-09-22 17:17:26 +0800

    chore: updated

--- src/instrumentation.node.ts
+++ src/instrumentation.node.ts
@@ -1,24 +1,4 @@
-import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
-import { PgInstrumentation } from '@opentelemetry/instrumentation-pg';
-import { resourceFromAttributes } from '@opentelemetry/resources';
-import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
-import { NodeSDK } from '@opentelemetry/sdk-node';
-import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from '@opentelemetry/semantic-conventions';
-
+import { register } from '@lobechat/observability-otel/node'
 import { version } from '../package.json';
 
-const sdk = new NodeSDK({
-  instrumentations: [new PgInstrumentation(), new HttpInstrumentation()],
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new OTLPMetricExporter(),
-  }),
-  resource: resourceFromAttributes({
-    [ATTR_SERVICE_NAME]: 'lobe-chat',
-    [ATTR_SERVICE_VERSION]: version,
-  }),
-  traceExporter: new OTLPTraceExporter(),
-});
-
-sdk.start();
+register({ version })
--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,5 @@
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
+  if (process.env.NEXT_RUNTIME === 'nodejs' && process.env.ENABLE_TELEMETRY) {
     await import('./instrumentation.node');
   }
 }
