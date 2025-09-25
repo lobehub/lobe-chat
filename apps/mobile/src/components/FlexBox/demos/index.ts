52a07d8dee8025089e01efec7efd7da36644dc14
canisminor1990 <i@canisminor.cc>
2025-09-25 20:05:31 +0800

    🐛 fix: Fix cva

--- apps/mobile/src/components/FlexBox/demos/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as alignment } from './alignment';
-export { default as basic } from './basic';
-export { default as example } from './example';
-export { default as wrap } from './wrap';
---
+++ apps/mobile/src/components/Flexbox/demos/basic.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Center, Flexbox } from '@/components';
+
+const BasicDemo = () => {
+  return (
+    <Flexbox gap={40} horizontal justify={'space-between'} padding={16} width={'100%'}>
+      <Center height={40} style={{ backgroundColor: 'cadetblue', borderRadius: 8 }} width={40}>
+        <Text style={{ color: 'white' }}>A</Text>
+      </Center>
+      <Center height={40} style={{ backgroundColor: 'cadetblue', borderRadius: 8 }} width={40}>
+        <Text style={{ color: 'white' }}>B</Text>
+      </Center>
+    </Flexbox>
+  );
+};
+
+export default BasicDemo;
