b541290fe5388235b6045e9734f391913ff56733
canisminor1990 <i@canisminor.cc>
2025-09-25 20:05:31 +0800

    🐛 fix: Fix cva

--- apps/mobile/src/components/Block/demos/index.ts
+++ apps/mobile/src/components/Block/demos/index.ts
@@ -1,4 +1,3 @@
 export { default as BasicDemo } from './basic';
 export { default as ClickableDemo } from './clickable';
 export { default as LayoutDemo } from './layout';
-export { default as StylePatternDemo } from './stylePattern';
