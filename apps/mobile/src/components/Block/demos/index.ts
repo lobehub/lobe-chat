6cbe99c0f11b667d83d573a109f28d9f02b19071
canisminor1990 <i@canisminor.cc>
2025-09-25 17:08:55 +0800

    🐛 fix: Fix new components in playgroun

--- apps/mobile/src/components/Block/demos/index.ts
+++ apps/mobile/src/components/Block/demos/index.ts
@@ -1,7 +1,4 @@
-export { default as basic } from './basic';
-export { default as clickable } from './clickable';
-export { default as compatibilityTest } from './compatibilityTest';
-export { default as cva } from './cva';
-export { default as layout } from './layout';
-export { default as stylePattern } from './stylePattern';
-export { default as stylish } from './stylish';
+export { default as BasicDemo } from './basic';
+export { default as ClickableDemo } from './clickable';
+export { default as LayoutDemo } from './layout';
+export { default as StylePatternDemo } from './stylePattern';
