6b42af41832c6e7d48843ce2072db87aad2a418b
canisminor1990 <i@canisminor.cc>
2025-09-25 17:08:55 +0800

    🐛 fix: Fix new components in playgroun

--- apps/mobile/app/playground/components/block.tsx
+++ apps/mobile/app/playground/components/block.tsx
@@ -1,16 +1,7 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { Header } from '@/components';
-import {
-  basic as BasicDemo,
-  cva as CVADemo,
-  clickable as ClickableDemo,
-  compatibilityTest as CompatibilityTestDemo,
-  layout as LayoutDemo,
-  stylePattern as StylePatternDemo,
-  stylish as StylishDemo,
-} from '@/components/Block/demos';
+import { PageContainer } from '@/components';
+import { BasicDemo, ClickableDemo, LayoutDemo, StylePatternDemo } from '@/components/Block/demos';
 import README from '@/components/Block/readme';
 import ComponentPlayground, { DemoItem } from '@/components/Playground';
 
@@ -20,18 +11,14 @@ const demos: DemoItem[] = [
   { component: <BasicDemo />, key: 'basic', title: '基础用法' },
   { component: <ClickableDemo />, key: 'clickable', title: '可点击状态' },
   { component: <LayoutDemo />, key: 'layout', title: '布局示例' },
-  { component: <CVADemo />, key: 'cva', title: 'RN CVA 示例' },
   { component: <StylePatternDemo />, key: 'stylePattern', title: 'Style 模式' },
-  { component: <StylishDemo />, key: 'stylish', title: 'Stylish 样式' },
-  { component: <CompatibilityTestDemo />, key: 'compatibilityTest', title: '兼容性测试' },
 ];
 
 export default function BlockPlaygroundPage() {
   const { styles } = useStyles();
   return (
-    <SafeAreaView edges={['bottom']} style={styles.safeAreaView}>
-      <Header showBack title="Block 块容器" />
+    <PageContainer showBack style={styles.safeAreaView} title="Block 组件">
       <ComponentPlayground demos={demos} readmeContent={README} />
-    </SafeAreaView>
+    </PageContainer>
   );
 }
