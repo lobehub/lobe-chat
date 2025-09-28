e82d4b727447eb6df1649d1750dcdcf48741268a
Arvin Xu <arvinx@foxmail.com>
2025-09-28 07:12:14 +0200

    🐛 fix: refactor tools-engine and fix search token count (#9448)
    * init toolsEngine
    
    * clean implement
    
    * refactor with tools-engine
    
    * add more tests
    
    * update
    
    * rename
    
    * refactor the tools engine
    
    * refactor code
    
    * refactor helpers
    
    * fix tools is empty issue
    
    * fix tests
    
    * refactor to remove enabledSchema
    
    * support defaultToolIds
    
    * fix tests
    
    * fix tests

--- src/store/tool/selectors/tool.ts
+++ src/store/tool/selectors/tool.ts
@@ -2,29 +2,15 @@ import { pluginPrompts } from '@lobechat/prompts';
 import { LobeChatPluginManifest } from '@lobehub/chat-plugin-sdk';
 
 import { MetaData } from '@/types/meta';
-import { ChatCompletionTool } from '@/types/openai/chat';
 import { LobeToolMeta } from '@/types/tool/tool';
 import { globalAgentContextManager } from '@/utils/client/GlobalAgentContextManager';
 import { hydrationPrompt } from '@/utils/promptTemplate';
 import { genToolCallingName } from '@/utils/toolCall';
-import { convertPluginManifestToToolsCalling } from '@/utils/toolManifest';
 
 import { pluginHelpers } from '../helpers';
 import { ToolStoreState } from '../initialState';
 import { builtinToolSelectors } from '../slices/builtin/selectors';
 import { pluginSelectors } from '../slices/plugin/selectors';
-
-const enabledSchema =
-  (tools: string[] = []) =>
-  (s: ToolStoreState): ChatCompletionTool[] => {
-    const manifests = pluginSelectors
-      .installedPluginManifestList(s)
-      .concat(s.builtinTools.map((b) => b.manifest as LobeChatPluginManifest))
-      // 如果存在 enabledPlugins，那么只启用 enabledPlugins 中的插件
-      .filter((m) => tools.includes(m?.identifier));
-
-    return convertPluginManifestToToolsCalling(manifests);
-  };
 
 const enabledSystemRoles =
   (tools: string[] = []) =>
@@ -122,7 +108,6 @@ const isToolHasUI = (id: string) => (s: ToolStoreState) => {
 };
 
 export const toolSelectors = {
-  enabledSchema,
   enabledSystemRoles,
   getManifestById,
   getManifestLoadingStatus,
