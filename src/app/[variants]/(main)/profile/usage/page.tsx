4e4f12834aa773819b25d49599d5f07865a0e201
cy948 <cy948@qq.com>
2025-10-01 17:33:26 +0800

    :lipstick: style: migrate panel UI

---
+++ src/app/[variants]/(main)/profile/usage/page.tsx
@@ -0,0 +1,22 @@
+import { metadataModule } from '@/server/metadata';
+import { translation } from '@/server/translation';
+import { DynamicLayoutProps } from '@/types/next';
+import { RouteVariants } from '@/utils/server/routeVariants';
+
+import Client from './Client';
+
+export const generateMetadata = async (props: DynamicLayoutProps) => {
+    const locale = await RouteVariants.getLocale(props);
+    return metadataModule.generate({
+        description: 'Usage',
+        title: 'Usage',
+        url: '/profile/usage',
+    });
+};
+
+const Page = async (props: DynamicLayoutProps) => {
+    const mobile = await RouteVariants.getIsMobile(props);
+    return <Client mobile={mobile} />;
+};
+
+export default Page;
