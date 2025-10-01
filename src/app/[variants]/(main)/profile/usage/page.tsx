cd5b63bd4d7b8037090f25cd922a5b733f72167a
cy948 <cy948@qq.com>
2025-10-01 19:49:29 +0800

    :white_check_mark: lint: fix

--- src/app/[variants]/(main)/profile/usage/page.tsx
+++ src/app/[variants]/(main)/profile/usage/page.tsx
@@ -6,17 +6,18 @@ import { RouteVariants } from '@/utils/server/routeVariants';
 import Client from './Client';
 
 export const generateMetadata = async (props: DynamicLayoutProps) => {
-    const locale = await RouteVariants.getLocale(props);
-    return metadataModule.generate({
-        description: 'Usage',
-        title: 'Usage',
-        url: '/profile/usage',
-    });
+  const locale = await RouteVariants.getLocale(props);
+  const { t } = await translation('auth', locale);
+  return metadataModule.generate({
+    description: t('header.desc'),
+    title: t('tab.usage'),
+    url: '/profile/usage',
+  });
 };
 
 const Page = async (props: DynamicLayoutProps) => {
-    const mobile = await RouteVariants.getIsMobile(props);
-    return <Client mobile={mobile} />;
+  const mobile = await RouteVariants.getIsMobile(props);
+  return <Client mobile={mobile} />;
 };
 
 export default Page;
