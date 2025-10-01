02456dfdb7dcf9615a5cac90ade3cf49544ebd42
cy948 <cy948@qq.com>
2025-10-01 19:49:29 +0800

    :white_check_mark: lint: fix

--- src/app/[variants]/(main)/profile/usage/features/UsageCards/index.tsx
+++ src/app/[variants]/(main)/profile/usage/features/UsageCards/index.tsx
@@ -1,22 +1,19 @@
 import { memo } from 'react';
-import { useTheme } from 'antd-style'
 import { Flexbox } from 'react-layout-kit';
 
 import { UsageChartProps } from '../../Client';
-import TodaySpend from './TodaySpend'
-import MonthSpend from './MonthSpend';
 import ActiveModels from './ActiveModels';
+import MonthSpend from './MonthSpend';
+import TodaySpend from './TodaySpend';
 
 const UsageCards = memo<UsageChartProps>(({ isLoading, data, groupBy }) => {
-
-    const theme = useTheme();
-        return (
-        <Flexbox gap={16} horizontal>
-            <TodaySpend data={data} isLoading={isLoading} />
-            <MonthSpend data={data} isLoading={isLoading} />
-            <ActiveModels data={data} isLoading={isLoading} groupBy={groupBy} />
-        </Flexbox>
-    );
+  return (
+    <Flexbox gap={16} horizontal>
+      <TodaySpend data={data} isLoading={isLoading} />
+      <MonthSpend data={data} isLoading={isLoading} />
+      <ActiveModels data={data} groupBy={groupBy} isLoading={isLoading} />
+    </Flexbox>
+  );
 });
 
 export default UsageCards;
