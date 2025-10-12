6f4562d5f4dd22d862f376be64e2584c8a351143
arvinxx <arvinx@foxmail.com>
2025-10-13 00:08:06 +0800

    add more tests

--- packages/model-runtime/src/providers/ai302/index.ts
+++ packages/model-runtime/src/providers/ai302/index.ts
@@ -1,6 +1,9 @@
 import { ModelProvider } from 'model-bank';
 
-import { createOpenAICompatibleRuntime } from '../../core/openaiCompatibleFactory';
+import {
+  OpenAICompatibleFactoryOptions,
+  createOpenAICompatibleRuntime,
+} from '../../core/openaiCompatibleFactory';
 import { ChatCompletionErrorPayload } from '../../types';
 import { AgentRuntimeErrorType } from '../../types/error';
 import { processMultiProviderModelList } from '../../utils/modelParse';
@@ -9,7 +12,7 @@ export interface Ai302ModelCard {
   id: string;
 }
 
-export const Lobe302AI = createOpenAICompatibleRuntime({
+export const params = {
   baseURL: 'https://api.302.ai/v1',
   chatCompletion: {
     handleError: (error: any): Omit<ChatCompletionErrorPayload, 'provider'> | undefined => {
@@ -45,4 +48,6 @@ export const Lobe302AI = createOpenAICompatibleRuntime({
     return processMultiProviderModelList(modelList, 'ai302');
   },
   provider: ModelProvider.Ai302,
-});
+} satisfies OpenAICompatibleFactoryOptions;
+
+export const Lobe302AI = createOpenAICompatibleRuntime(params);
