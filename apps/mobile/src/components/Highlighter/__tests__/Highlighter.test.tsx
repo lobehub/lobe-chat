e3fd09b5e91e9bd349af268937cecf271b3d3438
canisminor1990 <i@canisminor.cc>
2025-10-13 19:53:28 +0800

    🔧 chore: Rebase

--- apps/mobile/src/components/Highlighter/__tests__/Highlighter.test.tsx
+++ apps/mobile/src/components/Highlighter/__tests__/Highlighter.test.tsx
@@ -4,7 +4,7 @@ import { renderWithTheme } from '@/test/utils';
 
 import Highlighter from '../index';
 
-jest.mock('@/theme', () => ({
+jest.mock('@/components/theme', () => ({
   useTheme: () => ({
     isDarkMode: false,
   }),
@@ -143,7 +143,7 @@ describe('Highlighter', () => {
         if (n <= 1) return n;
         return fibonacci(n - 1) + fibonacci(n - 2);
       }
-      
+
       for (let i = 0; i < 10; i++) {
         console.log(fibonacci(i));
       }
