429bd860d981e2ce3435f15bafcfc138f855a1b5
canisminor1990 <i@canisminor.cc>
2025-10-14 15:41:55 +0800

    ♻️ refactor(wip): Refactor setting

--- apps/mobile/src/components/Block/type.ts
+++ apps/mobile/src/components/Block/type.ts
@@ -2,9 +2,8 @@ import type { FlexboxProps } from '../Flexbox';
 
 export interface BlockProps extends FlexboxProps {
   active?: boolean;
-  borderRadius?: number;
+  borderRadius?: boolean | number;
   clickable?: boolean;
-
   shadow?: boolean;
   variant?: 'filled' | 'outlined' | 'borderless';
 }
---
+++ apps/mobile/src/components/Cell/type.ts
@@ -0,0 +1,14 @@
+import { ReactNode } from 'react';
+
+import type { BlockProps } from '@/components/Block';
+import type { IconProps } from '@/components/Icon';
+
+export interface CellProps extends Omit<BlockProps, 'children'> {
+  arrowIcon?: IconProps['icon'];
+  description?: ReactNode | string;
+  extra?: ReactNode | string;
+  icon?: IconProps['icon'];
+  loading?: boolean;
+  showArrow?: boolean;
+  title: ReactNode | string;
+}
