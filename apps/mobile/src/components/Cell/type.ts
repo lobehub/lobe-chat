1ff17c4bd4c7c6b79419658f2511d783d46ae1fd
canisminor1990 <i@canisminor.cc>
2025-10-16 13:08:26 +0800

    style: update Model Setting

--- apps/mobile/src/components/Cell/type.ts
+++ apps/mobile/src/components/Cell/type.ts
@@ -2,14 +2,17 @@ import { ReactNode } from 'react';
 
 import type { BlockProps } from '@/components/Block';
 import type { IconProps } from '@/components/Icon';
+import type { TextProps } from '@/components/Text';
 
 export interface CellProps extends Omit<BlockProps, 'children'> {
   arrowIcon?: IconProps['icon'];
   description?: ReactNode | string;
+  descriptionProps?: Omit<TextProps, 'children'>;
   extra?: ReactNode | string;
   icon?: IconProps['icon'];
   iconSize?: number;
   loading?: boolean;
   showArrow?: boolean;
   title: ReactNode | string;
+  titleProps?: Omit<TextProps, 'children'>;
 }
