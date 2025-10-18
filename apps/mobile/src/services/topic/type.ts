0e913c2de966194f3c5e5f00e4985284d89bb39f
Tsuki <976499226@qq.com>
2025-10-18 16:05:28 +0800

    fix(mobile): topic issue

--- apps/mobile/src/services/topic/type.ts
+++ apps/mobile/src/services/topic/type.ts
@@ -1,8 +1,10 @@
+/* eslint-disable typescript-sort-keys/interface */
 import { ChatTopic, TopicRankItem } from '@lobechat/types';
 import { BatchTaskResult } from '@lobechat/types/src/service';
 
 export interface CreateTopicParams {
   favorite?: boolean;
+  groupId?: string | null;
   messages?: string[];
   sessionId?: string | null;
   title: string;
@@ -10,29 +12,29 @@ export interface CreateTopicParams {
 
 export interface QueryTopicParams {
   current?: number;
+  containerId?: string | null; // sessionId or groupId
   pageSize?: number;
-  sessionId: string;
 }
 
 export interface ITopicService {
+  createTopic(params: CreateTopicParams): Promise<string>;
   batchCreateTopics(importTopics: ChatTopic[]): Promise<BatchTaskResult>;
-  batchRemoveTopics(topics: string[]): Promise<any>;
   cloneTopic(id: string, newTitle?: string): Promise<string>;
 
+  getTopics(params: QueryTopicParams): Promise<ChatTopic[]>;
+  getAllTopics(): Promise<ChatTopic[]>;
   countTopics(params?: {
     endDate?: string;
     range?: [string, string];
     startDate?: string;
   }): Promise<number>;
-  createTopic(params: CreateTopicParams): Promise<string>;
-  getAllTopics(): Promise<ChatTopic[]>;
-  getTopics(params: QueryTopicParams): Promise<ChatTopic[]>;
   rankTopics(limit?: number): Promise<TopicRankItem[]>;
+  searchTopics(keyword: string, sessionId?: string, groupId?: string): Promise<ChatTopic[]>;
 
-  removeAllTopic(): Promise<any>;
+  updateTopic(id: string, data: Partial<ChatTopic>): Promise<any>;
 
   removeTopic(id: string): Promise<any>;
   removeTopics(sessionId: string): Promise<any>;
-  searchTopics(keyword: string, sessionId?: string): Promise<ChatTopic[]>;
-  updateTopic(id: string, data: Partial<ChatTopic>): Promise<any>;
+  batchRemoveTopics(topics: string[]): Promise<any>;
+  removeAllTopic(): Promise<any>;
 }
