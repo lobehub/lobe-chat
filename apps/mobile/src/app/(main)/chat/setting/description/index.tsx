a66e9a3ed49ae0b35511e70751a58dc6f5406ab6
Tsuki <976499226@qq.com>
2025-10-19 10:45:59 +0800

    feat(mobile): support update session meta

--- apps/mobile/src/app/(main)/chat/setting/description/index.tsx
+++ apps/mobile/src/app/(main)/chat/setting/description/index.tsx
@@ -1,13 +1,16 @@
 import { Button, Flexbox, PageContainer, TextArea } from '@lobehub/ui-rn';
 import { router } from 'expo-router';
 import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { useSessionStore } from '@/store/session';
 import { sessionMetaSelectors } from '@/store/session/slices/session/selectors';
 
 export default function DescriptionSetting() {
+  const { t } = useTranslation(['chat']);
   const [loading, setLoading] = useState(false);
   const defaultDescription = useSessionStore(sessionMetaSelectors.currentAgentDescription);
+  const updateSessionMeta = useSessionStore((s) => s.updateSessionMeta);
   const [description, setDescription] = useState(defaultDescription || '');
   return (
     <PageContainer
@@ -16,19 +19,18 @@ export default function DescriptionSetting() {
           loading={loading}
           onPress={async () => {
             setLoading(true);
-            console.log(description);
-            // TODO: 增加保存逻辑
+            await updateSessionMeta({ description });
             setLoading(false);
             router.back();
           }}
           size={'small'}
           type={'primary'}
         >
-          完成
+          {t('setting.done')}
         </Button>
       }
       showBack
-      title={'描述'}
+      title={t('setting.description')}
     >
       <Flexbox padding={16}>
         <TextArea
