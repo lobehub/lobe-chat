99e865363e65abe6ba3d7a7ce342d2207d5de8c1
ONLY-yours <1349021570@qq.com>
2025-10-21 20:28:49 +0800

    fix: change locals

--- src/app/[variants]/(main)/discover/(detail)/components/NotFound.tsx
+++ src/app/[variants]/(main)/discover/(detail)/components/NotFound.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from 'react-i18next';
 import { Flexbox } from 'react-layout-kit';
 
 const NotFound = memo(() => {
-  const { t } = useTranslation('common');
+  const { t } = useTranslation('error', { keyPrefix: 'notFound' });
 
   return (
     <Flexbox
@@ -15,7 +15,7 @@ const NotFound = memo(() => {
       style={{ minHeight: 400 }}
       width="100%"
     >
-      <h2>{t('notFound')}</h2>
+      <h2>{t('title')}</h2>
     </Flexbox>
   );
 });
