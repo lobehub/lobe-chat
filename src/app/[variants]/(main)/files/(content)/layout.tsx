c19974fae820c0afbaaa2d15b6afebdb81387c96
ONLY-yours <1349021570@qq.com>
2025-10-22 23:26:58 +0800

    fix: delete useless code

--- src/app/[variants]/(main)/files/(content)/layout.tsx
+++ src/app/[variants]/(main)/files/(content)/layout.tsx
@@ -7,5 +7,7 @@ export default () => {
   // if there is client db mode , tell user to switch to server mode
   if (!isServerMode) return <NotSupportClient />;
 
+  // We're using SPA routing now, so we ignore the Next.js parallel routes
+  // and use our Client component instead
   return <Client />;
 };
