d7844f390dc47ac99efc62d55133bc32346ee48b
canisminor1990 <i@canisminor.cc>
2025-10-23 20:22:28 +0800

    style: update discover detail and chatlist

--- apps/mobile/src/features/chat/ChatBubble/index.tsx
+++ apps/mobile/src/features/chat/ChatBubble/index.tsx
@@ -1,9 +1,11 @@
 import { ChatMessage } from '@lobechat/types';
-import { Markdown } from '@lobehub/ui-rn';
+import { Avatar, Block, Flexbox, Markdown, Text } from '@lobehub/ui-rn';
 import { memo, useMemo } from 'react';
 import { View } from 'react-native';
 
+import { DEFAULT_AVATAR } from '@/_const/meta';
 import { useSettingStore } from '@/store/setting';
+import { formatTime } from '@/utils/formatTime';
 
 import AssistantMenu from '../AssistantMenu';
 import LoadingDots from '../LoadingDots';
@@ -15,65 +17,84 @@ import { useStyles } from './style';
 interface ChatBubbleProps {
   isLoading?: boolean;
   message: ChatMessage;
+  showActions?: boolean;
+  showTime?: boolean;
+  showTitle?: boolean;
 }
 
-const ChatBubble = memo(({ message, isLoading }: ChatBubbleProps) => {
-  const isUser = message.role === 'user';
-  const isAssistant = message.role === 'assistant';
-  const hasError = !!message.error;
-  const { styles } = useStyles();
-  const { fontSize } = useSettingStore();
+const ChatBubble = memo(
+  ({ message, isLoading, showActions = true, showTime = true, showTitle }: ChatBubbleProps) => {
+    const isUser = message.role === 'user';
+    const hasError = !!message.error;
 
-  const content = useMemo(() => {
-    if (hasError && message.error?.type) {
-      return <ErrorContent error={message.error} />;
-    }
+    const { styles, theme } = useStyles();
+    const { fontSize } = useSettingStore();
 
-    if (isLoading) {
-      return (
-        <View style={styles.loadingDotsContainer}>
-          <LoadingDots />
-        </View>
-      );
-    }
+    const content = useMemo(() => {
+      if (hasError && message.error?.type) {
+        return <ErrorContent error={message.error} />;
+      }
 
-    return <Markdown fontSize={fontSize}>{message.content}</Markdown>;
-  }, [fontSize, hasError, isLoading, message.content, message.error]);
+      if (isLoading) {
+        return (
+          <View style={styles.loadingDotsContainer}>
+            <LoadingDots />
+          </View>
+        );
+      }
 
-  return (
-    <View
-      style={[
-        styles.bubbleContainer,
-        isUser ? styles.userBubbleContainer : styles.aiBubbleContainer,
-      ]}
-    >
-      {isAssistant ? (
-        <View style={styles.aiMessageContainer}>
-          <View style={styles.aiContentContainer}>
-            <AssistantMenu message={message}>
-              <View style={[styles.bubble, styles.aiBubble, hasError && styles.errorBubble]}>
-                {content}
-              </View>
-            </AssistantMenu>
-            {!isLoading && (message.content || hasError) && (
-              <MessageActions message={message} style={styles.messageActions} />
+      return <Markdown fontSize={fontSize}>{message.content}</Markdown>;
+    }, [fontSize, hasError, isLoading, message.content, message.error]);
+
+    const AssistantMessage = (
+      <Block style={[hasError && styles.errorBubble]} variant={'borderless'}>
+        {content}
+      </Block>
+    );
+
+    const UserMessage = (
+      <Block style={[hasError && styles.errorBubble]} variant={'borderless'}>
+        {content}
+      </Block>
+    );
+
+    return (
+      <Flexbox gap={12} horizontal paddingBlock={8}>
+        <Avatar avatar={message?.meta?.avatar || DEFAULT_AVATAR} size={32} />
+        <Flexbox flex={1} gap={4}>
+          <Flexbox align={'center'} gap={8} horizontal>
+            {showTitle && message?.meta?.title && (
+              <Text color={!isUser ? theme.geekblue : theme.colorWarning} fontSize={13} strong>
+                {message?.meta?.title}
+              </Text>
             )}
-          </View>
-        </View>
-      ) : (
-        <View style={styles.userMessageContainer}>
-          <View style={styles.userContentContainer}>
-            <UserContextMenu message={message}>
-              <View style={[styles.bubble, styles.userBubble, hasError && styles.errorBubble]}>
-                {content}
-              </View>
-            </UserContextMenu>
-          </View>
-        </View>
-      )}
-    </View>
-  );
-});
+            {showTime && message?.createdAt && (
+              <Text fontSize={12} type={'secondary'}>
+                {formatTime(message.createdAt)}
+              </Text>
+            )}
+          </Flexbox>
+          {!isUser ? (
+            <Flexbox gap={4}>
+              {showActions ? (
+                <AssistantMenu message={message}>{AssistantMessage}</AssistantMenu>
+              ) : (
+                AssistantMessage
+              )}
+              {showActions && !isLoading && (message.content || hasError) && (
+                <MessageActions message={message} />
+              )}
+            </Flexbox>
+          ) : showActions ? (
+            <UserContextMenu message={message}>{UserMessage}</UserContextMenu>
+          ) : (
+            UserMessage
+          )}
+        </Flexbox>
+      </Flexbox>
+    );
+  },
+);
 
 ChatBubble.displayName = 'ChatBubble';
 
