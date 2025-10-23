68dccdc4f858227919c921152ada57418081d675
canisminor1990 <i@canisminor.cc>
2025-10-23 20:52:48 +0800

    style: update discover detail and chatlist

--- apps/mobile/src/features/chat/ChatBubble/index.tsx
+++ apps/mobile/src/features/chat/ChatBubble/index.tsx
@@ -1,16 +1,14 @@
 import { ChatMessage } from '@lobechat/types';
 import { Avatar, Block, Flexbox, Markdown, Text } from '@lobehub/ui-rn';
 import { memo, useMemo } from 'react';
-import { View } from 'react-native';
 
 import { DEFAULT_AVATAR } from '@/_const/meta';
+import MessageContextMenu from '@/features/chat/MessageContextMenu';
 import { useSettingStore } from '@/store/setting';
 import { formatTime } from '@/utils/formatTime';
 
-import AssistantMenu from '../AssistantMenu';
 import LoadingDots from '../LoadingDots';
 import MessageActions from '../MessageActions';
-import UserContextMenu from '../UserContextMenu';
 import ErrorContent from './ErrorContent';
 import { useStyles } from './style';
 
@@ -18,12 +16,20 @@ interface ChatBubbleProps {
   isLoading?: boolean;
   message: ChatMessage;
   showActions?: boolean;
+  showActionsBar?: boolean;
   showTime?: boolean;
   showTitle?: boolean;
 }
 
 const ChatBubble = memo(
-  ({ message, isLoading, showActions = true, showTime = true, showTitle }: ChatBubbleProps) => {
+  ({
+    message,
+    isLoading,
+    showActions = true,
+    showTime = true,
+    showTitle,
+    showActionsBar,
+  }: ChatBubbleProps) => {
     const isUser = message.role === 'user';
     const hasError = !!message.error;
 
@@ -36,30 +42,14 @@ const ChatBubble = memo(
       }
 
       if (isLoading) {
-        return (
-          <View style={styles.loadingDotsContainer}>
-            <LoadingDots />
-          </View>
-        );
+        return <LoadingDots />;
       }
 
       return <Markdown fontSize={fontSize}>{message.content}</Markdown>;
     }, [fontSize, hasError, isLoading, message.content, message.error]);
 
-    const AssistantMessage = (
-      <Block style={[hasError && styles.errorBubble]} variant={'borderless'}>
-        {content}
-      </Block>
-    );
-
-    const UserMessage = (
-      <Block style={[hasError && styles.errorBubble]} variant={'borderless'}>
-        {content}
-      </Block>
-    );
-
-    return (
-      <Flexbox gap={12} horizontal paddingBlock={8}>
+    const node = (
+      <>
         <Avatar avatar={message?.meta?.avatar || DEFAULT_AVATAR} size={32} />
         <Flexbox flex={1} gap={4}>
           <Flexbox align={'center'} gap={8} horizontal>
@@ -74,23 +64,41 @@ const ChatBubble = memo(
               </Text>
             )}
           </Flexbox>
-          {!isUser ? (
+          {isUser ? (
+            <Block style={[hasError && styles.errorBubble]} variant={'borderless'}>
+              {content}
+            </Block>
+          ) : (
             <Flexbox gap={4}>
-              {showActions ? (
-                <AssistantMenu message={message}>{AssistantMessage}</AssistantMenu>
-              ) : (
-                AssistantMessage
-              )}
-              {showActions && !isLoading && (message.content || hasError) && (
+              <Block style={[hasError && styles.errorBubble]} variant={'borderless'}>
+                {content}
+              </Block>
+              {showActions && showActionsBar && !isLoading && (message.content || hasError) && (
                 <MessageActions message={message} />
               )}
             </Flexbox>
-          ) : showActions ? (
-            <UserContextMenu message={message}>{UserMessage}</UserContextMenu>
-          ) : (
-            UserMessage
           )}
         </Flexbox>
+      </>
+    );
+
+    if (showActions)
+      return (
+        <MessageContextMenu
+          borderRadius={false}
+          gap={12}
+          horizontal
+          message={message}
+          paddingBlock={8}
+          paddingInline={16}
+        >
+          {node}
+        </MessageContextMenu>
+      );
+
+    return (
+      <Flexbox gap={12} horizontal paddingBlock={8} paddingInline={16}>
+        {node}
       </Flexbox>
     );
   },
--- apps/mobile/src/features/chat/UserContextMenu/style.ts
+++
@@ -1,7 +0,0 @@
-import { createStyles } from '@/components/styles';
-
-export const useStyles = createStyles(({ token }) => ({
-  touchableWrapper: {
-    borderRadius: token.borderRadius,
-  },
-}));
