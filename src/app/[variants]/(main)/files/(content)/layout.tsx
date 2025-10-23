9906ddc4169b9a9aee1dc413b60870154dbce9c6
ONLY-yours <1349021570@qq.com>
2025-10-23 16:23:08 +0800

    feat: change the dir name and the path

--- src/app/[variants]/(main)/files/(content)/layout.tsx
+++ src/app/[variants]/(main)/files/(content)/layout.tsx
@@ -1,13 +1,16 @@
+import { PropsWithChildren } from 'react';
+
 import { isServerMode } from '@/const/version';
 
-import Client from './Client';
 import NotSupportClient from './NotSupportClient';
 
-export default () => {
+export default (props: PropsWithChildren) => {
+  const { children } = props;
+
   // if there is client db mode , tell user to switch to server mode
   if (!isServerMode) return <NotSupportClient />;
 
   // We're using SPA routing now, so we ignore the Next.js parallel routes
   // and use our Client component instead
-  return <Client />;
+  return children;
 };
