4d82b216d1b50765a3fad98a3beb63213b5f11fa
canisminor1990 <i@canisminor.cc>
2025-10-24 17:54:32 +0800

    style: update markdown

--- apps/mobile/src/components/Markdown/renderers/root.tsx
+++ apps/mobile/src/components/Markdown/renderers/root.tsx
@@ -8,6 +8,14 @@ export const RootRenderer = ({ node }: RendererArgs<Root>) => {
   const { renderers, styles } = useMarkdownContext();
   const { RootContentRenderer } = renderers;
 
+  // const renderItem = ({ item: node, index }: { index: number; item: any }) => (
+  //   <RootContentRenderer index={index} key={index} node={node} parent={node} />
+  // );
+  //
+  // return (
+  //   <FlashList data={node.children.filter(Boolean)} renderItem={renderItem} scrollEnabled={false} />
+  // );
+
   return (
     <View style={styles.container}>
       {node.children.filter(Boolean).map((node, index) => (
