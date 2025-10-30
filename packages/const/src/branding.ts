0861c1625ca8425d06857ad4a5775853bf010b6a
Waaade-QAQ <1@lllmy.top>
2025-10-30 14:45:16 +0800

    feat: 改名为“糯米世界”

--- packages/const/src/branding.ts
+++ packages/const/src/branding.ts
@@ -2,12 +2,12 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeHub Cloud';
+export const LOBE_CHAT_CLOUD = '糯米世界 Cloud';
 
-export const BRANDING_NAME = 'LobeHub';
+export const BRANDING_NAME = '糯米世界';
 export const BRANDING_LOGO_URL = '';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = '糯米世界';
 
 export const BRANDING_URL = {
   help: undefined,
