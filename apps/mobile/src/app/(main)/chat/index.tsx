8fe2128cd3e82de6b0a276a06c557659e9f5f620
Tsuki <976499226@qq.com>
2025-10-30 21:01:16 +0800

    fix(mobile): topic drawer stuttering

--- apps/mobile/src/app/(main)/chat/index.tsx
+++ apps/mobile/src/app/(main)/chat/index.tsx
@@ -6,7 +6,6 @@ import {
   MessageSquarePlusIcon,
   TextAlignStartIcon,
 } from 'lucide-react-native';
-import { darken } from 'polished';
 import { useTranslation } from 'react-i18next';
 import { KeyboardStickyView } from 'react-native-keyboard-controller';
 
@@ -40,7 +39,7 @@ export default function ChatWithDrawer() {
   const renderContent = () => {
     return (
       <PageContainer
-        backgroundColor={[theme.colorBgContainerSecondary, darken(0.04, theme.colorBgLayout)]}
+        backgroundColor={theme.colorBgLayout}
         extra={
           <Flexbox align={'center'} gap={1} horizontal>
             <ActionIcon
