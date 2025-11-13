fb24b6f1b7b7d7954ed9631468d8efd7894ff012
ONLY-yours <1349021570@qq.com>
2025-11-13 17:09:10 +0800

    fix: add nuqs back & useQueryState back in oath

--- src/app/[variants]/oauth/callback/error/page.tsx
+++ src/app/[variants]/oauth/callback/error/page.tsx
@@ -4,16 +4,15 @@ import { Button, Highlighter, Icon } from '@lobehub/ui';
 import { Result } from 'antd';
 import { ShieldX } from 'lucide-react';
 import Link from 'next/link';
+import { parseAsString, useQueryState } from 'nuqs';
 import React, { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Flexbox } from 'react-layout-kit';
 
-import { useQueryState } from '@/hooks/useQueryParam';
-
 const FailedPage = memo(() => {
   const { t } = useTranslation('oauth');
   const [reason] = useQueryState('reason');
-  const [errorMessage] = useQueryState('errorMessage');
+  const [errorMessage] = useQueryState<string>('errorMessage', parseAsString);
 
   return (
     <Result
