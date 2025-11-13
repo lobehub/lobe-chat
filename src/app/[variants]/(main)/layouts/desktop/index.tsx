287601f8ec15904ad73ad56e7f94f1b2a32582b6
ONLY-yours <1349021570@qq.com>
2025-11-13 19:06:37 +0800

    fix: close the loading in the layout loading

--- src/app/[variants]/(main)/layouts/desktop/index.tsx
+++ src/app/[variants]/(main)/layouts/desktop/index.tsx
@@ -5,9 +5,8 @@ import dynamic from 'next/dynamic';
 import { Suspense, memo } from 'react';
 import { HotkeysProvider } from 'react-hotkeys-hook';
 import { Flexbox } from 'react-layout-kit';
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
-import Loading from '@/components/Loading/BrandTextLoading';
 import { isDesktop } from '@/const/version';
 import { BANNER_HEIGHT } from '@/features/AlertBanner/CloudBanner';
 import TitleBar, { TITLE_BAR_HEIGHT } from '@/features/ElectronTitlebar';
@@ -28,17 +27,10 @@ const Layout = memo((props: { locale: Locales }) => {
   const { isPWA } = usePlatform();
   const theme = useTheme();
 
-  // 获取全局导航状态
-  const navigation = useNavigation();
-
-  // 当 navigation.state 为 'loading'时，意味着我们正在等待 loader
-  const isLoading = navigation.state === 'loading';
-
   const { showCloudPromotion } = useServerConfigStore(featureFlagsSelectors);
   return (
     <HotkeysProvider initiallyActiveScopes={[HotkeyScopeEnum.Global]}>
       {isDesktop && <TitleBar />}
-      {isLoading && <Loading />}
       {showCloudPromotion && <CloudBanner />}
       <Flexbox
         height={
