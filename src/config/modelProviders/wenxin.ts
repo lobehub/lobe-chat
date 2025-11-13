cf44c1ae1bcaa39a3c6c32378a8533c0cb549634
sxjeru <sxjeru@gmail.com>
2025-11-13 19:31:25 +0800

    feat(wenxin): update checkModel and modelsUrl for improved model access

--- src/config/modelProviders/wenxin.ts
+++ src/config/modelProviders/wenxin.ts
@@ -1,13 +1,12 @@
 import { ModelProviderCard } from '@/types/llm';
 
-// ref https://cloud.baidu.com/doc/WENXINWORKSHOP/s/Nlks5zkzu
 const BaiduWenxin: ModelProviderCard = {
   chatModels: [],
-  checkModel: 'ernie-speed-128k',
+  checkModel: 'ernie-4.5-turbo-latest',
   description:
     '企业级一站式大模型与AI原生应用开发及服务平台，提供最全面易用的生成式人工智能模型开发、应用开发全流程工具链',
   id: 'wenxin',
-  modelsUrl: 'https://cloud.baidu.com/doc/WENXINWORKSHOP/s/Nlks5zkzu#%E5%AF%B9%E8%AF%9Dchat',
+  modelsUrl: 'https://console.bce.baidu.com/qianfan/modelcenter/model/buildIn/list',
   name: 'Wenxin',
   settings: {
     proxyUrl: {
