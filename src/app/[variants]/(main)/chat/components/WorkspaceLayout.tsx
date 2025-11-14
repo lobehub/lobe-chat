97b0413020feaa50225747f263cf4af138a09fd0
Arvin Xu <arvinx@foxmail.com>
2025-11-14 18:09:16 +0800

    💄 style: revert background style (#10218)
    revert style

--- src/app/[variants]/(main)/chat/components/WorkspaceLayout.tsx
+++ src/app/[variants]/(main)/chat/components/WorkspaceLayout.tsx
@@ -1,4 +1,3 @@
-import { useTheme } from 'antd-style';
 import { Suspense, memo } from 'react';
 import { Flexbox } from 'react-layout-kit';
 
@@ -19,38 +18,30 @@ interface WorkspaceLayoutProps {
   mobile?: boolean;
 }
 
-const DesktopWorkspace = memo(() => {
-  const theme = useTheme();
-
-  return (
-    <>
-      <ChatHeaderDesktop />
-      <Flexbox
-        height={'100%'}
-        horizontal
-        style={{ overflow: 'hidden', position: 'relative' }}
-        width={'100%'}
-      >
-        <Flexbox
-          height={'100%'}
-          style={{ background: theme.colorBgContainer, overflow: 'hidden', position: 'relative' }}
-          width={'100%'}
-        >
-          <ConversationArea mobile={false} />
-        </Flexbox>
-        <Portal>
-          <Suspense fallback={<BrandTextLoading />}>
-            <PortalPanel mobile={false} />
-          </Suspense>
-        </Portal>
-        <TopicPanel>
-          <TopicSidebar mobile={false} />
-        </TopicPanel>
+const DesktopWorkspace = memo(() => (
+  <>
+    <ChatHeaderDesktop />
+    <Flexbox
+      height={'100%'}
+      horizontal
+      style={{ overflow: 'hidden', position: 'relative' }}
+      width={'100%'}
+    >
+      <Flexbox height={'100%'} style={{ overflow: 'hidden', position: 'relative' }} width={'100%'}>
+        <ConversationArea mobile={false} />
       </Flexbox>
-      <MainInterfaceTracker />
-    </>
-  );
-});
+      <Portal>
+        <Suspense fallback={<BrandTextLoading />}>
+          <PortalPanel mobile={false} />
+        </Suspense>
+      </Portal>
+      <TopicPanel>
+        <TopicSidebar mobile={false} />
+      </TopicPanel>
+    </Flexbox>
+    <MainInterfaceTracker />
+  </>
+));
 
 DesktopWorkspace.displayName = 'DesktopWorkspace';
 
