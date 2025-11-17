2d83300795163c61c45672669cd16648775709c5
ONLY-yours <1349021570@qq.com>
2025-11-17 10:51:20 +0800

    fix: delete useless code

--- src/app/[variants]/(main)/image/index.tsx
+++ src/app/[variants]/(main)/image/index.tsx
@@ -2,17 +2,10 @@
 
 import { Suspense, memo } from 'react';
 
-import { isServerMode } from '@/const/version';
-
 import ImageWorkspace from './features/ImageWorkspace';
 import SkeletonList from './features/ImageWorkspace/SkeletonList';
-import NotSupportClient from './NotSupportClient';
 
 const DesktopImagePage = memo(() => {
-  if (!isServerMode) {
-    return <NotSupportClient />;
-  }
-
   return (
     <Suspense fallback={<SkeletonList />}>
       <ImageWorkspace />
