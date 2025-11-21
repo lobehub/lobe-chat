4dca559f1c7ad9b6243d5fc0d547889d428272ee
arvinxx <arvinx@foxmail.com>
2025-11-21 21:00:32 +0800

    fix

--- src/libs/trpc/client/lambda.ts
+++ src/libs/trpc/client/lambda.ts
@@ -34,9 +34,6 @@ const errorHandlingLink: TRPCLink<LambdaRouter> = () => {
           // Don't show notifications for abort errors
           if (showError && !isAbortError) {
             const { loginRequired } = await import('@/components/Error/loginRequiredNotification');
-            const { fetchErrorNotification } = await import(
-              '@/components/Error/fetchErrorNotification'
-            );
 
             switch (status) {
               case 401: {
