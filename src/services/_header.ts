dc6f0118cc51d844face6c89f76c75028c935286
arvinxx <arvinx@foxmail.com>
2025-11-23 00:27:06 +0800

    refactor

--- src/services/_header.ts
+++ src/services/_header.ts
@@ -1,12 +1,6 @@
-import {
-  LOBE_CHAT_ACCESS_CODE,
-  LOBE_USER_ID,
-  OPENAI_API_KEY_HEADER_KEY,
-  OPENAI_END_POINT,
-} from '@/const/fetch';
+import { LOBE_USER_ID, OPENAI_API_KEY_HEADER_KEY, OPENAI_END_POINT } from '@/const/fetch';
 import { aiProviderSelectors, useAiInfraStore } from '@/store/aiInfra';
 import { useUserStore } from '@/store/user';
-import { keyVaultsConfigSelectors } from '@/store/user/selectors';
 
 /**
  * TODO: Need to be removed after tts refactor
@@ -22,7 +16,6 @@ export const createHeaderWithOpenAI = (header?: HeadersInit): HeadersInit => {
   // eslint-disable-next-line no-undef
   return {
     ...header,
-    [LOBE_CHAT_ACCESS_CODE]: keyVaultsConfigSelectors.password(state),
     [LOBE_USER_ID]: state.user?.id || '',
     [OPENAI_API_KEY_HEADER_KEY]: keyVaults.apiKey || '',
     [OPENAI_END_POINT]: keyVaults.baseURL || '',
