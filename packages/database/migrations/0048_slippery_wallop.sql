39bc921c2f4e173523f078bd12d0a0d6b4f6c1f5
ONLY-yours <1349021570@qq.com>
2025-11-24 21:34:05 +0800

    fix: update the db sql migration

--- packages/database/migrations/0048_slippery_wallop.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "files" DROP CONSTRAINT "files_slug_unique";--> statement-breakpoint
-ALTER TABLE "agents" ADD COLUMN "editor_content" jsonb;--> statement-breakpoint
-ALTER TABLE "documents" ADD COLUMN "slug" varchar(255);--> statement-breakpoint
-CREATE UNIQUE INDEX "documents_slug_user_id_unique" ON "documents" USING btree ("slug","user_id") WHERE "documents"."slug" is not null;--> statement-breakpoint
-ALTER TABLE "files" DROP COLUMN "slug";
