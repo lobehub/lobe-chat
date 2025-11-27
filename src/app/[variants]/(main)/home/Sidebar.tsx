26b4927d2526061f9140cb07ed41ca4a7b7d6a2d
canisminor1990 <i@canisminor.cc>
2025-11-27 13:34:47 +0800

    style: update navbar

--- src/app/[variants]/(main)/home/Sidebar.tsx
+++ src/app/[variants]/(main)/home/Sidebar.tsx
@@ -2,16 +2,12 @@ import { memo } from 'react';
 
 import { NavPanelPortal } from '@/features/NavPanel';
 import Content from '@/features/NavPanel/Content';
-import SessionHydration from '@/features/NavPanel/SessionHydration';
 
 const Sidebar = memo(() => {
   return (
-    <>
-      <NavPanelPortal navKey="home">
-        <Content />
-      </NavPanelPortal>
-      <SessionHydration />
-    </>
+    <NavPanelPortal navKey="home">
+      <Content />
+    </NavPanelPortal>
   );
 });
 
