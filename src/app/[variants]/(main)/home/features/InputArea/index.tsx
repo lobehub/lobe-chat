405466c1795c0a4bb2b9bcfc851f76444f368042
canisminor1990 <i@canisminor.cc>
2025-12-03 21:32:32 +0800

    style(wip): update home

--- src/app/[variants]/(main)/home/features/InputArea/index.tsx
+++ src/app/[variants]/(main)/home/features/InputArea/index.tsx
@@ -1,7 +1,10 @@
 import { memo } from 'react';
+import { Flexbox } from 'react-layout-kit';
 
 import { type ActionKeys, ChatInputProvider, DesktopChatInput } from '@/features/ChatInput';
 import { useChatStore } from '@/store/chat';
+
+import StarterList from './StarterList';
 
 const leftActions: ActionKeys[] = [
   'model',
@@ -14,22 +17,34 @@ const leftActions: ActionKeys[] = [
 
 const InputArea = memo(() => {
   return (
-    <ChatInputProvider
-      chatInputEditorRef={(instance) => {
-        if (!instance) return;
-        useChatStore.setState({ mainInputEditor: instance });
-      }}
-      leftActions={leftActions}
-      onMarkdownContentChange={(content) => {
-        useChatStore.setState({ inputMessage: content });
-      }}
-      onSend={() => {
-        // todo
-      }}
-      sendButtonProps={{ disabled: false, generating: false, onStop: () => {}, shape: 'round' }}
-    >
-      <DesktopChatInput />
-    </ChatInputProvider>
+    <Flexbox gap={8}>
+      <ChatInputProvider
+        chatInputEditorRef={(instance) => {
+          if (!instance) return;
+          useChatStore.setState({ mainInputEditor: instance });
+        }}
+        leftActions={leftActions}
+        onMarkdownContentChange={(content) => {
+          useChatStore.setState({ inputMessage: content });
+        }}
+        onSend={() => {
+          // todo
+        }}
+        sendButtonProps={{ disabled: false, generating: false, onStop: () => {}, shape: 'round' }}
+      >
+        <DesktopChatInput
+          inputContainerProps={{
+            minHeight: 88,
+            resize: false,
+            style: {
+              borderRadius: 20,
+              boxShadow: '0 12px 32px rgba(0,0,0,.04)',
+            },
+          }}
+        />
+      </ChatInputProvider>
+      <StarterList />
+    </Flexbox>
   );
 });
 
