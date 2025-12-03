7676f91e1595c05f7ef7c50d080a904afdd49a4a
canisminor1990 <i@canisminor.cc>
2025-12-03 21:06:36 +0800

    style(wip): update home

--- src/app/[variants]/(main)/home/features/InputArea/index.tsx
+++ src/app/[variants]/(main)/home/features/InputArea/index.tsx
@@ -1,8 +1,36 @@
-import { ChatInput } from '@lobehub/editor/react';
 import { memo } from 'react';
 
+import { type ActionKeys, ChatInputProvider, DesktopChatInput } from '@/features/ChatInput';
+import { useChatStore } from '@/store/chat';
+
+const leftActions: ActionKeys[] = [
+  'model',
+  'search',
+  'typo',
+  'fileUpload',
+  'knowledgeBase',
+  'tools',
+];
+
 const InputArea = memo(() => {
-  return <ChatInput>todo</ChatInput>;
+  return (
+    <ChatInputProvider
+      chatInputEditorRef={(instance) => {
+        if (!instance) return;
+        useChatStore.setState({ mainInputEditor: instance });
+      }}
+      leftActions={leftActions}
+      onMarkdownContentChange={(content) => {
+        useChatStore.setState({ inputMessage: content });
+      }}
+      onSend={() => {
+        // todo
+      }}
+      sendButtonProps={{ disabled: false, generating: false, onStop: () => {}, shape: 'round' }}
+    >
+      <DesktopChatInput />
+    </ChatInputProvider>
+  );
 });
 
 export default InputArea;
