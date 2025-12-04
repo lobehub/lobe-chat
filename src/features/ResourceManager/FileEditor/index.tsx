89adc557298c813aefdfa65d3472070f8226f001
Rene Wang <yungeeker@gmail.com>
2025-12-04 21:20:53 +0800

    style: Adjust padding

--- src/features/ResourceManager/FileEditor/index.tsx
+++ src/features/ResourceManager/FileEditor/index.tsx
@@ -2,7 +2,7 @@
 
 import { ActionIcon } from '@lobehub/ui';
 import { ChatHeader } from '@lobehub/ui/chat';
-import { ArrowLeft } from 'lucide-react';
+import { XIcon } from 'lucide-react';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Flexbox } from 'react-layout-kit';
@@ -27,7 +27,7 @@ const FileEditor = memo<PreviewModeProps>(
         <ChatHeader
           left={
             <Flexbox align={'center'} gap={4} horizontal style={{ minHeight: 32 }}>
-              <ActionIcon icon={ArrowLeft} onClick={onBack} title={t('back')} />
+              <ActionIcon icon={XIcon} onClick={onBack} title={t('back')} />
               <Flexbox align={'center'} style={{ marginLeft: 12 }}>
                 <Breadcrumb
                   category={category}
@@ -41,7 +41,7 @@ const FileEditor = memo<PreviewModeProps>(
             left: { padding: 0 },
           }}
         />
-        <Flexbox flex={1} style={{ overflow: 'hidden', paddingTop: 48 }}>
+        <Flexbox flex={1} style={{ backgroundColor: 'blue', overflow: 'hidden', paddingTop: 48 }}>
           <FileContent fileId={currentViewItemId} />
         </Flexbox>
       </Flexbox>
