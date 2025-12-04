1fe0b2e9f3849e17dbcb0bdafb2bebdcc8655c6d
canisminor1990 <i@canisminor.cc>
2025-12-04 17:38:13 +0800

    style: clean add button

--- src/app/[variants]/(main)/home/_layout/Body/index.tsx
+++ src/app/[variants]/(main)/home/_layout/Body/index.tsx
@@ -5,7 +5,6 @@ import { memo } from 'react';
 import { Flexbox } from 'react-layout-kit';
 
 import Agent from './Agent';
-import { AgentModalProvider } from './Agent/ModalProvider';
 import BottomMenu from './BottomMenu';
 
 export enum GroupKey {
@@ -16,15 +15,13 @@ export enum GroupKey {
 const Body = memo(() => {
   // const { enableKnowledgeBase } = useServerConfigStore(featureFlagsSelectors);
   return (
-    <AgentModalProvider>
-      <Flexbox paddingInline={4}>
-        <Accordion defaultExpandedKeys={[GroupKey.Project, GroupKey.Agent]} gap={8}>
-          {/*{enableKnowledgeBase && <Repo itemKey={GroupKey.Project} />}*/}
-          <Agent itemKey={GroupKey.Agent} />
-          <BottomMenu />
-        </Accordion>
-      </Flexbox>
-    </AgentModalProvider>
+    <Flexbox paddingInline={4}>
+      <Accordion defaultExpandedKeys={[GroupKey.Project, GroupKey.Agent]} gap={8}>
+        {/*{enableKnowledgeBase && <Repo itemKey={GroupKey.Project} />}*/}
+        <Agent itemKey={GroupKey.Agent} />
+        <BottomMenu />
+      </Accordion>
+    </Flexbox>
   );
 });
 
