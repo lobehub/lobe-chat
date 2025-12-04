09a63f2c091a39a9d705cc8b7da8a1929903fb1a
Rene Wang <yungeeker@gmail.com>
2025-12-04 21:10:57 +0800

    feat: Support JS rendering

--- src/features/FileViewer/Renderer/index.ts
+++ src/features/FileViewer/Renderer/index.ts
@@ -1,6 +1,15 @@
 import ImageRenderer from './Image';
+import JavaScriptRenderer from './JavaScript';
 import MSDocRenderer from './MSDoc';
+import MarkdownRenderer from './Markdown';
 import TXTRenderer from './TXT';
 import VideoRenderer from './Video';
 
-export const FileViewRenderers = [TXTRenderer, ImageRenderer, MSDocRenderer, VideoRenderer];
+export const FileViewRenderers = [
+  TXTRenderer,
+  MarkdownRenderer,
+  JavaScriptRenderer,
+  ImageRenderer,
+  MSDocRenderer,
+  VideoRenderer,
+];
