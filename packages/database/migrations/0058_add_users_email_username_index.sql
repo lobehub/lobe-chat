80ee2f0edeee5992ebd6aee268d4eebafef1d460
YuTengjing <ytj2713151713@gmail.com>
2025-12-04 21:51:49 +0800

    🗄️ Add users email/username indexes

---
+++ packages/database/migrations/0058_add_users_email_username_index.sql
@@ -0,0 +1,2 @@
+CREATE INDEX IF NOT EXISTS "users_email_idx" ON "users" USING btree ("email");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "users_username_idx" ON "users" USING btree ("username");
