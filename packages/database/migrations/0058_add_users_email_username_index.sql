5b8a7ad1b58f2a815989ec52d93b01689fb062e7
YuTengjing <ytj2713151713@gmail.com>
2025-12-04 22:47:42 +0800

    chore: remove unused sql

--- packages/database/migrations/0058_add_users_email_username_index.sql
+++
@@ -1,2 +0,0 @@
-CREATE INDEX IF NOT EXISTS "users_email_idx" ON "users" USING btree ("email");--> statement-breakpoint
-CREATE INDEX IF NOT EXISTS "users_username_idx" ON "users" USING btree ("username");
