50330c9f44ac41f27013220612dae25db68fdeaf
canisminor1990 <i@canisminor.cc>
2025-12-06 20:30:40 +0800

    fix: fix mobile

--- src/app/[variants]/(mobile)/chat/index.tsx
+++ src/app/[variants]/(mobile)/chat/index.tsx
@@ -8,7 +8,7 @@ import MainInterfaceTracker from '@/components/Analytics/MainInterfaceTracker';
 
 import PageTitle from '../../(main)/chat/features/PageTitle';
 import TelemetryNotification from '../../(main)/chat/features/TelemetryNotification';
-import Topic from './topic';
+import Topic from './features/Topic';
 
 const MobileChatPage = memo(() => {
   return (
