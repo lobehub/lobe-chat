fbed2713bb8105b75e7352ba0c1998b380f7d08b
canisminor1990 <i@canisminor.cc>
2025-12-06 12:27:02 +0800

    feat: add Suspense debug

--- src/app/[variants]/(main)/(mobile)/me/(home)/layout.tsx
+++ src/app/[variants]/(main)/(mobile)/me/(home)/layout.tsx
@@ -1,15 +1,15 @@
-import MobileContentLayout from "@/components/server/MobileNavLayout";
-import Loading from "@/components/Loading/BrandTextLoading";
-import { Outlet } from "react-router-dom";
-import Header from "./features/Header";
-import { Suspense } from "react";
+import { Outlet } from 'react-router-dom';
+
+import MobileContentLayout from '@/components/server/MobileNavLayout';
+
+import Header from './features/Header';
 
 const Layout = () => {
-    return <MobileContentLayout header={<Header />} withNav>
-        <Suspense fallback={<Loading />}>
-            <Outlet />
-        </Suspense>
+  return (
+    <MobileContentLayout header={<Header />} withNav>
+      <Outlet />
     </MobileContentLayout>
-}
+  );
+};
 
 export default Layout;
