fff1e4f10e891fdada3b91bc82ec415ea96c41ba
arvinxx <arvinx@foxmail.com>
2025-12-07 23:34:29 +0800

    fix create agent flow

---
+++ src/hooks/useInitBuiltinAgent.ts
@@ -0,0 +1,17 @@
+import { BuiltinAgentSlug } from '@lobechat/builtin-agents';
+
+import { useAgentStore } from '@/store/agent';
+import { useUserStore } from '@/store/user';
+import { authSelectors } from '@/store/user/selectors';
+
+/**
+ * If a targetAgentId is provided, use it to fetch the agent config directly.
+ * Otherwise, use the active session id to fetch the config.
+ */
+export const useInitBuiltinAgent = (slug: BuiltinAgentSlug) => {
+  const useInitBuiltinAgent = useAgentStore((s) => s.useInitBuiltinAgent);
+
+  const isLogin = useUserStore(authSelectors.isLogin);
+
+  useInitBuiltinAgent(slug, { isLogin });
+};
