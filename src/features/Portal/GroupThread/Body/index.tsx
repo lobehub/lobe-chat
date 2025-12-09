4a9d73b9a2d2c5fec309812de45e65a19e755404
arvinxx <arvinx@foxmail.com>
2025-12-09 16:00:51 +0800

    clean chat input

--- src/features/Portal/GroupThread/Body/index.tsx
+++ src/features/Portal/GroupThread/Body/index.tsx
@@ -3,20 +3,20 @@
 import { memo } from 'react';
 import { Flexbox } from 'react-layout-kit';
 
-import ChatInput from '@/app/[variants]/(main)/chat/features/Conversation/ChatInput';
-import { useChatGroupStore } from '@/store/chatGroup';
+// import ChatInput from '@/app/[variants]/(main)/chat/features/Conversation/ChatInput';
+// import { useChatGroupStore } from '@/store/chatGroup';
 
 import ThreadChatList from './ThreadChatList';
 
 const Body = memo(() => {
-  const activeThreadAgentId = useChatGroupStore((s) => s.activeThreadAgentId);
+  // const activeThreadAgentId = useChatGroupStore((s) => s.activeThreadAgentId);
 
   return (
     <Flexbox height={'100%'}>
       <Flexbox flex={1} style={{ overflow: 'hidden', position: 'relative' }}>
         <ThreadChatList />
       </Flexbox>
-      {activeThreadAgentId && <ChatInput targetMemberId={activeThreadAgentId} />}
+      {/*{activeThreadAgentId && <ChatInput targetMemberId={activeThreadAgentId} />}*/}
     </Flexbox>
   );
 });
