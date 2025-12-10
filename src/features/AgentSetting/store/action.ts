689533e4aa0674844941aec0838a69c03c7d6440
bbbugg <daming20120101@163.com>
2025-12-10 23:51:33 +0800

    🐛 fix: support multiple chunk types in stream update functions

--- src/features/AgentSetting/store/action.ts
+++ src/features/AgentSetting/store/action.ts
@@ -1,4 +1,4 @@
-import { MessageTextChunk } from '@lobechat/fetch-sse';
+import { MessageContentPartChunk, MessageTextChunk } from '@lobechat/fetch-sse';
 import {
   chainPickEmoji,
   chainSummaryAgentName,
@@ -306,11 +306,17 @@ export const store: StateCreator<Store, [['zustand/devtools', never]]> = (set, g
 
   streamUpdateMetaArray: (key: keyof MetaData) => {
     let value = '';
-    return (chunk: MessageTextChunk) => {
+    return (chunk: MessageTextChunk | MessageContentPartChunk) => {
       switch (chunk.type) {
         case 'text': {
           value += chunk.text;
           get().dispatchMeta({ type: 'update', value: { [key]: value.split(',') } });
+          break;
+        }
+        case 'content_part': {
+          value += chunk.content;
+          get().dispatchMeta({ type: 'update', value: { [key]: value.split(',') } });
+          break;
         }
       }
     };
@@ -318,11 +324,17 @@ export const store: StateCreator<Store, [['zustand/devtools', never]]> = (set, g
 
   streamUpdateMetaString: (key: keyof MetaData) => {
     let value = '';
-    return (chunk: MessageTextChunk) => {
+    return (chunk: MessageTextChunk | MessageContentPartChunk) => {
       switch (chunk.type) {
         case 'text': {
           value += chunk.text;
           get().dispatchMeta({ type: 'update', value: { [key]: value } });
+          break;
+        }
+        case 'content_part': {
+          value += chunk.content;
+          get().dispatchMeta({ type: 'update', value: { [key]: value } });
+          break;
         }
       }
     };
