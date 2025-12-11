473b6e69d0c061c79dbf929a967a01149eaae55a
Neko Ayaka <neko@ayaka.moe>
2025-12-11 22:34:04 +0800

    fix: migration

--- packages/database/migrations/0062_add_memory_user_id_index.sql
+++ packages/database/migrations/0062_add_memory_user_id_index.sql
@@ -1,5 +1,5 @@
-DROP INDEX "user_memories_summary_vector_1024_index";--> statement-breakpoint
-DROP INDEX "user_memories_details_vector_1024_index";--> statement-breakpoint
+DROP INDEX IF EXISTS "user_memories_summary_vector_1024_index";--> statement-breakpoint
+DROP INDEX IF EXISTS "user_memories_details_vector_1024_index";--> statement-breakpoint
 CREATE INDEX IF NOT EXISTS "user_memories_user_id_index" ON "user_memories" USING btree ("user_id");--> statement-breakpoint
 CREATE INDEX IF NOT EXISTS "user_memories_contexts_user_id_index" ON "user_memories_contexts" USING btree ("user_id");--> statement-breakpoint
 CREATE INDEX IF NOT EXISTS "user_memories_experiences_user_id_index" ON "user_memories_experiences" USING btree ("user_id");--> statement-breakpoint
