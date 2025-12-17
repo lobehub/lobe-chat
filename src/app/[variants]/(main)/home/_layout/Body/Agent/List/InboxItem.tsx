c8c1c3402a52068bdf98980da5ca52d59e07d58d
canisminor1990 <i@canisminor.cc>
2025-12-17 18:08:27 +0800

    style: update sytle

--- src/app/[variants]/(main)/home/_layout/Body/Agent/List/InboxItem.tsx
+++ src/app/[variants]/(main)/home/_layout/Body/Agent/List/InboxItem.tsx
@@ -42,7 +42,7 @@ const InboxItem = memo<InboxItemProps>(({ className, style }) => {
       active={isActive}
       className={className}
       icon={
-        <Avatar avatar={DEFAULT_INBOX_AVATAR} emojiScaleWithBackground shape={'square'} size={22} />
+        <Avatar avatar={DEFAULT_INBOX_AVATAR} emojiScaleWithBackground shape={'square'} size={24} />
       }
       loading={isLoading}
       onClick={handleClick}
