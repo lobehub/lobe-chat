08efa2c90b61cc768efc49966cc2837a41e5770b
arvinxx <arvinx@foxmail.com>
2025-12-20 21:58:51 +0800

    ✨ feat: code-interpreter tool

--- src/server/routers/tools/index.ts
+++ src/server/routers/tools/index.ts
@@ -1,10 +1,12 @@
 import { publicProcedure, router } from '@/libs/trpc/lambda';
 
+import { codeInterpreterRouter } from './codeInterpreter';
 import { klavisRouter } from './klavis';
 import { mcpRouter } from './mcp';
 import { searchRouter } from './search';
 
 export const toolsRouter = router({
+  codeInterpreter: codeInterpreterRouter,
   healthcheck: publicProcedure.query(() => "i'm live!"),
   klavis: klavisRouter,
   mcp: mcpRouter,
