7a92867d1166eb3edb0e9bdb15366ec9bf9b0876
rdmclin2 <rdmclin2@gmail.com>
2025-12-23 11:38:39 +0800

    fix: passkey statement

--- packages/database/migrations/0065_add_passkey.sql
+++ packages/database/migrations/0065_add_passkey.sql
@@ -12,6 +12,11 @@ CREATE TABLE IF NOT EXISTS "passkey" (
 	"userId" text NOT NULL
 );
 --> statement-breakpoint
-ALTER TABLE "passkey" ADD CONSTRAINT IF NOT EXISTS "passkey_userId_users_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+DO $$ BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'passkey_userId_users_id_fk') THEN
+    ALTER TABLE "passkey" ADD CONSTRAINT "passkey_userId_users_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+  END IF;
+END $$;
+--> statement-breakpoint	
 CREATE UNIQUE INDEX IF NOT EXISTS "passkey_credential_id_unique" ON "passkey" USING btree ("credentialID");--> statement-breakpoint
 CREATE INDEX IF NOT EXISTS "passkey_user_id_idx" ON "passkey" USING btree ("userId");
