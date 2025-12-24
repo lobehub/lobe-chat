a2d1f4dc3d0f4204fd030796cfd3903ae22c5a00
YuTengjing <ytj2713151713@gmail.com>
2025-12-24 17:57:35 +0800

    chore: fix proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,10 +1,46 @@
 import { defineConfig } from '@/libs/proxy/define-config';
 
-const { config, middleware } = defineConfig({
-  config: {
-    matcher: [],
-  },
-});
+const { middleware } = defineConfig();
 
-export { config };
+// required to be literal
+export const config = {
+  matcher: [
+    // include any files in the api or trpc folders that might have an extension
+    '/(api|trpc|webapi)(.*)',
+    // include the /
+    '/',
+    '/community',
+    '/community(.*)',
+    '/labs',
+    '/agent',
+    '/agent(.*)',
+    '/group',
+    '/group(.*)',
+    '/changelog(.*)',
+    '/settings(.*)',
+    '/image',
+    '/resource',
+    '/resource(.*)',
+    '/profile(.*)',
+    '/page',
+    '/page(.*)',
+    '/me',
+    '/me(.*)',
+    '/desktop-onboarding',
+    '/desktop-onboarding(.*)',
+    '/onboarding',
+
+    '/login(.*)',
+    '/signup(.*)',
+    '/signin(.*)',
+    '/verify-email(.*)',
+    '/reset-password(.*)',
+    '/auth-error(.*)',
+    '/next-auth/(.*)',
+    '/oauth(.*)',
+    '/oidc(.*)',
+    '/market-auth-callback(.*)',
+  ],
+};
+
 export default middleware;
