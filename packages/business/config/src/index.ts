92ca00eb85fef511f77eb4787dbc588f4d5c5bb4
YuTengjing <ytj2713151713@gmail.com>
2025-12-25 21:19:19 +0800

    refactor: remove some unused business code

--- packages/business/config/src/index.ts
+++ packages/business/config/src/index.ts
@@ -1,2 +1,1 @@
 export * from './llm';
-export * from './url';
--- packages/business/const/src/url.ts
+++ packages/business/const/src/url.ts
@@ -1,5 +1,1 @@
-import { BRANDING_EMAIL, BRANDING_URL } from './branding';
-
 export const UTM_SOURCE = 'chat_preview';
-
-export const SUPPORT_URL = BRANDING_URL.support ?? `mailto:${BRANDING_EMAIL.support}`;
