1737b7fe30d619ea242497131383b5d27fde245b
YuTengjing <ytj2713151713@gmail.com>
2025-12-26 16:08:04 +0800

    fix: update getSubscriptionPlan to return default plan

--- src/business/server/user.ts
+++ src/business/server/user.ts
@@ -1,12 +1,12 @@
 /* eslint-disable unused-imports/no-unused-vars, @typescript-eslint/no-unused-vars */
-import { type Plans, type ReferralStatusString } from '@lobechat/types';
+import { Plans, type ReferralStatusString } from '@lobechat/types';
 
 export async function getReferralStatus(userId: string): Promise<ReferralStatusString | undefined> {
   return undefined;
 }
 
-export async function getSubscriptionPlan(userId: string): Promise<Plans | undefined> {
-  return undefined;
+export async function getSubscriptionPlan(userId: string): Promise<Plans> {
+  return Plans.Free;
 }
 
 export async function initNewUserForBusiness(
