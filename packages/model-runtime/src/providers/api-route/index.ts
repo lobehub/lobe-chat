55987da098f8654dbf1438fbc598a2382a394cd9
DennyHo0917 <dennyho0917@gmail.com>
2026-09-07 23:14:40 +0800

    🐛 fix: enable API Route chat debug predicate

--- packages/model-runtime/src/providers/api-route/index.ts
+++ packages/model-runtime/src/providers/api-route/index.ts
@@ -5,6 +5,9 @@ import { createOpenAICompatibleRuntime } from '../../core/openaiCompatibleFactor
 
 export const params = {
   baseURL: 'https://global.api-route.com/v1',
+  debug: {
+    chatCompletion: () => process.env.DEBUG_API_ROUTE_CHAT_COMPLETION === '1',
+  },
   provider: ModelProvider.ApiRoute,
 } satisfies OpenAICompatibleFactoryOptions;
 
